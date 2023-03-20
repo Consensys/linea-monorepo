67b0a5c3e3b612d6f3154682b9c60bcab39af03f
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-03-20 16:32:23 +1100

    Merge zkBesu-tracer into this repo

---
+++ besu-plugins/linea-sequencer/src/main/java/net/consensys/zktracer/ZkTraceBuilder.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.zktracer;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import net.consensys.zktracer.ZkTrace;
+import net.consensys.zktracer.module.lookupTable.ShfRtTrace;
+import org.apache.tuweni.bytes.Bytes32;
+
+public class ZkTraceBuilder {
+  private final Map<String, Object> traceResults = new HashMap<>();
+
+  public void addTrace(final String moduleName, final Object traceResult) {
+    traceResults.put(moduleName, traceResult);
+  }
+
+  public net.consensys.zktracer.ZkTrace build() {
+    if (!traceResults.containsKey("shfRT")) {
+      traceResults.put("shfRT", ShfRtTrace.generate());
+    }
+
+    return new ZkTrace(
+        null, Bytes32.ZERO, BigInteger.ZERO, BigInteger.ZERO, BigInteger.ZERO, traceResults);
+  }
+}
