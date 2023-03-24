6c9c613bb688536ee52fa183cb906cc8f1a103a8
Sally MacFarlane <macfarla.github@gmail.com>
2023-03-24 11:34:25 +1000

    builder
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- tracer/src/main/java/net/consensys/zktracer/module/alu/add/AddTracer.java
+++ tracer/src/main/java/net/consensys/zktracer/module/alu/add/AddTracer.java
@@ -1,6 +1,7 @@
 package net.consensys.zktracer.module.alu.add;
 
 import net.consensys.zktracer.OpCode;
+import net.consensys.zktracer.bytes.Bytes16;
 import net.consensys.zktracer.module.ModuleTracer;
 import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.besu.evm.frame.MessageFrame;
@@ -9,6 +10,8 @@ import java.util.List;
 
 public class AddTracer implements ModuleTracer {
 
+
+    private int stamp = 0;
     @Override
     public String jsonKey() {
         return "add";
@@ -22,10 +25,24 @@ public class AddTracer implements ModuleTracer {
     @SuppressWarnings({"UnusedVariable"})
     @Override
     public Object trace(MessageFrame frame) {
+        // TODO duplicated code
         final Bytes32 arg1 = Bytes32.wrap(frame.getStackItem(0));
         final Bytes32 arg2 = Bytes32.wrap(frame.getStackItem(1));
 
+        // TODO duplicated code
+        final Bytes16 arg1Hi = Bytes16.wrap(arg1.slice(0, 16));
+        final Bytes16 arg1Lo = Bytes16.wrap(arg1.slice(16));
+        final Bytes16 arg2Hi = Bytes16.wrap(arg2.slice(0, 16));
+        final Bytes16 arg2Lo = Bytes16.wrap(arg2.slice(16));
+
         final OpCode opCode = OpCode.of(frame.getCurrentOperation().getOpcode());
-        return null;
+
+
+        final AddTrace.Trace.Builder builder = AddTrace.Trace.Builder.newInstance();
+
+        stamp++;
+
+        builder.setStamp(stamp);
+        return builder.build();
     }
 }
