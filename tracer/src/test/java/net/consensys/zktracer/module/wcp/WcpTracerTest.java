b9ca0392201f46b89771906ff3a94428f1a4482f
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-03-30 16:52:49 +1100

    Update src/test/java/net/consensys/zktracer/module/wcp/WcpTracerTest.java
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>

--- tracer/src/test/java/net/consensys/zktracer/module/wcp/WcpTracerTest.java
+++ tracer/src/test/java/net/consensys/zktracer/module/wcp/WcpTracerTest.java
@@ -76,7 +76,7 @@ class WcpTracerTest {
     traceOperation(SGT, arg1, arg2);
   }
 
-  public static Stream<Arguments> provideNonRandomAddArguments() {
+  public static Stream<Arguments> provideNonRandomArguments() {
     final List<Arguments> arguments = new ArrayList<>();
     for (OpCode opCode : new WcpTracer().supportedOpCodes()) {
       for (int i = 0; i <= TEST_REPETITIONS; i++) {
