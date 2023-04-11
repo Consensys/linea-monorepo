9358cecc505a08f450295c4cb35a420c2c3baa27
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-04-12 08:13:07 +1000

    bug fixes and tests

--- tracer/src/test/java/net/consensys/linea/zktracer/module/AbstractModuleTracerTest.java
+++ tracer/src/test/java/net/consensys/linea/zktracer/module/AbstractModuleTracerTest.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package net.consensys.linea.zktracer.module;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,7 +40,7 @@ import org.mockito.Mock;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public abstract class AbstractModuleTracerTest {
-  private static final Random rand = new Random();
+  static final Random rand = new Random();
   private static final int TEST_REPETITIONS = 4;
 
   private ZkTracer zkTracer;
@@ -49,7 +63,7 @@ public abstract class AbstractModuleTracerTest {
     runTest(opCode, arg1, arg2);
   }
 
-  private void runTest(OpCode opCode, final Bytes32 arg1, Bytes32 arg2) {
+  protected void runTest(OpCode opCode, final Bytes32 arg1, Bytes32 arg2) {
     when(mockOperation.getOpcode()).thenReturn((int) opCode.value);
     when(mockFrame.getStackItem(0)).thenReturn(arg1);
     when(mockFrame.getStackItem(1)).thenReturn(arg2);
