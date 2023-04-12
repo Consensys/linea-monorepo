86de404426bd90bf162fcb5f4228632a0897081a
Sally MacFarlane <macfarla.github@gmail.com>
2023-04-12 13:57:43 +1000

    twoAdicity
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- besu-plugins/linea-sequencer/src/test/java/net/consensys/linea/zktracer/module/alu/mul/MulUtilsTest.java
+++ besu-plugins/linea-sequencer/src/test/java/net/consensys/linea/zktracer/module/alu/mul/MulUtilsTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.math.BigInteger;
 
+import org.apache.tuweni.units.bigints.UInt256;
 import org.junit.jupiter.api.Test;
 
 public class MulUtilsTest {
@@ -13,5 +14,13 @@ public class MulUtilsTest {
     assertThat(MulData.isTiny(BigInteger.ONE)).isTrue();
     assertThat(MulData.isTiny(BigInteger.TWO)).isFalse();
     assertThat(MulData.isTiny(BigInteger.TEN)).isFalse();
+  }
+
+  @Test
+  public void twoAdicity() {
+    assertThat(MulData.twoAdicity(UInt256.MIN_VALUE)).isEqualTo(256);
+    // TODO no idea what these should be
+    //    assertThat(MulData.twoAdicity(UInt256.MAX_VALUE)).isEqualTo(0);
+    //    assertThat(MulData.twoAdicity(UInt256.valueOf(1))).isEqualTo(0);
   }
 }
