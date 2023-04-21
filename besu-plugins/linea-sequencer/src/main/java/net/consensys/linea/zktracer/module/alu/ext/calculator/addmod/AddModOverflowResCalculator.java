ce27189b81a5ac4218da46984990643b00b01c7c
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-04-21 16:24:35 +1000

    add comments

--- besu-plugins/linea-sequencer/src/main/java/net/consensys/linea/zktracer/module/alu/ext/calculator/addmod/AddModOverflowResCalculator.java
+++ besu-plugins/linea-sequencer/src/main/java/net/consensys/linea/zktracer/module/alu/ext/calculator/addmod/AddModOverflowResCalculator.java
@@ -20,7 +20,22 @@ import static net.consensys.linea.zktracer.module.Util.getOverflow;
 import net.consensys.linea.zktracer.bytestheta.BaseBytes;
 import org.apache.tuweni.units.bigints.UInt256;
 
+/**
+ * A utility class to calculate overflow values for the AddMod operation. The class provides methods
+ * for calculating overflow values for two input arguments of the AddMod operation. The calculation
+ * involves calculating the sum of the two input arguments and determining if the sum overflows the
+ * bit size of the operands. The method returns a boolean array of size 2 that contains the overflow
+ * values for lambda and mu respectively.
+ */
 public class AddModOverflowResCalculator {
+  /**
+   * Calculates the overflow values of the result for two input arguments of the AddMod operation.
+   *
+   * @param arg1 the first input argument of the AddMod operation.
+   * @param arg2 the second input argument of the AddMod operation.
+   * @return a boolean array of size 2 that contains the overflow values for lambda and mu
+   *     respectively.
+   */
   static boolean[] calculateOverflow(BaseBytes arg1, BaseBytes arg2) {
     boolean[] overflowRes = new boolean[8];
 
@@ -33,11 +48,26 @@ public class AddModOverflowResCalculator {
     return overflowRes;
   }
 
+  /**
+   * Calculates the lambda value.
+   *
+   * @param arg1 the first input argument of the AddMod operation.
+   * @param arg2 the second input argument of the AddMod operation.
+   * @return the lambda value.
+   */
   private static long calculateLambda(BaseBytes arg1, BaseBytes arg2) {
     UInt256 sum = UInt256.fromBytes(arg1.getLow()).add(UInt256.fromBytes(arg2.getLow()));
     return getOverflow(sum, 1, "lambda out of range (ADDMOD)");
   }
 
+  /**
+   * Calculates the mu value.
+   *
+   * @param lambda the lambda value.
+   * @param arg1 the first input argument of the AddMod operation.
+   * @param arg2 the second input argument of the AddMod operation.
+   * @return the mu value.
+   */
   private static long calculateMu(long lambda, BaseBytes arg1, BaseBytes arg2) {
     UInt256 sum = UInt256.valueOf(lambda);
     sum = sum.add(UInt256.fromBytes(arg1.getHigh()));
