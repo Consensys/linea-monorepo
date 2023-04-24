4a16e212f3bc09c0b2b21563f53bba099397cab3
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-04-24 15:47:33 +1000

    Implement test by file

--- besu-plugins/linea-sequencer/src/test/java/net/consensys/linea/zktracer/AbstractModuleTracerCorsetTest.java
+++ besu-plugins/linea-sequencer/src/test/java/net/consensys/linea/zktracer/AbstractModuleTracerCorsetTest.java
@@ -12,42 +12,24 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package net.consensys.linea.zktracer.module;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
-import org.hyperledger.besu.evm.frame.MessageFrame;
-import org.hyperledger.besu.evm.operation.Operation;
+package net.consensys.linea.zktracer;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Stream;
 
-import net.consensys.linea.CorsetValidator;
-import net.consensys.linea.zktracer.OpCode;
-import net.consensys.linea.zktracer.ZkTraceBuilder;
-import net.consensys.linea.zktracer.ZkTracer;
 import org.apache.tuweni.bytes.Bytes32;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Mock;
 
 @TestInstance(Lifecycle.PER_CLASS)
-public abstract class AbstractModuleTracerTest {
+public abstract class AbstractModuleTracerCorsetTest extends AbstractBaseModuleTracerTest {
   static final Random rand = new Random();
   private static final int TEST_REPETITIONS = 8;
-
-  private ZkTracer zkTracer;
-  private ZkTraceBuilder zkTraceBuilder;
-  @Mock MessageFrame mockFrame;
-  @Mock Operation mockOperation;
-  static ModuleTracer moduleTracer;
 
   @ParameterizedTest()
   @MethodSource("provideRandomArguments")
@@ -59,22 +41,6 @@ public abstract class AbstractModuleTracerTest {
   @MethodSource("provideNonRandomArguments")
   void nonRandomArgumentsTest(OpCode opCode, final Bytes32 arg1, Bytes32 arg2) {
     runTest(opCode, arg1, arg2);
-  }
-
-  protected void runTest(OpCode opCode, final Bytes32 arg1, Bytes32 arg2) {
-    when(mockOperation.getOpcode()).thenReturn((int) opCode.value);
-    when(mockFrame.getStackItem(0)).thenReturn(arg1);
-    when(mockFrame.getStackItem(1)).thenReturn(arg2);
-    zkTracer.tracePreExecution(mockFrame);
-    assertThat(CorsetValidator.isValid(zkTraceBuilder.build().toJson())).isTrue();
-  }
-
-  @BeforeEach
-  void setUp() {
-    zkTraceBuilder = new ZkTraceBuilder();
-    moduleTracer = getModuleTracer();
-    zkTracer = new ZkTracer(zkTraceBuilder, List.of(moduleTracer));
-    when(mockFrame.getCurrentOperation()).thenReturn(mockOperation);
   }
 
   protected abstract Stream<Arguments> provideNonRandomArguments();
@@ -89,9 +55,7 @@ public abstract class AbstractModuleTracerTest {
     return arguments.stream();
   }
 
-  protected abstract ModuleTracer getModuleTracer();
-
-  protected OpCode getRandomSupportedOpcode() {
+  public OpCode getRandomSupportedOpcode() {
     var supportedOpCodes = getModuleTracer().supportedOpCodes();
     int index = rand.nextInt(supportedOpCodes.size());
     return supportedOpCodes.get(index);
