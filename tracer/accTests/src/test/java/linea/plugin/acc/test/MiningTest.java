a33089bd6bf13eaf50ec4fd7e77d2c336681f9b7
Stefan <stefan.pingel@consensys.net>
2023-06-13 17:02:47 +1000

    use jvmArgs instead of systemProperty, because that did not work on circle ci
    Signed-off-by: Stefan <stefan.pingel@consensys.net>

--- tracer/accTests/src/test/java/linea/plugin/acc/test/MiningTest.java
+++ tracer/accTests/src/test/java/linea/plugin/acc/test/MiningTest.java
@@ -48,6 +48,7 @@ public class MiningTest extends AcceptanceTestBase {
     @BeforeEach
     public void setUp() throws Exception {
         final String pluginDir = System.getProperty("besu.plugins.dir");
+        // this system property is set in the test task of accTests (accTests/build.gradle)
         if (pluginDir == null) {
             throw new RuntimeException("besu.plugins.dir is not set");
         }
