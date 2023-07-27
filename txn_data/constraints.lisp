993a2bfe874771cfd4daae194b5a5eab0f0c9b93
OlivierBBB <olivier.begassat.cours@gmail.com>
2023-07-27 21:32:32 +0200

    feat: implemented constraints up to an including section 2.5 Cumulative gas

--- txn_data/constraints.lisp
+++ txn_data/constraints.lisp
@@ -1,9 +1,18 @@
 (module txn_data)
 
 (defconst
-	nROWS_TYPE_0 6
-	nROWS_TYPE_1 7
-	nROWS_TYPE_2 7)
+	nROWS0 6
+	nROWS1 7
+	nROWS2 7)
+
+;; sum of transaction type flags
+(defun (tx_type_sum) (+ TYPE0 TYPE1 TYPE2))
+
+;; constraint imposing that STAMP[i + 1] ∈ { STAMP[i], 1 + STAMP[i] }
+(depurefun (stamp_progression STAMP)
+	   (vanishes! (*
+			(will-remain-constant STAMP)
+			(will-inc STAMP 1)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                     ;;
@@ -11,17 +20,155 @@
 ;;                     ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
 (defconstraint first_row (:domain {0})
 	       (vanishes! ABS))
 
-;; (defconstraint heartbeat ()
-;; 	       (begin
-;; 		 (* (will-rmain-constant! STAMP) (will-inc! STAMP 1))
-;; 		 (if-not-zero (will-remain-constant! STAMP) (vanishes! (next CT)))
-;; 		 (if-not-zero STAMP
-;; 			      (if-eq-else CT LLARGEMO
-;; 					  (will-inc! STAMP 1)
-;; 					  (will-inc! CT 1)))))
+(defconstraint padding () 
+	       (if-zero ABS
+			(begin
+			  (vanishes! CT)
+			  (vanishes! (tx_type_sum)))))
+
+(defconstraint abs_tx_num_increments () (stamp_progression ABS))
+
+(defconstraint heartbeat (:guard ABS)
+	       (begin
+		 (= (tx_type_sum) 1)
+		 (if-not-zero TYPE0
+			      (if-eq-else CT nROWS0
+					  (will-inc! ABS 1)
+					  (will-inc! CT  1)))
+		 (if-not-zero TYPE1
+			      (if-eq-else CT nROWS1
+					  (will-inc! ABS 1)
+					  (will-inc! CT  1)))
+		 (if-not-zero TYPE2
+			      (if-eq-else CT nROWS2
+					  (will-inc! ABS 1)
+					  (will-inc! CT  1)))
+		 ))
+
 
 (defconstraint final_row (:domain {-1})
-	       (vanishes! ABS))
+	       (begin
+		 (= ABS ABS_MAX)
+		 (= BTC BTC_MAX)
+		 (= REL REL_MAX)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                       ;;
+;;    2.2 Constancies    ;;
+;;                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defun (transaction-constant X) (if-not-zero (- ABS (+ 1 (prev ABS))) (= X (prev X))))
+
+(defconstraint constancies ()
+	       (begin
+		 (transaction-constant BTC)
+		 (transaction-constant REL)
+		 (transaction-constant FROM_HI)
+		 (transaction-constant FROM_LO)
+		 (transaction-constant NONCE)
+		 (transaction-constant IBAL)
+		 (transaction-constant VALUE)
+		 (transaction-constant TO_HI)
+		 (transaction-constant TO_LO)
+		 (transaction-constant IS_DEP)
+		 (transaction-constant GLIM)
+		 (transaction-constant IGAS)
+		 (transaction-constant GPRC)
+		 (transaction-constant BASEFEE)
+		 (transaction-constant DATA_SIZE)
+		 (transaction-constant TYPE0)
+		 (transaction-constant TYPE1)
+		 (transaction-constant TYPE2)
+		 (transaction-constant REQ_EVM)
+		 (transaction-constant LEFTOVER_GAS)
+		 (transaction-constant REF_CNT)
+		 (transaction-constant REF_AMT)
+		 (transaction-constant CUM_GAS)
+		 (transaction-constant STATUS_CODE)
+		 (transaction-constant CFI)))   
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                              ;;
+;;    2.3 Binary constraints    ;;
+;;                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defconstraint binary_constraints ()
+	       (begin
+		 (is-binary TYPE0)
+		 (is-binary TYPE1)
+		 (is-binary TYPE2)
+		 (is-binary IS_DEP)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                ;;
+;;    2.4 Batch numbers and transaction number    ;;
+;;                                                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defconstraint total_number_constancies ()
+	       (begin
+		 (if-zero ABS
+			(begin
+			  (vanishes! ABS_MAX)
+			  (vanishes! BTC_MAX)
+			  (vanishes! REL_MAX))
+			(begin
+			  (will-remain-constant! ABS_MAX)
+			  (will-remain-constant! BTC_MAX)))
+		 (if-not-zero (will-inc! BTC 1)
+			      (will-remain-constant! REL_MAX))
+		 ))
+
+(defconstraint batch_num_increments () (stamp_progression BTC))
+
+(defconstraint batchNum_txNum_lexicographic ()
+	       (begin
+		 (if-zero ABS
+			  (begin
+			    (vanishes! BTC)
+			    (vanishes! REL)
+			    (if-not-zero (will-remain-constant ABS)
+					 (begin
+					   (= BTC 1)
+					   (= REL 1))))
+			  (if-not-zero (will-remain-constant ABS)
+				       (if-not-zero (- REL_MAX REL)
+						    (begin
+						      (will-remain-constant! BTC)
+						      (will-inc REL 1))
+						    (begin
+						      (will-inc BTC 1)
+						      (= REL 1))
+						    )))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          ;;
+;;    2.5 Cumulative gas    ;;
+;;                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+			      
+(defconstraint cumulative_gas ()
+	       (begin
+		 (if-zero ABS (vanishes! CUM_GAS))
+		 (if-not-zero (will-remain-constant! BTC)
+			      ; BTC[i + 1] != BTC[i]
+			      (will-remain-constant! CUM_GAS))
+		 (if-not-zero (will-inc! BTC 1)
+			      ; BTC[i + 1] != 1 + BTC[i] i.e. BTC[i + 1] == BTC[i]
+			      (if-not-zero (will-remain-constant! ABS)
+					   (will-eq 
+					     CUM_GAS
+					     (- GLIM REF_AMT))))))
