e0ef62c37f4369dfbf877519488ce3d98d646c18
Sally MacFarlane <macfarla.github@gmail.com>
2023-09-04 14:24:50 +1000

    fixed  byte decomposition constraint for ACC_T
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>

--- trm/constraints.lisp
+++ trm/constraints.lisp
@@ -15,7 +15,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                     ;;
-;;    1.3 heartbeat    ;;
+;;    2.1 heartbeat    ;;
 ;;                     ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -25,13 +25,7 @@
 (defconstraint heartbeat ()
   (begin
    (* (will-remain-constant! STAMP) (will-inc! STAMP 1))
-   (if-not-zero (will-remain-constant! STAMP) (vanishes! (next ADDR_HI)))
-   (if-not-zero (will-remain-constant! STAMP) (vanishes! (next ADDR_LO)))
-   (if-not-zero (will-remain-constant! STAMP) (vanishes! (next TRM_ADDR_HI)))
-   (if-not-zero (will-remain-constant! STAMP) (vanishes! (next IS_PREC)))
    (if-not-zero (will-remain-constant! STAMP) (vanishes! (next CT)))
-   (if-not-zero (will-remain-constant! STAMP) (vanishes! (next BYTE_HI)))
-   (if-not-zero (will-remain-constant! STAMP) (vanishes! (next BYTE_LO)))
    (if-not-zero STAMP
                 (if-eq-else CT LLARGEMO
                             (will-inc! STAMP 1)
@@ -39,6 +33,13 @@
 
 (defconstraint last-row (:domain {-1})
   (if-not-zero STAMP  (= CT LLARGEMO)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                           ;;
+;;    2.2 stamp constancy    ;;
+;;                           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defconstraint stamp-constancies ()
   (begin
@@ -50,7 +51,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                                                   ;;
-;;    1.4 binary, bytehood and byte decompositions   ;;
+;;    2.4 binary, bytehood and byte decompositions   ;;
 ;;                                                   ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -60,7 +61,8 @@
    (is-binary ONES)
    (byte-decomposition CT ACC_HI BYTE_HI)
    (byte-decomposition CT ACC_LO BYTE_LO)
-   (byte-decomposition CT ACC_T TRM_ADDR_HI)))
+   (byte-decomposition CT ACC_T (* PBIT BYTE_HI))
+))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -72,15 +74,14 @@
 (defconstraint trm-constraints ()
   (if-eq CT LLARGEMO
          (begin (= ADDR_HI ACC_HI)
-                (= ADDR_LO ACC_LO)
-                (= TRM_ADDR_HI ACC_T)
-                (if-not-zero (+ TRM_ADDR_HI (- ADDR_LO BYTE_LO))
-                             (= IS_PREC 0))
-                (if-zero (+ TRM_ADDR_HI (- ADDR_LO BYTE_LO))
-                         (if-zero BYTE_LO
-                                  (= IS_PREC 0)))
-)))
-
+                (= ADDR_LO ACC_LO))))
+;;
+;;                (= TRM_ADDR_HI ACC_T)
+;;                (if-not-zero (+ TRM_ADDR_HI (- ADDR_LO BYTE_LO))
+;;                             (= IS_PREC 0))
+;;                (if-zero (+ TRM_ADDR_HI (- ADDR_LO BYTE_LO))
+;;                         (if-zero BYTE_LO
+;;                                  (= IS_PREC 0)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                                 ;;
