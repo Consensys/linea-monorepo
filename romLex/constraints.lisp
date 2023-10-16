78d55d97ec4a74a5d4914dd98df747dd70d01d76
Francois Bojarski <francois.bojarski@consensys.net>
2023-10-16 14:32:02 +0530

    feat(romLex): add constraints

--- romLex/constraints.lisp
+++ romLex/constraints.lisp
@@ -1,6 +1,26 @@
 (module romLex)
 
+(defconstraint binarities ()
+  (begin (is-binary DEP_STATUS)
+         (is-binary READ_FROM_STATE)
+         (is-binary COMMIT_TO_STATE)))
+
 (defconstraint initialization (:domain {0})
   (vanishes! CODE_FRAGMENT_INDEX))
+
+(defconstraint cfi-evolution ()
+  (or! (will-inc! CFI 1) (will-remain-constant! CFI)))
+
+(defconstraint finalisation (:domain {-1})
+  (if-not-zero CFI
+               (eq! CFI CODE_FRAGMENT_INDEX_INFTY)))
+
+(defconstraint cfi-rules ()
+  (if-zero CFI
+           (vanishes! CODE_FRAGMENT_INDEX_INFTY)
+           (begin (will-inc! CFI 1)
+                  (will-remain-constant! CODE_FRAGMENT_INDEX_INFTY)
+                  ;;TODO: add lexicographic ordering
+                  )))
 
 
