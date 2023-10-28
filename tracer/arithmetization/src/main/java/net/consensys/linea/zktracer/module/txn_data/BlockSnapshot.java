d69966da3188117280343fcb7d606aa381ee7a30
delehef <franklin.delehelle@consensys.net>
2023-10-28 15:53:21 +0200

    perf: use collapsing instead of inline iterators in `StackedSet` (#271)

--- tracer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/txn_data/BlockSnapshot.java
+++ tracer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/txn_data/BlockSnapshot.java
@@ -75,13 +75,11 @@ public class BlockSnapshot {
    */
   void endTx(long cumulativeGasUsed, long leftoverGas, long refundCounter, boolean status) {
     final long effectiveGasRefund = this.getEffectiveGasRefund(leftoverGas, refundCounter);
-    if (!this.txs.isEmpty()) {
-      this.currentTx().status(status);
-      this.currentTx().refundCounter(refundCounter);
-      this.currentTx().leftoverGas(leftoverGas);
-      this.currentTx().effectiveGasRefund(effectiveGasRefund);
-      this.currentTx().cumulativeGasConsumption(cumulativeGasUsed);
-    }
+    this.currentTx().status(status);
+    this.currentTx().refundCounter(refundCounter);
+    this.currentTx().leftoverGas(leftoverGas);
+    this.currentTx().effectiveGasRefund(effectiveGasRefund);
+    this.currentTx().cumulativeGasConsumption(cumulativeGasUsed);
   }
 
   /**
