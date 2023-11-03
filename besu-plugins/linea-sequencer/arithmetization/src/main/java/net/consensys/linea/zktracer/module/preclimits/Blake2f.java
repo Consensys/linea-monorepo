55f908fb54d7ac6c597f7e6db25626f6ce53e060
delehef <franklin.delehelle@consensys.net>
2023-11-03 13:06:25 +0100

    feat: implement precompiles limits (#258)
    Fixes #257 
    
    Signed-off-by: franklin.delehelle@consensys.net
    
    Co-authored-by: Francois Bojarski <francois.bojarski@consensys.net>

---
+++ besu-plugins/linea-sequencer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/preclimits/Blake2f.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.consensys.linea.zktracer.module.preclimits;
+
+import java.util.Stack;
+
+import lombok.extern.slf4j.Slf4j;
+import net.consensys.linea.zktracer.module.Module;
+import net.consensys.linea.zktracer.module.ModuleTrace;
+import net.consensys.linea.zktracer.opcode.OpCode;
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.evm.frame.MessageFrame;
+import org.hyperledger.besu.evm.internal.Words;
+
+@Slf4j
+public final class Blake2f implements Module {
+  private final Stack<Integer> counts = new Stack<Integer>();
+
+  @Override
+  public String jsonKey() {
+    return null;
+  }
+
+  private final int black2fDataSize = 213;
+
+  @Override
+  public void enterTransaction() {
+    counts.push(0);
+  }
+
+  @Override
+  public void popTransaction() {
+    counts.pop();
+  }
+
+  @Override
+  public void tracePreOpcode(MessageFrame frame) {
+    final OpCode opCode = OpCode.of(frame.getCurrentOperation().getOpcode());
+
+    switch (opCode) {
+      case CALL, STATICCALL, DELEGATECALL, CALLCODE -> {
+        final Address target = Words.toAddress(frame.getStackItem(1));
+        if (target == Address.BLAKE2B_F_COMPRESSION) {
+          long length = 0;
+          long offset = 0;
+          switch (opCode) {
+            case CALL, CALLCODE -> {
+              length = Words.clampedToLong(frame.getStackItem(4));
+              offset = Words.clampedToLong(frame.getStackItem(3));
+            }
+            case DELEGATECALL, STATICCALL -> {
+              length = Words.clampedToLong(frame.getStackItem(3));
+              offset = Words.clampedToLong(frame.getStackItem(2));
+            }
+          }
+          if (length == black2fDataSize) {
+            final int f = frame.shadowReadMemory(offset, length).get(black2fDataSize - 1);
+            if (f == 0 || f == 1) {
+              final int r =
+                  frame
+                      .shadowReadMemory(offset, length)
+                      .slice(0, 4)
+                      .toInt(); // The number of round is equal to the gas to pay
+              final long gasPaid = Words.clampedToLong(frame.getStackItem(0));
+              if (gasPaid >= r) {
+                this.counts.push(this.counts.pop() + r);
+              }
+            }
+          }
+        }
+      }
+      default -> {}
+    }
+  }
+
+  @Override
+  public int lineCount() {
+    return this.counts.stream().mapToInt(x -> x).sum();
+  }
+
+  @Override
+  public ModuleTrace commit() {
+    return null;
+  }
+}
---
+++ besu-plugins/linea-sequencer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/preclimits/Ecadd.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.consensys.linea.zktracer.module.preclimits;
+
+import java.util.Stack;
+
+import net.consensys.linea.zktracer.module.Module;
+import net.consensys.linea.zktracer.module.ModuleTrace;
+import net.consensys.linea.zktracer.opcode.OpCode;
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.evm.frame.MessageFrame;
+import org.hyperledger.besu.evm.internal.Words;
+
+public final class Ecadd implements Module {
+  private final Stack<Integer> counts = new Stack<Integer>();
+
+  @Override
+  public String jsonKey() {
+    return "ecadd";
+  }
+
+  private final int precompileGasFee = 150; // cf EIP-1108
+
+  @Override
+  public void enterTransaction() {
+    counts.push(0);
+  }
+
+  @Override
+  public void popTransaction() {
+    counts.pop();
+  }
+
+  @Override
+  public void tracePreOpcode(MessageFrame frame) {
+    final OpCode opCode = OpCode.of(frame.getCurrentOperation().getOpcode());
+
+    switch (opCode) {
+      case CALL, STATICCALL, DELEGATECALL, CALLCODE -> {
+        final Address target = Words.toAddress(frame.getStackItem(1));
+        if (target == Address.ALTBN128_ADD) {
+          final long gasPaid = Words.clampedToLong(frame.getStackItem(0));
+          if (gasPaid >= precompileGasFee) {
+            this.counts.push(this.counts.pop() + 1);
+          }
+        }
+      }
+      default -> {}
+    }
+  }
+
+  @Override
+  public int lineCount() {
+    return this.counts.stream().mapToInt(x -> x).sum();
+  }
+
+  @Override
+  public ModuleTrace commit() {
+    throw new IllegalStateException("should never be called");
+  }
+}
