bde883fd753f23263b66fb1b33f71e00a0053bbb
letypequividelespoubelles <54240434+letypequividelespoubelles@users.noreply.github.com>
2023-11-10 11:03:26 +0100

    fix(tx-chain-id): fix for missing chainId on current transaction (#37)
    Signed-off-by: Francois Bojarski <francois.bojarsk@consensys.net>

--- rlp_txn/constraints.lisp
+++ rlp_txn/constraints.lisp
@@ -704,55 +704,41 @@
            (begin (vanishes! (+ (- 1 LT) LX))
                   (eq! nSTEP 8))))
 
+(defun (w-minus-two-seven)
+  (- [INPUT 1] 27))
+
 (defconstraint phase11-rlp-w (:guard [PHASE 11])
   (if-not-zero (* LT (- 1 LX))
                (begin (rlpPrefixInt [INPUT 1] CT nSTEP DONE [BYTE 1] [ACC 1] ACC_BYTESIZE POWER BIT BIT_ACC LIMB LC nBYTES)
                       (if-eq DONE 1
                              (begin (limbShifting [INPUT 1] POWER ACC_BYTESIZE LIMB nBYTES)
-                                    (vanishes! (+ LC_CORRECTION
-                                                  PHASE_END
-                                                  (next LT)
-                                                  (- 1 (next LX)))))))))
-
-(defconstraint phase11-rlpbeta-init (:guard [PHASE 11])
-  (if-not-zero (* LX
-                  (- 1 (prev LX)))
-               (eq! IS_PREFIX 1)))
+                                    (vanishes! LC_CORRECTION)
+                                    (if-eq-else (^ (w-minus-two-seven) 2) (w-minus-two-seven)
+                                                (eq! PHASE_END 1)
+                                                (begin (vanishes! (+ (next PHASE_END)
+                                                                     (next LT)
+                                                                     (- 1 (next LX))
+                                                                     (- 1 (next IS_PREFIX)))))))))))
 
 (defconstraint phase11-rlp-beta (:guard [PHASE 11])
   (if-not-zero (* LX IS_PREFIX)
-               (if-eq-else (^ (- [INPUT 1] 27) 2) (- [INPUT 1] 27)
-                           (begin (eq! nSTEP 1)
-                                  (eq! 2 (+ LC_CORRECTION PHASE_END)))
-                           (begin (eq! nSTEP 8)
-                                  (rlpPrefixInt [INPUT 1]
-                                                CT
-                                                nSTEP
-                                                DONE
-                                                [BYTE 1]
-                                                [ACC 1]
-                                                ACC_BYTESIZE
-                                                POWER
-                                                BIT
-                                                BIT_ACC
-                                                LIMB
-                                                LC
-                                                nBYTES)
-                                  (if-eq DONE 1
-                                         (begin (limbShifting [INPUT 1] POWER ACC_BYTESIZE LIMB nBYTES)
-                                                (vanishes! (+ LC_CORRECTION
-                                                              PHASE_END
-                                                              (next IS_PREFIX)
-                                                              (next LT)
-                                                              (- 1 (next LX))
-                                                              (next LC_CORRECTION)
-                                                              (- 1 (next PHASE_END))
-                                                              (next LC_CORRECTION)))
-                                                (eq! (next nSTEP) 1)
-                                                (eq! (next LIMB)
-                                                     (+ (* INT_SHORT (^ 256 LLARGEMO))
-                                                        (* INT_SHORT (^ 256 14))))
-                                                (eq! (next nBYTES) 2)))))))
+               (begin (eq! nSTEP 8)
+                      (rlpPrefixInt [INPUT 1] CT nSTEP DONE [BYTE 1] [ACC 1] ACC_BYTESIZE POWER BIT BIT_ACC LIMB LC nBYTES)
+                      (if-eq DONE 1
+                             (begin (limbShifting [INPUT 1] POWER ACC_BYTESIZE LIMB nBYTES)
+                                    (vanishes! (+ LC_CORRECTION
+                                                  PHASE_END
+                                                  (next IS_PREFIX)
+                                                  (next LT)
+                                                  (- 1 (next LX))
+                                                  (next LC_CORRECTION)
+                                                  (- 1 (next PHASE_END))
+                                                  (next LC_CORRECTION)))
+                                    (eq! (next nSTEP) 1)
+                                    (eq! (next LIMB)
+                                         (+ (* INT_SHORT (^ 256 LLARGEMO))
+                                            (* INT_SHORT (^ 256 14))))
+                                    (eq! (next nBYTES) 2))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                       ;;
@@ -760,7 +746,8 @@
 ;;                       ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defconstraint phase12 (:guard [PHASE 12])
-  (begin (eq! nSTEP 1)
+  (begin (is-binary [INPUT 1])
+         (eq! nSTEP 1)
          (if-zero [INPUT 1]
                   (eq! LIMB
                        (* INT_SHORT (^ 256 LLARGEMO)))
