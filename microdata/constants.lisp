ba997c6cfbfec772b1f4138c5de771bdaca479ff
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2024-01-25 19:38:10 +0200

    feat(microdata): add microdata module, containing constants (#83)
    Resolves: #82 
    Signed-off-by: Tsvetan Dimitrov <tsvetan.dimitrov@consensys.net>

---
+++ microdata/constants.lisp
@@ -0,0 +1,16 @@
+(module microdata)
+
+(defconst 
+  ;transplants
+  RAM_TO_RAM                                601
+  EXO_TO_RAM                                602
+  RAM_IS_EXO                                603
+  KILLING_ONE                               604
+  PUSH_TWO_RAM_TO_STACK                     605
+  PUSH_ONE_RAM_TO_STACK                     606
+  EXCEPTIONAL_RAM_TO_STACK_3_TO_2_FULL_FAST 607
+  PUSH_TWO_STACK_TO_RAM                     608
+  STORE_X_IN_A_THREE_REQUIRED               609
+  STORE_X_IN_B                              610
+  STORE_X_IN_C                              611)
+
