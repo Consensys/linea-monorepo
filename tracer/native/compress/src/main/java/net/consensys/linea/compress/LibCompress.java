e8f7af53f9d337493321c5bbc89f3171b73f76c4
Fabio Di Fabio <fabio.difabio@consensys.net>
2024-02-27 16:27:51 +0100

    Use the right classloader to load the native library (#628)
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- tracer/native/compress/src/main/java/net/consensys/linea/compress/LibCompress.java
+++ tracer/native/compress/src/main/java/net/consensys/linea/compress/LibCompress.java
@@ -15,53 +15,57 @@
  */
 package net.consensys.linea.compress;
 
-import com.sun.jna.Native;
-
 import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-/**
- * Java interface to compress
- */
+import com.sun.jna.Native;
+import lombok.extern.slf4j.Slf4j;
+
+/** Java interface to compress */
+@Slf4j
 public class LibCompress {
 
-    @SuppressWarnings("WeakerAccess")
-    public static final boolean ENABLED;
+  @SuppressWarnings("WeakerAccess")
+  public static final boolean ENABLED;
 
-    static {
-        boolean enabled;
-        try {
-            final File compressJni = Native.extractFromResourcePath("compress_jni");
-            Native.register(LibCompress.class, compressJni.getAbsolutePath());
+  static {
+    try {
+      final File compressJni =
+          Native.extractFromResourcePath("compress_jni", LibCompress.class.getClassLoader());
+      Native.register(LibCompress.class, compressJni.getAbsolutePath());
 
-            Path dictFilePath = Files.createTempFile("tempCompressor_dict", "bin");
-            try (InputStream stream = LibCompress.class.getClassLoader().getResourceAsStream("compressor_dict.bin")) {
-                Files.copy(stream, dictFilePath, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
-                dictFilePath.toFile().deleteOnExit();
-            } catch (Exception e) {
-                System.out.println("Problem creating temp file from compressor_dict.bin resource: " + e.getMessage());
-                dictFilePath.toFile().delete();
-                System.exit(-1);
-            }
+      Path dictFilePath = Files.createTempFile("tempCompressor_dict", "bin");
+      try (InputStream stream =
+          LibCompress.class.getClassLoader().getResourceAsStream("compressor_dict.bin")) {
+        Files.copy(stream, dictFilePath, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+        dictFilePath.toFile().deleteOnExit();
+      } catch (Exception e) {
+        log.error(
+            "Problem creating temp file from compressor_dict.bin resource: " + e.getMessage());
+        dictFilePath.toFile().delete();
+        System.exit(1);
+      }
 
-            final String dictPath = dictFilePath.toAbsolutePath().toString();
-            if (!Init(dictPath)) {
-                throw new RuntimeException(Error());
-            }
-            enabled = true;
-        } catch (final Throwable t) {
-            t.printStackTrace();
-            enabled = false;
-        }
-        ENABLED = enabled;
+      final String dictPath = dictFilePath.toAbsolutePath().toString();
+      if (!Init(dictPath)) {
+        throw new RuntimeException(Error());
+      }
+      log.info(
+          "Loaded compress_jni native library from {} with compressor_dict {}",
+          compressJni,
+          dictPath);
+    } catch (final Throwable t) {
+      log.error("Error loading native compress_jni library", t);
+      System.exit(1);
     }
+    ENABLED = true;
+  }
 
-    public static native boolean Init(String dictPath);
-    
-    public static native int CompressedSize(
-            byte[] i, int i_len);
+  public static native boolean Init(String dictPath);
 
-    public static native String Error();
+  public static native int CompressedSize(byte[] i, int i_len);
+
+  public static native String Error();
 }
