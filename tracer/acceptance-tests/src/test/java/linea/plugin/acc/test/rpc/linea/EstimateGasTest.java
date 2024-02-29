a37d3bea6727100409d7ad5258b99eb896332996
Fabio Di Fabio <fabio.difabio@consensys.net>
2024-02-29 09:35:36 +0100

    linea_estimateGas compatibility switch (#634)
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>
    Co-authored-by: Sally MacFarlane <macfarla.github@gmail.com>

--- tracer/acceptance-tests/src/test/java/linea/plugin/acc/test/rpc/linea/EstimateGasTest.java
+++ tracer/acceptance-tests/src/test/java/linea/plugin/acc/test/rpc/linea/EstimateGasTest.java
@@ -18,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import linea.plugin.acc.test.LineaPluginTestBase;
@@ -28,6 +29,7 @@ import net.consensys.linea.config.LineaTransactionSelectorConfiguration;
 import net.consensys.linea.rpc.linea.LineaEstimateGas;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.units.bigints.UInt64;
+import org.bouncycastle.crypto.digests.KeccakDigest;
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Wei;
 import org.hyperledger.besu.tests.acceptance.dsl.account.Account;
@@ -38,25 +40,28 @@ import org.junit.jupiter.api.Test;
 import org.web3j.protocol.core.Request;
 
 public class EstimateGasTest extends LineaPluginTestBase {
-  private static final int VERIFICATION_GAS_COST = 1_200_000;
-  private static final int VERIFICATION_CAPACITY = 90_000;
-  private static final int GAS_PRICE_RATIO = 15;
-  private static final double MIN_MARGIN = 1.0;
-  private static final double ESTIMATE_GAS_MIN_MARGIN = 1.0;
-  private static final Wei MIN_GAS_PRICE = Wei.of(1_000_000_000);
-  public static final int MAX_TRANSACTION_GAS_LIMIT = 30_000_000;
-  private LineaTransactionSelectorConfiguration txSelectorConf;
+  protected static final int VERIFICATION_GAS_COST = 1_200_000;
+  protected static final int VERIFICATION_CAPACITY = 90_000;
+  protected static final int GAS_PRICE_RATIO = 15;
+  protected static final double MIN_MARGIN = 1.0;
+  protected static final double ESTIMATE_GAS_MIN_MARGIN = 1.0;
+  protected static final Wei MIN_GAS_PRICE = Wei.of(1_000_000_000);
+  protected static final int MAX_TRANSACTION_GAS_LIMIT = 30_000_000;
+  protected LineaTransactionSelectorConfiguration txSelectorConf;
 
   @Override
   public List<String> getTestCliOptions() {
+    return getTestCommandLineOptionsBuilder().build();
+  }
+
+  protected TestCommandLineOptionsBuilder getTestCommandLineOptionsBuilder() {
     return new TestCommandLineOptionsBuilder()
         .set("--plugin-linea-verification-gas-cost=", String.valueOf(VERIFICATION_GAS_COST))
         .set("--plugin-linea-verification-capacity=", String.valueOf(VERIFICATION_CAPACITY))
         .set("--plugin-linea-gas-price-ratio=", String.valueOf(GAS_PRICE_RATIO))
         .set("--plugin-linea-min-margin=", String.valueOf(MIN_MARGIN))
         .set("--plugin-linea-estimate-gas-min-margin=", String.valueOf(ESTIMATE_GAS_MIN_MARGIN))
-        .set("--plugin-linea-max-tx-gas-limit=", String.valueOf(MAX_TRANSACTION_GAS_LIMIT))
-        .build();
+        .set("--plugin-linea-max-tx-gas-limit=", String.valueOf(MAX_TRANSACTION_GAS_LIMIT));
   }
 
   @BeforeEach
@@ -81,7 +86,8 @@ public class EstimateGasTest extends LineaPluginTestBase {
 
     final Account sender = accounts.getSecondaryBenefactor();
 
-    final CallParams callParams = new CallParams(sender.getAddress(), null);
+    final CallParams callParams =
+        new CallParams(sender.getAddress(), sender.getAddress(), null, Bytes.EMPTY.toHexString());
 
     final var reqEth = new RawEstimateGasRequest(callParams);
     final var reqLinea = new LineaEstimateGasRequest(callParams);
@@ -95,28 +101,58 @@ public class EstimateGasTest extends LineaPluginTestBase {
 
     final Account sender = accounts.getSecondaryBenefactor();
 
-    final CallParams callParams = new CallParams(sender.getAddress(), null);
+    final KeccakDigest keccakDigest = new KeccakDigest(256);
+    final StringBuilder txData = new StringBuilder();
+    txData.append("0x");
+    for (int i = 0; i < 5; i++) {
+      keccakDigest.update(new byte[] {(byte) i}, 0, 1);
+      final byte[] out = new byte[32];
+      keccakDigest.doFinal(out, 0);
+      txData.append(new BigInteger(out).abs());
+    }
+    final var payload = Bytes.wrap(txData.toString().getBytes(StandardCharsets.UTF_8));
+
+    final CallParams callParams =
+        new CallParams(sender.getAddress(), sender.getAddress(), null, payload.toHexString());
 
     final var reqLinea = new LineaEstimateGasRequest(callParams);
     final var respLinea = reqLinea.execute(minerNode.nodeRequests());
 
-    final var gasLimit = UInt64.fromHexString(respLinea.gasLimit()).toLong();
+    final var estimatedGasLimit = UInt64.fromHexString(respLinea.gasLimit()).toLong();
     final var baseFee = Wei.fromHexString(respLinea.baseFeePerGas());
-    final var priorityFee = Wei.fromHexString(respLinea.priorityFeePerGas());
-    final var maxGasPrice = baseFee.add(priorityFee);
+    final var estimatedPriorityFee = Wei.fromHexString(respLinea.priorityFeePerGas());
+    final var estimatedMaxGasPrice = baseFee.add(estimatedPriorityFee);
 
     final var tx =
         org.hyperledger.besu.ethereum.core.Transaction.builder()
             .sender(Address.fromHexString(sender.getAddress()))
-            .gasLimit(gasLimit)
-            .gasPrice(maxGasPrice)
+            .to(Address.fromHexString(sender.getAddress()))
+            .gasLimit(estimatedGasLimit)
+            .gasPrice(estimatedMaxGasPrice)
             .chainId(BigInteger.valueOf(CHAIN_ID))
             .value(Wei.ZERO)
-            .payload(Bytes.EMPTY)
+            .payload(payload)
             .signature(LineaEstimateGas.FAKE_SIGNATURE_FOR_SIZE_CALCULATION)
             .build();
 
+    assertIsProfitable(tx, estimatedPriorityFee, estimatedMaxGasPrice, estimatedGasLimit);
+  }
+
+  protected void assertIsProfitable(
+      final org.hyperledger.besu.ethereum.core.Transaction tx,
+      final Wei estimatedPriorityFee,
+      final Wei estimatedMaxGasPrice,
+      final long estimatedGasLimit) {
+
+    final var minGasPrice = minerNode.getMiningParameters().getMinTransactionGasPrice();
+
     final var profitabilityCalculator = new TransactionProfitabilityCalculator(txSelectorConf);
+
+    final var profitablePriorityFee =
+        profitabilityCalculator.profitablePriorityFeePerGas(tx, minGasPrice, estimatedGasLimit);
+
+    assertThat(profitablePriorityFee.greaterThan(minGasPrice)).isTrue();
+
     assertThat(
             profitabilityCalculator.isProfitable(
                 "Test",
@@ -126,12 +162,30 @@ public class EstimateGasTest extends LineaPluginTestBase {
                     .getMinTransactionGasPrice()
                     .getAsBigInteger()
                     .doubleValue(),
-                maxGasPrice.getAsBigInteger().doubleValue(),
-                gasLimit))
+                estimatedMaxGasPrice.getAsBigInteger().doubleValue(),
+                estimatedGasLimit))
         .isTrue();
   }
 
-  class LineaEstimateGasRequest implements Transaction<LineaEstimateGasRequest.Response> {
+  @Test
+  public void lineaEstimateGasPriorityFeeMinGasPriceLowerBound() {
+
+    final Account sender = accounts.getSecondaryBenefactor();
+
+    final CallParams callParams = new CallParams(sender.getAddress(), null, "", "");
+
+    final var reqLinea = new LineaEstimateGasRequest(callParams);
+    final var respLinea = reqLinea.execute(minerNode.nodeRequests());
+
+    final var baseFee = Wei.fromHexString(respLinea.baseFeePerGas());
+    final var estimatedPriorityFee = Wei.fromHexString(respLinea.priorityFeePerGas());
+    final var estimatedMaxGasPrice = baseFee.add(estimatedPriorityFee);
+    final var minGasPrice = minerNode.getMiningParameters().getMinTransactionGasPrice();
+
+    assertThat(estimatedMaxGasPrice).isEqualTo(minGasPrice);
+  }
+
+  static class LineaEstimateGasRequest implements Transaction<LineaEstimateGasRequest.Response> {
     private final CallParams callParams;
 
     public LineaEstimateGasRequest(final CallParams callParams) {
@@ -158,7 +212,7 @@ public class EstimateGasTest extends LineaPluginTestBase {
     record Response(String gasLimit, String baseFeePerGas, String priorityFeePerGas) {}
   }
 
-  class RawEstimateGasRequest implements Transaction<String> {
+  static class RawEstimateGasRequest implements Transaction<String> {
     private final CallParams callParams;
 
     public RawEstimateGasRequest(final CallParams callParams) {
@@ -183,5 +237,5 @@ public class EstimateGasTest extends LineaPluginTestBase {
     static class RawEstimateGasResponse extends org.web3j.protocol.core.Response<String> {}
   }
 
-  record CallParams(String from, String value) {}
+  record CallParams(String from, String to, String value, String data) {}
 }
