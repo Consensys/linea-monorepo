3a3a3c0528b28ef70daa1eb5ed063d0c007f1045
Fabio Di Fabio <fabio.difabio@consensys.net>
2024-03-25 09:57:32 +0100

    On Windows also build Linux native lib so it can run on WSL (#651)
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- besu-plugins/linea-sequencer/native/wsl.sh
+++ besu-plugins/linea-sequencer/native/wsl.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/bin/bash -x
 
-docker build -f Dockerfile-win-dockcross -t native-windows-cross-compile .
+docker build -f Dockerfile-win-dockcross --build-arg IMAGE=windows-shared-x64 -t native-windows-shared-x64-cross-compile .
+docker build -f Dockerfile-win-dockcross --build-arg IMAGE=linux-x64 -t native-linux-x64-cross-compile .
 
-docker run --rm native-windows-cross-compile > compress/build/native/native-windows-cross-compile
+mkdir -p compress/build/native/
 
-compress/build/native/native-windows-cross-compile \
+docker run --rm native-windows-shared-x64-cross-compile > compress/build/native/native-windows-shared-x64-cross-compile
+docker run --rm native-linux-x64-cross-compile > compress/build/native/native-linux-x64-cross-compile
+
+compress/build/native/native-windows-shared-x64-cross-compile --image native-windows-shared-x64-cross-compile \
   bash -c "cd compress/compress-jni &&
     CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -buildmode=c-shared -o ../build/native/compress_jni.dll compress-jni.go"
 
+compress/build/native/native-linux-x64-cross-compile --image native-linux-x64-cross-compile \
+  bash -c "cd compress/compress-jni &&
+    CGO_ENABLED=1 go build -buildmode=c-shared -o ../build/native/libcompress_jni.so compress-jni.go"
