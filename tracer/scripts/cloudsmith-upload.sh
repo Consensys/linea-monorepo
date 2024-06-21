ddf7a6b8f1020c03c1544bacdc792aa70d12d4a8
Tsvetan Dimitrov <tsvetan.dimitrov@consensys.net>
2024-06-21 15:43:12 +0300

    feat: modify cloudsmith upload script to upload a maven artifact to Cloudsmith (#781)
    Resolves: #780 
    Signed-off-by: Tsvetan Dimitrov <tsvetan.dimitrov@consensys.net>

--- tracer/scripts/cloudsmith-upload.sh
+++ tracer/scripts/cloudsmith-upload.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-LINEA_ARITHMETIZATION_VERSION=${1:?Must specify Linea Arithmetization version}
-TAR_DIST=${2:?Must specify path to tar distribution}
-ZIP_DIST=${3:?Must specify path to zip distribution}
+VERSION=${1:?Must specify Linea Arithmetization version}
+BUILD_DIR=${2:?Must specify Linea Arithmetization module build directory}
 
 ENV_DIR=./build/tmp/cloudsmith-env
 if [[ -d ${ENV_DIR} ]] ; then
@@ -15,5 +14,5 @@ fi
 
 python3 -m pip install --upgrade cloudsmith-cli
 
-cloudsmith push raw consensys/linea-arithmetization $TAR_DIST --republish --name 'linea-arithmetization.tar.gz' --version "${LINEA_ARITHMETIZATION_VERSION}" --summary "Linea Arithmetization ${LINEA_ARITHMETIZATION_VERSION} binary distribution" --description "Binary distribution of Linea Arithmetization ${LINEA_ARITHMETIZATION_VERSION}." --content-type 'application/tar+gzip'
-cloudsmith push raw consensys/linea-arithmetization $ZIP_DIST --republish --name 'linea-arithmetization.zip' --version "${LINEA_ARITHMETIZATION_VERSION}" --summary "Linea Arithmetization ${LINEA_ARITHMETIZATION_VERSION} binary distribution" --description "Binary distribution of Linea Arithmetization ${LINEA_ARITHMETIZATION_VERSION}." --content-type 'application/zip'
+echo ">>>>>>>>>>>>>> Uploading Maven Artifact for linea-arithmetization-${VERSION} to Cloudsmith ..."
+cloudsmith push maven consensys/linea-arithmetization ${BUILD_DIR}/libs/linea-arithmetization-${VERSION}.jar --pom-file ${BUILD_DIR}/publications/mavenJava/pom-default.xml
