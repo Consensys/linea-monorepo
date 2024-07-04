52e9d264035847a189ece1c1daa717811bb083e1
Sally MacFarlane <macfarla.github@gmail.com>
2024-07-04 13:04:05 +1000

    update arithmetization to 0.1.5-rc4 (#36)
    * update arithmetization to 1.5.0-rc4
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    * update module limits files
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    * ordering and add missing modules
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    * added missing modules blake_modexp_data and shakira_data and gas
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    * rename rlp txnrcpt
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    
    * Update module limit files and fix test
    
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
    
    ---------
    
    Signed-off-by: Sally MacFarlane <macfarla.github@gmail.com>
    Signed-off-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
    Co-authored-by: Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>

--- besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/rpc/linea/EstimateGasModuleLimitOverflowTest.java
+++ besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/rpc/linea/EstimateGasModuleLimitOverflowTest.java
@@ -58,6 +58,6 @@ public class EstimateGasModuleLimitOverflowTest extends LineaPluginTestBase {
     final var respLinea = reqLinea.execute(minerNode.nodeRequests());
     assertThat(respLinea.getCode()).isEqualTo(-32000);
     assertThat(respLinea.getMessage())
-        .isEqualTo("Transaction line count for module SHF=32 is above the limit 20");
+        .isEqualTo("Transaction line count for module SHF=31 is above the limit 20");
   }
 }
