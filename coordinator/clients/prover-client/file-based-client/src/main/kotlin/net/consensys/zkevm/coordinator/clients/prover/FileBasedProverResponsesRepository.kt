6f2cba4b006e258835880dcd49e3a7c165cd823d
Bradley Bown <bradbown@googlemail.com>
2024-08-12 16:21:24 +0100

    Coordinator - Removed ExecutionProofResponse data class and response parsing (#3765)

--- coordinator/clients/prover-client/file-based-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/prover/FileBasedProverResponsesRepository.kt
+++ coordinator/clients/prover-client/file-based-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/prover/FileBasedProverResponsesRepository.kt
@@ -1,67 +1,21 @@
 package net.consensys.zkevm.coordinator.clients.prover
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
-import com.github.michaelbull.result.Result
-import com.github.michaelbull.result.mapError
-import io.vertx.core.Vertx
 import net.consensys.linea.errors.ErrorResponse
 import net.consensys.zkevm.coordinator.clients.ProverErrorType
 import net.consensys.zkevm.domain.ProofIndex
 import net.consensys.zkevm.fileio.FileMonitor
-import net.consensys.zkevm.fileio.FileReader
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import org.apache.tuweni.bytes.Bytes
-import org.apache.tuweni.bytes.Bytes32
-import tech.pegasys.teku.ethereum.executionclient.serialization.Bytes32Deserializer
-import tech.pegasys.teku.ethereum.executionclient.serialization.BytesDeserializer
 import tech.pegasys.teku.infrastructure.async.SafeFuture
 import java.nio.file.Path
-import java.time.Instant
 import kotlin.io.path.notExists
 
-data class ExecutionProofResponse(
-  @JsonDeserialize(using = BytesDeserializer::class) val proof: Bytes,
-  val verifierIndex: Long,
-  @JsonProperty("parentStateRootHash")
-  @JsonDeserialize(using = Bytes32Deserializer::class)
-  val zkParentStateRootHash: Bytes32,
-  val blocksData: List<BlockData>
-) {
-  data class BlockData(
-    /** Type 2 State Manager RootHash */
-    @JsonProperty("rootHash")
-    @JsonDeserialize(using = Bytes32Deserializer::class)
-    val zkRootHash: Bytes32,
-    /** Unix timestamp in seconds */
-    val timestamp: Instant,
-    val rlpEncodedTransactions: List<String>,
-    val batchReceptionIndices: List<UShort>,
-    /** L2->L1 Message service Smart Contract Logs abi encoded */
-    val l2ToL1MsgHashes: List<String>,
-    @JsonDeserialize(using = BytesDeserializer::class)
-    val fromAddresses: Bytes
-  )
-}
-
 class FileBasedProverResponsesRepository(
-  private val vertx: Vertx,
   private val config: Config,
-  private val mapper: ObjectMapper,
   private val proofResponseFileNameProvider: ProverFileNameProvider,
-  private val fileMonitor: FileMonitor,
-  private val responseReader: FileReader<ExecutionProofResponse> = FileReader(
-    vertx,
-    mapper,
-    ExecutionProofResponse::class.java
-  ),
-  @Suppress("UNCHECKED_CAST")
-  private val errorReader: FileReader<ErrorResponse<ProverErrorType>> =
-    FileReader(vertx, mapper, ErrorResponse::class.java) as FileReader<ErrorResponse<ProverErrorType>>
+  private val fileMonitor: FileMonitor
 ) : ProverResponsesRepository {
   private val log: Logger = LogManager.getLogger(this::class.java)
 
@@ -87,20 +41,18 @@ class FileBasedProverResponsesRepository(
 
   override fun find(
     index: ProofIndex
-  ): SafeFuture<Result<ExecutionProofResponse, ErrorResponse<ProverErrorType>>> {
+  ): SafeFuture<Unit> {
     val outputFile = outputFileNameForBlockInterval(index)
     log.trace("Polling for file {}", outputFile)
     return fileMonitor.fileExists(outputFile).thenCompose {
       if (it == true) {
-        parseResponseFile(outputFile)
+        SafeFuture.completedFuture(Unit)
       } else {
-        SafeFuture.completedFuture(
-          Err(
-            ErrorResponse(
-              ProverErrorType.ResponseNotFound,
-              "Response file '$outputFile' wasn't found in the repo"
-            )
-          )
+        SafeFuture.failedFuture(
+          ErrorResponse(
+            ProverErrorType.ResponseNotFound,
+            "Response file '$outputFile' wasn't found in the repo"
+          ).asException()
         )
       }
     }
@@ -108,46 +60,15 @@ class FileBasedProverResponsesRepository(
 
   override fun monitor(
     index: ProofIndex
-  ): SafeFuture<Result<ExecutionProofResponse, ErrorResponse<ProverErrorType>>> {
+  ): SafeFuture<Unit> {
     val outputFile = outputFileNameForBlockInterval(index)
     return fileMonitor.monitor(outputFile).thenCompose {
       when (it) {
-        is Ok -> parseResponseFile(it.value)
+        is Ok -> SafeFuture.completedFuture(Unit)
         is Err -> {
           when (it.error) {
-            FileMonitor.ErrorType.TIMED_OUT -> SafeFuture.completedFuture(
-              Err(
-                ErrorResponse(ProverErrorType.ResponseTimeout, "Monitoring timed out")
-              )
-            )
-          }
-        }
-      }
-    }
-  }
-
-  private fun parseResponseFile(
-    outputFile: Path
-  ): SafeFuture<Result<ExecutionProofResponse, ErrorResponse<ProverErrorType>>> {
-    return responseReader.read(outputFile).thenApply { result ->
-      result
-        .mapError { error ->
-          ErrorResponse(
-            ProverErrorType.ParseError,
-            "Failed to parse $outputFile, ${error.message}"
-          )
-        }
-    }.thenCompose { responseResult ->
-      when (responseResult) {
-        is Ok -> SafeFuture.completedFuture(Ok(responseResult.value))
-        is Err -> errorReader.read(outputFile).thenApply {
-          when (it) {
-            is Ok -> Err(it.value)
-            is Err -> Err(
-              ErrorResponse(
-                ProverErrorType.ParseError,
-                "Failed to parse $outputFile, ${it.error.message}"
-              )
+            FileMonitor.ErrorType.TIMED_OUT -> SafeFuture.failedFuture(
+              ErrorResponse(ProverErrorType.ResponseTimeout, "Monitoring timed out").asException()
             )
           }
         }
--- coordinator/clients/prover-client/file-based-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/prover/ProverResponsesRepository.kt
+++ coordinator/clients/prover-client/file-based-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/prover/ProverResponsesRepository.kt
@@ -1,12 +1,9 @@
 package net.consensys.zkevm.coordinator.clients.prover
 
-import com.github.michaelbull.result.Result
-import net.consensys.linea.errors.ErrorResponse
-import net.consensys.zkevm.coordinator.clients.ProverErrorType
 import net.consensys.zkevm.domain.ProofIndex
 import tech.pegasys.teku.infrastructure.async.SafeFuture
 
 interface ProverResponsesRepository {
-  fun find(index: ProofIndex): SafeFuture<Result<ExecutionProofResponse, ErrorResponse<ProverErrorType>>>
-  fun monitor(index: ProofIndex): SafeFuture<Result<ExecutionProofResponse, ErrorResponse<ProverErrorType>>>
+  fun find(index: ProofIndex): SafeFuture<Unit>
+  fun monitor(index: ProofIndex): SafeFuture<Unit>
 }
