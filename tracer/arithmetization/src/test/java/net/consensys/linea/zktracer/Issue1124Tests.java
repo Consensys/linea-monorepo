9b5c25f5f5e55ff09742a803072c57e4dd719ff6
Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>
2024-09-07 01:18:08 +0200

    Added single range ... that isn't problematic (#1128)

---
+++ tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/Issue1124Tests.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.zktracer;
+
+import static net.consensys.linea.zktracer.ReplayTests.replay;
+
+import org.junit.jupiter.api.Test;
+
+public class Issue1124Tests {
+
+  // STP constraints were failing for these ranges
+
+  // @Disabled
+  @Test
+  void issue_1124_range_4323962_4324012() {
+    replay("4323962-4324012.json.gz", false);
+  }
+
+  // @Disabled
+  @Test
+  void issue_1124_range_4343434_4343473() {
+    replay("4343434-4343473.json.gz", false);
+  }
+}
