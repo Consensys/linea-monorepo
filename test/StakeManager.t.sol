6b86d961423243043e0fbcce40c6d5f144a565f6
Andrea Franz <andrea@gravityblast.com>
2024-09-10 00:05:27 +0200

    update license and solc version

--- test/StakeManager.t.sol
+++ test/StakeManager.t.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
 import {Test, console} from "forge-std/Test.sol";
 import {StakeManager} from "../src/StakeManager.sol";
