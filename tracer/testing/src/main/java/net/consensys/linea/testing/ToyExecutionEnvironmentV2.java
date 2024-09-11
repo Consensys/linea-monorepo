e425bd4238cd894434acc374441a33e42fea21f1
Gaurav Ahuja <gauravahuja9@gmail.com>
2024-09-11 20:10:48 +0530

    [996] Refactor ReplayExecutionEnviroment and ToyExecutionEnvironment (#1159)

--- tracer/testing/src/main/java/net/consensys/linea/testing/ToyExecutionEnvironmentV2.java
+++ tracer/testing/src/main/java/net/consensys/linea/testing/ToyExecutionEnvironmentV2.java
@@ -18,7 +18,6 @@ package net.consensys.linea.testing;
 import static net.consensys.linea.zktracer.module.constants.GlobalConstants.LINEA_BASE_FEE;
 import static net.consensys.linea.zktracer.module.constants.GlobalConstants.LINEA_BLOCK_GAS_LIMIT;
 import static net.consensys.linea.zktracer.module.constants.GlobalConstants.LINEA_DIFFICULTY;
-import static net.consensys.linea.zktracer.runtime.stack.Stack.MAX_STACK_SIZE;
 
 import java.math.BigInteger;
 import java.util.*;
@@ -30,28 +29,14 @@ import lombok.Builder;
 import lombok.Singular;
 import lombok.extern.slf4j.Slf4j;
 import net.consensys.linea.zktracer.ZkTracer;
-import net.consensys.linea.zktracer.module.constants.GlobalConstants;
 import net.consensys.linea.zktracer.module.hub.Hub;
 import org.hyperledger.besu.datatypes.*;
 import org.hyperledger.besu.ethereum.core.*;
 import org.hyperledger.besu.ethereum.core.Transaction;
-import org.hyperledger.besu.ethereum.core.feemarket.CoinbaseFeePriceCalculator;
-import org.hyperledger.besu.ethereum.mainnet.LondonTargetingGasLimitCalculator;
-import org.hyperledger.besu.ethereum.mainnet.MainnetTransactionProcessor;
-import org.hyperledger.besu.ethereum.mainnet.TransactionValidatorFactory;
-import org.hyperledger.besu.ethereum.mainnet.feemarket.FeeMarket;
-import org.hyperledger.besu.ethereum.mainnet.feemarket.LondonFeeMarket;
+import org.hyperledger.besu.ethereum.mainnet.ProtocolSpec;
 import org.hyperledger.besu.ethereum.processing.TransactionProcessingResult;
 import org.hyperledger.besu.ethereum.referencetests.GeneralStateTestCaseEipSpec;
 import org.hyperledger.besu.ethereum.referencetests.ReferenceTestWorldState;
-import org.hyperledger.besu.evm.EVM;
-import org.hyperledger.besu.evm.MainnetEVMs;
-import org.hyperledger.besu.evm.gascalculator.GasCalculator;
-import org.hyperledger.besu.evm.internal.EvmConfiguration;
-import org.hyperledger.besu.evm.precompile.MainnetPrecompiledContracts;
-import org.hyperledger.besu.evm.precompile.PrecompileContractRegistry;
-import org.hyperledger.besu.evm.processor.ContractCreationProcessor;
-import org.hyperledger.besu.evm.processor.MessageCallProcessor;
 
 @Builder
 @Slf4j
@@ -59,17 +44,10 @@ public class ToyExecutionEnvironmentV2 {
   public static final BigInteger CHAIN_ID = BigInteger.valueOf(1337);
   public static final Address DEFAULT_COINBASE_ADDRESS =
       Address.fromHexString("0xc019ba5e00000000c019ba5e00000000c019ba5e");
-  private static final Wei DEFAULT_BASE_FEE = Wei.of(LINEA_BASE_FEE);
-
-  private static final GasCalculator gasCalculator = ZkTracer.gasCalculator;
-  private static final Address coinbaseAddress = Address.fromHexString("0x1234532342");
   private static final long DEFAULT_BLOCK_NUMBER = 6678980;
   private static final long DEFAULT_TIME_STAMP = 1347310;
   private static final Hash DEFAULT_HASH =
       Hash.fromHexStringLenient("0xdeadbeef123123666dead666dead666");
-  private static final FeeMarket feeMarket = FeeMarket.london(-1);
-
-  @Builder.Default private BigInteger chainId = CHAIN_ID;
 
   @Builder.Default private final List<ToyAccount> accounts = Collections.emptyList();
 
@@ -86,21 +64,19 @@ public class ToyExecutionEnvironmentV2 {
   private final ZkTracer tracer = new ZkTracer();
 
   public void run() {
-    final EVM evm = MainnetEVMs.london(this.chainId, EvmConfiguration.DEFAULT);
+    ProtocolSpec protocolSpec = ExecutionEnvironment.getProtocolSpec(CHAIN_ID);
+    GeneralStateTestCaseEipSpec generalStateTestCaseEipSpec =
+        this.buildGeneralStateTestCaseSpec(protocolSpec);
+
     GeneralStateReferenceTestTools.executeTest(
-        this.buildGeneralStateTestCaseSpec(evm),
-        tracer,
-        getMainnetTransactionProcessor(evm),
-        feeMarket,
-        testValidator,
-        zkTracerValidator);
+        generalStateTestCaseEipSpec, protocolSpec, tracer, testValidator, zkTracerValidator);
   }
 
   public Hub getHub() {
     return tracer.getHub();
   }
 
-  public GeneralStateTestCaseEipSpec buildGeneralStateTestCaseSpec(EVM evm) {
+  public GeneralStateTestCaseEipSpec buildGeneralStateTestCaseSpec(ProtocolSpec protocolSpec) {
     Map<String, ReferenceTestWorldState.AccountMock> accountMockMap =
         accounts.stream()
             .collect(
@@ -108,14 +84,14 @@ public class ToyExecutionEnvironmentV2 {
                     toyAccount -> toyAccount.getAddress().toHexString(),
                     ToyAccount::toAccountMock));
     ReferenceTestWorldState referenceTestWorldState =
-        ReferenceTestWorldState.create(accountMockMap, evm.getEvmConfiguration());
+        ReferenceTestWorldState.create(accountMockMap, protocolSpec.getEvm().getEvmConfiguration());
     BlockHeader blockHeader =
         BlockHeaderBuilder.createDefault()
-            .baseFee(DEFAULT_BASE_FEE)
+            .baseFee(Wei.of(LINEA_BASE_FEE))
             .gasLimit(LINEA_BLOCK_GAS_LIMIT)
             .difficulty(Difficulty.of(LINEA_DIFFICULTY))
             .number(DEFAULT_BLOCK_NUMBER)
-            .coinbase(coinbaseAddress)
+            .coinbase(DEFAULT_COINBASE_ADDRESS)
             .timestamp(DEFAULT_TIME_STAMP)
             .parentHash(DEFAULT_HASH)
             .buildBlockHeader();
@@ -126,7 +102,7 @@ public class ToyExecutionEnvironmentV2 {
     }
 
     return new GeneralStateTestCaseEipSpec(
-        /*fork*/ evm.getEvmVersion().getName().toLowerCase(),
+        /*fork*/ protocolSpec.getEvm().getEvmVersion().getName().toLowerCase(),
         txSuppliers,
         referenceTestWorldState,
         /*expectedRootHash*/ null,
@@ -136,36 +112,5 @@ public class ToyExecutionEnvironmentV2 {
         /*gasIndex*/ -1,
         /*valueIndex*/ -1,
         /*expectException*/ null);
-  }
-
-  private MainnetTransactionProcessor getMainnetTransactionProcessor(EVM evm) {
-    PrecompileContractRegistry precompileContractRegistry = new PrecompileContractRegistry();
-
-    MainnetPrecompiledContracts.populateForIstanbul(
-        precompileContractRegistry, evm.getGasCalculator());
-
-    final MessageCallProcessor messageCallProcessor =
-        new MessageCallProcessor(evm, precompileContractRegistry);
-
-    final ContractCreationProcessor contractCreationProcessor =
-        new ContractCreationProcessor(evm, false, List.of(), 1);
-
-    return new MainnetTransactionProcessor(
-        gasCalculator,
-        new TransactionValidatorFactory(
-            gasCalculator,
-            new LondonTargetingGasLimitCalculator(0L, new LondonFeeMarket(0)),
-            new LondonFeeMarket(0L),
-            false,
-            Optional.of(this.chainId),
-            Set.of(TransactionType.FRONTIER, TransactionType.ACCESS_LIST, TransactionType.EIP1559),
-            GlobalConstants.MAX_CODE_SIZE),
-        contractCreationProcessor,
-        messageCallProcessor,
-        true,
-        true,
-        MAX_STACK_SIZE,
-        feeMarket,
-        CoinbaseFeePriceCalculator.eip1559());
   }
 }
