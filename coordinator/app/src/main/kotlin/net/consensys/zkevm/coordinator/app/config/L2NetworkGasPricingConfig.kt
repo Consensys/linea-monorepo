ec7cd979b8dd985a17086fab43b52fee31d0130e
Roman <4833306+Filter94@users.noreply.github.com>
2024-09-12 16:16:43 +0200

    Added a comment about defaults

--- coordinator/app/src/main/kotlin/net/consensys/zkevm/coordinator/app/config/L2NetworkGasPricingConfig.kt
+++ coordinator/app/src/main/kotlin/net/consensys/zkevm/coordinator/app/config/L2NetworkGasPricingConfig.kt
@@ -14,6 +14,7 @@ import java.net.URL
 import java.time.Duration
 import kotlin.time.toKotlinDuration
 
+// Defaults to a compressed plain transfer transaction
 data class SampleTransactionGasPricingTomlDto(
   val plainTransferCostMultiplier: Double = 1.0,
   val compressedTxSize: Int = 125,
