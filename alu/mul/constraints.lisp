5aa9dcc567447c6542fc08f4f8b646bb842fba0f
Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>
2024-09-12 15:40:18 +0200

    Fixed the debug constraints (#338)
    Signed-off-by: Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>

--- alu/mul/constraints.lisp
+++ alu/mul/constraints.lisp
@@ -78,8 +78,8 @@
          (vanishes! ARG_1_LO)
          (vanishes! ARG_2_HI)
          (vanishes! ARG_2_LO)
-         (debug (vanishes! RES_HI))
-         (debug (vanishes! RES_LO))))
+         (debug (eq! RES_HI 0))
+         (debug (eq! RES_LO 1))))
 
 (defun (first-row)
   (if-not-zero (- (prev STAMP) STAMP)
