912d0bd100dcea55f88a349e8b8244a0ce1188f8
Andrea Franz <andrea@gravityblast.com>
2024-09-13 16:36:23 +0200

    add tests on reward index after staking and unstaking

---
+++ test/mocks/MockToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+}
