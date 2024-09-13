28a8998c08b475861b25aaea9d707b527905c501
Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>
2024-09-13 16:54:59 +0200

    Lookup fix (#345)

--- hub/lookups/hub_into_hub.lisp
+++ hub/lookups/hub_into_hub.lisp
@@ -1,15 +1,22 @@
-(deflookup hub-into-hub-for-DEP_NUM_INFTY-coherence
-           ;; target columns
+(defun    (hub-into-hub-source-selector)    hub.scp_PEEK_AT_STORAGE)
+(defun    (hub-into-hub-target-selector)    hub.acp_PEEK_AT_ACCOUNT) ;; ""
+
+(deflookup hub-into-hub---FIRST-FINAL-in-block-deployment-number-coherence
+	   ;; target columns
 	   ( 
-	     (* hub.account/ADDRESS_HI                         hub.PEEK_AT_ACCOUNT)
-	     (* hub.account/ADDRESS_HI                         hub.PEEK_AT_ACCOUNT)
-	     (* hub.account/DEPLOYMENT_NUMBER_INFTY            hub.PEEK_AT_ACCOUNT)
-           )
-           ;; source columns
+	     (* hub.acp_ADDRESS_HI                                 (hub-into-hub-target-selector))
+	     (* hub.acp_ADDRESS_LO                                 (hub-into-hub-target-selector))
+	     (* hub.acp_REL_BLK_NUM                                (hub-into-hub-target-selector))
+	     (* hub.acp_DEPLOYMENT_NUMBER_FIRST_IN_BLOCK           (hub-into-hub-target-selector))
+	     (* hub.acp_DEPLOYMENT_NUMBER_FINAL_IN_BLOCK           (hub-into-hub-target-selector))
+	     )
+	   ;; source columns
 	   (
-	     (* hub.storage/ADDRESS_HI                         hub.PEEK_AT_STORAGE)
-	     (* hub.storage/ADDRESS_HI                         hub.PEEK_AT_STORAGE)
-	     (* hub.storage/DEPLOYMENT_NUMBER_INFTY            hub.PEEK_AT_STORAGE)
-           )
-)
+	    (* hub.scp_ADDRESS_HI                                 (hub-into-hub-source-selector))
+	    (* hub.scp_ADDRESS_LO                                 (hub-into-hub-source-selector))
+	    (* hub.scp_REL_BLK_NUM                                (hub-into-hub-source-selector))
+	    (* hub.scp_DEPLOYMENT_NUMBER_FIRST_IN_BLOCK           (hub-into-hub-source-selector))
+	    (* hub.scp_DEPLOYMENT_NUMBER_FINAL_IN_BLOCK           (hub-into-hub-source-selector))
+	    )
+	   )
 
