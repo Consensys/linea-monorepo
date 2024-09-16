0bf4bd974fb383c5101f9d1c82601e5ff35adee9
Soleimani193 <azam.soleimanian@ens.fr>
2024-09-16 13:18:00 +0200

    minor

--- prover/protocol/column/verifiercol/expand_verifcol.go
+++ prover/protocol/column/verifiercol/expand_verifcol.go
@@ -2,11 +2,11 @@ package verifiercol
 
 import (
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/zkevm-monorepo/prover/maths/common/smartvectors"
-	"github.com/consensys/zkevm-monorepo/prover/maths/common/vector"
-	"github.com/consensys/zkevm-monorepo/prover/maths/field"
-	"github.com/consensys/zkevm-monorepo/prover/protocol/ifaces"
-	"github.com/consensys/zkevm-monorepo/prover/protocol/wizard"
+	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
+	"github.com/consensys/linea-monorepo/prover/maths/common/vector"
+	"github.com/consensys/linea-monorepo/prover/maths/field"
+	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 )
 
 // compile check to enforce the struct to belong to the corresponding interface
