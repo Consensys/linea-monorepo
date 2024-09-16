5b635284659ec941a4ace0e1a1521286a987c616
Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>
2024-09-16 05:59:42 +0200

    fix: added STP (#1184)

--- tracer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/Hub.java
+++ tracer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/hub/Hub.java
@@ -383,6 +383,7 @@ public class Hub implements Module {
                 rlpTxnRcpt,
                 rom,
                 shf,
+                stp,
                 trm,
                 txnData,
                 wcp,
