26aea5e3a2df542c243b376cbe3bf1581879271d
Soleimani193 <azam.soleimanian@ens.fr>
2024-09-16 13:18:00 +0200

    minor

--- prover/protocol/compiler/splitter/stitcher/stitcher.go
+++ prover/protocol/compiler/splitter/stitcher/stitcher.go
@@ -3,16 +3,16 @@ package stitcher
 import (
 	"strings"
 
-	"github.com/consensys/zkevm-monorepo/prover/maths/common/smartvectors"
-	"github.com/consensys/zkevm-monorepo/prover/maths/common/vector"
-	"github.com/consensys/zkevm-monorepo/prover/maths/field"
-	"github.com/consensys/zkevm-monorepo/prover/protocol/column"
-	"github.com/consensys/zkevm-monorepo/prover/protocol/column/verifiercol"
-	"github.com/consensys/zkevm-monorepo/prover/protocol/ifaces"
-	"github.com/consensys/zkevm-monorepo/prover/protocol/wizard"
-	"github.com/consensys/zkevm-monorepo/prover/symbolic"
-	"github.com/consensys/zkevm-monorepo/prover/utils"
-	"github.com/consensys/zkevm-monorepo/prover/utils/profiling"
+	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
+	"github.com/consensys/linea-monorepo/prover/maths/common/vector"
+	"github.com/consensys/linea-monorepo/prover/maths/field"
+	"github.com/consensys/linea-monorepo/prover/protocol/column"
+	"github.com/consensys/linea-monorepo/prover/protocol/column/verifiercol"
+	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+	"github.com/consensys/linea-monorepo/prover/symbolic"
+	"github.com/consensys/linea-monorepo/prover/utils"
+	"github.com/consensys/linea-monorepo/prover/utils/profiling"
 )
 
 type stitchingContext struct {
@@ -364,7 +364,7 @@ func isExprEligible(ctx stitchingContext, board symbolic.ExpressionBoard) bool {
 	if allAreVeriferCol {
 		// 4. we expect no expression involving only and only the verifierCols.
 		// We expect that this case wont happen.
-		// Otherwise should be handled in the [github.com/consensys/zkevm-monorepo/prover/protocol/query] package.
+		// Otherwise should be handled in the [github.com/consensys/linea-monorepo/prover/protocol/query] package.
 		// Namely, Local/Global queries should be checked directly by the verifer.
 		panic("all the columns in the expression are verifierCols, unsupported by the compiler")
 	}
