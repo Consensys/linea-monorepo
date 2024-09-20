2bdbf8f11bf4ed1ba219c80002631867d59bab48
Leo Jeong <dreamerty@postech.ac.kr>
2024-09-21 03:29:58 +0900

    Prover: fix type mismatch in TestOpBasicEdgeCases (#76)
    * fix type mismatch in TestOpBasicEdgeCases
    
    * fix gofmt

--- prover/maths/common/smartvectors/arithmetic_test.go
+++ prover/maths/common/smartvectors/arithmetic_test.go
@@ -237,7 +237,7 @@ func TestOpBasicEdgeCases(t *testing.T) {
 		t.Run(fmt.Sprintf("case-%v", i), func(t *testing.T) {
 			t.Logf("test-case details: %v", testCase.explainer)
 			res := testCase.fn(testCase.inputs...).(*Pooled)
-			require.Equal(t, testCase.expectedRes, res.Regular, "expectedRes=%v\nres=%v", testCase.expectedRes.Pretty(), res.Pretty())
+			require.Equal(t, testCase.expectedRes, &res.Regular, "expectedRes=%v\nres=%v", testCase.expectedRes.Pretty(), res.Pretty())
 		})
 	}
 }
