11e26fbfcbf0998e48c0d0172ce783dc52289d5d
thedarkjester <grant.southey@consensys.net>
2024-09-20 12:46:26 +0100

    add dev comment to Integration Test Verifier

--- contracts/contracts/test-contracts/IntegrationTestTrueVerifier.sol
+++ contracts/contracts/test-contracts/IntegrationTestTrueVerifier.sol
@@ -5,6 +5,8 @@ import { IPlonkVerifier } from "../interfaces/l1/IPlonkVerifier.sol";
 
 /// @dev Test verifier contract that returns true.
 contract IntegrationTestTrueVerifier is IPlonkVerifier {
+
+  /// @dev Always returns true for quick turnaround testing.
   function Verify(bytes calldata, uint256[] calldata) external pure returns (bool) {
     return true;
   }
