705c44dbe28ae1d3bd864134b1f6847b21f25b4c
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-09-24 20:23:21 +0200

    fix: splitting of constraints + formatting

--- blockdata/constants.lisp
+++ blockdata/constants.lisp
@@ -1,13 +1,13 @@
 (module blockdata)
 
 (defconst 
-  MAX_CT               6 ;;need to update the lookup into wcp if changed
-  ROW_SHIFT_COINBASE   0
-  ROW_SHIFT_TIMESTAMP  1
-  ROW_SHIFT_NUMBER     2
-  ROW_SHIFT_DIFFICULTY 3
-  ROW_SHIFT_GASLIMIT   4
-  ROW_SHIFT_CHAINID    5
-  ROW_SHIFT_BASEFEE    6)
+  CT_MAX_FOR_BLOCKDATA    6 ;;need to update the lookup into wcp if changed
+  ROW_SHIFT_COINBASE      0
+  ROW_SHIFT_TIMESTAMP     1
+  ROW_SHIFT_NUMBER        2
+  ROW_SHIFT_DIFFICULTY    3
+  ROW_SHIFT_GASLIMIT      4
+  ROW_SHIFT_CHAINID       5
+  ROW_SHIFT_BASEFEE       6)
 
 
