b792d4efb52e0428bb73979bb7f1832c90363e63
Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>
2024-09-24 16:56:06 +0200

    feat: added test (which, again, works out of the box ...) (#1290)

---
+++ tracer/arithmetization/src/test/java/net/consensys/linea/replaytests/Issue1279Tests.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.replaytests;
+
+import static net.consensys.linea.replaytests.ReplayTestTools.replay;
+import static net.consensys.linea.testing.ReplayExecutionEnvironment.LINEA_MAINNET;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("nightly")
+@Tag("replay")
+public class Issue1279Tests {
+
+  @Test
+  void issue_1274_mainnet_block_7768557() {
+    replay(LINEA_MAINNET, "7768557-7768557.json.gz");
+  }
+}
