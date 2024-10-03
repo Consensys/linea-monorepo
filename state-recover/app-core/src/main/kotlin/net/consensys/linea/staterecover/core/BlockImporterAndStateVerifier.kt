126061afa07d0e7cb00f543440fd9574ea610771
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-10-03 18:48:37 +0100

    state-recover: initial implementation of BlockImporterAndStateVerifierV1

--- state-recover/app-core/src/main/kotlin/net/consensys/linea/staterecover/core/BlockImporterAndStateVerifier.kt
+++ state-recover/app-core/src/main/kotlin/net/consensys/linea/staterecover/core/BlockImporterAndStateVerifier.kt
@@ -1,7 +1,15 @@
 package net.consensys.linea.staterecover.core
 
+import io.vertx.core.Vertx
+import net.consensys.linea.BlockInterval
+import net.consensys.linea.BlockParameter
+import net.consensys.linea.async.AsyncRetryer
+import net.consensys.linea.staterecover.clients.elclient.ExecutionLayerClient
 import net.consensys.linea.staterecover.domain.BlockL1RecoveredData
+import net.consensys.zkevm.coordinator.clients.StateManagerClientV1
 import tech.pegasys.teku.infrastructure.async.SafeFuture
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 data class ImportResult(
   val blockNumber: ULong,
@@ -28,4 +36,52 @@ data class ImportResult(
 
 interface BlockImporterAndStateVerifier {
   fun importBlocks(blocks: List<BlockL1RecoveredData>): SafeFuture<ImportResult>
+}
+
+class BlockImporterAndStateVerifierV1(
+  private val vertx: Vertx,
+  private val elClient: ExecutionLayerClient,
+  private val stateManagerClient: StateManagerClientV1,
+  private val stateManagerImportTimeoutPerBlock: Duration
+) : BlockImporterAndStateVerifier {
+  override fun importBlocks(blocks: List<BlockL1RecoveredData>): SafeFuture<ImportResult> {
+    return elClient
+      .lineaEngineImportBlocksFromBlob(blocks)
+      .thenCombine(
+        elClient.getBlockNumberAndHash(blockParameter = BlockParameter.Tag.FINALIZED)
+      ) { _, elFinalizedBlock ->
+        elClient.lineaEngineForkChoiceUpdated(
+          finalizedBlockHash = elFinalizedBlock.hash.toArray(),
+          headBlockHash = blocks.last().blockHash
+        )
+      }.thenCompose {
+        getBlockStateRootHash(
+          blockNumber = blocks.last().blockNumber,
+          timeout = stateManagerImportTimeoutPerBlock.times(blocks.size)
+        )
+      }
+      .thenApply { stateRootHash ->
+        ImportResult(
+          blockNumber = blocks.last().blockNumber,
+          zkStateRootHash = stateRootHash
+        )
+      }
+  }
+
+  private fun getBlockStateRootHash(
+    blockNumber: ULong,
+    timeout: Duration
+  ): SafeFuture<ByteArray> {
+    return AsyncRetryer
+      .retry(
+        vertx,
+        backoffDelay = 1.seconds,
+        timeout = timeout,
+        stopRetriesPredicate = { headBlockNumber -> headBlockNumber >= blockNumber },
+        action = { stateManagerClient.rollupGetHeadBlockNumber() }
+      )
+      .thenCompose {
+        stateManagerClient.rollupGetStateMerkleProof(BlockInterval(blockNumber, blockNumber))
+      }.thenApply { proofResponse -> proofResponse.zkEndStateRootHash.toArray() }
+  }
 }
