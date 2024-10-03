4bfe816e4e74d10e48c0d17181590a6bf2a71610
VGau <victorien.gauch@consensys.net>
2024-10-03 09:49:50 +0200

    fix: refactor test utils structure

---
+++ contracts/test/utils/constants/messageStatuses.ts
@@ -0,0 +1,7 @@
+export const INBOX_STATUS_UNKNOWN = 0;
+export const INBOX_STATUS_RECEIVED = 1;
+export const INBOX_STATUS_CLAIMED = 2;
+
+export const OUTBOX_STATUS_UNKNOWN = 0;
+export const OUTBOX_STATUS_SENT = 1;
+export const OUTBOX_STATUS_RECEIVED = 2;
