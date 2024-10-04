5f24343af36b0a8bc3d86f474152c5c1cec1249e
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-10-04 12:57:24 +0100

    state-recover: move packages to build.linea

---
+++ state-recover/clients/blobscan-client/src/main/kotlin/build/linea/staterecover/clients/blobscan/BlobScanClient.kt
@@ -0,0 +1,34 @@
+package build.linea.staterecover.clients.blobscan
+
+import build.linea.staterecover.clients.BlobFetcher
+import io.vertx.core.json.JsonObject
+import net.consensys.decodeHex
+import net.consensys.encodeHex
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import tech.pegasys.teku.infrastructure.async.SafeFuture
+
+class BlobScanClient(
+  val restClient: RestClient<JsonObject>,
+  val log: Logger = LogManager.getLogger(BlobScanClient::class.java)
+) : BlobFetcher {
+  fun getBlobById(id: String): SafeFuture<ByteArray> {
+    return restClient
+      .get("/blobs/$id")
+      .thenApply { response ->
+        if (response.statusCode == 200) {
+          response.body!!.getString("data").decodeHex()
+        } else {
+          throw RuntimeException(
+            "error fetching blobId=$id " +
+              "errorMessage=${response.body?.getString("message") ?: ""}"
+          )
+        }
+      }
+  }
+
+  override fun fetchBlobsByHash(blobVersionedHashes: List<ByteArray>): SafeFuture<List<ByteArray>> {
+    return SafeFuture
+      .collectAll(blobVersionedHashes.map { hash -> getBlobById(hash.encodeHex()) }.stream())
+  }
+}
