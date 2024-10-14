fdfb5c251a900d6b4ef4cc8cadf0a0d0bbf15433
François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
2024-10-14 14:36:06 +0530

    fix: add CFI to the lookup txnData into rlpTxn (#439)
    Signed-off-by: François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
    Co-authored-by: Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>

--- txndata/lookups/txndata_into_rlptxn.lisp
+++ txndata/lookups/txndata_into_rlptxn.lisp
@@ -4,6 +4,7 @@
   (
     rlptxn.ABS_TX_NUM_INFINY
     rlptxn.ABS_TX_NUM
+    rlptxn.CODE_FRAGMENT_INDEX
     rlptxn.PHASE
     rlptxn.DATA_HI
     rlptxn.DATA_LO
@@ -12,6 +13,7 @@
   (
     txndata.ABS_TX_NUM_MAX
     txndata.ABS_TX_NUM
+    txndata.CODE_FRAGMENT_INDEX
     txndata.PHASE_RLP_TXN
     txndata.OUTGOING_HI
     txndata.OUTGOING_LO
