d197249b2bc2b69bcd448dbb3d3f9309c7012939
François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
2024-10-14 13:53:51 +0530

    delete size into nBytes in the lookup mmio into rom (#443)
    Signed-off-by: François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>

--- mmio/lookups/mmio_into_rom.lisp
+++ mmio/lookups/mmio_into_rom.lisp
@@ -5,7 +5,6 @@
     rom.CFI
     rom.INDEX
     rom.LIMB
-    rom.nBYTES
     rom.CODE_SIZE
   )
   ;source columns
@@ -13,7 +12,6 @@
     (* mmio.EXO_IS_ROM mmio.EXO_ID)
     (* mmio.EXO_IS_ROM mmio.INDEX_X)
     (* mmio.EXO_IS_ROM mmio.LIMB)
-    (* mmio.EXO_IS_ROM mmio.SIZE)
     (* mmio.EXO_IS_ROM mmio.TOTAL_SIZE)
   ))
 
