6aa1632bb595b0b6cc5730aa2166597b6e1978fe
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-10-17 17:23:23 +0100

    Jackson Serialiser and Deserialisers (#194)
    * adds jackson Serilaiser and Deserialisers
    
    * improve jackson Bytes SerDe

---
+++ jvm-libs/generic/serialization/jackson/src/main/kotlin/build/linea/s11n/jackson/InstantISO8601SerDe.kt
@@ -0,0 +1,22 @@
+package build.linea.s11n.jackson
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import kotlinx.datetime.Instant
+
+object InstantISO8601Serializer : JsonSerializer<Instant>() {
+  override fun serialize(value: Instant, gen: JsonGenerator, serializers: SerializerProvider) {
+    gen.writeString(value.toString())
+  }
+}
+
+// To uncomment and add the tests when necessary
+object InstantISO8601Deserializer : JsonDeserializer<Instant>() {
+  override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Instant {
+    return Instant.parse(p.text)
+  }
+}
