e50754dc2fa18a56ccc8a8ca1aba0089257fdd6e
thedarkjester <grant.southey@consensys.net>
2024-10-19 12:48:44 +0100

    set default admin for token bridge on reinit

--- contracts/contracts/tokenBridge/TokenBridge.sol
+++ contracts/contracts/tokenBridge/TokenBridge.sol
@@ -154,21 +154,30 @@ contract TokenBridge is
   /**
    * @notice Sets permissions for a list of addresses and their roles as well as initialises the PauseManager pauseType:role mappings.
    * @dev This function is a reinitializer and can only be called once per version. Should be called using an upgradeAndCall transaction to the ProxyAdmin.
+   * @param _securityCouncil The security council to get default admin.
    * @param _roleAddresses The list of addresses and roles to assign permissions to.
    * @param _pauseTypeRoles The list of pause types to associate with roles.
    * @param _unpauseTypeRoles The list of unpause types to associate with roles.
    */
   function reinitializePauseTypesAndPermissions(
+    address _securityCouncil,
     RoleAddress[] calldata _roleAddresses,
     PauseTypeRole[] calldata _pauseTypeRoles,
     PauseTypeRole[] calldata _unpauseTypeRoles
   ) external reinitializer(2) {
+    if (_securityCouncil == address(0)) {
+      revert ZeroAddressNotAllowed();
+    }
+
+    _grantRole(DEFAULT_ADMIN_ROLE, _securityCouncil);
+
     assembly {
       /// @dev Wiping the storage slot 101 of _owner as it is replaced by AccessControl and there is now the ERC165 __gap in its place.
-      /// @dev Wiping the storage slot 213 of _status as it is replaced by ReentrancyGuardUpgradeable at slot 1.
       sstore(101, 0)
+      /// @dev Wiping the storage slot 213 of _status as it is replaced by ReentrancyGuardUpgradeable at slot 1.
       sstore(213, 0)
     }
+
     __ReentrancyGuard_init();
     __PauseManager_init(_pauseTypeRoles, _unpauseTypeRoles);
     __Permissions_init(_roleAddresses);
