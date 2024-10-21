1bce69cef8fe02afe89b0150d264cdd511379aaa
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-10-21 14:38:27 +0100

    jvm-libs layout rearrange (#213)
    * jvm-libs layout rearrange

---
+++ jvm-libs/linea/testing/file-system/src/test/kotlin/net/consensys/linea/testing/filesystem/FilesTest.kt
@@ -0,0 +1,24 @@
+package net.consensys.linea.testing.filesystem
+
+import net.consensys.encodeHex
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+class FilesTest {
+  @Test
+  fun `when file exists shall return path to it`() {
+    val file = findPathTo("build.gradle")
+    assertThat(file).isNotNull
+    assertThat(file.toString()).contains("file-system/build.gradle")
+
+    assertThat(findPathTo("settings.gradle", lookupParentDir = false)).isNull()
+    assertThat(findPathTo("settings.gradle", lookupParentDir = true)).exists()
+  }
+
+  @Test
+  fun `when file does not exists shall return null`() {
+    val file = findPathTo("non-existing-file-${Random.nextBytes(10).encodeHex()}")
+    assertThat(file).isNull()
+  }
+}
