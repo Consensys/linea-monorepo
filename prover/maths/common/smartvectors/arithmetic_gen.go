1647b69b8915cbbb5f744a682df1aca3585a07ce
Soleimani193 <azam.soleimanian@ens.fr>
2024-10-21 16:22:22 +0200

    added *Pooled to the polyEval cases

--- prover/maths/common/smartvectors/arithmetic_gen.go
+++ prover/maths/common/smartvectors/arithmetic_gen.go
@@ -110,12 +110,12 @@ func processOperator(op operator, coeffs []int, svecs []SmartVector, p ...mempoo
 
 	switch {
 	case matchedRegular == totalToMatch:
-		return &regularRes.Regular
+		return regularRes
 	case matchedRegular+matchedConst == totalToMatch:
 		// In this case, there are no windowed in the list. This means we only
 		// need to merge the const one into the regular one before returning
 		op.constTermIntoVec(regularRes.Regular, &constRes.val)
-		return &regularRes.Regular
+		return regularRes
 	default:
 
 		// If windowRes is a regular (can happen if all windows arguments cover the full circle)
