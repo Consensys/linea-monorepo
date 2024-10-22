92755fe5ef153def9bde3270bb3834c0c9b3a44d
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-10-22 20:21:39 +0100

    fix test jvm-libs:generic:extensions:tuweni

--- jvm-libs/generic/extensions/tuweni/src/test/kotlin/build/linea/tuweni/Bytes32Test.kt
+++ jvm-libs/generic/extensions/tuweni/src/test/kotlin/build/linea/tuweni/Bytes32Test.kt
@@ -5,12 +5,20 @@ import org.apache.tuweni.bytes.Bytes32
 import org.apache.tuweni.units.bigints.UInt256
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.random.Random
 
 class Bytes32Test {
+  @BeforeEach
+  fun setUp() {
+    // workaround: need this to load the functions otherwise JUNit gets stuck ¯\_(ツ)_/¯
+    Random.Default.nextBytes(3 * 32 - 1).sliceAsBytes32(0)
+    UInt256.ZERO.toBytes().toULong()
+  }
+
   @Test
-  fun sliceAsBytes32() {
+  fun testSliceAsBytes32() {
     val bytes = Random.Default.nextBytes(3 * 32 - 1)
     assertThat(bytes.sliceAsBytes32(0)).isEqualTo(Bytes32.wrap(bytes, 0))
     assertThat(bytes.sliceAsBytes32(1)).isEqualTo(Bytes32.wrap(bytes, 32))
@@ -19,7 +27,7 @@ class Bytes32Test {
   }
 
   @Test
-  fun toULong() {
+  fun testToULong() {
     assertThat(UInt256.ZERO.toBytes().toULong()).isEqualTo(0uL)
     assertThat(UInt256.valueOf(Long.MAX_VALUE).toULong()).isEqualTo(Long.MAX_VALUE.toULong())
     assertThat(UInt256.valueOf(Long.MAX_VALUE).add(UInt256.ONE).toULong()).isEqualTo(Long.MAX_VALUE.toULong() + 1UL)
