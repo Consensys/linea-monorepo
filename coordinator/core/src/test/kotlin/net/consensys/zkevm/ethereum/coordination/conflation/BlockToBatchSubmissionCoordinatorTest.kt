cfdc667c650cdcb75abe767a0df3afd714cd3fa7
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-10-22 23:43:35 +0100

    increase timeout (optimistically too low)

--- coordinator/core/src/test/kotlin/net/consensys/zkevm/ethereum/coordination/conflation/BlockToBatchSubmissionCoordinatorTest.kt
+++ coordinator/core/src/test/kotlin/net/consensys/zkevm/ethereum/coordination/conflation/BlockToBatchSubmissionCoordinatorTest.kt
@@ -25,6 +25,7 @@ import org.mockito.kotlin.whenever
 import tech.pegasys.teku.ethereum.executionclient.schema.randomExecutionPayload
 import tech.pegasys.teku.infrastructure.async.SafeFuture
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 
 @ExtendWith(VertxExtension::class)
@@ -79,7 +80,7 @@ class BlockToBatchSubmissionCoordinatorTest {
 
     val captor = argumentCaptor<Throwable>()
     Assertions.assertThat(blockToBatchSubmissionCoordinator.acceptBlock(baseBlock)).isCompleted
-    Awaitility.await().atMost(200.milliseconds.toJavaDuration())
+    Awaitility.await().atMost(1.seconds.toJavaDuration())
       .untilAsserted {
         verify(testLogger, times(1)).error(
           eq("Failed to conflate block={} errorMessage={}"),
