8a01ae95681ba22288ce9cba6cd36b6dc6ee27cf
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-10-23 09:39:49 +0100

    small tweak: improve name consistency on URIExtensions.kt

--- jvm-libs/generic/extensions/kotlin/src/main/kotlin/build/linea/URIExtensions.kt
+++ jvm-libs/generic/extensions/kotlin/src/main/kotlin/build/linea/URIExtensions.kt
@@ -2,14 +2,14 @@ package build.linea
 
 import java.net.URI
 
-fun URI.getPortWithSchemaDefaults(): Int {
+fun URI.getPortWithSchemeDefaults(): Int {
   return if (port != -1) {
     port
   } else {
     when (scheme.lowercase()) {
       "http" -> 80
       "https" -> 443
-      // Focous on HTTP as it what we need for now
+      // Focous on HTTP as it is what we need for now
       else -> throw IllegalArgumentException("Unsupported scheme: $scheme")
     }
   }
