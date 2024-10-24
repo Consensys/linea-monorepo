660e9a7bb0e6978842fab3312734398438b95ee1
olegfomenko <oleg.fomenko2002@gmail.com>
2024-10-25 01:26:06 +0300

    adding execution pool

---
+++ prover/utils/parallel/pool.go
@@ -0,0 +1,40 @@
+package parallel
+
+import (
+	"runtime"
+	"sync"
+)
+
+var queue chan func()
+var available chan struct{}
+var once sync.Once
+
+func ExecutePool(task func()) chan struct{} {
+	once.Do(run)
+
+	ch := make(chan struct{}, 1)
+	queue <- func() {
+		task()
+		close(ch)
+	}
+	return ch
+}
+
+func run() {
+	for i := 0; i < runtime.GOMAXPROCS(0); i++ {
+		available <- struct{}{}
+	}
+
+	go scheduler()
+}
+
+func scheduler() {
+	for {
+		<-available
+		task := <-queue
+		go func() {
+			task()
+			available <- struct{}{}
+		}()
+	}
+}
