0ea8cdac2b1fbc169fd87d86f8a9e3ee389d2f35
olegfomenko <oleg.fomenko2002@gmail.com>
2024-10-25 23:42:54 +0300

    adding ExecutePoolChunkyWithAllocated

--- prover/utils/parallel/pool/pool.go
+++ prover/utils/parallel/pool/pool.go
@@ -2,6 +2,7 @@ package pool
 
 import (
 	"github.com/consensys/linea-monorepo/prover/maths/common/mempool"
+	sv "github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
 	"runtime"
 	"sync"
 )
@@ -20,6 +21,34 @@ func ExecutePool(task func()) {
 	}
 
 	<-ch
+}
+
+func ExecutePoolChunkyWithAllocated(nbIterations int, sz int, work func(k int, subQuotientReg sv.SmartVector)) {
+	once.Do(run)
+
+	wg := sync.WaitGroup{}
+	wg.Add(nbIterations)
+
+	pool := make([]sv.SmartVector, 0, runtime.GOMAXPROCS(0))
+	poolIndex := make(chan int, runtime.GOMAXPROCS(0))
+
+	for i := 0; i < runtime.GOMAXPROCS(0); i++ {
+		poolIndex <- i
+		pool = append(pool, sv.AllocateRegular(sz))
+	}
+
+	for i := 0; i < nbIterations; i++ {
+		k := i
+		poolI := <-poolIndex
+
+		queue <- func() {
+			work(k, pool[poolI])
+			poolIndex <- poolI
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
 }
 
 func ExecutePoolChunkyWithCache(nbIterations int, lagerPool mempool.MemPool, work func(k int, localPool *mempool.SliceArena)) {
