fa4cb11f2e70dacc036d1a2713c2b9fce64cd1cb
olegfomenko <oleg.fomenko2002@gmail.com>
2024-10-27 01:20:47 +0300

    fix multi_to_single_point.go councurency waitning, retry quotient with ExecutePoolChunky

--- prover/protocol/compiler/univariates/multi_to_single_point.go
+++ prover/protocol/compiler/univariates/multi_to_single_point.go
@@ -240,7 +240,10 @@ func (ctx mptsCtx) accumulateQuotients(run *wizard.ProverRuntime) {
 		// precompute the lagrange polynomials
 		lagranges = getLagrangesPolys(hs)
 		pool      = mempool.CreateFromSyncPool(ctx.targetSize).Prewarm(runtime.NumCPU())
+		mainWg    = &sync.WaitGroup{}
 	)
+
+	mainWg.Add(runtime.GOMAXPROCS(0))
 
 	parallel.ExecuteFromChan(len(ctx.polys), func(wg *sync.WaitGroup, index *parallel.AtomicCounter) {
 
@@ -347,7 +350,11 @@ func (ctx mptsCtx) accumulateQuotients(run *wizard.ProverRuntime) {
 		quoLock.Lock()
 		quotient = sv.PolyAdd(quotient, subQuotientReg)
 		quoLock.Unlock()
+
+		mainWg.Done()
 	})
+
+	mainWg.Wait()
 
 	if quotient.Len() < ctx.targetSize {
 		quo := sv.IntoRegVec(quotient)
