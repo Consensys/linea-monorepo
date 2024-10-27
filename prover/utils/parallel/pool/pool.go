08bf6ed987f45724e3ef55e716d8888b04fbf2d0
olegfomenko <oleg.fomenko2002@gmail.com>
2024-10-27 18:12:24 +0200

    remove unused pool functions

--- prover/utils/parallel/pool/pool.go
+++ prover/utils/parallel/pool/pool.go
@@ -1,8 +1,6 @@
 package pool
 
 import (
-	"github.com/consensys/linea-monorepo/prover/maths/common/mempool"
-	sv "github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
 	"runtime"
 	"sync"
 )
@@ -21,65 +19,6 @@ func ExecutePool(task func()) {
 	}
 
 	<-ch
-}
-
-func ExecutePoolChunkyWithAllocated(nbIterations int, sz int, work func(k int, subQuotientReg sv.SmartVector)) {
-	once.Do(run)
-
-	wg := sync.WaitGroup{}
-	wg.Add(nbIterations)
-
-	pool := make([]sv.SmartVector, 0, runtime.GOMAXPROCS(0))
-	poolIndex := make(chan int, runtime.GOMAXPROCS(0))
-
-	for i := 0; i < runtime.GOMAXPROCS(0); i++ {
-		poolIndex <- i
-		pool = append(pool, sv.AllocateRegular(sz))
-	}
-
-	for i := 0; i < nbIterations; i++ {
-		k := i
-		poolI := <-poolIndex
-
-		queue <- func() {
-			work(k, pool[poolI])
-			poolIndex <- poolI
-			wg.Done()
-		}
-	}
-
-	wg.Wait()
-}
-
-func ExecutePoolChunkyWithCache(nbIterations int, lagerPool mempool.MemPool, work func(k int, localPool *mempool.SliceArena)) {
-	once.Do(run)
-
-	wg := sync.WaitGroup{}
-	wg.Add(nbIterations)
-
-	pool := make(chan *mempool.SliceArena, runtime.GOMAXPROCS(0))
-	for i := 0; i < runtime.GOMAXPROCS(0); i++ {
-		pool <- mempool.WrapsWithMemCache(lagerPool)
-	}
-
-	for i := 0; i < nbIterations; i++ {
-		k := i
-		localPool := <-pool
-
-		queue <- func() {
-			work(k, localPool)
-			pool <- localPool
-			wg.Done()
-		}
-	}
-
-	wg.Wait()
-
-	close(pool)
-
-	for localPool := range pool {
-		localPool.TearDown()
-	}
 }
 
 func ExecutePoolChunky(nbIterations int, work func(k int)) {
