5f044e1c3a9ccae9f5318c2a180d1052bb8c6544
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-11-09 11:22:39 +0100

    MXP to ID lookup fix 52 (#479)
    Co-authored-by: Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>

--- mxp/lookups/mxp_into_instdecoder.lisp
+++ mxp/lookups/mxp_into_instdecoder.lisp
@@ -1,24 +1,24 @@
 (deflookup mxp-into-instdecoder
     ;; source columns
     (
-        instdecoder.MXP_TYPE_1
-        instdecoder.MXP_TYPE_2
-        instdecoder.MXP_TYPE_3
-        instdecoder.MXP_TYPE_4
-        instdecoder.MXP_TYPE_5
-        instdecoder.BILLING_PER_WORD
-        instdecoder.BILLING_PER_BYTE
-        instdecoder.OPCODE
+        instdecoder.OPCODE ;; INST in the specs   
+        instdecoder.BILLING_PER_WORD ;; ♢GWORD in the specs
+        instdecoder.BILLING_PER_BYTE ;; ♢GBYTE in the specs
+        instdecoder.MXP_TYPE_1 ;; ♢TYPE_1 in the specs
+        instdecoder.MXP_TYPE_2 ;; ♢TYPE_2 in the specs
+        instdecoder.MXP_TYPE_3 ;; ♢TYPE_3 in the specs
+        instdecoder.MXP_TYPE_4 ;; ♢TYPE_4 in the specs
+        instdecoder.MXP_TYPE_5 ;; ♢TYPE_5 in the specs
     )
     ;target columns
-    (
-        [mxp.MXP_TYPE 1]
-        [mxp.MXP_TYPE 2]
-        [mxp.MXP_TYPE 3]
-        [mxp.MXP_TYPE 4]
-        [mxp.MXP_TYPE 5]
-        mxp.GWORD
-        mxp.GBYTE
-        mxp.INST
+    (   
+        mxp.INST 
+        mxp.GWORD 
+        mxp.GBYTE 
+        [mxp.MXP_TYPE 1] ;; TYPE_1 in the specs
+        [mxp.MXP_TYPE 2] ;; TYPE_2 in the specs
+        [mxp.MXP_TYPE 3] ;; TYPE_3 in the specs
+        [mxp.MXP_TYPE 4] ;; TYPE_4 in the specs
+        [mxp.MXP_TYPE 5] ;; TYPE_5 in the specs    
     )
 )
