e46893f56494880949991f620647fbc3a0a323d7
olegfomenko <oleg.fomenko2002@gmail.com>
2024-11-19 15:35:47 +0200

    fixed - removing redundant comments

--- prover/crypto/keccak/subtle/xor.go
+++ prover/crypto/keccak/subtle/xor.go
@@ -1,7 +1,3 @@
-// Copyright 2022 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package subtle
 
 import (
@@ -29,18 +25,3 @@ func XorIn(state *[25]uint64, buf [17]uint64) {
 	y := (*[17 * 64 / 8]byte)(unsafe.Pointer(&buf))
 	XORBytes(x[:], x[:], y[:])
 }
-
-//
-// xorIn xors the bytes in buf into the state.
-//func xorIn(d *state, buf []byte) {
-//	if cpu.IsBigEndian {
-//		for i := 0; len(buf) >= 8; i++ {
-//			a := binary.LittleEndian.Uint64(buf)
-//			d.a[i] ^= a
-//			buf = buf[8:]
-//		}
-//	} else {
-//		ab := (*[25 * 64 / 8]byte)(unsafe.Pointer(&d.a))
-//		XORBytes(ab[:], ab[:], buf)
-//	}
-//}
