5c94e797583d5b24a6962e9c567577c742a4ca0c
Tabaie <arya.pourtabatabaie@gmail.com>
2024-11-21 00:41:41 +0000

    dummy commit: real SRS works

--- prover/integration/all-backend/run-test/run-test.go
+++ prover/integration/all-backend/run-test/run-test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var flagNoExecution = flag.Bool("no-execution", true, "don't create new execution proofs")
+var flagNoExecution = flag.Bool("no-execution", false, "don't create new execution proofs")
 var flagNoDecompression = flag.Bool("no-decompression", false, "don't create new decompression proofs")
 
 func main() {
