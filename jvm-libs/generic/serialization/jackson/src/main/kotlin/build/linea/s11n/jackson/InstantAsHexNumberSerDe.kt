ccdff62826ef62ade84d52828ab3cb93e79009a2
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-11-25 10:06:43 +0000

    coordinator: adds new InstantAsHexNumberSerDe

---
+++ jvm-libs/generic/serialization/jackson/src/main/kotlin/build/linea/s11n/jackson/InstantAsHexNumberSerDe.kt
@@ -0,0 +1,21 @@
+package build.linea.s11n.jackson
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import kotlinx.datetime.Instant
+
+object InstantAsHexNumberSerializer : JsonSerializer<Instant>() {
+  override fun serialize(value: Instant, gen: JsonGenerator, serializers: SerializerProvider) {
+    gen.writeString("0x${value.epochSeconds.toString(16)}")
+  }
+}
+
+object InstantAsHexNumberDeserializer : JsonDeserializer<Instant>() {
+  override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Instant {
+    return Instant.fromEpochSeconds(p.text.replace("0x", "").toLong(16))
+  }
+}
