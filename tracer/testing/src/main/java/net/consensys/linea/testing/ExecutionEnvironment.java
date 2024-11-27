e1fa8367ab7b25574a51eefc01a14c35c47a5b54
François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
2024-11-27 21:25:31 +0100

    delete lt file if corset check passes (#1575)
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- tracer/testing/src/main/java/net/consensys/linea/testing/ExecutionEnvironment.java
+++ tracer/testing/src/main/java/net/consensys/linea/testing/ExecutionEnvironment.java
@@ -75,7 +75,6 @@ public class ExecutionEnvironment {
     } catch (IOException e) {
       throw new RuntimeException(e);
     } finally {
-      /*
       if (traceFilePath != null && traceValidated) {
         if (System.getenv("PRESERVE_TRACE_FILES") == null) {
           boolean traceFileDeleted = traceFilePath.toFile().delete();
@@ -84,7 +83,6 @@ public class ExecutionEnvironment {
               log -> log.debug("trace file {} deleted {}", finalTraceFilePath, traceFileDeleted));
         }
       }
-       */
     }
   }
 
