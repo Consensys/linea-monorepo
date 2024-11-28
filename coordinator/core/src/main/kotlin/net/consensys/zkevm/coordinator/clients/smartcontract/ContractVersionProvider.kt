64c178052e4faef91572b98b2d7e1a98b5a31e87
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2024-11-28 11:14:11 +0000

    coordinator: carve out LineaRollupSmartContractClientReadOnly

--- coordinator/core/src/main/kotlin/net/consensys/zkevm/coordinator/clients/smartcontract/ContractVersionProvider.kt
+++
@@ -1,7 +0,0 @@
-package net.consensys.zkevm.coordinator.clients.smartcontract
-
-import tech.pegasys.teku.infrastructure.async.SafeFuture
-
-fun interface ContractVersionProvider<T> {
-  fun getVersion(): SafeFuture<T>
-}
