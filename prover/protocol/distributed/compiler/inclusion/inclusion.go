20df8d456f06bec8eb17a0170e2534e4bacebbdf
AlexandreBelling <alexandrebelling8@gmail.com>
2024-11-28 15:01:35 +0100

    commit all ideas

---
+++ prover/protocol/distributed/compiler/inclusion/inclusion.go
@@ -0,0 +1,7 @@
+package inclusion
+
+import "github.com/consensys/linea-monorepo/prover/protocol/wizard"
+
+func IntoLogDerivativeSum(comp *wizard.CompiledIOP) {
+	panic("unimplemented")
+}
---
+++ prover/protocol/distributed/compiler/permutation/permutation.go
@@ -0,0 +1,7 @@
+package permutation
+
+import "github.com/consensys/linea-monorepo/prover/protocol/wizard"
+
+func IntoGrandProduct(comp *wizard.CompiledIOP) {
+	panic("unimplemented")
+}
