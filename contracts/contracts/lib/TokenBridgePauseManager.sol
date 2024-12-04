17dfcbc41bab34b5861578ee0659bd96b6b6adae
kyzooghost <jeffersonzjtang@gmail.com>
2024-12-04 22:37:40 +1100

    Revert "did natspec for TokenBridgePauseManager (#379)"
    This reverts commit 77783808711ae3fd9fba808bd989ef6fe3d2d34f.

--- contracts/contracts/lib/TokenBridgePauseManager.sol
+++ contracts/contracts/lib/TokenBridgePauseManager.sol
@@ -9,15 +9,8 @@ import { PauseManager } from "./PauseManager.sol";
  * @custom:security-contact security-report@linea.build
  */
 abstract contract TokenBridgePauseManager is PauseManager {
-  /// @notice This is used to pause token bridging initiation.
   bytes32 public constant PAUSE_INITIATE_TOKEN_BRIDGING_ROLE = keccak256("PAUSE_INITIATE_TOKEN_BRIDGING_ROLE");
-
-  /// @notice This is used to unpause token bridging initiation.
   bytes32 public constant UNPAUSE_INITIATE_TOKEN_BRIDGING_ROLE = keccak256("UNPAUSE_INITIATE_TOKEN_BRIDGING_ROLE");
-
-  /// @notice This is used to pause token bridging completion.
   bytes32 public constant PAUSE_COMPLETE_TOKEN_BRIDGING_ROLE = keccak256("PAUSE_COMPLETE_TOKEN_BRIDGING_ROLE");
-
-  /// @notice This is used to unpause token bridging completion.
   bytes32 public constant UNPAUSE_COMPLETE_TOKEN_BRIDGING_ROLE = keccak256("UNPAUSE_COMPLETE_TOKEN_BRIDGING_ROLE");
 }
--- contracts/docs/api/lib/TokenBridgePauseManager.md
+++ contracts/docs/api/lib/TokenBridgePauseManager.md
@@ -8,15 +8,11 @@
 bytes32 PAUSE_INITIATE_TOKEN_BRIDGING_ROLE
 ```
 
-This is used to pause token bridging initiation.
-
 ### UNPAUSE_INITIATE_TOKEN_BRIDGING_ROLE
 
 ```solidity
 bytes32 UNPAUSE_INITIATE_TOKEN_BRIDGING_ROLE
 ```
-
-This is used to unpause token bridging initiation.
 
 ### PAUSE_COMPLETE_TOKEN_BRIDGING_ROLE
 
@@ -24,13 +20,9 @@ This is used to unpause token bridging initiation.
 bytes32 PAUSE_COMPLETE_TOKEN_BRIDGING_ROLE
 ```
 
-This is used to pause token bridging completion.
-
 ### UNPAUSE_COMPLETE_TOKEN_BRIDGING_ROLE
 
 ```solidity
 bytes32 UNPAUSE_COMPLETE_TOKEN_BRIDGING_ROLE
 ```
-
-This is used to unpause token bridging completion.
 
