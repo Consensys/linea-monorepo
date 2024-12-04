3f16b6c91c638399091e011c6c89c54b7cb6b161
Tabaie <arya.pourtabatabaie@gmail.com>
2024-12-04 15:42:05 +0000

    feat flag for target number of constraints

--- prover/circuits/blobdecompression/large-tests/maximize_blob_size/main.go
+++ prover/circuits/blobdecompression/large-tests/maximize_blob_size/main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"runtime"
+
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/scs"
 	v1 "github.com/consensys/linea-monorepo/prover/circuits/blobdecompression/v1"
 	blob "github.com/consensys/linea-monorepo/prover/lib/compressor/blob/v1"
-	"runtime"
 )
-
-const maxNbConstraints = 1 << 27
 
 func nbConstraints(blobSize int) int {
 	fmt.Printf("*********************\nfor blob of size %dB or %.2fKB:\n", blobSize, float32(blobSize)/1024)
@@ -27,10 +26,10 @@ func nbConstraints(blobSize int) int {
 	} else {
 		res := cs.GetNbConstraints()
 		cmp := "match"
-		if res > maxNbConstraints {
+		if res > *flagTargetNbConstraints {
 			cmp = "over"
 		}
-		if res < maxNbConstraints {
+		if res < *flagTargetNbConstraints {
 			cmp = "under"
 		}
 		fmt.Printf("%d constraints (%s)\n", res, cmp)
@@ -39,8 +38,9 @@ func nbConstraints(blobSize int) int {
 }
 
 var (
-	flagCrawlStep = flag.Int("step", 1000, "the crawl step") // TODO @Tabaie fix mixed metaphor
-	flagStart     = flag.Int("start", blob.MaxUncompressedBytes, "initial size in bytes")
+	flagCrawlStep           = flag.Int("step", 1000, "the crawl step") // TODO @Tabaie fix mixed metaphor
+	flagStart               = flag.Int("start", blob.MaxUncompressedBytes, "initial size in bytes")
+	flagTargetNbConstraints = flag.Int("target", 1<<27, "target number of constraints")
 )
 
 func main() {
@@ -49,24 +49,24 @@ func main() {
 	v := nbConstraints(*flagStart)
 	a, b := *flagStart, *flagStart
 
-	if v > maxNbConstraints {
+	if v > *flagTargetNbConstraints {
 		fmt.Println("crawling downward")
-		for v > maxNbConstraints {
+		for v > *flagTargetNbConstraints {
 			b = a
 			a = max(a-*flagCrawlStep, 0)
 			v = nbConstraints(a)
 			*flagCrawlStep *= 2
 		}
-	} else if v < maxNbConstraints {
+	} else if v < *flagTargetNbConstraints {
 		fmt.Println("crawling upward")
-		for v < maxNbConstraints {
+		for v < *flagTargetNbConstraints {
 			a = b
 			b += *flagCrawlStep
 			v = nbConstraints(b)
 			*flagCrawlStep *= 2
 		}
 	}
-	if v == maxNbConstraints {
+	if v == *flagTargetNbConstraints {
 		fmt.Println("wow what are the odds")
 		return
 	}
@@ -75,13 +75,13 @@ func main() {
 	for b > a {
 		m := (b + a) / 2
 		v = nbConstraints(m)
-		if v > maxNbConstraints {
+		if v > *flagTargetNbConstraints {
 			b = m
 		}
-		if v < maxNbConstraints {
+		if v < *flagTargetNbConstraints {
 			a = v
 		}
-		if v == maxNbConstraints {
+		if v == *flagTargetNbConstraints {
 			return
 		}
 	}
