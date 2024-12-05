71c180e3f6165aed1652a9b1c78eca721c2b104d
AlexandreBelling <alexandrebelling8@gmail.com>
2024-12-05 18:44:42 +0100

    test and fix the encoding-decoding of transaction with an empty to

--- prover/lib/compressor/blob/v0/encode_test.go
+++ prover/lib/compressor/blob/v0/encode_test.go
@@ -1,24 +1,18 @@
-package v1_test
+package v0_test
 
 import (
 	"bytes"
 	"crypto/ecdsa"
 	"crypto/rand"
-	"encoding/base64"
-	"encoding/json"
 	"math/big"
-	"os"
-	"path"
 	"testing"
 
-	"github.com/consensys/linea-monorepo/prover/backend/blobdecompression"
-	v1 "github.com/consensys/linea-monorepo/prover/lib/compressor/blob/v1"
+	v0 "github.com/consensys/linea-monorepo/prover/lib/compressor/blob/v0"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestEncodeDecodeTx(t *testing.T) {
@@ -117,7 +111,7 @@ func TestEncodeDecodeTx(t *testing.T) {
 				addr = &common.Address{}
 			)
 
-			if err := v1.EncodeTxForCompression(tx, buf); err != nil {
+			if err := v0.EncodeTxForCompression(tx, buf); err != nil {
 				t.Fatalf("could not encode the transaction")
 			}
 
@@ -126,7 +120,7 @@ func TestEncodeDecodeTx(t *testing.T) {
 				r    = bytes.NewReader(data)
 			)
 
-			txData2, err := v1.DecodeTxFromUncompressed(r, addr)
+			txData2, err := v0.DecodeTxFromUncompressed(r, addr)
 
 			if err != nil {
 				t.Fatalf("could not deserialize the transaction err=%v", err.Error())
@@ -136,85 +130,6 @@ func TestEncodeDecodeTx(t *testing.T) {
 
 			assert.Equal(t, tx.To(), tx2.To(), "field `to` mismatches")
 
-		})
-
-	}
-
-}
-
-func TestEncodeDecodeFromResponse(t *testing.T) {
-
-	var (
-		testDir        = "../testdata/v1/prover-responses"
-		testFiles, err = os.ReadDir(testDir)
-	)
-
-	if err != nil {
-		t.Fatalf("can't read test files: %v", err)
-	}
-
-	for _, testFile := range testFiles {
-
-		if testFile.IsDir() {
-			continue
-		}
-
-		fName := testFile.Name()
-
-		if fName == ".DS_Store" {
-			continue
-		}
-
-		t.Run(fName, func(t *testing.T) {
-
-			var (
-				filePath = path.Join(testDir, fName)
-				f, err   = os.Open(filePath)
-			)
-
-			require.NoErrorf(t, err, "could not open the test file path=%v err=%v", filePath, err)
-
-			var response = &blobdecompression.Response{}
-			if err = json.NewDecoder(f).Decode(response); err != nil {
-				t.Fatalf("could not deserialize testfile path=%v err=%v", filePath, err)
-			}
-
-			data, err := base64.StdEncoding.DecodeString(response.CompressedData)
-			if err != nil {
-				t.Fatalf("could not deserialize the bsae64 decompression data err=%v", err)
-			}
-
-			batches, err := decompressBlob(data)
-			if err != nil {
-				t.Fatalf("could not decompress the blob: err=%v", err)
-			}
-
-			for batchI := range batches {
-				for blockI := range batches[batchI] {
-
-					r := bytes.NewReader(batches[batchI][blockI])
-					_, err := v1.DecodeBlockFromUncompressed(r)
-
-					if err != nil {
-						t.Fatalf("could not decode block: %v", err)
-					}
-
-					// for txI, txData := range blockData.Txs {
-					// 	tx := types.NewTx(txData)
-
-					// 	if tx.To() == nil {
-					// 		t.Logf("batch=%v block=%v tx=%v tx.to=%v", batchI, blockData, txI, tx.To())
-					// 		t.Fail()
-					// 	}
-
-					// 	if tx.To() == (&common.Address{}) {
-					// 		t.Logf("batch=%v block=%v tx=%v tx.to=%v", batchI, blockData, txI, tx.To())
-					// 		t.Fail()
-					// 	}
-					// }
-
-				}
-			}
 		})
 
 	}
