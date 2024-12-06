73aef0ea5c0fc6a42961674a7d8fb12d0ba951a5
Soleimani193 <azam.soleimanian@ens.fr>
2024-12-06 15:15:34 +0100

    minor typos

--- prover/protocol/distributed/compiler/inclusion/inclusion.go
+++ prover/protocol/distributed/compiler/inclusion/inclusion.go
@@ -6,10 +6,10 @@ import (
 )
 
 // CompileDist compiles inclusion queries distributedly.
-// It receives a compiledIop object relevant to a segment.
+// It receives a compiledIOP object relevant to a segment.
 // The seed is a random coin from randomness beacon (FS of all LPP commitments).
 // All the compilation steps are similar to the permutation compilation apart from:
-//   - random coins \alpha and \gamma are generated from the seed.
+//   - random coins \alpha and \gamma are generated from the seed (and the tableName).
 //   - no verifierAction is needed over the ZOpening.
 //   - ZOpenings are declared as public input.
 func CompileDist(comp *wizard.CompiledIOP, seed coin.Info) {
