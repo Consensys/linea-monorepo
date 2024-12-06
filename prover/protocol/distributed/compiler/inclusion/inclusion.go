c10afd43d39524a5bee29a207feac9bec3088de3
Soleimani193 <azam.soleimanian@ens.fr>
2024-12-06 15:06:23 +0100

    minor

--- prover/protocol/distributed/compiler/inclusion/inclusion.go
+++ prover/protocol/distributed/compiler/inclusion/inclusion.go
@@ -1,10 +1,17 @@
 package inclusion
 
 import (
+	"github.com/consensys/linea-monorepo/prover/protocol/coin"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 )
 
-// CompileDist compiles a LogDerivativeSum query distributedly.
-func CompileDist(comp *wizard.CompiledIOP) {
+// CompileDist compiles inclusion queries distributedly.
+// It receives a compiledIop object relevant to a segment.
+// The seed is a random coin from randomness beacon (FS of all LPP commitments).
+// All the compilation steps are similar to the permutation compilation apart from:
+//   - random coins \alpha and \gamma are generated from the seed.
+//   - no verifierAction is needed over the ZOpening.
+//   - ZOpenings are declared as public input.
+func CompileDist(comp *wizard.CompiledIOP, seed coin.Info) {
 
 }
