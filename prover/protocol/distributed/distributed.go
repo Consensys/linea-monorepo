9ac6ac01be8ace5635dbbc66dead7f75fefc863e
Soleimani193 <azam.soleimanian@ens.fr>
2024-12-06 10:28:24 +0100

    adding details to top-level

--- prover/protocol/distributed/distributed.go
+++ prover/protocol/distributed/distributed.go
@@ -1,12 +1,9 @@
 package distributed
 
 import (
-	"github.com/consensys/linea-monorepo/prover/maths/field"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/mimc"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/specialqueries"
-	"github.com/consensys/linea-monorepo/prover/protocol/distributed/compiler/global"
 	"github.com/consensys/linea-monorepo/prover/protocol/distributed/compiler/inclusion"
-	"github.com/consensys/linea-monorepo/prover/protocol/distributed/compiler/local"
 	"github.com/consensys/linea-monorepo/prover/protocol/distributed/compiler/permutation"
 	"github.com/consensys/linea-monorepo/prover/protocol/distributed/compiler/projection"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
@@ -16,52 +13,57 @@ import (
 type moduleName = string
 
 type DistributedWizard struct {
-	Bootstrapper       *wizard.CompiledIOP
+	// initializedWizard which is ready for distribution.
+	Bootstrapper *wizard.CompiledIOP
+	// compiledIOPs for a segment of each module.
+	// Since splits are fair/uniform over a module. One segment can represent all the segments.
 	DistributedModules []DistributedModule
-	Aggregator         *wizard.CompiledIOP
+	// it is a compiledIOP object representing the consistency checks among the segments.
+	Aggregator *wizard.CompiledIOP
 }
 
-// DistributedModule implements the utilities relevant to a single segment.
+// DistributedModule implements the utilities relevant to a single segment from a module.
 type DistributedModule struct {
 	LookupPermProj *wizard.CompiledIOP
 	GlobalLocal    *wizard.CompiledIOP
-	VK             [2]field.Element
 }
 
+// ModuleDiscoverer a set of methods responsible for the horizontal splittings (i.e., splitting to modules)
 type ModuleDiscoverer interface {
 	// Analyze is responsible for letting the module discoverer compute how to
 	// group best the columns into modules.
 	Analyze(comp *wizard.CompiledIOP)
-	NbModules() moduleName
 	ModuleList(comp *wizard.CompiledIOP) []string
 	FindModule(col ifaces.Column) moduleName
+	FindNbSegments(moduleName string, SegParam map[string]int) int
 }
 
 // This transforms the initial wizard. So it is not really the initial
 // wizard anymore. That means the caller can forget about "initialWizard"
 // after calling the function.
-// maxNbSegment is a large max for the number of segments in a module.
-func Distribute(initialWizard *wizard.CompiledIOP, disc ModuleDiscoverer, maxNbSegments int) DistributedWizard {
-
-	// prepare the  initialWizard for the distribution. e.g.,
-	// adding auxiliary columns or dividing a lookup query to two queries one over T and the other over S.
+// maxSegmentSize is a static parameter for the max size of the columns in segments.
+// maxNumSegment give the max number of segments in a module.
+func Distribute(initialWizard *wizard.CompiledIOP, disc ModuleDiscoverer, maxSegmentSize, maxNumSegment int) DistributedWizard {
+	// prepare the  initialWizard for the distribution.
+	// This adds intermediates queries that are simpler to be distributed.
 	prepare(initialWizard)
-	// it updates the map of Modules-Columns (that is a field of initialWizard).
+
+	// analyze the initialWizard to split it to modules.
 	disc.Analyze(initialWizard)
 
 	moduleLs := disc.ModuleList(initialWizard)
 	distModules := []DistributedModule{}
 
 	for _, modName := range moduleLs {
-		// Segment Compilation;
-		// Compile every dist module with the same sequence of compilation steps for uniformity
-		distMod := extractDistModule(initialWizard, disc, modName)
+		// CompiledIOP of Segment; it mainly represent the queries over the segment
+		// Due to the fair distribution over a module. the CompiledIOP for the segments from the same module are the same.
+		// Compile every dist module with the same sequence of compilation steps for uniformity.
+		distMod := extractDistModule(initialWizard, disc, modName, maxSegmentSize, maxNumSegment)
 		distModules = append(distModules, distMod)
 	}
 
-	// for each [DistributedModule] it checks the consistency among
-	// its replications where the number of replications is maxNbSegments.
-	aggr := aggregator(maxNbSegments, distModules, moduleLs)
+	// it output a compiledIOP object declaring the queries for the consistency among segments/modules
+	aggr := aggregator(distModules, maxNumSegment)
 
 	return DistributedWizard{
 		Bootstrapper:       initialWizard,
@@ -70,28 +72,34 @@ func Distribute(initialWizard *wizard.CompiledIOP, disc ModuleDiscoverer, maxNbS
 	}
 }
 
-// It adds the compilation steps
+// It compiles any other query to LPP or GL.
+// then transfer LPP  queries into the queries proper for distribution.
 func prepare(comp *wizard.CompiledIOP) {
-
+	// compile other queries to LPP and GL.
 	mimc.CompileMiMC(comp)
 	specialqueries.RangeProof(comp)
 	specialqueries.CompileFixedPermutations(comp)
 
+	// prepare LPP queries for the distribution.
 	inclusion.IntoLogDerivativeSum(comp)
 	permutation.IntoGrandProduct(comp)
 	projection.IntoGrandSum(comp)
-	local.IntoDistributedLocal(comp)
-	global.IntoDistributedGlobal(comp)
 }
 
 func addSplittingStep(comp *wizard.CompiledIOP, disc ModuleDiscoverer) {
 	panic("unimplemented")
 }
 
-func extractDistModule(comp *wizard.CompiledIOP, disc ModuleDiscoverer, moduleName moduleName) DistributedModule {
+// it should scan comp and based on module name build compiledIOP for LPP and for GL.
+func extractDistModule(
+	comp *wizard.CompiledIOP, disc ModuleDiscoverer,
+	moduleName moduleName,
+	maxSegmentSize, maxNumSegment int,
+) DistributedModule {
 	panic("unimplemented")
 }
 
-func aggregator(n int, idsModules []DistributedModule, moduleNames []string) *wizard.CompiledIOP {
+// It builds a CompiledIOP object that contains the consistency checks among the segments.
+func aggregator(distModules []DistributedModule, maxNumSegments int) *wizard.CompiledIOP {
 	panic("unimplemented")
 }
