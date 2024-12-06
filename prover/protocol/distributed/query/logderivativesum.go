6a63b4a4229de2aa092b8f73686061c908b1b836
Soleimani193 <azam.soleimanian@ens.fr>
2024-12-06 10:46:01 +0100

    minor on lookup

--- prover/protocol/distributed/query/logderivativesum.go
+++ prover/protocol/distributed/query/logderivativesum.go
@@ -7,24 +7,25 @@ import (
 	"github.com/consensys/linea-monorepo/prover/symbolic"
 )
 
-// LogDerivativeSum describes the mathematical expression required in the lookup
-// \sum_i f_i/(s_i+\gamma)-\sum_i m_i/(t_i+\gamma)
+// LogDerivativeSum describes the mathematical expression required in the lookup.
+// \sum_i f_i/(s_i+\gamma)-\sum_i m_i/(t_i+\gamma) = 1
 type LogDerivativeSum struct {
-	// for S it keeps the filter value 0/1
+	// for S it keeps the filter value f_i = 0/1
 	// for T it keeps  -(m_i)
+	// we have one expression per S_collapsed or T_collapsed.
 	Nominator []symbolic.Expression
-	// for S it keeps s_i+\gamma (s_i might be collapsing of several columns)
+	// for S it keeps s_i+\gamma (s_i  = \sum _j \alpha^j.s_{i,j} might be collapsing of several columns)
 	// for T keeps t_i+\gamma (t_i might be collapsing of several columns)
+	// we have one expression per S_collapsed or T_collapsed.
 	Dominator []symbolic.Expression
 	// ID stores the identifier string of the query
 	ID ifaces.QueryID
-	// coins used in the Nominator/Dominator
-	alpha, beta coin.Info
-	// size is later used for z-Packing
-	 size int
+	// coins used in the Nominator/Dominator.
+	// the number of coins depends on the number of different T tables.
+	alpha, beta []coin.Info
 }
 
-// NewLogDerivativeSum constructs an inclusion. Will panic if it is mal-formed
+// NewLogDerivativeSum constructs an wLogDerivativeSum. Will panic if it is mal-formed
 func NewLogDerivativeSum(
 	id ifaces.QueryID,
 	nominator []symbolic.Expression,
@@ -51,13 +52,3 @@ func (r LogDerivativeSum) Check(run ifaces.Runtime) error {
 func (i LogDerivativeSum) CheckGnark(api frontend.API, run ifaces.GnarkRuntime) {
 	panic("UNSUPPORTED : can't check an inclusion query directly into the circuit")
 }
-
-// it is very similar to LogDerivaitiveSum, but with the difference that it can have prover step.
-// this is the query that is imposed over the segment.
-type LogDerivativeSegment struct {
-	logDerivSum  LogDerivativeSum
-	// the coin used for generating alpha and beta
-	seed coin.Info
-}
-
-// all the methods relevant to the query interface.
