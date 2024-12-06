157c5486b11e2f5da060870dd019ed5465d1a359
Soleimani193 <azam.soleimanian@ens.fr>
2024-12-06 14:56:30 +0100

    removing preperation phase

--- prover/protocol/distributed/compiler/projection/projection.go
+++ prover/protocol/distributed/compiler/projection/projection.go
@@ -1,13 +1,17 @@
 package projection
 
-import "github.com/consensys/linea-monorepo/prover/protocol/wizard"
-
-// IntoGrandSu reduces all the projection queries into a GrandSum query
-func IntoGrandSum(comp *wizard.CompiledIOP) {
-	panic("unimplemented")
-}
+import (
+	"github.com/consensys/linea-monorepo/prover/protocol/coin"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+)
 
 // CompileDist compiles a GrandSum query distributedly.
-func CompileDist(comp *wizard.CompiledIOP) {
+// It receives a compiledIop object relevant to a segment.
+// The seed is a random coin from randomness beacon (FS of all LPP commitments).
+// All the compilation steps are similar to the permutation compilation apart from:
+//   - random coins \alpha and \gamma are generated from the seed.
+//   - no verifierAction is needed over the ZOpening.
+//   - ZOpenings are declared as public input.
+func CompileDist(comp *wizard.CompiledIOP, seed coin.Info) {
 
 }
