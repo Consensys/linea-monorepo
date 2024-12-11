c0d0f67abde478ed774e3655c4338be05c0effac
Soleimani193 <azam.soleimanian@ens.fr>
2024-12-11 17:34:49 +0100

    added documentation

--- prover/protocol/distributed/compiler/inclusion/lookup_discoverer.go
+++ prover/protocol/distributed/compiler/inclusion/lookup_discoverer.go
@@ -14,11 +14,13 @@ import (
 // compiler as a shorthand to make the code more eye-parseable.
 type table = []ifaces.Column
 
-// captureModuleLookupTables inspects comp and looks for Inclusion queries that are relevant to the module.
+// captureModuleLookupTables inspects comp and looks for Inclusion queries.
 // It groups the matched queries by lookup table and marks them as ignored.
+// It creates a list of lookupTables and checkedTables present in the module.
+//
 // The input is a compiledIOP object that stores the columns relevant to the module (in its Column field)
 // Note that for a lookup query the module may contain only S or T table (and not necessarily both).
-func captureModuleLookupTables(comp *wizard.CompiledIOP) lookUp.MainLookupCtx {
+func captureModuleLookupTables(moduleComp *wizard.CompiledIOP) lookUp.MainLookupCtx {
 	var (
 		ctx = lookUp.MainLookupCtx{
 			LookupTables:    [][]table{},
@@ -29,16 +31,16 @@ func captureModuleLookupTables(comp *wizard.CompiledIOP) lookUp.MainLookupCtx {
 	)
 
 	// Collect all the lookup queries into "lookups"
-	for _, qName := range comp.QueriesNoParams.AllUnignoredKeys() {
+	for _, qName := range moduleComp.QueriesNoParams.AllUnignoredKeys() {
 
 		// Filter out non-lookup queries
-		lookup, ok := comp.QueriesNoParams.Data(qName).(query.Inclusion)
+		lookup, ok := moduleComp.QueriesNoParams.Data(qName).(query.Inclusion)
 		if !ok {
 			continue
 		}
 
 		// Determine if the query is relevant to the module
-		relevantPart := determineRelevantPart(lookup, comp.Columns)
+		relevantPart := determineRelevantPart(lookup, moduleComp.Columns)
 		if relevantPart == "" {
 			continue
 		}
@@ -46,7 +48,7 @@ func captureModuleLookupTables(comp *wizard.CompiledIOP) lookUp.MainLookupCtx {
 		// This ensures that the lookup query is not used again in the
 		// compilation process. We know that the query was already ignored at
 		// the beginning because we are iterating over the unignored keys.
-		comp.QueriesNoParams.MarkAsIgnored(qName)
+		moduleComp.QueriesNoParams.MarkAsIgnored(qName)
 
 		var (
 			// checkedTable corresponds to the "included" table and lookupTable
@@ -87,6 +89,11 @@ func captureModuleLookupTables(comp *wizard.CompiledIOP) lookUp.MainLookupCtx {
 		}
 
 		// Add only the relevant part to the context
+		if relevantPart == "Both" {
+			ctx.IncludedFilters[tableName] = append(ctx.IncludedFilters[tableName], includedFilter)
+			ctx.CheckedTables[tableName] = append(ctx.CheckedTables[tableName], checkedTable)
+			ctx.LookupTables = append(ctx.LookupTables, lookupTable)
+		}
 		if relevantPart == "S" {
 			ctx.IncludedFilters[tableName] = append(ctx.IncludedFilters[tableName], includedFilter)
 			ctx.CheckedTables[tableName] = append(ctx.CheckedTables[tableName], checkedTable)
@@ -94,7 +101,7 @@ func captureModuleLookupTables(comp *wizard.CompiledIOP) lookUp.MainLookupCtx {
 			ctx.LookupTables = append(ctx.LookupTables, lookupTable)
 		}
 
-		ctx.Rounds[tableName] = max(ctx.Rounds[tableName], comp.QueriesNoParams.Round(lookup.ID))
+		ctx.Rounds[tableName] = max(ctx.Rounds[tableName], moduleComp.QueriesNoParams.Round(lookup.ID))
 
 	}
 
@@ -103,15 +110,26 @@ func captureModuleLookupTables(comp *wizard.CompiledIOP) lookUp.MainLookupCtx {
 
 // determineRelevantPart checks if the lookup query involves columns from the module and returns the relevant part (S or T).
 func determineRelevantPart(lookup query.Inclusion, moduleColumns column.Store) string {
+	var hasT, hasS bool
 	// Check if any column in S part is in the module
 	if moduleColumns.Exists(lookup.Included[0].GetColID()) {
-		return "S"
+		hasS = true
 	}
 
 	// Check if any column in T part is in the module
 	if moduleColumns.Exists(lookup.Including[0][0].GetColID()) {
+		hasT = true
+	}
+	if hasT && hasS {
+		return "Both"
+	}
+	if hasT && !hasS {
 		return "T"
 	}
+	if hasS && !hasT {
+		return "S"
+	} else {
+		panic("the module contains a query that is not relevant to any column")
+	}
 
-	return ""
 }
