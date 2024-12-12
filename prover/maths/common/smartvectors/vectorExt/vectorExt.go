449605bad896a0449ee7603f3289a0a7242e53c5
Bogdan Ursu <bogdanursuoffice@gmail.com>
2024-12-12 02:51:01 +0100

    utilfunctions wip

--- prover/maths/common/smartvectors/vectorExt/vectorExt.go
+++ prover/maths/common/smartvectors/vectorExt/vectorExt.go
@@ -4,6 +4,7 @@ package vectorExt
 
 import (
 	"fmt"
+	"github.com/consensys/linea-monorepo/prover/maths/common/vector"
 	"github.com/consensys/linea-monorepo/prover/maths/field/fext"
 	"math/rand"
 
@@ -252,7 +253,7 @@ func Equal(a, b []fext.Element) bool {
 func PseudoRand(rng *rand.Rand, size int) []fext.Element {
 	slice := make([]fext.Element, size)
 	for i := range slice {
-		slice[i] = PseudoRandElement(rng)
+		slice[i] = vector.PseudoRandElement[fext.Element](rng)
 	}
 	return slice
 }
