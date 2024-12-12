50072962d7acf8b074cdd7577a5d956f87de2a86
Soleimani193 <azam.soleimanian@ens.fr>
2024-12-12 15:26:49 +0100

    implementation of extractDistModule()

--- prover/protocol/distributed/module_discover.go
+++ prover/protocol/distributed/module_discover.go
@@ -1,6 +1,9 @@
 package distributed
 
-import "github.com/consensys/linea-monorepo/prover/protocol/wizard"
+import (
+	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+)
 
 // it implement [ModuleDiscoverer], it splits the compiler horizontally.
 type HorizontalSplitting struct {
@@ -12,5 +15,11 @@ func (split HorizontalSplitting) Analyze(comp *wizard.CompiledIOP) {
 }
 
 func (split HorizontalSplitting) Split(comp *wizard.CompiledIOP) {
+
+}
+
+// QueryIsInModule checks if the given query is inside the given module
+func (split HorizontalSplitting) QueryIsInModule(ifaces.Query, moduleName) bool {
+	panic("unimplemented")
 
 }
