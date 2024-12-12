17dd068bd13ed455217214c21280aad22d0c852e
Soleimani193 <azam.soleimanian@ens.fr>
2024-12-12 15:35:19 +0100

    minor

--- prover/protocol/distributed/compiler/inclusion/preparation.go
+++
@@ -1,9 +0,0 @@
-package inclusion
-
-import "github.com/consensys/linea-monorepo/prover/protocol/wizard"
-
-// It scans the initial compiledIOP and group all the checkedTables related to the same lookupTable,
-// It creates a frequency column M for all such pairs (checkedTables, lookupTable).
-func prepareLookup(initalComp *wizard.CompiledIOP) {
-	panic("unimplemented")
-}
