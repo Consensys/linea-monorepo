4f3fe256e58b1f2e740b76de5de88cfd8ca25cda
arijitdutta67 <arijit.dutta67@gmail.com>
2024-12-13 15:52:12 +0530

    added a test, not working yet

--- prover/protocol/query/grand_product.go
+++ prover/protocol/query/grand_product.go
@@ -1,6 +1,8 @@
 package query
 
 import (
+	"fmt"
+
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/symbolic"
@@ -10,18 +12,18 @@ import (
 // The GrandProduct query enables splitting of the Permutation query between sub-provers
 // by splitting the grand product Z = \prod(A_i+\beta)/(B_i+\beta) itself.
 type GrandProduct struct {
-	ID          ifaces.QueryID
+	ID           ifaces.QueryID
 	Numerators   []*symbolic.Expression // stores A as multi-column
 	Denominators []*symbolic.Expression // stores B as multi-column
-	Round       int
+	Round        int
 }
 
 func NewGrandProduct(round int, id ifaces.QueryID, numerators, denominators []*symbolic.Expression) GrandProduct {
 	return GrandProduct{
-		ID:          id,
+		ID:           id,
 		Numerators:   numerators,
 		Denominators: denominators,
-		Round:       round,
+		Round:        round,
 	}
 }
 
@@ -30,7 +32,23 @@ func (g GrandProduct) Name() ifaces.QueryID {
 }
 
 func (g GrandProduct) Check(run ifaces.Runtime) error {
-	utils.Panic("Unimplemented")
+	var (
+		numNumerators   = len(g.Numerators)
+		numDenominators = len(g.Denominators)
+		numProd         = symbolic.NewConstant(1)
+		denProd         = symbolic.NewConstant(1)
+	)
+
+	for i := 0; i < numNumerators; i++ {
+		numProd = symbolic.Mul(numProd, g.Numerators[i])
+	}
+	for j := 0; j < numDenominators; j++ {
+		denProd = symbolic.Mul(denProd, g.Denominators[j])
+	}
+	if numProd != denProd {
+		return fmt.Errorf("the grand product query %v is not satisfied, numProd = %v, denProd = %v", g.ID, numProd, denProd)
+	}
+
 	return nil
 }
 
---
+++ prover/protocol/query/grand_product_test.go
@@ -0,0 +1,47 @@
+package query_test
+
+import (
+	"testing"
+
+	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+	"github.com/consensys/linea-monorepo/prover/symbolic"
+)
+
+func TestGrandProduct(t *testing.T) {
+
+	var (
+		runS *wizard.ProverRuntime
+		G    ifaces.Query
+	)
+
+	define := func(builder *wizard.Builder) {
+		A := []*symbolic.Expression{
+			symbolic.NewConstant(1),
+			symbolic.NewConstant(2),
+			symbolic.NewConstant(3),
+			symbolic.NewConstant(4),
+		}
+		B := []*symbolic.Expression{
+			symbolic.NewConstant(4),
+			symbolic.NewConstant(3),
+			symbolic.NewConstant(2),
+			symbolic.NewConstant(1),
+		}
+		G = builder.CompiledIOP.InsertGrandProduct(0, "G", A, B)
+	}
+
+	prove := func(run *wizard.ProverRuntime) {
+		runS = run
+	}
+
+	var (
+		comp = wizard.Compile(define)
+		_    = wizard.Prove(comp, prove)
+		errG = G.Check(runS)
+	)
+
+	if errG != nil {
+		t.Fatalf("error verifying the grand product: %v", errG.Error())
+	}
+}
