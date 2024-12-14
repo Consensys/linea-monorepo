4f4b373832940563f0f1089ccbee830f0f8622b0
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-12-14 06:14:47 +0100

    test: extend INVALID opcode (#1645)

--- tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/InvalidOpcodeExceptionTest.java
+++ tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/exceptions/InvalidOpcodeExceptionTest.java
@@ -14,7 +14,12 @@
  */
 package net.consensys.linea.zktracer.exceptions;
 
+import static net.consensys.linea.zktracer.opcode.OpCodes.valueToOpCodeDataMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import net.consensys.linea.UnitTestWatcher;
 import net.consensys.linea.testing.BytecodeCompiler;
@@ -23,6 +28,9 @@ import net.consensys.linea.zktracer.module.hub.signals.TracedException;
 import net.consensys.linea.zktracer.opcode.OpCode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @ExtendWith(UnitTestWatcher.class)
 public class InvalidOpcodeExceptionTest {
@@ -36,5 +44,28 @@ public class InvalidOpcodeExceptionTest {
     assertEquals(
         TracedException.INVALID_OPCODE,
         bytecodeRunner.getHub().previousTraceSection().commonValues.tracedException());
+  }
+
+  @ParameterizedTest
+  @MethodSource("nonOpcodeExceptionSource")
+  void nonOpcodeExceptionTest(int value) {
+    BytecodeCompiler program = BytecodeCompiler.newProgram();
+    program.immediate(value);
+    BytecodeRunner bytecodeRunner = BytecodeRunner.of(program.compile());
+    bytecodeRunner.run();
+    assertEquals(
+        TracedException.INVALID_OPCODE,
+        bytecodeRunner.getHub().previousTraceSection().commonValues.tracedException());
+  }
+
+  static Stream<Arguments> nonOpcodeExceptionSource() {
+    List<Arguments> arguments = new ArrayList<>();
+    for (int value = 0; value < 256; value++) {
+      // If value is not in the map, then it is not an OpCode
+      if (!valueToOpCodeDataMap.containsKey(value)) {
+        arguments.add(Arguments.of(value));
+      }
+    }
+    return arguments.stream();
   }
 }
