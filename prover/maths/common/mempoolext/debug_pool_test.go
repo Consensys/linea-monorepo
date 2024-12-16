12fa4cf980b89b0698bd8bdf58d2e9a6cc95725f
Bogdan Ursu <bogdanursuoffice@gmail.com>
2024-12-16 16:31:15 +0100

    mempool for fext

---
+++ prover/maths/common/mempoolext/debug_pool_test.go
@@ -0,0 +1,51 @@
+package mempool
+
+import (
+	"github.com/consensys/linea-monorepo/prover/maths/field/fext"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestDebugPool(t *testing.T) {
+
+	t.Run("leak-detection", func(t *testing.T) {
+
+		pool := NewDebugPool(CreateFromSyncPool(32))
+
+		for i := 0; i < 16; i++ {
+			func() {
+				_ = pool.Alloc()
+			}()
+		}
+
+		err := pool.Errors().Error()
+		assert.True(t, strings.HasPrefix(err, "leaked a vector out of the pool"))
+	})
+
+	t.Run("double-free", func(t *testing.T) {
+
+		pool := NewDebugPool(CreateFromSyncPool(32))
+
+		v := pool.Alloc()
+
+		for i := 0; i < 16; i++ {
+			pool.Free(v)
+		}
+
+		err := pool.Errors().Error()
+		assert.Truef(t, strings.HasPrefix(err, "vector was freed multiple times concurrently"), err)
+	})
+
+	t.Run("foreign-free", func(t *testing.T) {
+
+		pool := NewDebugPool(CreateFromSyncPool(32))
+
+		v := make([]fext.Element, 32)
+		pool.Free(&v)
+
+		err := pool.Errors().Error()
+		assert.Truef(t, strings.HasPrefix(err, "freed a vector that was not from the pool"), err)
+	})
+
+}
---
+++ prover/maths/common/mempoolext/slice_arena.go
@@ -0,0 +1,51 @@
+package mempool
+
+import (
+	"github.com/consensys/linea-monorepo/prover/maths/field/fext"
+)
+
+// SliceArena is a simple not-threadsafe arena implementation that uses a
+// mempool to carry its allocation. It will only put back free memory in the
+// the parent pool when TearDown is called.
+type SliceArena struct {
+	frees  []*[]fext.Element
+	parent MemPool
+}
+
+func WrapsWithMemCache(pool MemPool) *SliceArena {
+	return &SliceArena{
+		frees:  make([]*[]fext.Element, 0, 1<<7),
+		parent: pool,
+	}
+}
+
+func (m *SliceArena) Prewarm(nbPrewarm int) MemPool {
+	m.parent.Prewarm(nbPrewarm)
+	return m
+}
+
+func (m *SliceArena) Alloc() *[]fext.Element {
+
+	if len(m.frees) == 0 {
+		return m.parent.Alloc()
+	}
+
+	last := m.frees[len(m.frees)-1]
+	m.frees = m.frees[:len(m.frees)-1]
+	return last
+}
+
+func (m *SliceArena) Free(v *[]fext.Element) error {
+	m.frees = append(m.frees, v)
+	return nil
+}
+
+func (m *SliceArena) Size() int {
+	return m.parent.Size()
+}
+
+func (m *SliceArena) TearDown() {
+	for i := range m.frees {
+		m.parent.Free(m.frees[i])
+	}
+}
