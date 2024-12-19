77e677671b78f92d4fd3d82c855f8a99f9ee4b14
Bogdan Ursu <bogdanursuoffice@gmail.com>
2024-12-19 11:28:59 +0100

    setInt and setUint touch only one coordinate

--- prover/maths/field/fext/additional.go
+++ prover/maths/field/fext/additional.go
@@ -8,25 +8,21 @@ import (
 // SetUint64 sets z to v and returns z
 func (z *Element) SetUint64(v uint64) *Element {
 	//  sets z LSB to v (non-Montgomery form) and convert z to Montgomery form
-	temp := fr.Element{v}
-	temp.Mul(&temp, &rSquare)
-	res := Element{field.Zero(), temp}
-	return &res // z.toMont()
+	var x fr.Element
+	x.SetUint64(v)
+	y := field.Zero()
+	z = &Element{x, y}
+	return z // z.toMont()
 }
 
 // SetInt64 sets z to v and returns z
 func (z *Element) SetInt64(v int64) *Element {
 
-	// absolute value of v
-	m := v >> 63
-	z.SetUint64(uint64((v ^ m) - m))
-
-	if m != 0 {
-		// v is negative
-		z.Neg(z)
-	}
-
-	return z
+	var x fr.Element
+	x.SetInt64(v)
+	y := field.Zero()
+	z = &Element{x, y}
+	return z // z.toMont()
 }
 
 func (z *Element) Uint64() (uint64, uint64) {
