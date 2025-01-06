f67f2a003a2e82f7a06f157eb4575dce0dfbc4b4
Bogdan Ursu <bogdanursuoffice@gmail.com>
2025-01-06 15:26:10 +0100

    bug fix

--- prover/maths/field/fext/additional.go
+++ prover/maths/field/fext/additional.go
@@ -1,27 +1,18 @@
 package fext
 
-import (
-	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
-	"github.com/consensys/linea-monorepo/prover/maths/field"
-)
-
 // SetUint64 sets z to v and returns z
 func (z *Element) SetUint64(v uint64) *Element {
 	//  sets z LSB to v (non-Montgomery form) and convert z to Montgomery form
-	var x fr.Element
-	x.SetUint64(v)
-	y := field.Zero()
-	z = &Element{x, y}
+	z.A0.SetUint64(v)
+	z.A1.SetZero()
 	return z // z.toMont()
 }
 
 // SetInt64 sets z to v and returns z
 func (z *Element) SetInt64(v int64) *Element {
 
-	var x fr.Element
-	x.SetInt64(v)
-	y := field.Zero()
-	z = &Element{x, y}
+	z.A0.SetInt64(v)
+	z.A1.SetZero()
 	return z // z.toMont()
 }
 
