0004e7c76399c54563c90a9c142071d398fca989
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2025-01-10 14:55:59 +0000

    staterecovery: remove jackson debug plugin

--- state-recover/besu-plugin-jackson-test/src/main/kotlin/linea/staterecover/plugin/MyDataK.kt
+++
@@ -1,9 +0,0 @@
-package linea.staterecover.plugin
-
-import kotlin.random.Random
-
-data class MyDataK(
-  val description: String = "KotlinDataClass",
-  val someNumber: Int = 20,
-  val someBytes: ByteArray = Random.nextBytes(20)
-)
