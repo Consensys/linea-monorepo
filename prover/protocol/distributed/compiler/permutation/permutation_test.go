a38bd16f37e7f3508f23094eca232811a8d92d4c
Arijit Dutta <37040536+arijitdutta67@users.noreply.github.com>
2025-01-10 17:45:16 +0530

    Prover/Polish distributed permutation (#527)
    * adding size and zCatalog in the query
    
    ---------
    
    Co-authored-by: AlexandreBelling <alexandrebelling8@gmail.com>

--- prover/protocol/distributed/compiler/permutation/permutation_test.go
+++ prover/protocol/distributed/compiler/permutation/permutation_test.go
@@ -4,168 +4,170 @@ import (
 	"testing"
 
 	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/dummy"
 	dist_permutation "github.com/consensys/linea-monorepo/prover/protocol/distributed/compiler/permutation"
+	modulediscoverer "github.com/consensys/linea-monorepo/prover/protocol/distributed/module_discoverer"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+	"github.com/stretchr/testify/require"
 )
 
-func TestDistPermutationNoMultiColumnNoFragment(t *testing.T) {
+func TestPermutation(t *testing.T) {
+
 	var (
-		runS    *wizard.ProverRuntime
-		G       ifaces.Query
-		permCtx *dist_permutation.PermutationIntoGrandProductCtx
+		moduleAName = "MODULE_A"
+		// Initialise the period separating module discoverer
+		disc = modulediscoverer.PeriodSeperatingModuleDiscoverer{}
 	)
-	permCtx = dist_permutation.NewPermutationIntoGrandProductCtx(dist_permutation.Settings{MaxNumOfQueryPerModule: 4})
-	initialDefine := func(builder *wizard.Builder) {
-		A := []ifaces.Column{
-			builder.RegisterCommit("MODULE_A.A0", 4),
-		}
-		B := []ifaces.Column{
-			builder.RegisterCommit("MODULE_B.B0", 4),
-		}
-		C := []ifaces.Column{
-			builder.RegisterCommit("MODULE_C.C0", 4),
-		}
-		_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_A_MOD_B", A, B)
-		_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_C_MOD_A", C, A)
-		_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_B_MOD_C", B, C)
+
+	testcases := []struct {
+		Name       string
+		DefineFunc func(builder *wizard.Builder)
+	}{
+		{
+			Name: "single-column-no-fragment",
+			DefineFunc: func(builder *wizard.Builder) {
+				a := []ifaces.Column{
+					builder.RegisterCommit("MODULE_A.A0", 4),
+				}
+				b := []ifaces.Column{
+					builder.RegisterCommit("MODULE_B.B0", 4),
+				}
+				c := []ifaces.Column{
+					builder.RegisterCommit("MODULE_C.C0", 4),
+				}
+				_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_A_MOD_B", a, b)
+				_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_C_MOD_A", c, a)
+				_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_B_MOD_C", b, c)
+			},
+		},
+		{
+			Name: "multi-column-no-fragment",
+			DefineFunc: func(builder *wizard.Builder) {
+				a := []ifaces.Column{
+					builder.RegisterCommit("MODULE_A.A0", 4),
+					builder.RegisterCommit("MODULE_A.A1", 4),
+					builder.RegisterCommit("MODULE_A.A2", 4),
+				}
+				b := []ifaces.Column{
+					builder.RegisterCommit("MODULE_B.B0", 4),
+					builder.RegisterCommit("MODULE_B.B1", 4),
+					builder.RegisterCommit("MODULE_B.B2", 4),
+				}
+				c := []ifaces.Column{
+					builder.RegisterCommit("MODULE_C.C0", 4),
+					builder.RegisterCommit("MODULE_C.C1", 4),
+					builder.RegisterCommit("MODULE_C.C2", 4),
+				}
+				_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_A_MOD_B", a, b)
+				_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_C_MOD_A", c, a)
+				_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_B_MOD_C", b, c)
+			},
+		},
+		{
+			Name: "multi-column-multi-fragment",
+			DefineFunc: func(builder *wizard.Builder) {
+				a := [][]ifaces.Column{
+					{
+						builder.RegisterCommit("MODULE_A.A00", 4),
+						builder.RegisterCommit("MODULE_A.A10", 4),
+						builder.RegisterCommit("MODULE_A.A20", 4),
+					},
+					{
+						builder.RegisterCommit("MODULE_A.A01", 4),
+						builder.RegisterCommit("MODULE_A.A11", 4),
+						builder.RegisterCommit("MODULE_A.A21", 4),
+					},
+				}
+				b := [][]ifaces.Column{
+					{
+						builder.RegisterCommit("MODULE_B.B00", 4),
+						builder.RegisterCommit("MODULE_B.B10", 4),
+						builder.RegisterCommit("MODULE_B.B20", 4),
+					},
+					{
+						builder.RegisterCommit("MODULE_B.B01", 4),
+						builder.RegisterCommit("MODULE_B.B11", 4),
+						builder.RegisterCommit("MODULE_B.B21", 4),
+					},
+				}
+				c := [][]ifaces.Column{
+					{
+						builder.RegisterCommit("MODULE_C.C00", 4),
+						builder.RegisterCommit("MODULE_C.C10", 4),
+						builder.RegisterCommit("MODULE_C.C20", 4),
+					},
+					{
+						builder.RegisterCommit("MODULE_C.C01", 4),
+						builder.RegisterCommit("MODULE_C.C11", 4),
+						builder.RegisterCommit("MODULE_C.C21", 4),
+					},
+				}
+				_ = builder.CompiledIOP.InsertFragmentedPermutation(0, "P_MOD_A_MOD_B", a, b)
+				_ = builder.CompiledIOP.InsertFragmentedPermutation(0, "P_MOD_C_MOD_A", c, a)
+				_ = builder.CompiledIOP.InsertFragmentedPermutation(0, "P_MOD_B_MOD_C", b, c)
+			},
+		},
 	}
 
-	moduleADefine := func(builder *wizard.Builder) {
-		builder.RegisterCommit("MODULE_A.A0", 4)
+	for _, tc := range testcases {
+
+		t.Run(tc.Name, func(t *testing.T) {
+
+			initialComp := wizard.Compile(tc.DefineFunc)
+
+			disc.Analyze(initialComp)
+
+			moduleAComp := wizard.Compile(func(build *wizard.Builder) {
+
+				for _, colName := range initialComp.Columns.AllKeys() {
+
+					col := initialComp.Columns.GetHandle(colName)
+					if !disc.ColumnIsInModule(col, moduleAName) {
+						continue
+					}
+
+					build.RegisterCommit(col.GetColID(), col.Size())
+				}
+			}, dummy.CompileAtProverLvl)
+
+			var (
+				_ = dist_permutation.NewPermutationIntoGrandProductCtx(
+					dist_permutation.Settings{TargetModuleName: moduleAName},
+					initialComp, moduleAComp, &disc,
+				)
+				initialRun *wizard.ProverRuntime
+			)
+
+			initialProve := func(run *wizard.ProverRuntime) {
+				for _, colName := range run.Spec.Columns.AllKeys() {
+					run.AssignColumn(colName, smartvectors.ForTest(1, 2, 3, 4))
+				}
+
+				initialRun = run
+			}
+
+			_ = wizard.Prove(initialComp, initialProve)
+
+			moduleAProve := func(run *wizard.ProverRuntime) {
+				for _, colName := range initialComp.Columns.AllKeys() {
+
+					col := initialComp.Columns.GetHandle(colName)
+					if !disc.ColumnIsInModule(col, moduleAName) {
+						continue
+					}
+
+					c := initialRun.GetColumn(colName)
+					run.AssignColumn(colName, c)
+				}
+			}
+
+			proof := wizard.Prove(moduleAComp, moduleAProve)
+			valid := wizard.Verify(moduleAComp, proof)
+			require.NoError(t, valid)
+
+		})
+
 	}
 
-	initialComp := wizard.Compile(initialDefine)
-	moduleAComp := wizard.Compile(moduleADefine)
-
-	moduleAProve := func(run *wizard.ProverRuntime) {
-		runS = run
-		run.AssignColumn("MODULE_A.A0", smartvectors.ForTest(1, 2, 3, 4))
-		G = permCtx.AddGdProductQuery(initialComp, moduleAComp, "MODULE_A", run)
-	}
-	_ = wizard.Prove(moduleAComp, moduleAProve)
-	errG := G.Check(runS)
-
-	if errG != nil {
-		t.Fatalf("error verifying the grand product: %v", errG.Error())
-	}
-}
-
-func TestDistPermutationNoFragment(t *testing.T) {
-	var (
-		runS    *wizard.ProverRuntime
-		G       ifaces.Query
-		permCtx *dist_permutation.PermutationIntoGrandProductCtx
-	)
-	permCtx = dist_permutation.NewPermutationIntoGrandProductCtx(dist_permutation.Settings{MaxNumOfQueryPerModule: 4})
-	initialDefine := func(builder *wizard.Builder) {
-		A := []ifaces.Column{
-			builder.RegisterCommit("MODULE_A.A0", 4),
-			builder.RegisterCommit("MODULE_A.A1", 4),
-			builder.RegisterCommit("MODULE_A.A2", 4),
-		}
-		B := []ifaces.Column{
-			builder.RegisterCommit("MODULE_B.B0", 4),
-			builder.RegisterCommit("MODULE_B.B1", 4),
-			builder.RegisterCommit("MODULE_B.B2", 4),
-		}
-		C := []ifaces.Column{
-			builder.RegisterCommit("MODULE_C.C0", 4),
-			builder.RegisterCommit("MODULE_C.C1", 4),
-			builder.RegisterCommit("MODULE_C.C2", 4),
-		}
-		_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_A_MOD_B", A, B)
-		_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_C_MOD_A", C, A)
-		_ = builder.CompiledIOP.InsertPermutation(0, "P_MOD_B_MOD_C", B, C)
-	}
-
-	moduleADefine := func(builder *wizard.Builder) {
-		builder.RegisterCommit("MODULE_A.A0", 4)
-		builder.RegisterCommit("MODULE_A.A1", 4)
-		builder.RegisterCommit("MODULE_A.A2", 4)
-	}
-
-	initialComp := wizard.Compile(initialDefine)
-	moduleAComp := wizard.Compile(moduleADefine)
-
-	moduleAProve := func(run *wizard.ProverRuntime) {
-		runS = run
-		run.AssignColumn("MODULE_A.A0", smartvectors.ForTest(1, 2, 3, 4))
-		run.AssignColumn("MODULE_A.A1", smartvectors.ForTest(1, 2, 3, 4))
-		run.AssignColumn("MODULE_A.A2", smartvectors.ForTest(1, 2, 3, 4))
-		G = permCtx.AddGdProductQuery(initialComp, moduleAComp, "MODULE_A", run)
-	}
-	_ = wizard.Prove(moduleAComp, moduleAProve)
-	errG := G.Check(runS)
-
-	if errG != nil {
-		t.Fatalf("error verifying the grand product: %v", errG.Error())
-	}
-}
-
-func TestDistPermutationFragment(t *testing.T) {
-	var (
-		runS    *wizard.ProverRuntime
-		G       ifaces.Query
-		permCtx *dist_permutation.PermutationIntoGrandProductCtx
-	)
-	permCtx = dist_permutation.NewPermutationIntoGrandProductCtx(dist_permutation.Settings{MaxNumOfQueryPerModule: 4})
-	initialDefine := func(builder *wizard.Builder) {
-		A := [][]ifaces.Column{
-			{builder.RegisterCommit("MODULE_A.A00", 4),
-				builder.RegisterCommit("MODULE_A.A10", 4),
-				builder.RegisterCommit("MODULE_A.A20", 4)},
-			{builder.RegisterCommit("MODULE_A.A01", 4),
-				builder.RegisterCommit("MODULE_A.A11", 4),
-				builder.RegisterCommit("MODULE_A.A21", 4)},
-		}
-		B := [][]ifaces.Column{
-			{builder.RegisterCommit("MODULE_B.B00", 4),
-				builder.RegisterCommit("MODULE_B.B10", 4),
-				builder.RegisterCommit("MODULE_B.B20", 4)},
-			{builder.RegisterCommit("MODULE_B.B01", 4),
-				builder.RegisterCommit("MODULE_B.B11", 4),
-				builder.RegisterCommit("MODULE_B.B21", 4)},
-		}
-		C := [][]ifaces.Column{
-			{builder.RegisterCommit("MODULE_C.C00", 4),
-				builder.RegisterCommit("MODULE_C.C10", 4),
-				builder.RegisterCommit("MODULE_C.C20", 4)},
-			{builder.RegisterCommit("MODULE_C.C01", 4),
-				builder.RegisterCommit("MODULE_C.C11", 4),
-				builder.RegisterCommit("MODULE_C.C21", 4)},
-		}
-		_ = builder.CompiledIOP.InsertFragmentedPermutation(0, "P_MOD_A_MOD_B", A, B)
-		_ = builder.CompiledIOP.InsertFragmentedPermutation(0, "P_MOD_C_MOD_A", C, A)
-		_ = builder.CompiledIOP.InsertFragmentedPermutation(0, "P_MOD_B_MOD_C", B, C)
-	}
-
-	moduleADefine := func(builder *wizard.Builder) {
-		builder.RegisterCommit("MODULE_A.A00", 4)
-		builder.RegisterCommit("MODULE_A.A10", 4)
-		builder.RegisterCommit("MODULE_A.A20", 4)
-		builder.RegisterCommit("MODULE_A.A01", 4)
-		builder.RegisterCommit("MODULE_A.A11", 4)
-		builder.RegisterCommit("MODULE_A.A21", 4)
-	}
-
-	initialComp := wizard.Compile(initialDefine)
-	moduleAComp := wizard.Compile(moduleADefine)
-
-	moduleAProve := func(run *wizard.ProverRuntime) {
-		runS = run
-		run.AssignColumn("MODULE_A.A00", smartvectors.ForTest(1, 2, 3, 4))
-		run.AssignColumn("MODULE_A.A10", smartvectors.ForTest(1, 2, 3, 4))
-		run.AssignColumn("MODULE_A.A20", smartvectors.ForTest(1, 2, 3, 4))
-		run.AssignColumn("MODULE_A.A01", smartvectors.ForTest(1, 2, 3, 4))
-		run.AssignColumn("MODULE_A.A11", smartvectors.ForTest(1, 2, 3, 4))
-		run.AssignColumn("MODULE_A.A21", smartvectors.ForTest(1, 2, 3, 4))
-		G = permCtx.AddGdProductQuery(initialComp, moduleAComp, "MODULE_A", run)
-	}
-	_ = wizard.Prove(moduleAComp, moduleAProve)
-	errG := G.Check(runS)
-
-	if errG != nil {
-		t.Fatalf("error verifying the grand product: %v", errG.Error())
-	}
 }
--- prover/protocol/wizard/compiled.go
+++ prover/protocol/wizard/compiled.go
@@ -649,9 +649,9 @@ func (c *CompiledIOP) RegisterVerifierAction(round int, action VerifierAction) {
 }
 
 // Register a GrandProduct query
-func (c *CompiledIOP) InsertGrandProduct(round int, id ifaces.QueryID, numerators, denominators []*symbolic.Expression) *query.GrandProduct {
+func (c *CompiledIOP) InsertGrandProduct(round int, id ifaces.QueryID, in map[int]*query.GrandProductInput) *query.GrandProduct {
 	c.assertConsistentRound(round)
-	q := query.NewGrandProduct(round, id, numerators, denominators)
+	q := query.NewGrandProduct(round, in, id)
 	// Finally registers the query
 	c.QueriesParams.AddToRound(round, q.Name(), q)
 	return q
