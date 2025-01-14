874ec8e413896a9ba8b42d9722826170c778c64e
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-01-14 23:21:30 +0000

    test: run tests on generated data

--- prover/zkevm/prover/ecpair/ecpair_with_circuit_test.go
+++ prover/zkevm/prover/ecpair/ecpair_with_circuit_test.go
@@ -17,3 +17,22 @@ func TestMembershipCircuit(t *testing.T) {
 		testModule(t, tc, false, true, false, true)
 	}
 }
+
+func TestGeneratedData(t *testing.T) {
+	var generatedData []pairingDataTestCase
+	filepath.Walk("testdata/generated", func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(path, "input.csv") {
+			tc := pairingDataTestCase{
+				InputFName:       path,
+				NbMillerLoops:    5,
+				NbFinalExps:      1,
+				NbSubgroupChecks: 5,
+			}
+			generatedData = append(generatedData, tc)
+		}
+		return nil
+	})
+	for _, tc := range generatedData {
+		testModule(t, tc, true, true, true, true)
+	}
+}
