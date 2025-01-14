6a8476580da40a4dae47fd1ec248bbd1614ba829
Bogdan Ursu <bogdanursuoffice@gmail.com>
2025-01-14 15:38:28 +0100

    polyext tests

--- prover/maths/common/polyext/poly_test.go
+++ prover/maths/common/polyext/poly_test.go
@@ -70,19 +70,28 @@ func TestMul(t *testing.T) {
 
 	t.Run("a-is-larger", func(t *testing.T) {
 		var (
-			a        = vectorext.ForTest(-1, 0, 1)
-			b        = vectorext.ForTest(1, 1)
-			expected = vectorext.ForTest(-1, -1, 1, 1)
-			res      = polyext.Mul(a, b)
+			a        = vectorext.ForTestFromPairs(-1, 1, 0, 0, 1, 2) // (-1+a)+(1+2a)Xˆ2
+			b        = vectorext.ForTestFromPairs(1, 1, 2, -1)       // (1+a)+(2-a)X
+			expected = vectorext.ForTestFromPairs(
+				-1+fext.RootPowers[1],
+				0,
+				-fext.RootPowers[1]-2,
+				3,
+				2*fext.RootPowers[1]+1,
+				3,
+				-2*fext.RootPowers[1]+2,
+				3,
+			) // (2+3a-2a^2)X^3+(1+3a+2a^2)Xˆ2+(-2+3a-aˆ2)X+(-1+a^2)
+			res = polyext.Mul(a, b)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
 	})
 
 	t.Run("a-is-empty", func(t *testing.T) {
 		var (
-			a        = vectorext.ForTest()
-			b        = vectorext.ForTest(1, 1)
-			expected = vectorext.ForTest()
+			a        = vectorext.ForTestFromPairs()
+			b        = vectorext.ForTestFromPairs(1, 1, 2, 2)
+			expected = vectorext.ForTestFromPairs()
 			res      = polyext.Mul(a, b)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
@@ -90,9 +99,9 @@ func TestMul(t *testing.T) {
 
 	t.Run("b-is-empty", func(t *testing.T) {
 		var (
-			a        = vectorext.ForTest(1, 1)
-			b        = vectorext.ForTest()
-			expected = vectorext.ForTest()
+			a        = vectorext.ForTestFromPairs(1, 1, 2, 2)
+			b        = vectorext.ForTestFromPairs()
+			expected = vectorext.ForTestFromPairs()
 			res      = polyext.Mul(a, b)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
@@ -103,9 +112,9 @@ func TestAdd(t *testing.T) {
 
 	t.Run("same-size", func(t *testing.T) {
 		var (
-			a        = vectorext.ForTest(1, 1)
-			b        = vectorext.ForTest(-1, 1)
-			expected = vectorext.ForTest(0, 2)
+			a        = vectorext.ForTestFromPairs(1, 1, 2, 1)
+			b        = vectorext.ForTestFromPairs(-1, 1, 3, 3)
+			expected = vectorext.ForTestFromPairs(0, 2, 5, 4)
 			res      = polyext.Add(a, b)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
@@ -113,9 +122,9 @@ func TestAdd(t *testing.T) {
 
 	t.Run("a-is-smaller", func(t *testing.T) {
 		var (
-			a        = vectorext.ForTest(1, 1)
-			b        = vectorext.ForTest(1, 1, 1, 1)
-			expected = vectorext.ForTest(2, 2, 1, 1)
+			a        = vectorext.ForTestFromPairs(1, 1, 2, 2)
+			b        = vectorext.ForTestFromPairs(1, 1, 1, 1, 3, 2, 7, 8)
+			expected = vectorext.ForTestFromPairs(2, 2, 3, 3, 3, 2, 7, 8)
 			res      = polyext.Add(a, b)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
@@ -123,9 +132,9 @@ func TestAdd(t *testing.T) {
 
 	t.Run("a-is-larger", func(t *testing.T) {
 		var (
-			a        = vectorext.ForTest(1, 1, 1, 1)
-			b        = vectorext.ForTest(1, 1)
-			expected = vectorext.ForTest(2, 2, 1, 1)
+			a        = vectorext.ForTestFromPairs(1, 1, 1, 1, 2, 2, 3, 3)
+			b        = vectorext.ForTestFromPairs(1, 1, 4, 4)
+			expected = vectorext.ForTestFromPairs(2, 2, 5, 5, 2, 2, 3, 3)
 			res      = polyext.Add(a, b)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
@@ -133,9 +142,9 @@ func TestAdd(t *testing.T) {
 
 	t.Run("a-is-empty", func(t *testing.T) {
 		var (
-			a        = vectorext.ForTest()
-			b        = vectorext.ForTest(1, 1)
-			expected = vectorext.ForTest(1, 1)
+			a        = vectorext.ForTestFromPairs()
+			b        = vectorext.ForTestFromPairs(1, 1, 2, 2)
+			expected = vectorext.ForTestFromPairs(1, 1, 2, 2)
 			res      = polyext.Add(a, b)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
@@ -143,9 +152,9 @@ func TestAdd(t *testing.T) {
 
 	t.Run("b-is-empty", func(t *testing.T) {
 		var (
-			a        = vectorext.ForTest(1, 1)
-			b        = vectorext.ForTest()
-			expected = vectorext.ForTest(1, 1)
+			a        = vectorext.ForTestFromPairs(1, 1, 2, 2)
+			b        = vectorext.ForTestFromPairs()
+			expected = vectorext.ForTestFromPairs(1, 1, 2, 2)
 			res      = polyext.Add(a, b)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
@@ -156,19 +165,26 @@ func TestScalarMul(t *testing.T) {
 
 	t.Run("normal-vec", func(t *testing.T) {
 		var (
-			vec      = vectorext.ForTest(1, 2, 3, 4)
-			x        = fext.NewElement(2, 0)
-			expected = vectorext.ForTest(2, 4, 6, 8)
-			res      = polyext.ScalarMul(vec, x)
+			vec      = vectorext.ForTestFromPairs(1, 2, 3, 4, 2, 1)
+			x        = fext.NewElement(2, 1)
+			expected = vectorext.ForTestFromPairs(
+				2+2*fext.RootPowers[1],
+				5,
+				6+4*fext.RootPowers[1],
+				11,
+				4+fext.RootPowers[1],
+				4,
+			)
+			res = polyext.ScalarMul(vec, x)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
 	})
 
 	t.Run("empty-vec", func(t *testing.T) {
 		var (
-			vec      = vectorext.ForTest()
-			x        = fext.NewElement(2, 0)
-			expected = vectorext.ForTest()
+			vec      = vectorext.ForTestFromPairs()
+			x        = fext.NewElement(2, 2)
+			expected = vectorext.ForTestFromPairs()
 			res      = polyext.ScalarMul(vec, x)
 		)
 		require.Equal(t, vectorext.Prettify(expected), vectorext.Prettify(res))
@@ -181,9 +197,9 @@ func TestEvaluateLagrangeAnyDomain(t *testing.T) {
 	t.Run("single-point-domain", func(t *testing.T) {
 		var (
 			domain   = []fext.Element{fext.Element{field.Zero(), field.Zero()}}
-			x        = fext.NewElement(42, 0)
+			x        = fext.NewElement(42, 42)
 			ys       = polyext.EvaluateLagrangesAnyDomain(domain, x)
-			expected = vectorext.ForTest(1)
+			expected = vectorext.ForTestFromPairs(1, 0)
 		)
 		require.Equal(t, expected, ys)
 	})
@@ -191,10 +207,10 @@ func TestEvaluateLagrangeAnyDomain(t *testing.T) {
 	t.Run("many-point-domain", func(t *testing.T) {
 		var (
 			// the first lagrange poly is 1-X and the second one is X
-			domain   = vectorext.ForTest(0, 1)
+			domain   = vectorext.ForTestFromPairs(0, 0, 1, 0)
 			x        = fext.NewElement(42, 0)
 			ys       = polyext.EvaluateLagrangesAnyDomain(domain, x)
-			expected = vectorext.ForTest(-41, 42)
+			expected = vectorext.ForTestFromPairs(-41, 0, 42, 0)
 		)
 		require.Equal(t, expected, ys)
 	})
