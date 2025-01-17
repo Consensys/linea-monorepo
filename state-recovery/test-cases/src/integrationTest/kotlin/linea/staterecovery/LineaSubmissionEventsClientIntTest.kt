a45c88322b01fd1d9e4ae6b7e88de59c9ec2c2ba
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2025-01-17 10:33:48 +0000

    staterecovery: rename packages to staterecovery

---
+++ state-recovery/test-cases/src/integrationTest/kotlin/linea/staterecovery/LineaSubmissionEventsClientIntTest.kt
@@ -0,0 +1,155 @@
+package linea.staterecovery
+
+import build.linea.contract.l1.LineaContractVersion
+import io.vertx.core.Vertx
+import io.vertx.junit5.Timeout
+import io.vertx.junit5.VertxExtension
+import io.vertx.junit5.VertxTestContext
+import linea.domain.RetryConfig
+import linea.web3j.Web3JLogsSearcher
+import net.consensys.linea.BlockParameter
+import net.consensys.linea.testing.submission.AggregationAndBlobs
+import net.consensys.linea.testing.submission.loadBlobsAndAggregationsSortedAndGrouped
+import net.consensys.linea.testing.submission.submitBlobsAndAggregationsAndWaitExecution
+import net.consensys.zkevm.coordinator.clients.smartcontract.LineaRollupSmartContractClient
+import net.consensys.zkevm.domain.Aggregation
+import net.consensys.zkevm.ethereum.ContractsManager
+import net.consensys.zkevm.ethereum.Web3jClientManager
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.concurrent.TimeUnit
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.toJavaDuration
+
+@ExtendWith(VertxExtension::class)
+class LineaSubmissionEventsClientIntTest {
+  private lateinit var contractClient: LineaRollupSmartContractClient
+  private val testDataDir = "testdata/coordinator/prover/v2/"
+
+  // 1-block-per-blob test data has 3 aggregations: 1..7, 8..14, 15..21.
+  // We will upgrade the contract in the middle of 2nd aggregation: 12
+  // shall submit blob 12, stop submission, upgrade the contract and resume with blob 13
+  // val lastSubmittedBlobs = blobs.filter { it.startBlockNumber == 7UL }
+  private lateinit var aggregationsAndBlobs: List<AggregationAndBlobs>
+  private lateinit var submissionEventsFetcher: LineaRollupSubmissionEventsClient
+
+  private fun setupTest(
+    vertx: Vertx
+  ) {
+    val rollupDeploymentFuture = ContractsManager.get()
+      .deployLineaRollup(numberOfOperators = 2, contractVersion = LineaContractVersion.V6)
+    // load files from FS while smc deploy
+    aggregationsAndBlobs = loadBlobsAndAggregationsSortedAndGrouped(
+      blobsResponsesDir = "$testDataDir/compression/responses",
+      aggregationsResponsesDir = "$testDataDir/aggregation/responses"
+    )
+    // wait smc deployment finishes
+    val rollupDeploymentResult = rollupDeploymentFuture.get()
+    contractClient = rollupDeploymentResult.rollupOperatorClient
+    val eventsFetcherWeb3jClient = Web3jClientManager.buildL1Client(
+      log = LogManager.getLogger("test.clients.l1.events-fetcher"),
+      requestResponseLogLevel = Level.INFO,
+      failuresLogLevel = Level.WARN
+    )
+    submissionEventsFetcher = LineaSubmissionEventsClientImpl(
+      logsSearcher = Web3JLogsSearcher(
+        vertx = vertx,
+        web3jClient = eventsFetcherWeb3jClient,
+        config = Web3JLogsSearcher.Config(
+          backoffDelay = 1.milliseconds,
+          requestRetryConfig = RetryConfig.noRetries
+        )
+      ),
+      smartContractAddress = rollupDeploymentResult.contractAddress,
+      l1EarliestSearchBlock = BlockParameter.Tag.EARLIEST,
+      l1LatestSearchBlock = BlockParameter.Tag.LATEST,
+      logsBlockChunkSize = 100
+    )
+  }
+
+  @Test
+  @Timeout(3, timeUnit = TimeUnit.MINUTES)
+  fun `submission works with contract V6`(
+    vertx: Vertx,
+    testContext: VertxTestContext
+  ) {
+    testSubmission(vertx, testContext)
+  }
+
+  private fun testSubmission(
+    vertx: Vertx,
+    testContext: VertxTestContext
+  ) {
+    setupTest(vertx)
+
+    submitBlobsAndAggregationsAndWaitExecution(
+      contractClient = contractClient,
+      aggregationsAndBlobs = aggregationsAndBlobs,
+      blobChunksSize = 6,
+      l1Web3jClient = Web3jClientManager.l1Client
+    )
+
+    val expectedSubmissionEventsToFind: List<Pair<DataFinalizedV3, List<DataSubmittedV3>>> =
+      getExpectedSubmissionEventsFromRecords(aggregationsAndBlobs)
+
+    expectedSubmissionEventsToFind
+      .forEach { (expectedFinalizationEvent, expectedDataSubmittedEvents) ->
+        assertThat(
+          submissionEventsFetcher
+            .findDataSubmittedV3EventsUntilNextFinalization(
+              l2StartBlockNumberInclusive = expectedFinalizationEvent.startBlockNumber
+            )
+        )
+          .succeedsWithin(1.minutes.toJavaDuration())
+          .extracting { submissionEvents ->
+            val dataFinalizedEvent = submissionEvents?.dataFinalizedEvent?.event
+            val dataSubmittedEvents = submissionEvents?.dataSubmittedEvents?.map { it.event }
+            Pair(dataFinalizedEvent, dataSubmittedEvents)
+          }
+          .isEqualTo(Pair(expectedFinalizationEvent, expectedDataSubmittedEvents))
+      }
+
+    // non happy path
+    val invalidStartBlockNumber = expectedSubmissionEventsToFind.last().first.startBlockNumber + 1UL
+    assertThat(
+      submissionEventsFetcher
+        .findDataSubmittedV3EventsUntilNextFinalization(
+          l2StartBlockNumberInclusive = invalidStartBlockNumber
+        ).get()
+    )
+      .isNull()
+
+    testContext.completeNow()
+  }
+
+  private fun getExpectedSubmissionEventsFromRecords(
+    aggregationsAndBlobs: List<AggregationAndBlobs>
+  ): List<Pair<DataFinalizedV3, List<DataSubmittedV3>>> {
+    return aggregationsAndBlobs
+      .filter { it.aggregation != null }
+      .map { (aggregation, aggBlobs) ->
+        val expectedDataSubmittedEvents: List<DataSubmittedV3> = aggBlobs
+          .chunked(6)
+          .map { blobsChunk ->
+            DataSubmittedV3(
+              parentShnarf = blobsChunk.first().blobCompressionProof!!.prevShnarf,
+              shnarf = blobsChunk.last().expectedShnarf,
+              finalStateRootHash = blobsChunk.last().blobCompressionProof!!.finalStateRootHash
+            )
+          }
+
+        aggregation as Aggregation
+        DataFinalizedV3(
+          startBlockNumber = aggregation.startBlockNumber,
+          endBlockNumber = aggregation.endBlockNumber,
+          shnarf = aggBlobs.last().expectedShnarf,
+          parentStateRootHash = aggBlobs.first().blobCompressionProof!!.parentStateRootHash,
+          finalStateRootHash = aggBlobs.last().blobCompressionProof!!.finalStateRootHash
+        ) to expectedDataSubmittedEvents
+      }
+  }
+}
---
+++ state-recovery/test-cases/src/integrationTest/kotlin/linea/staterecovery/StateRecoverAppWithFakeExecutionClientIntTest.kt
@@ -0,0 +1,371 @@
+package linea.staterecovery
+
+import build.linea.contract.l1.LineaContractVersion
+import build.linea.contract.l1.LineaRollupSmartContractClientReadOnly
+import build.linea.contract.l1.Web3JLineaRollupSmartContractClientReadOnly
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
+import io.vertx.core.Vertx
+import io.vertx.junit5.VertxExtension
+import linea.domain.RetryConfig
+import linea.log4j.configureLoggers
+import linea.staterecovery.clients.VertxTransactionDetailsClient
+import linea.staterecovery.clients.blobscan.BlobScanClient
+import linea.staterecovery.test.FakeExecutionLayerClient
+import linea.staterecovery.test.FakeStateManagerClient
+import linea.staterecovery.test.FakeStateManagerClientBasedOnBlobsRecords
+import linea.web3j.Web3JLogsSearcher
+import net.consensys.linea.BlockNumberAndHash
+import net.consensys.linea.BlockParameter
+import net.consensys.linea.jsonrpc.client.RequestRetryConfig
+import net.consensys.linea.jsonrpc.client.VertxHttpJsonRpcClientFactory
+import net.consensys.linea.metrics.micrometer.MicrometerMetricsFacade
+import net.consensys.linea.testing.submission.AggregationAndBlobs
+import net.consensys.linea.testing.submission.loadBlobsAndAggregationsSortedAndGrouped
+import net.consensys.linea.testing.submission.submitBlobsAndAggregationsAndWaitExecution
+import net.consensys.zkevm.coordinator.clients.smartcontract.LineaRollupSmartContractClient
+import net.consensys.zkevm.ethereum.ContractsManager
+import net.consensys.zkevm.ethereum.Web3jClientManager
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.Awaitility.await
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.net.URI
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
+
+@ExtendWith(VertxExtension::class)
+class StateRecoverAppWithFakeExecutionClientIntTest {
+  private val log = LogManager.getLogger("test.case.StateRecoverAppWithFakeExecutionClientIntTest")
+  private lateinit var stateRecoverApp: StateRecoverApp
+  private lateinit var aggregationsAndBlobs: List<AggregationAndBlobs>
+  private lateinit var executionLayerClient: FakeExecutionLayerClient
+  private lateinit var fakeStateManagerClient: FakeStateManagerClient
+  private lateinit var transactionDetailsClient: TransactionDetailsClient
+  private lateinit var lineaContractClient: LineaRollupSmartContractClientReadOnly
+
+  private lateinit var contractClientForSubmissions: LineaRollupSmartContractClient
+  private val testDataDir = run {
+    "testdata/coordinator/prover/v3"
+  }
+
+  private val l1RpcUrl = "http://localhost:8445"
+  private val blobScanUrl = "http://localhost:4001"
+
+  @BeforeEach
+  fun beforeEach(vertx: Vertx) {
+    val jsonRpcFactory = VertxHttpJsonRpcClientFactory(
+      vertx = vertx,
+      metricsFacade = MicrometerMetricsFacade(SimpleMeterRegistry())
+    )
+    aggregationsAndBlobs = loadBlobsAndAggregationsSortedAndGrouped(
+      blobsResponsesDir = "$testDataDir/compression/responses",
+      aggregationsResponsesDir = "$testDataDir/aggregation/responses"
+    )
+    executionLayerClient = FakeExecutionLayerClient(
+      headBlock = BlockNumberAndHash(number = 0uL, hash = ByteArray(32) { 0 }),
+      initialStateRecoverStartBlockNumber = null,
+      loggerName = "test.fake.clients.l1.fake-execution-layer"
+    )
+    fakeStateManagerClient =
+      FakeStateManagerClientBasedOnBlobsRecords(blobRecords = aggregationsAndBlobs.flatMap { it.blobs })
+    transactionDetailsClient = VertxTransactionDetailsClient.create(
+      jsonRpcClientFactory = jsonRpcFactory,
+      endpoint = URI(l1RpcUrl),
+      retryConfig = RequestRetryConfig(
+        backoffDelay = 10.milliseconds,
+        timeout = 2.seconds
+      ),
+      logger = LogManager.getLogger("test.clients.l1.transaction-details")
+    )
+
+    val rollupDeploymentResult = ContractsManager.get()
+      .deployLineaRollup(numberOfOperators = 2, contractVersion = LineaContractVersion.V6).get()
+
+    lineaContractClient = Web3JLineaRollupSmartContractClientReadOnly(
+      web3j = Web3jClientManager.buildL1Client(
+        log = LogManager.getLogger("test.clients.l1.linea-contract"),
+        requestResponseLogLevel = Level.INFO,
+        failuresLogLevel = Level.WARN
+      ),
+      contractAddress = rollupDeploymentResult.contractAddress
+    )
+    val logsSearcher = Web3JLogsSearcher(
+      vertx = vertx,
+      web3jClient = Web3jClientManager.buildL1Client(
+        log = LogManager.getLogger("test.clients.l1.events-fetcher"),
+        requestResponseLogLevel = Level.TRACE,
+        failuresLogLevel = Level.WARN
+      ),
+      Web3JLogsSearcher.Config(
+        backoffDelay = 1.milliseconds,
+        requestRetryConfig = RetryConfig.noRetries
+      ),
+      log = LogManager.getLogger("test.clients.l1.events-fetcher")
+    )
+
+    contractClientForSubmissions = rollupDeploymentResult.rollupOperatorClient
+    val blobScanClient = BlobScanClient.create(
+      vertx = vertx,
+      endpoint = URI(blobScanUrl),
+      requestRetryConfig = RequestRetryConfig(
+        backoffDelay = 10.milliseconds,
+        timeout = 2.seconds
+      ),
+      responseLogMaxSize = 1000u,
+      logger = LogManager.getLogger("test.clients.l1.blobscan")
+    )
+
+    stateRecoverApp = StateRecoverApp(
+      vertx = vertx,
+      elClient = executionLayerClient,
+      blobFetcher = blobScanClient,
+      ethLogsSearcher = logsSearcher,
+      stateManagerClient = fakeStateManagerClient,
+      transactionDetailsClient = transactionDetailsClient,
+      blockHeaderStaticFields = BlockHeaderStaticFields.localDev,
+      lineaContractClient = lineaContractClient,
+      config = StateRecoverApp.Config(
+        l1LatestSearchBlock = BlockParameter.Tag.LATEST,
+        l1PollingInterval = 10.milliseconds,
+        executionClientPollingInterval = 1.seconds,
+        smartContractAddress = lineaContractClient.getAddress()
+      )
+    )
+
+    configureLoggers(
+      rootLevel = Level.INFO,
+      log.name to Level.INFO,
+      "net.consensys.linea.contract.Web3JContractAsyncHelper" to Level.WARN, // silence noisy gasPrice Caps logs
+      "test.clients.l1.executionlayer" to Level.DEBUG,
+      "test.clients.l1.web3j-default" to Level.INFO,
+      "test.clients.l1.state-manager" to Level.INFO,
+      "test.clients.l1.transaction-details" to Level.INFO,
+      "test.clients.l1.linea-contract" to Level.INFO,
+      "test.clients.l1.events-fetcher" to Level.INFO,
+      "test.clients.l1.blobscan" to Level.INFO,
+      "net.consensys.linea.contract.l1" to Level.INFO,
+      "test.fake.clients.l1.fake-execution-layer" to Level.INFO
+    )
+  }
+
+  private fun submitDataToL1ContactAndWaitExecution(
+    contractClient: LineaRollupSmartContractClient = contractClientForSubmissions,
+    aggregationsAndBlobs: List<AggregationAndBlobs> = this.aggregationsAndBlobs,
+    blobChunksSize: Int = 6,
+    waitTimeout: Duration = 2.minutes
+  ) {
+    submitBlobsAndAggregationsAndWaitExecution(
+      contractClient = contractClient,
+      aggregationsAndBlobs = aggregationsAndBlobs,
+      blobChunksSize = blobChunksSize,
+      waitTimeout = waitTimeout,
+      l1Web3jClient = Web3jClientManager.l1Client
+    )
+  }
+
+  /*
+  1. when state recovery disabled: enables with lastFinalizedBlock + 1
+  1.1 no finalizations and start from genesis
+  1.2 headBlockNumber > lastFinalizedBlock -> enable with headBlockNumber + 1
+  1.3 headBlockNumber < lastFinalizedBlock -> enable with lastFinalizedBlock + 1
+
+  2. when state recovery enabled:
+  2.1 recoveryStartBlockNumber > headBlockNumber: pull for head block number until is reached and start recovery there
+  2.2 recoveryStartBlockNumber <= headBlockNumber: resume recovery from headBlockNumber
+  */
+  @Test
+  fun `when state recovery disabled and is starting from genesis`() {
+    stateRecoverApp.start().get()
+    submitDataToL1ContactAndWaitExecution()
+
+    val lastAggregation = aggregationsAndBlobs.findLast { it.aggregation != null }!!.aggregation
+    await()
+      .atMost(1.minutes.toJavaDuration())
+      .untilAsserted {
+        assertThat(stateRecoverApp.lastSuccessfullyRecoveredFinalization?.event?.endBlockNumber)
+          .isEqualTo(lastAggregation!!.endBlockNumber)
+      }
+
+    assertThat(executionLayerClient.lineaGetStateRecoveryStatus().get())
+      .isEqualTo(
+        StateRecoveryStatus(
+          headBlockNumber = lastAggregation!!.endBlockNumber,
+          stateRecoverStartBlockNumber = 1UL
+        )
+      )
+  }
+
+  @Test
+  fun `when recovery is disabled and headBlock is before lastFinalizedBlock resumes from lastFinalizedBlock+1`() {
+    val finalizationToResumeFrom = aggregationsAndBlobs.get(1).aggregation!!
+    // assert that the finalization event to resume from has at least 1 middle block
+    assertThat(finalizationToResumeFrom.endBlockNumber)
+      .isGreaterThan(finalizationToResumeFrom.startBlockNumber + 1UL)
+      .withFailMessage("finalizationEventToResumeFrom must at least 3 blocks for this test")
+
+    val finalizationsBeforeCutOff = aggregationsAndBlobs
+      .filter { it.aggregation != null }
+      .filter { it.aggregation!!.endBlockNumber < finalizationToResumeFrom.startBlockNumber }
+
+    val finalizationsAfterCutOff = aggregationsAndBlobs
+      .filter { it.aggregation != null }
+      .filter { it.aggregation!!.startBlockNumber >= finalizationToResumeFrom.startBlockNumber }
+
+    log.debug(
+      "finalizations={} finalizationToStartRecoveryFrom={}",
+      aggregationsAndBlobs.map { it.aggregation?.intervalString() },
+      finalizationToResumeFrom.intervalString()
+    )
+
+    submitDataToL1ContactAndWaitExecution(
+      aggregationsAndBlobs = finalizationsBeforeCutOff
+    )
+
+    executionLayerClient.headBlock = BlockNumberAndHash(
+      number = 1UL,
+      hash = ByteArray(32) { 0 }
+    )
+
+    val lastFinalizedBlockNumber = finalizationsBeforeCutOff.last().aggregation!!.endBlockNumber
+    val expectedStateRecoverStartBlockNumber = lastFinalizedBlockNumber + 1UL
+    stateRecoverApp.start().get()
+
+    await()
+      .atMost(4.minutes.toJavaDuration())
+      .pollInterval(1.seconds.toJavaDuration())
+      .untilAsserted {
+        assertThat(executionLayerClient.stateRecoverStatus).isEqualTo(
+          StateRecoveryStatus(
+            headBlockNumber = 1UL,
+            stateRecoverStartBlockNumber = expectedStateRecoverStartBlockNumber
+          )
+        )
+        log.info("stateRecoverStatus={}", executionLayerClient.stateRecoverStatus)
+      }
+
+    // simulate that execution client has synced up to the last finalized block through P2P network
+    executionLayerClient.headBlock = BlockNumberAndHash(
+      number = lastFinalizedBlockNumber,
+      hash = ByteArray(32) { 0 }
+    )
+
+    // continue finalizing the rest of the aggregations
+    submitDataToL1ContactAndWaitExecution(
+      aggregationsAndBlobs = finalizationsAfterCutOff
+    )
+
+    val lastAggregation = aggregationsAndBlobs.findLast { it.aggregation != null }!!.aggregation
+    await()
+      .atMost(1.minutes.toJavaDuration())
+      .pollInterval(1.seconds.toJavaDuration())
+      .untilAsserted {
+        assertThat(executionLayerClient.headBlock.number).isEqualTo(lastAggregation!!.endBlockNumber)
+      }
+
+    // assert it imports correct blocks
+    val importedBlocks = executionLayerClient.importedBlockNumbersInRecoveryMode
+    assertThat(importedBlocks.first()).isEqualTo(expectedStateRecoverStartBlockNumber)
+    assertThat(importedBlocks.last()).isEqualTo(lastAggregation!!.endBlockNumber)
+  }
+
+  @Test
+  fun `when node starts with headblock greater lastFinalizedBlock`() {
+    val finalizationToResumeFrom = aggregationsAndBlobs.get(1).aggregation!!
+    // assert that the finalization event to resume from has at least 1 middle block
+    assertThat(finalizationToResumeFrom.endBlockNumber)
+      .isGreaterThan(finalizationToResumeFrom.startBlockNumber + 1UL)
+      .withFailMessage("finalizationEventToResumeFrom must at least 3 blocks for this test")
+
+    val finalizationsBeforeCutOff = aggregationsAndBlobs
+      .filter { it.aggregation != null }
+      .filter { it.aggregation!!.endBlockNumber < finalizationToResumeFrom.startBlockNumber }
+
+    val finalizationsAfterCutOff = aggregationsAndBlobs
+      .filter { it.aggregation != null }
+      .filter { it.aggregation!!.startBlockNumber >= finalizationToResumeFrom.startBlockNumber }
+
+    log.debug(
+      "finalizations={} finalizationToStartRecoveryFrom={}",
+      aggregationsAndBlobs.map { it.aggregation?.intervalString() },
+      finalizationToResumeFrom.intervalString()
+    )
+
+    submitDataToL1ContactAndWaitExecution(
+      aggregationsAndBlobs = finalizationsBeforeCutOff
+    )
+
+    // set execution layer head block after latest finalization
+    val headBlockNumberAtStart = finalizationsBeforeCutOff.last().aggregation!!.endBlockNumber + 1UL
+    executionLayerClient.headBlock = BlockNumberAndHash(
+      number = headBlockNumberAtStart,
+      hash = ByteArray(32) { 0 }
+    )
+
+    stateRecoverApp.start().get()
+    await()
+      .atMost(2.minutes.toJavaDuration())
+      .pollInterval(1.seconds.toJavaDuration())
+      .untilAsserted {
+        assertThat(executionLayerClient.stateRecoverStatus).isEqualTo(
+          StateRecoveryStatus(
+            headBlockNumber = headBlockNumberAtStart,
+            stateRecoverStartBlockNumber = headBlockNumberAtStart + 1UL
+          )
+        )
+        log.debug("stateRecoverStatus={}", executionLayerClient.stateRecoverStatus)
+      }
+
+    // continue finalizing the rest of the aggregations
+    submitDataToL1ContactAndWaitExecution(
+      aggregationsAndBlobs = finalizationsAfterCutOff
+    )
+
+    val lastAggregation = aggregationsAndBlobs.findLast { it.aggregation != null }!!.aggregation
+    await()
+      .atMost(2.minutes.toJavaDuration())
+      .pollInterval(1.seconds.toJavaDuration())
+      .untilAsserted {
+        assertThat(executionLayerClient.stateRecoverStatus)
+          .isEqualTo(
+            StateRecoveryStatus(
+              headBlockNumber = lastAggregation!!.endBlockNumber,
+              stateRecoverStartBlockNumber = headBlockNumberAtStart + 1UL
+            )
+          )
+      }
+    // assert it does not try to import blocks behind the head block
+    assertThat(executionLayerClient.importedBlockNumbersInRecoveryMode.minOrNull())
+      .isEqualTo(headBlockNumberAtStart + 1UL)
+  }
+
+  @Test
+  fun `should stop recovery as soon as stateroot mismatches`() {
+    fakeStateManagerClient.setBlockStateRootHash(
+      aggregationsAndBlobs[1].aggregation!!.endBlockNumber,
+      ByteArray(32) { 1 }
+    )
+    log.debug(
+      "aggregations={} forcedMismatchAggregation={}",
+      aggregationsAndBlobs.map { it.aggregation?.intervalString() },
+      aggregationsAndBlobs[1].aggregation!!.intervalString()
+    )
+
+    stateRecoverApp.start().get()
+    submitDataToL1ContactAndWaitExecution()
+
+    await()
+      .atMost(1.minutes.toJavaDuration())
+      .untilAsserted {
+        assertThat(stateRecoverApp.stateRootMismatchFound).isTrue()
+      }
+
+    assertThat(executionLayerClient.headBlock.number)
+      .isEqualTo(aggregationsAndBlobs[1].aggregation!!.endBlockNumber)
+  }
+}
