041240183c8bc883b355e805db5777c466cf1566
AlexandreBelling <alexandrebelling8@gmail.com>
2025-01-18 01:18:29 +0100

    fix(fsHook): adds fsHook in the equalizer

--- prover/protocol/wizard/builder.go
+++ prover/protocol/wizard/builder.go
@@ -310,4 +310,12 @@ func (b *Builder) equalizeRounds(numRounds int) {
 		utils.Panic("Bug : numRounds is %v but %v rounds are registered for the verifier. %v", numRounds, comp.SubVerifiers.Len(), helpMsg)
 	}
 	comp.SubVerifiers.Reserve(numRounds)
+
+	/*
+		Check and reserve for the FiatShamirHooks
+	*/
+	if comp.FiatShamirHooks.Len() > numRounds {
+		utils.Panic("Bug : numRounds is %v but %v rounds are registered for the FiatShamirHooks. %v", numRounds, comp.FiatShamirHooks.Len(), helpMsg)
+	}
+	comp.FiatShamirHooks.Reserve(numRounds)
 }
