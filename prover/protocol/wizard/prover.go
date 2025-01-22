984a32621cc2db7126fd6b7e13f9ec7e9ecc9e92
Soleimani193 <azam.soleimanian@ens.fr>
2025-01-22 17:14:48 +0100

    minor, fixing a complain

--- prover/protocol/wizard/prover.go
+++ prover/protocol/wizard/prover.go
@@ -598,32 +598,6 @@ func (run *ProverRuntime) goNextRound() {
 		if info.Type == coin.FromSeed {
 			// if it is of type FromSeed, sample a coin based on the seed
 			if seed, ok := run.ParentRuntime.Coins.MustGet("SEED").(field.Element); ok {
-				value = info.SampleFromSeed( run.FS,seed)
-			}
-		} else {
-			// otherwise sample based on the transcript.
-			value = info.Sample(run.FS)
-		}
-		run.Coins.InsertNew(myCoin, value)
-	}
-
-	finalState := run.FS.State()
-
-	run.FiatShamirHistory[run.currRound] = [2][]field.Element{
-		initialState,
-		finalState,
-	}
-}
-
-
-		var (
-			info  = run.Spec.Coins.Data(myCoin)
-			value interface{}
-		)
-
-		if info.Type == coin.FromSeed {
-			// if it is of type FromSeed, sample a coin based on the seed
-			if seed, ok := run.ParentRuntime.Coins.MustGet("SEED").(field.Element); ok {
 				value = info.SampleFromSeed(seed, run.FS)
 			}
 		} else {
