abc95c1030bddefaa480042f49537ea1828ae0f4
Soleimani193 <azam.soleimanian@ens.fr>
2025-01-23 16:40:10 +0100

    minor

--- prover/backend/execution/prove.go
+++ prover/backend/execution/prove.go
@@ -190,7 +190,8 @@ func mustProveAndPass(
 	case config.ProverModeEncodeOnly:
 
 		profiling.ProfileTrace("encode-decode-no-circuit", true, false, func() {
-			filepath := "/tmp/wizard-assignment/blob-" + strconv.Itoa(rand.Int()) + ".bin" //nolint:gosec // Ignoring weak randomness error
+			//nolint:gosec // Ignoring weak randomness error
+			filepath := "/tmp/wizard-assignment/blob-" + strconv.Itoa(rand.Int()) + ".bin"
 
 			encodeOnlyZkEvm := zkevm.EncodeOnlyZkEvm(traces)
 			numChunks := runtime.GOMAXPROCS(0)
