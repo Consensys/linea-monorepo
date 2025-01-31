a3add2c25a8032a0335bd0a157e04e9b2635b557
Pedro Novais <1478752+jpnovais@users.noreply.github.com>
2025-01-31 11:29:51 +0000

    jvm-libs: decompressor adds test for compression -> decompression loop

--- jvm-libs/linea/blob-decompressor/src/test/kotlin/net/consensys/linea/blob/GoNativeBlobDecompressorTest.kt
+++ jvm-libs/linea/blob-decompressor/src/test/kotlin/net/consensys/linea/blob/GoNativeBlobDecompressorTest.kt
@@ -16,18 +16,20 @@ import net.consensys.toBigInteger
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import kotlin.jvm.optionals.getOrNull
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GoNativeBlobDecompressorTest {
   private val blobCompressedLimit = 30 * 1024
-  private lateinit var compressor: BlobCompressor
-  private lateinit var decompressor: BlobDecompressor
+  private val compressor: BlobCompressor = GoBackedBlobCompressor
+    .getInstance(BlobCompressorVersion.V1_0_1, blobCompressedLimit.toUInt())
+  private val decompressor: BlobDecompressor =
+    GoNativeBlobDecompressorFactory.getInstance(BlobDecompressorVersion.V1_1_0)
 
   @BeforeEach
   fun beforeEach() {
-    compressor = GoBackedBlobCompressor
-      .getInstance(BlobCompressorVersion.V1_0_1, blobCompressedLimit.toUInt())
-    decompressor = GoNativeBlobDecompressorFactory.getInstance(BlobDecompressorVersion.V1_1_0)
+    compressor.reset()
   }
 
   @Test
@@ -47,24 +49,31 @@ class GoNativeBlobDecompressorTest {
   @Test
   fun `should decompress original data`() {
     val tx0 = TransactionFactory.createTransactionFrontier(
-      nonce = 1uL,
+      nonce = 10uL,
       gasLimit = 22_0000uL,
       to = null,
       value = 1uL.eth.toBigInteger(),
       input = byteArrayOf()
     )
     val tx1 = TransactionFactory.createTransactionEip1559(
-      nonce = 2uL,
+      nonce = 123uL,
       gasLimit = 23_0000uL,
       to = null,
       value = 2uL.eth.toBigInteger(),
       input = "0x1234".toByteArray(),
       accessList = listOf(
         AccessListEntry(
-          address = "0x00000000000000000000000000000000000000ff".decodeHex(),
+          address = "0x0000000000000000000000000000000000000001".decodeHex(),
           storageKeys = listOf(
             "0x0000000000000000000000000000000000000000000000000000000000000001".decodeHex(),
             "0x0000000000000000000000000000000000000000000000000000000000000002".decodeHex()
+          )
+        ),
+        AccessListEntry(
+          address = "0x0000000000000000000000000000000000000002".decodeHex(),
+          storageKeys = listOf(
+            "0x0000000000000000000000000000000000000000000000000000000000000011".decodeHex(),
+            "0x0000000000000000000000000000000000000000000000000000000000000012".decodeHex()
           )
         )
       )
@@ -83,35 +92,53 @@ class GoNativeBlobDecompressorTest {
     val decodedBlock = BesuRlpBlobDecoder.decode(decompressedBlock)
 
     // Only BlockHash and Timestamp are compressed to the Blob
-    assertThat(decodedBlock.header.hash.toArray()).isEqualTo(originalBesuBlock.header.hash)
+    assertThat(decodedBlock.header.hash).isEqualTo(originalBesuBlock.header.hash)
     assertThat(decodedBlock.header.timestamp).isEqualTo(Instant.parse("2025-01-02T12:23:45Z").epochSeconds)
 
     assertThat(decodedBlock.body.transactions).hasSize(2)
     val decompressedTx0 = decodedBlock.body.transactions[0]
-    val decompressedTx1 = decodedBlock.body.transactions[0]
+    val decompressedTx1 = decodedBlock.body.transactions[1]
 
     assertThat(decompressedTx0.type).isEqualTo(tx0.type.toBesu())
     assertThat(decompressedTx0.sender.toArray()).isEqualTo(tx0.toBesu().sender.toArray())
     assertThat(decompressedTx0.nonce.toULong()).isEqualTo(tx0.nonce)
     assertThat(decompressedTx0.gasLimit.toULong()).isEqualTo(tx0.gasLimit)
-    assertThat(decompressedTx0.maxFeePerGas.getOrNull()?.asBigInteger).isEqualTo(tx0.maxFeePerGas)
-    assertThat(decompressedTx0.maxPriorityFeePerGas.getOrNull()?.asBigInteger).isEqualTo(tx0.maxPriorityFeePerGas)
-    assertThat(decompressedTx0.gasPrice.getOrNull()?.asBigInteger).isEqualTo(tx0.gasPrice)
+    assertThat(decompressedTx0.maxFeePerGas.getOrNull()).isNull()
+    assertThat(decompressedTx0.maxPriorityFeePerGas.getOrNull()).isNull()
+    assertThat(decompressedTx0.gasPrice.getOrNull()?.asBigInteger).isEqualTo(tx0.gasPrice!!.toBigInteger())
     assertThat(decompressedTx0.to.getOrNull()?.toArray()).isEqualTo(tx0.to)
     assertThat(decompressedTx0.value.asBigInteger).isEqualTo(tx0.value)
     assertThat(decompressedTx0.payload.toArray()).isEqualTo(tx0.input)
-    assertThat(decompressedTx0.accessList).isNull()
+    assertThat(decompressedTx0.accessList.getOrNull()).isNull()
 
-    assertThat(decompressedTx1.type.serializedType.toUByte()).isEqualTo(tx1.type)
+    assertThat(decompressedTx1.type).isEqualTo(tx1.type.toBesu())
     assertThat(decompressedTx1.sender.toArray()).isEqualTo(tx1.toBesu().sender.toArray())
     assertThat(decompressedTx1.nonce.toULong()).isEqualTo(tx1.nonce)
     assertThat(decompressedTx1.gasLimit.toULong()).isEqualTo(tx1.gasLimit)
-    assertThat(decompressedTx1.maxFeePerGas.getOrNull()?.asBigInteger).isEqualTo(tx1.maxFeePerGas)
-    assertThat(decompressedTx1.maxPriorityFeePerGas.getOrNull()?.asBigInteger).isEqualTo(tx1.maxPriorityFeePerGas)
-    assertThat(decompressedTx1.gasPrice.getOrNull()?.asBigInteger).isEqualTo(tx1.gasPrice)
+    assertThat(decompressedTx1.maxFeePerGas.getOrNull()?.asBigInteger)
+      .isEqualTo(tx1.maxFeePerGas?.toBigInteger())
+    assertThat(decompressedTx1.maxPriorityFeePerGas.getOrNull()?.asBigInteger)
+      .isEqualTo(tx1.maxPriorityFeePerGas?.toBigInteger())
+    assertThat(decompressedTx1.gasPrice.getOrNull()).isNull()
     assertThat(decompressedTx1.to.getOrNull()?.toArray()).isEqualTo(tx1.to)
     assertThat(decompressedTx1.value.asBigInteger).isEqualTo(tx1.value)
     assertThat(decompressedTx1.payload.toArray()).isEqualTo(tx1.input)
-    assertThat(decompressedTx1.accessList).isNull()
+    assertThat(decompressedTx1.accessList.getOrNull()).isNotNull
+    decompressedTx1.accessList.getOrNull()!!.also { decompressedAccList ->
+      assertThat(decompressedAccList).hasSize(2)
+      assertThat(decompressedAccList[0]!!.address.toArray())
+        .isEqualTo(tx1.accessList!![0].address)
+      assertThat(decompressedAccList[0]!!.storageKeys[0].toArray())
+        .isEqualTo(tx1.accessList!![0].storageKeys[0])
+      assertThat(decompressedAccList[0]!!.storageKeys[1].toArray())
+        .isEqualTo(tx1.accessList!![0].storageKeys[1])
+
+      assertThat(decompressedAccList[1]!!.address.toArray())
+        .isEqualTo(tx1.accessList!![1].address)
+      assertThat(decompressedAccList[1]!!.storageKeys[0].toArray())
+        .isEqualTo(tx1.accessList!![1].storageKeys[0])
+      assertThat(decompressedAccList[1]!!.storageKeys[1].toArray())
+        .isEqualTo(tx1.accessList!![1].storageKeys[1])
+    }
   }
 }
