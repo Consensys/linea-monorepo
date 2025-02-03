b91a24c19c8348036236c4faebf37b330d97431a
Soleimani193 <azam.soleimanian@ens.fr>
2025-02-03 13:05:55 +0100

    distribution of global queries

---
+++ prover/protocol/distributed/compiler/global/global_test.go
@@ -0,0 +1,88 @@
+package global_test
+
+import (
+	"testing"
+
+	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
+	"github.com/consensys/linea-monorepo/prover/protocol/column"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/dummy"
+	"github.com/consensys/linea-monorepo/prover/protocol/distributed"
+	"github.com/consensys/linea-monorepo/prover/protocol/distributed/compiler/global"
+	md "github.com/consensys/linea-monorepo/prover/protocol/distributed/namebaseddiscoverer"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+	"github.com/consensys/linea-monorepo/prover/symbolic"
+	"github.com/stretchr/testify/require"
+)
+
+// It tests DistributedLogDerivSum.
+func TestSeedGeneration(t *testing.T) {
+	const (
+		numSegModule = 2
+	)
+
+	//initialComp
+	define := func(b *wizard.Builder) {
+
+		var (
+			col0 = b.CompiledIOP.InsertCommit(0, "module.col0", 8)
+			col1 = b.CompiledIOP.InsertCommit(0, "module.col1", 8)
+			// verifCol = verifiercol.NewConstantCol(field.NewElement(7), 8)
+		)
+
+		b.CompiledIOP.InsertGlobal(0, "global0",
+			symbolic.Sub(col1, column.Shift(col0, 3)))
+
+	}
+
+	// initialProver
+	prover := func(run *wizard.ProverRuntime) {
+		run.AssignColumn("module.col0", smartvectors.ForTest(1, 0, 2, 1, 7, 1, 11, 2))
+		run.AssignColumn("module.col1", smartvectors.ForTest(1, 7, 1, 11, 2, 1, 0, 2))
+	}
+
+	// initial compiledIOP is the parent to LPPComp and all the SegmentModuleComp objects.
+	initialComp := wizard.Compile(define)
+
+	// Initialize the period separating module discoverer
+	disc := &md.PeriodSeperatingModuleDiscoverer{}
+	disc.Analyze(initialComp)
+
+	// distribute the columns among modules and segments; this includes also multiplicity columns
+	// for all the segments from the same module, compiledIOP object is the same.
+	moduleComp := distributed.GetFreshSegmentModuleComp(
+		distributed.SegmentModuleInputs{
+			InitialComp:         initialComp,
+			Disc:                disc,
+			ModuleName:          "module",
+			NumSegmentsInModule: numSegModule,
+		},
+	)
+
+	// distribute the query LogDerivativeSum among modules.
+	// The seed is used to generate randomness for each moduleComp.
+	global.DistributeGlobal(global.DistributionInputs{
+		ModuleComp:  moduleComp,
+		InitialComp: initialComp,
+		Disc:        disc,
+		ModuleName:  "module",
+		NumSegments: numSegModule,
+	})
+
+	// This compiles the log-derivative queries into global/local queries.
+	wizard.ContinueCompilation(moduleComp, dummy.Compile)
+
+	// run the initial runtime
+	initialRuntime := wizard.ProverOnlyFirstRound(initialComp, prover)
+
+	// Compile and prove for module0
+	for proverID := 0; proverID < numSegModule; proverID++ {
+		proof := wizard.Prove(moduleComp, func(run *wizard.ProverRuntime) {
+			run.ParentRuntime = initialRuntime
+			// inputs for vertical splitting of the witness
+			run.ProverID = proverID
+		})
+		valid := wizard.Verify(moduleComp, proof)
+		require.NoError(t, valid)
+	}
+
+}
