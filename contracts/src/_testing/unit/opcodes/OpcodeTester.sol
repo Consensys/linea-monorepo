4700e9078c061740f999cfc8171b57e40b33fb5a
The Dark Jester <thedarkjester@users.noreply.github.com>
2025-02-11 13:57:10 +0000

    [Chore] - use defined account for opcode testing (#671)
    * use defined account for opcode testing
    
    * add output to show execution

--- contracts/src/_testing/unit/opcodes/OpcodeTester.sol
+++ contracts/src/_testing/unit/opcodes/OpcodeTester.sol
@@ -6,7 +6,7 @@ import { ErrorAndDestructionTesting } from "./ErrorAndDestructionTesting.sol";
 contract OpcodeTester {
   mapping(bytes2 => uint256) public opcodeExecutions;
   address public yulContract;
-  bytes32 rollingBlockDetailComputations;
+  bytes32 public rollingBlockDetailComputations;
 
   // The opcodes are logged here for completeness sake even though not used.
   // NOTE: For looping we make it 2 bytes instead of one, so the real value is actually missing the 00 from 0x0001 (0x01) etc.
