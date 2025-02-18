3397433b035f9db002a571259d1a96b8cca04e96
AlexandreBelling <alexandrebelling8@gmail.com>
2025-02-18 19:13:01 +0100

    feat: adds the possibility to run the checker in "bench" mode

--- prover/cmd/dev-tools/corset-checker/main.go
+++ prover/cmd/dev-tools/corset-checker/main.go
@@ -4,7 +4,14 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/consensys/linea-monorepo/prover/config"
+	"github.com/consensys/linea-monorepo/prover/crypto/ringsis"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/cleanup"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/dummy"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/mimc"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/selfrecursion"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/vortex"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 )
 
@@ -17,8 +24,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	suite := []func(*wizard.CompiledIOP){
+		dummy.Compile,
+	}
+
+	if cfg.Execution.ProverMode == config.ProverModeBench {
+		suite = fullSuite()
+	}
+
 	var (
-		comp  = wizard.Compile(MakeDefine(cfg), dummy.Compile)
+		comp  = wizard.Compile(MakeDefine(cfg), suite...)
 		proof = wizard.Prove(comp, MakeProver(traceFile))
 		vErr  = wizard.Verify(comp, proof)
 	)
@@ -33,4 +48,49 @@ func main() {
 		os.Exit(1)
 	}
 
+}
+
+func fullSuite() []func(*wizard.CompiledIOP) {
+
+	sisInstance := ringsis.Params{LogTwoBound: 16, LogTwoDegree: 6}
+
+	return []func(*wizard.CompiledIOP){
+		mimc.CompileMiMC,
+		compiler.Arcane(1<<10, 1<<19, false),
+		vortex.Compile(
+			2,
+			vortex.ForceNumOpenedColumns(256),
+			vortex.WithSISParams(&sisInstance),
+		),
+
+		selfrecursion.SelfRecurse,
+		cleanup.CleanUp,
+		mimc.CompileMiMC,
+		compiler.Arcane(1<<10, 1<<18, false),
+		vortex.Compile(
+			2,
+			vortex.ForceNumOpenedColumns(256),
+			vortex.WithSISParams(&sisInstance),
+		),
+
+		selfrecursion.SelfRecurse,
+		cleanup.CleanUp,
+		mimc.CompileMiMC,
+		compiler.Arcane(1<<10, 1<<16, false),
+		vortex.Compile(
+			8,
+			vortex.ForceNumOpenedColumns(64),
+			vortex.WithSISParams(&sisInstance),
+		),
+
+		selfrecursion.SelfRecurse,
+		cleanup.CleanUp,
+		mimc.CompileMiMC,
+		compiler.Arcane(1<<10, 1<<13, false),
+		vortex.Compile(
+			8,
+			vortex.ForceNumOpenedColumns(64),
+			vortex.ReplaceSisByMimc(),
+		),
+	}
 }
