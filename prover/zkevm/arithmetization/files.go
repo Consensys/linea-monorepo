c8946636d1b0fdedab6801e0af16b21519e49247
DavePearce <dave01001110@gmail.com>
2025-02-20 09:48:18 +1300

    extract metadata from zkevm.bin
    This exposes the metadata stored in the zkevm.bin file, such that it can
    be used for cross-checking purposes.  For example, the metadata includes
    the git commit of the enclosing repository when it was built.

--- prover/zkevm/arithmetization/files.go
+++ prover/zkevm/arithmetization/files.go
@@ -25,34 +25,38 @@ const TraceOverflowExitCode = 77
 //go:embed zkevm.bin
 var zkevmStr string
 
-// ReadZkEvmBin parses and compiles a "zkevm.bin" into an air.Schema. f is closed
-// at the end of the function call.
-func ReadZkevmBin(optConfig *mir.OptimisationConfig) (*air.Schema, string, error) {
+// ReadZkevmBin parses and compiles a "zkevm.bin" file into an air.Schema,
+// whilst applying whatever optimisations are requested. Optimisations can
+// impact the size of the generated schema and, consequently, the size of the
+// expanded trace.  For example, certain optimisations eliminate unnecessary
+// columns creates for multiplicative inverses.  However, optimisations do not
+// always improve overall performance, as they can increase the complexity of
+// other constraints.  The DEFAULT_OPTIMISATION_LEVEL is the recommended level
+// to use in general, whilst others are intended for testing purposes (i.e. to
+// try out new optimisations to see whether they help or hinder, etc).
+//
+// This additionally extracts the metadata map from the zkevm.bin file.  This
+// contains information which can be used to cross-check the zkevm.bin file,
+// such as the git commit of the enclosing repository when it was built.
+func ReadZkevmBin(optConfig *mir.OptimisationConfig) (schema *air.Schema, metadata map[string]string, err error) {
 	var (
-		commit string
-		binf   binfile.BinaryFile
-		buf    []byte = []byte(zkevmStr)
+		binf binfile.BinaryFile
+		buf  []byte = []byte(zkevmStr)
 	)
 	// TODO: why is only this one needed??
 	gob.Register(binfile.Attribute(&corset.SourceMap{}))
 	// Parse zkbinary file
-	err := binf.UnmarshalBinary(buf)
+	err = binf.UnmarshalBinary(buf)
 	// Sanity check for errors
 	if err != nil {
-		return nil, "", fmt.Errorf("could not parse the read bytes of the 'zkevm.bin' file into an hir.Schema: %w", err)
+		return nil, nil, fmt.Errorf("could not parse the read bytes of the 'zkevm.bin' file into an hir.Schema: %w", err)
 	}
 	// Extract schema
 	hirSchema := &binf.Schema
-	// Attempt to extract metadata
-	metadata, err := binf.Header.GetMetaData()
-	//
-	if err != nil {
-		return nil, "", fmt.Errorf("error extracting metadata from 'zkevm.bin':", err)
-	}
-	// Extract git commit from metadata
-	commit = metadata["commit"]
+	// Attempt to extract metadata from bin file.
+	metadata, err = binf.Header.GetMetaData()
 	// This performs the corset compilation
-	return hirSchema.LowerToMir().LowerToAir(*optConfig), commit, nil
+	return hirSchema.LowerToMir().LowerToAir(*optConfig), metadata, err
 }
 
 func ReadLtTraces(f io.ReadCloser, sch *air.Schema) (trace.Trace, error) {
