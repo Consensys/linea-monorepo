60feaa5a258e6f49b30fd55d5c1ee133d6cb7c53
AlexandreBelling <alexandrebelling8@gmail.com>
2025-02-25 13:01:19 +0100

    clean(builer): reprecate the builder

--- prover/protocol/wizard/builder.go
+++ prover/protocol/wizard/builder.go
@@ -22,7 +22,7 @@ In particular, Builder provides the utilities to
   - Declare random coins
   - Declare queries
 
-Deprecated: @alex: we should deprecate this and directly embed the "round"
+@alex: we should deprecate this and directly embed the "round"
 tracking capability within the [CompiledIOP] struct. The round-tracking
 mechanism does not allow for a smooth way to decompose the user's protocol into
 sub-protocols that spans on multiple rounds efficiently as a new round will be
