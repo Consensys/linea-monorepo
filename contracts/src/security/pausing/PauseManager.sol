b1ae1a85a1fc266d8dc0e6ec8413053c3668580c
kyzooghost <jeffersonzjtang@gmail.com>
2025-02-28 22:38:38 +1100

    fix unchecked

--- contracts/src/security/pausing/PauseManager.sol
+++ contracts/src/security/pausing/PauseManager.sol
@@ -156,9 +156,7 @@ abstract contract PauseManager is IPauseManager, AccessControlUpgradeable {
       if (block.timestamp < pauseExpiry + COOLDOWN_DURATION) {
         revert PauseUnavailableDueToCooldown(pauseExpiry + COOLDOWN_DURATION);
       }
-      unchecked{
-          pauseExpiry = block.timestamp + PAUSE_DURATION;
-      }
+      unchecked { pauseExpiry = block.timestamp + PAUSE_DURATION; }
     }
     _pauseTypeStatusesBitMap |= 1 << uint256(_pauseType);
     emit Paused(_msgSender(), _pauseType);
