9825f684157c7d5ab5c904b1649b3091b46b2969
David Pearce <david.pearce@consensys.net>
2025-02-28 19:27:46 +1300

    add explicit types for blockdata gas limit (#609)
    This adds explicit types to the constants `GAS_LIMIT_MINIMUM` and `GAS_LIMIT_MAXIMUM` for all three currently supported changes: `Linea`, `Ethereum` and `OldLinea`.  This change is necessary to ensure that, when the `Trace.java` file is generated, the constant always has the same type.  Since these are given the type `i64` here, they will always generate a Java constant of type `BigInteger`.

--- blockdata/processing/gaslimit/linea.lisp
+++ blockdata/processing/gaslimit/linea.lisp
@@ -1,7 +1,7 @@
 (module blockdata)
 
-(defconst GAS_LIMIT_MINIMUM LINEA_GAS_LIMIT_MINIMUM)
-(defconst GAS_LIMIT_MAXIMUM LINEA_GAS_LIMIT_MAXIMUM)
+(defconst (GAS_LIMIT_MINIMUM :i64) LINEA_GAS_LIMIT_MINIMUM)
+(defconst (GAS_LIMIT_MAXIMUM :i64) LINEA_GAS_LIMIT_MAXIMUM)
 
 (defconstraint block-gas-limit-value (:guard IS_GASLIMIT)
     (eq! BLOCK_GAS_LIMIT LINEA_BLOCK_GAS_LIMIT))
