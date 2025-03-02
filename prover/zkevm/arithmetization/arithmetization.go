c872948311f02e9a93a795f34cdb50fecaecb23a
DavePearce <dave01001110@gmail.com>
2025-03-03 11:31:21 +1300

    feat: add binfile / tracefile compatibility check
    This adds a compatibility check between the zkevm.bin file and the lt
    trace file.  The compatibility check extracts the constraints commit
    used to generate the respective asset, and ensures a match.  If not, the
    code panics with an error.  Likewise, if the metadata is missing then
    code will panic with an error.

--- prover/zkevm/arithmetization/arithmetization.go
+++ prover/zkevm/arithmetization/arithmetization.go
@@ -1,13 +1,16 @@
 package arithmetization
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/consensys/go-corset/pkg/air"
 	"github.com/consensys/go-corset/pkg/mir"
+	"github.com/consensys/go-corset/pkg/schema"
 	"github.com/consensys/linea-monorepo/prover/backend/files"
 	"github.com/consensys/linea-monorepo/prover/config"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+	"github.com/sirupsen/logrus"
 )
 
 // Settings specifies the parameters for the arithmetization part of the zkEVM.
@@ -25,13 +28,18 @@ type Settings struct {
 // signature verification.
 type Arithmetization struct {
 	Settings *Settings
-	Schema   *air.Schema
+	// Schema defines the columns, constraints and computations used to expand a
+	// given trace, and to subsequently to check satisfiability.
+	Schema *air.Schema
+	// Metadata embedded in the zkevm.bin file, as needed to check
+	// compatibility.  Guaranteed non-nil.
+	Metadata map[string]string
 }
 
 // NewArithmetization is the function that declares all the columns and the constraints of
 // the zkEVM in the input builder object.
 func NewArithmetization(builder *wizard.Builder, settings Settings) *Arithmetization {
-	schema, _, errS := ReadZkevmBin(settings.OptimisationLevel)
+	schema, metadata, errS := ReadZkevmBin(settings.OptimisationLevel)
 	if errS != nil {
 		panic(errS)
 	}
@@ -41,19 +49,38 @@ func NewArithmetization(builder *wizard.Builder, settings Settings) *Arithmetiza
 	return &Arithmetization{
 		Schema:   schema,
 		Settings: &settings,
+		Metadata: metadata,
 	}
 }
 
 // Assign the arithmetization related columns. Namely, it will open the file
 // specified in the witness object, call corset and assign the prover runtime
 // columns.
+//
+// and then expands that column data according to the given schema.   The
+// expansion process is about filling in computed columns with concrete values,
+// such for determining multiplicative inverses, etc.
 func (a *Arithmetization) Assign(run *wizard.ProverRuntime, traceFile string) {
-
 	traceF := files.MustRead(traceFile)
-	trace, errT := ReadLtTraces(traceF, a.Schema)
+	// Parse trace file and extract raw column data.
+	rawColumns, metadata, errT := ReadLtTraces(traceF, a.Schema)
 	if errT != nil {
 		fmt.Printf("error loading the trace fpath=%q err=%v", traceFile, errT.Error())
 	}
-
-	AssignFromLtTraces(run, a.Schema, trace, a.Settings.Limits)
+	// Compatibility check between zkevm.bin and trace file.
+	if zkevmBinCommit, ok := a.Metadata["commit"]; !ok {
+		panic("missing constraints commit metadata in 'zkevm.bin'")
+	} else if traceFileCommit, ok := metadata["commit"]; !ok {
+		panic("missing constraints commit metadata in '.lt' file")
+	} else if zkevmBinCommit != traceFileCommit {
+		msg := fmt.Sprintf("zkevm.bin (commit %s) incompatible with trace file (commit %s)", zkevmBinCommit, traceFileCommit)
+		panic(msg)
+	}
+	// Perform trace expansion
+	expandedTrace, errs := schema.NewTraceBuilder(a.Schema).Build(rawColumns)
+	if len(errs) > 0 {
+		logrus.Warnf("corset expansion gave the following errors: %v", errors.Join(errs...).Error())
+	}
+	// Passed
+	AssignFromLtTraces(run, a.Schema, expandedTrace, a.Settings.Limits)
 }
