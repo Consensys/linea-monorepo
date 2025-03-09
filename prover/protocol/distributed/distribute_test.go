758de0f773b26cbbae1779262a163e9065da90f5
AlexandreBelling <alexandrebelling8@gmail.com>
2025-03-09 21:09:32 +0000

    testing(distribute): adds a test bench for the distributed prover where we use the actual compilers

--- prover/protocol/distributed/distribute_test.go
+++ prover/protocol/distributed/distribute_test.go
@@ -2,17 +2,28 @@ package experiment
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/rand/v2"
 	"testing"
+	"time"
 
 	"github.com/consensys/linea-monorepo/prover/backend/execution"
 	"github.com/consensys/linea-monorepo/prover/backend/files"
 	"github.com/consensys/linea-monorepo/prover/config"
+	"github.com/consensys/linea-monorepo/prover/crypto/ringsis"
 	"github.com/consensys/linea-monorepo/prover/maths/field"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/cleanup"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/dummy"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/logdata"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/mimc"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/plonkinwizard"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/selfrecursion"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/vortex"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	"github.com/consensys/linea-monorepo/prover/utils"
 	"github.com/consensys/linea-monorepo/prover/zkevm"
+	"github.com/sirupsen/logrus"
 )
 
 // TestDistributeWizard attempts to run and compile the distributed protocol.
@@ -125,6 +136,178 @@ func TestDistributeWizard(t *testing.T) {
 			t.Errorf("verifier failed for segment %v, reason=%v", i, verLPPErr)
 		}
 	}
+}
+
+// TestBenchDistributedWizard runs the distributed wizard will all the compilations
+func TestBenchDistributedWizard(t *testing.T) {
+
+	logrus.SetLevel(logrus.FatalLevel)
+
+	var (
+		rng              = rand.New(utils.NewRandSource(0))
+		sharedRandomness = field.PseudoRand(rng)
+		zkevm            = GetZkEVM()
+		disc             = &StandardModuleDiscoverer{
+			TargetWeight: 1 << 28,
+		}
+
+		// This tests the compilation of the compiled-IOP
+		distWizard = DistributeWizard(zkevm.WizardIOP, disc)
+
+		// Minimal witness size to compile
+		minCompilationSize = 1 << 10
+	)
+
+	// This applies the dummy.Compiler to all parts of the distributed wizard.
+	for i := range distWizard.GLs {
+
+		if cells := logdata.CountCells(distWizard.GLs[i].Wiop); cells.TotalCells() > minCompilationSize {
+			fmt.Printf("[%v] Starting to compile module GL for %v\n", time.Now(), distWizard.ModuleNames[i])
+			CompileSegmentFully(distWizard.GLs[i].Wiop)
+			fmt.Printf("[%v] Done compiling module GL for %v\n", time.Now(), distWizard.ModuleNames[i])
+		}
+
+		if cells := logdata.CountCells(distWizard.LPPs[i].Wiop); cells.TotalCells() > minCompilationSize {
+			fmt.Printf("[%v] Starting to compile module LPP for %v\n", time.Now(), distWizard.ModuleNames[i])
+			CompileSegmentFully(distWizard.LPPs[i].Wiop)
+			fmt.Printf("[%v] Done compiling module LPP for %v\n", time.Now(), distWizard.ModuleNames[i])
+		}
+	}
+
+	var (
+		reqFile      = files.MustRead("/home/ubuntu/beta-v2.1-rc1-trace/16303874-16303874-etv0.2.0-stv2.2.2-getZkProof.json")
+		cfgFilePath  = "/home/ubuntu/zkevm-monorepo/prover/config/config-sepolia-full.toml"
+		req          = &execution.Request{}
+		reqDecodeErr = json.NewDecoder(reqFile).Decode(req)
+		cfg, cfgErr  = config.NewConfigFromFileUnchecked(cfgFilePath)
+	)
+
+	if reqDecodeErr != nil {
+		t.Fatalf("could not read the request file: %v", reqDecodeErr)
+	}
+
+	if cfgErr != nil {
+		t.Fatalf("could not read the config file: err=%v, cfg=%++v", cfgErr, cfg)
+	}
+
+	t.Logf("loaded config: %++v", cfg)
+
+	var (
+		witness     = GetZkevmWitness(req, cfg)
+		runtimeBoot = wizard.RunProver(distWizard.Bootstrapper, zkevm.GetMainProverStep(witness))
+		proof       = runtimeBoot.ExtractProof()
+		verBootErr  = wizard.Verify(distWizard.Bootstrapper, proof)
+	)
+
+	if verBootErr != nil {
+		t.Fatalf("")
+	}
+
+	witnessGLs, witnessLPPs := SegmentRuntime(runtimeBoot, &distWizard)
+
+	for i := range witnessGLs {
+
+		var (
+			witnessGL   = witnessGLs[i]
+			witnessLPP  = witnessLPPs[i]
+			moduleIndex = witnessGLs[i].ModuleIndex
+			moduleName  = witnessGLs[i].ModuleName
+		)
+
+		witnessLPP.InitialFiatShamirState = sharedRandomness
+
+		t.Logf("segment(total)=%v module=%v segment.index=%v", i, witnessGL.ModuleName, witnessGL.ModuleIndex)
+
+		if witnessLPP.ModuleName != moduleName || witnessLPP.ModuleIndex != moduleIndex {
+			t.Fatalf(
+				"GL and LPP are not aligned LPP[name=%v index=%v] GL[name=%v index=%v]",
+				witnessLPPs[i].ModuleName, witnessLPPs[i].ModuleIndex,
+				witnessGLs[i].ModuleName, witnessGLs[i].ModuleIndex,
+			)
+		}
+
+		var (
+			moduleGL  *ModuleGL
+			moduleLPP *ModuleLPP
+		)
+
+		for k := range distWizard.ModuleNames {
+
+			if distWizard.ModuleNames[k] != witnessGLs[i].ModuleName {
+				continue
+			}
+
+			moduleGL = distWizard.GLs[k]
+			moduleLPP = distWizard.LPPs[k]
+		}
+
+		if moduleGL == nil {
+			t.Fatalf("module does not exists")
+		}
+
+		var (
+			proofGL  = wizard.Prove(moduleGL.Wiop, moduleGL.GetMainProverStep(witnessGLs[i]))
+			verGLErr = wizard.Verify(moduleGL.Wiop, proofGL)
+		)
+
+		if verGLErr != nil {
+			t.Errorf("verifier failed for segment %v, reason=%v", i, verGLErr)
+		}
+
+		var (
+			proofLPP  = wizard.Prove(moduleLPP.Wiop, moduleLPP.GetMainProverStep(witnessLPPs[i]))
+			verLPPErr = wizard.Verify(moduleLPP.Wiop, proofLPP)
+		)
+
+		if verLPPErr != nil {
+			t.Errorf("verifier failed for segment %v, reason=%v", i, verLPPErr)
+		}
+	}
+}
+
+// GetSegmentCompilationSuite returns the compilation suite for the segments
+func CompileSegmentFully(comp *wizard.CompiledIOP) {
+
+	sisInstance := ringsis.Params{LogTwoBound: 16, LogTwoDegree: 6}
+
+	wizard.ContinueCompilation(comp,
+		logdata.Log("at-the-beginning-of-the-segment"),
+		mimc.CompileMiMC,
+		plonkinwizard.Compile,
+		compiler.Arcane(1<<10, 1<<18, false),
+		vortex.Compile(
+			2,
+			vortex.ForceNumOpenedColumns(256),
+			vortex.WithSISParams(&sisInstance),
+		),
+
+		selfrecursion.SelfRecurse,
+
+		logdata.Log("post-selfrecursion"),
+		cleanup.CleanUp,
+		mimc.CompileMiMC,
+		compiler.Arcane(1<<10, 1<<16, false),
+		vortex.Compile(
+			8,
+			vortex.ForceNumOpenedColumns(64),
+			vortex.WithSISParams(&sisInstance),
+		),
+
+		selfrecursion.SelfRecurse,
+
+		logdata.Log("post-selfrecursion-final"),
+		cleanup.CleanUp,
+		mimc.CompileMiMC,
+		compiler.Arcane(1<<10, 1<<13, false),
+		vortex.Compile(
+			8,
+			vortex.ForceNumOpenedColumns(64),
+			vortex.WithSISParams(&sisInstance),
+		),
+
+		logdata.Log("final-post-vortex"),
+	)
+
 }
 
 // GetZkevmWitness returns a [zkevm.Witness]
