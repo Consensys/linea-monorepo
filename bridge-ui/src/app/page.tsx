df9627fee027aaa1a1476f30bb4935d634291551
VGau <victorien.gauch@consensys.net>
2025-03-18 10:32:30 +0100

    fix: update usdc banner message

--- bridge-ui/src/app/page.tsx
+++ bridge-ui/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <TopBanner
         text="The Linea Mainnet USDC bridge has been paused on Sunday 16th of March 20:00 UTC for an upgrade and will
-              remain paused until CCTP V2 integration is complete. All pending messages have been automatically claimed.
+              remain paused until CCTP V2 integration is complete. All pending messages were automatically claimed.
               To bridge USDC between Linea and Ethereum, you can use alternative bridge providers.
               Linea Sepolia (Testnet) is currently being upgraded to support CCTP V2."
         href="https://www.circle.com/blog/linea-to-become-the-first-bridged-usdc-standard-blockchain-to-upgrade-to-native-usdc"
