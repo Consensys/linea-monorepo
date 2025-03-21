d5068d0f72edfaf738f86638b97160f830284067
kyzooghost <jeffersonzjtang@gmail.com>
2025-03-21 22:52:42 +1100

    add USDC_SYMBOL

--- bridge-ui/src/utils/index.ts
+++ bridge-ui/src/utils/index.ts
@@ -4,7 +4,7 @@ export { estimateEthGasFee, estimateERC20GasFee } from "./fees";
 export { formatAddress, formatBalance, formatHex, formatTimestamp, safeGetAddress } from "./format";
 export { fetchTransactionsHistory } from "./history";
 export { computeMessageHash, computeMessageStorageSlot, isCctpV2BridgeMessage, isNativeBridgeMessage } from "./message";
-export { isEth, isCctp } from "./tokens";
+export { isEth, isCctp, USDC_SYMBOL } from "./tokens";
 export { isEmptyObject } from "./utils";
 export {
   CCTP_TRANSFER_MAX_FEE_FALLBACK,
