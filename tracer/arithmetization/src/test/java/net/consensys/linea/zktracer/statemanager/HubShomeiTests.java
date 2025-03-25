f8810856d8ce4b423d8f101bb7c8ed946c74ad02
François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
2025-03-25 10:10:49 +0100

    tests: state manager shomei (#1924)
    * tests: state manager shomei
    
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>
    
    * decouple tests with sstore and sload
    
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>
    
    * dead code + typos
    
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>
    
    ---------
    
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

---
+++ tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/statemanager/HubShomeiTests.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright ConsenSys Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package net.consensys.linea.zktracer.statemanager;
+
+import static net.consensys.linea.testing.BytecodeCompiler.newProgram;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import net.consensys.linea.testing.ToyAccount;
+import net.consensys.linea.testing.ToyExecutionEnvironmentV2;
+import net.consensys.linea.testing.ToyTransaction;
+import net.consensys.linea.zktracer.opcode.OpCode;
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
+import org.hyperledger.besu.crypto.KeyPair;
+import org.hyperledger.besu.crypto.SECP256K1;
+import org.hyperledger.besu.datatypes.AccessListEntry;
+import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.ethereum.core.Transaction;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Those tests are intended to produce LT trace to test the HUB <-> Shomei lookups. */
+@Tag("weekly")
+public class HubShomeiTests {
+
+  private static final Bytes32 key1 = Bytes32.repeat((byte) 1);
+  private static final Bytes32 key2 = Bytes32.repeat((byte) 2);
+  private static final Bytes32 value = Bytes32.leftPad(Bytes.fromHexString("0x7a12e"));
+
+  private static final Address DEFAULT =
+      Address.fromHexString("0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef");
+
+  private static final Bytes SSLOAD1 =
+      newProgram().push(key1).op(OpCode.SLOAD).op(OpCode.POP).compile();
+
+  private static final Bytes SSTORE1 =
+      newProgram().push(value).push(key1).op(OpCode.SSTORE).compile();
+
+  /**
+   * In this test we have two transactions. In the first one we prewarm a storage key, we SSTORE or
+   * SLOAD it. In the second transaction we prewarm the same storage key. The aim of this test is to
+   * have the bit FIRST_IN_BLOCK and LAST_IN_BLOCK on prewarming rows, not in execution rows
+   */
+  @ParameterizedTest
+  @MethodSource("opcodeProvider")
+  void sandwichPrewarming(OpCode opcode) {
+
+    final KeyPair keyPair = new SECP256K1().generateKeyPair();
+    final Address senderAddress =
+        Address.extract(Hash.hash(keyPair.getPublicKey().getEncodedBytes()));
+    final ToyAccount senderAccount =
+        ToyAccount.builder().balance(Wei.of(100000000)).address(senderAddress).build();
+
+    final Bytes code =
+        switch (opcode) {
+          case SSTORE -> Bytes.concatenate(SSTORE1);
+          case SLOAD -> Bytes.concatenate(SSLOAD1);
+          default -> throw new IllegalStateException("Unexpected value: " + opcode);
+        };
+
+    final ToyAccount recipientAccount =
+        ToyAccount.builder()
+            .balance(Wei.of(10000))
+            .address(DEFAULT)
+            .code(Bytes.concatenate(code))
+            .build();
+
+    final AccessListEntry accessListEntry =
+        new AccessListEntry(recipientAccount.getAddress(), List.of(key1));
+
+    final Transaction tx1 =
+        ToyTransaction.builder()
+            .sender(senderAccount)
+            .keyPair(keyPair)
+            .gasLimit(1000000L)
+            .gasPrice(Wei.of(10L))
+            .accessList(List.of(accessListEntry))
+            .to(recipientAccount)
+            .build();
+
+    final Transaction tx2 =
+        ToyTransaction.builder()
+            .sender(senderAccount)
+            .nonce(senderAccount.getNonce() + 1)
+            .keyPair(keyPair)
+            .gasLimit(1000000L)
+            .gasPrice(Wei.of(10L))
+            .accessList(List.of(accessListEntry))
+            .payload(newProgram().push(1).push(1).op(OpCode.ADD).compile())
+            .build();
+
+    ToyExecutionEnvironmentV2.builder()
+        .accounts(List.of(senderAccount, recipientAccount))
+        .transactions(List.of(tx1, tx2))
+        .build()
+        .run();
+  }
+
+  /** In this test we prewarm two storage key, but only one will be used during execution */
+  @ParameterizedTest
+  @MethodSource("opcodeProvider")
+  void uselessPrewarming(OpCode opcode) {
+
+    final KeyPair keyPair = new SECP256K1().generateKeyPair();
+    final Address senderAddress =
+        Address.extract(Hash.hash(keyPair.getPublicKey().getEncodedBytes()));
+    final ToyAccount senderAccount =
+        ToyAccount.builder().balance(Wei.of(100000000)).address(senderAddress).build();
+
+    final Bytes code =
+        switch (opcode) {
+          case SSTORE -> Bytes.concatenate(SSTORE1);
+          case SLOAD -> Bytes.concatenate(SSLOAD1);
+          default -> throw new IllegalStateException("Unexpected value: " + opcode);
+        };
+
+    final ToyAccount recipientAccount =
+        ToyAccount.builder().balance(Wei.of(10000)).address(DEFAULT).code(code).build();
+
+    final AccessListEntry accessListEntry =
+        new AccessListEntry(recipientAccount.getAddress(), List.of(key1, key2));
+
+    final Transaction tx =
+        ToyTransaction.builder()
+            .sender(senderAccount)
+            .keyPair(keyPair)
+            .gasLimit(1000000L)
+            .gasPrice(Wei.of(10L))
+            .accessList(List.of(accessListEntry))
+            .to(recipientAccount)
+            .build();
+
+    ToyExecutionEnvironmentV2.builder()
+        .accounts(List.of(senderAccount, recipientAccount))
+        .transaction(tx)
+        .build()
+        .run();
+  }
+
+  private static Stream<Arguments> opcodeProvider() {
+    List<Arguments> arguments = new ArrayList<>();
+    arguments.add(Arguments.of(OpCode.SSTORE, OpCode.SLOAD));
+    return arguments.stream();
+  }
+}
