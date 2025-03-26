47b4dac943c044f3d7f998ab4bf4e6ef41315638
kyzooghost <jeffersonzjtang@gmail.com>
2025-03-26 18:42:14 +1100

    try

--- bridge-ui/test/advancedFixtures.ts
+++ bridge-ui/test/advancedFixtures.ts
@@ -34,11 +34,11 @@ export const test = metaMaskFixtures(setup).extend<{
       return nativeBridgeBtn;
     });
   },
-  connectMetamaskToDapp: async ({ page, metamask }, use) => {
+  connectMetamaskToDapp: async ({ page, metamask, context }, use) => {
     await use(async () => {
       // https://playwright.dev/docs/network#modify-requests
       // Circumvent cors error in CI workflow
-      await page.route('https://app.dynamicauth.com/api/**', async route => {
+      await context.route('https://app.dynamicauth.com/api/**', async route => {
         await route.continue({ headers: {
           ...route.request().headers(), 
           'Origin': 'http://localhost:3000',
