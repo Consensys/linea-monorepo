60101122fb9957dc73f5f0054c7b931fc0d7d008
Victorien Gauch <85494462+VGau@users.noreply.github.com>
2025-03-26 16:43:18 +0100

    Fix: remove native tokens, fix receive amount value and bump next js version (#816)
    * fix: remove native tokens that don't have L1 address in the token list + fix receive amount
    
    * fix: bump next js version

--- bridge-ui/src/components/bridge/claiming/received-amount/index.tsx
+++ bridge-ui/src/components/bridge/claiming/received-amount/index.tsx
@@ -4,12 +4,21 @@ import { useTokenPrices } from "@/hooks";
 import { useConfigStore, useChainStore, useFormStore } from "@/stores";
 import { formatBalance } from "@/utils";
 import { ETH_SYMBOL } from "@/constants";
+import { ChainLayer } from "@/types";
+import { useMemo } from "react";
 
-function formatReceivedAmount(amount: bigint, tokenSymbol: string, bridgingFees: bigint) {
-  if (tokenSymbol === ETH_SYMBOL) {
-    return amount - bridgingFees;
+function formatReceivedAmount(
+  amount: bigint,
+  tokenSymbol: string,
+  bridgingFees: bigint,
+  minimumFees: bigint,
+  fromChainLayer: ChainLayer,
+) {
+  if (tokenSymbol !== ETH_SYMBOL) {
+    return amount;
   }
-  return amount;
+
+  return fromChainLayer === ChainLayer.L1 ? amount - bridgingFees : amount - minimumFees;
 }
 
 export default function ReceivedAmount() {
@@ -18,22 +27,28 @@ export default function ReceivedAmount() {
   const amount = useFormStore((state) => state.amount);
   const token = useFormStore((state) => state.token);
   const bridgingFees = useFormStore((state) => state.bridgingFees);
+  const minimumFees = useFormStore((state) => state.minimumFees);
 
   const { data: tokenPrices } = useTokenPrices([token[fromChain.layer]], fromChain.id);
+
+  const receivedAmount = useMemo(
+    () =>
+      formatUnits(
+        formatReceivedAmount(amount || 0n, token.symbol, bridgingFees, minimumFees, fromChain.layer),
+        token.decimals,
+      ),
+    [amount, token.symbol, bridgingFees, minimumFees, fromChain.layer, token.decimals],
+  );
 
   return (
     <div className={styles.value}>
       <p className={styles.crypto}>
-        {formatBalance(formatUnits(formatReceivedAmount(amount || 0n, token.symbol, bridgingFees), token.decimals), 6)}{" "}
-        {token.symbol}
+        {formatBalance(receivedAmount, 6)} {token.symbol}
       </p>
       {tokenPrices?.[token[fromChain.layer].toLowerCase()] &&
         tokenPrices?.[token[fromChain.layer].toLowerCase()] > 0 && (
           <p className={styles.amount}>
-            {(
-              Number(formatUnits(formatReceivedAmount(amount || 0n, token.symbol, bridgingFees), token.decimals)) *
-              tokenPrices?.[token[fromChain.layer].toLowerCase()]
-            ).toLocaleString("en-US", {
+            {(Number(receivedAmount) * tokenPrices?.[token[fromChain.layer].toLowerCase()]).toLocaleString("en-US", {
               style: "currency",
               currency: currency.label,
               maximumFractionDigits: 8,
