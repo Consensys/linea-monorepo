d166dd4b9fa42b3ce11fa51ad2ab58da5149f11a
AlexandreBelling <alexandrebelling8@gmail.com>
2025-03-28 14:39:06 +0100

    reorg(counterPrecomputed): creates a specific type of precomputed columns that caches itself

---
+++ prover/protocol/dedicated/counter_precomputed.go
@@ -0,0 +1,32 @@
+package dedicated
+
+import (
+	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
+	"github.com/consensys/linea-monorepo/prover/maths/field"
+	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+	"github.com/consensys/linea-monorepo/prover/utils"
+)
+
+// CounterPrecomputed generates a precomputed column counting "from" ... "to",
+// (excluding "to"). If the columns is already declared, the column is reused.
+// The function expects a "power of two"-sized range, so that "to - from = 2**N"
+func CounterPrecomputed(comp *wizard.CompiledIOP, from, to int) ifaces.Column {
+
+	name := ifaces.ColIDf("COUNTER_%v_%v_%v", from, to, comp.SelfRecursionCount)
+
+	if comp.Columns.Exists(name) {
+		return comp.Columns.GetHandle(name)
+	}
+
+	if from >= to || !utils.IsPowerOfTwo(to-from) {
+		utils.Panic("invalid range from=%v to=%v", from, to)
+	}
+
+	value := make([]field.Element, from-to)
+	for i := from; i < to; i++ {
+		value[i] = field.NewElement(uint64(i))
+	}
+
+	return comp.InsertPrecomputed(name, smartvectors.NewRegular(value))
+}
