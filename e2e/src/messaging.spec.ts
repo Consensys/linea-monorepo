a6ae129b6d8df7fd05b99a605bd3371274bd9a96
kyzooghost <jeffersonzjtang@gmail.com>
2025-04-02 00:38:10 +1100

    start e2e case

--- e2e/src/messaging.spec.ts
+++ e2e/src/messaging.spec.ts
@@ -112,6 +112,42 @@ async function sendL2ToL1Message(
   return { tx, receipt };
 }
 
+async function invokeL1GriefClaimTxContract(
+  logger: Logger,
+  {
+    l1Account,
+  }: {
+    l1Account: Wallet;
+  },
+) {
+  const griefClaimMessageContract = config.getGriefClaimMessageContract(l1Account);
+  const l1Provider = config.getL1Provider();
+  const { maxPriorityFeePerGas, maxFeePerGas } = await l1Provider.getFeeData();
+
+  logger.debug(`Fetched fee data. maxPriorityFeePerGas=${maxPriorityFeePerGas} maxFeePerGas=${maxFeePerGas}`);
+
+  const nonce = await l1Provider.getTransactionCount(l1Account.address, "pending");
+  logger.debug(`Fetched nonce. nonce=${nonce} account=${l1Account.address}`);
+
+  const tx = await griefClaimMessageContract.spam({
+    nonce,
+    maxPriorityFeePerGas,
+    maxFeePerGas,
+  });
+
+  logger.debug(`spam transaction sent. transactionHash=${tx.hash}`);
+
+  let receipt = await tx.wait();
+  while (!receipt) {
+    logger.debug(`Waiting for transaction to be mined... transactionHash=${tx.hash}`);
+    receipt = await tx.wait();
+  }
+
+  logger.debug(`Transaction mined. transactionHash=${tx.hash} status=${receipt.status}`);
+
+  return { tx, receipt };
+}
+
 const l1AccountManager = config.getL1AccountManager();
 const l2AccountManager = config.getL2AccountManager();
 
@@ -225,26 +261,29 @@ describe.only("Messaging test suite", () => {
   }, 150_000);
 
   it.only("Griefing claim message on L2", async () => {
-    // const [l1Account, l2Account] = await Promise.all([
-    //   l1AccountManager.generateAccount(),
-    //   l2AccountManager.generateAccount(),
-    // ]);
-    // const lineaRollup = config.getLineaRollupContract();
-    // const { tx, receipt } = await sendL2ToL1Message(logger, { l1Account, l2Account, withCalldata: false });
-    // const [messageSentEvent] = receipt.logs.filter((log) => log.topics[0] === MESSAGE_SENT_EVENT_SIGNATURE);
-    // const messageHash = messageSentEvent.topics[3];
-    // logger.debug(`L2 message sent. messageHash=${messageHash} transaction=${JSON.stringify(tx)}`);
-    // logger.debug(`Waiting for L2MessagingBlockAnchored event... blockNumber=${messageSentEvent.blockNumber}`);
-    // await waitForEvents(lineaRollup, lineaRollup.filters.L2MessagingBlockAnchored(messageSentEvent.blockNumber), 1_000);
-    // logger.debug(`Waiting for MessageClaimed event on L1. messageHash=${messageHash}`);
-    // const [messageClaimedEvent] = await waitForEvents(
-    //   lineaRollup,
-    //   lineaRollup.filters.MessageClaimed(messageHash),
-    //   1_000,
-    // );
-    // expect(messageClaimedEvent).toBeDefined();
-    // logger.debug(
-    //   `Message claimed on L1. messageHash=${messageClaimedEvent.args._messageHash} transactionHash=${messageClaimedEvent.transactionHash}`,
-    // );
+    const [l1Account, l2Account] = await Promise.all([
+      l1AccountManager.generateAccount(),
+      l2AccountManager.generateAccount(),
+    ]);
+
+    console.log("l2Account:", l2Account);
+
+    const { tx, receipt } = await invokeL1GriefClaimTxContract(logger, { l1Account });
+
+    const [messageSentEvent] = receipt.logs.filter((log) => log.topics[0] === MESSAGE_SENT_EVENT_SIGNATURE);
+    const messageHash = messageSentEvent.topics[3];
+    logger.debug(`L1 message sent. messageHash=${messageHash} transaction=${JSON.stringify(tx)}`);
+
+    logger.debug(`Waiting for MessageClaimed event on L2. messageHash=${messageHash}`);
+    const l2MessageService = config.getL2MessageServiceContract();
+    const [messageClaimedEvent] = await waitForEvents(
+      l2MessageService,
+      l2MessageService.filters.MessageClaimed(messageHash),
+    );
+
+    expect(messageClaimedEvent).toBeDefined();
+    logger.debug(
+      `Message claimed on L2. messageHash=${messageClaimedEvent.args._messageHash} transactionHash=${messageClaimedEvent.transactionHash}`,
+    );
   }, 150_000);
 });
