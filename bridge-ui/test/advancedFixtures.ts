586da42639525887465eee646d4de92049fa7c35
kyzooghost <jeffersonzjtang@gmail.com>
2025-04-09 23:39:16 +1000

    rewrite

--- bridge-ui/test/advancedFixtures.ts
+++ bridge-ui/test/advancedFixtures.ts
@@ -174,8 +174,8 @@ export const test = metaMaskFixtures(setup).extend<{
       // await notificationPage.waitForLoadState("domcontentloaded", { timeout: PAGE_TIMEOUT });
       // await notificationPage.waitForLoadState("networkidle", { timeout: PAGE_TIMEOUT });
       await metamask.page.reload();
-      // await metamask.page.waitForLoadState("domcontentloaded", { timeout: PAGE_TIMEOUT });
-      // await metamask.page.waitForLoadState("networkidle", { timeout: PAGE_TIMEOUT });
+      await metamask.page.waitForLoadState("domcontentloaded", { timeout: PAGE_TIMEOUT });
+      await metamask.page.waitForLoadState("networkidle", { timeout: PAGE_TIMEOUT });
       const nextBtn = metamask.page.getByRole("button", { name: "Next", exact: true });
       await expect(nextBtn).toBeVisible();
       await expect(nextBtn).toBeEnabled();
