ef0e9cd454fea78b4a15bfaaecbc4f82184eb96a
srinathln7 <srinath.nandakumar@consensys.net>
2025-04-09 13:11:39 +0000

    vortex compiler tests pass

--- prover/protocol/compiler/vortex/verifier.go
+++ prover/protocol/compiler/vortex/verifier.go
@@ -212,6 +212,15 @@ func (ctx *Ctx) explicitPublicEvaluation(vr wizard.Runtime) error {
 	return nil
 }
 
+type shadowRowProverAction struct {
+	name ifaces.ColID
+	size int
+}
+
+func (a *shadowRowProverAction) Run(run *wizard.ProverRuntime) {
+	run.AssignColumn(a.name, smartvectors.NewConstant(field.Zero(), a.size))
+}
+
 // A shadow row is a row filled with zeroes that we **may** add at the end of
 // the rounds commitment. Its purpose is to ensure the number of "SIS limbs" in
 // a row divides the degree of the ring-SIS instance.
@@ -220,8 +229,13 @@ func autoAssignedShadowRow(comp *wizard.CompiledIOP, size, round, id int) ifaces
 	name := ifaces.ColIDf("VORTEX_%v_SHADOW_ROUND_%v_ID_%v", comp.SelfRecursionCount, round, id)
 	col := comp.InsertCommit(round, name, size)
 
-	comp.SubProvers.AppendToInner(round, func(assi *wizard.ProverRuntime) {
-		assi.AssignColumn(name, smartvectors.NewConstant(field.Zero(), size))
+	// comp.SubProvers.AppendToInner(round, func(assi *wizard.ProverRuntime) {
+	// 	assi.AssignColumn(name, smartvectors.NewConstant(field.Zero(), size))
+	// })
+
+	comp.RegisterProverAction(round, &shadowRowProverAction{
+		name: name,
+		size: size,
 	})
 
 	return col
