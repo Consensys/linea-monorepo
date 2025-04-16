3a85448a8f19f08f18b6cb872b23f501ed0ca2ca
kyzooghost <jeffersonzjtang@gmail.com>
2025-04-16 21:44:19 +1000

    did boilerplate for migration test

--- contracts/test/hardhat/rollup/helpers/deploy.ts
+++ contracts/test/hardhat/rollup/helpers/deploy.ts
@@ -36,6 +36,13 @@ export async function deployPlonkVerifierMainnetFull(): Promise<string> {
   return await verifier.getAddress();
 }
 
+export async function deployPlonkVerifierForMultiTypeDataAggregation(): Promise<string> {
+  const plonkVerifier = await ethers.getContractFactory("PlonkVerifierForMultiTypeDataAggregation");
+  const verifier = await plonkVerifier.deploy();
+  await verifier.waitForDeployment();
+  return await verifier.getAddress();
+}
+
 export async function deployCallForwardingProxy(target: string): Promise<CallForwardingProxy> {
   const callForwardingProxyFactory = await ethers.getContractFactory("CallForwardingProxy");
   const callForwardingProxy = await callForwardingProxyFactory.deploy(target);
