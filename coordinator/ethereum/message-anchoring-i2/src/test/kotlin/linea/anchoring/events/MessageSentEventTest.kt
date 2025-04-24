98e31c29e30f2da03e5dbfd937bdd748bd491a7b
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-04-24 17:22:01 +0100

    coordinator: fix MessageSentEvent.kt serialization

--- coordinator/ethereum/message-anchoring-i2/src/test/kotlin/linea/anchoring/events/MessageSentEventTest.kt
+++ coordinator/ethereum/message-anchoring-i2/src/test/kotlin/linea/anchoring/events/MessageSentEventTest.kt
@@ -3,35 +3,61 @@ package linea.anchoring.events
 import linea.domain.EthLog
 import linea.domain.EthLogEvent
 import linea.kotlin.decodeHex
+import linea.kotlin.toBigInteger
 import linea.kotlin.toULongFromHex
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.math.BigInteger
 
 class MessageSentEventTest {
 
   @Test
-  fun `fromEthLog should map EthLog to MessageSentEvent correctly`() {
+  fun `fromEthLog should map EthLog to MessageSentEvent correctly 2`() {
+    /**
+     * curl -s -H 'content-type:application/json' --data '{"jsonrpc":"2.0","id":"53","method":"eth_getLogs","params":[{"address":["0xB218f8A4Bc926cF1cA7b3423c154a0D627Bdb7E5"], "topics":["0xe856c2b8bd4eb0027ce32eeaf595c21b0b6b4644b326e5b7bd80a1cf8db72e6c",null,null,"0x6011E7F01AAE0E2CD2650CBE229A330F93821D5ED8A2E8830D1E64BA3C76CC3F"], "fromBlock":"earliest","toBlock":"latest"}]}' $URL_SEPOLIA  | jq '.result'
+     * [
+     *   {
+     *     "removed": false,
+     *     "logIndex": "0x8c",
+     *     "transactionIndex": "0x36",
+     *     "transactionHash": "0x7fd853e580f967cf9004a019bcce26b8d4d2b880ef1106f5af29e2e23695e9ec",
+     *     "blockHash": "0x8c08c5c7f1d8a256a392f5d261b9e4ba39337903646ba009f16a0b2029447c20",
+     *     "blockNumber": "0x7cec04",
+     *     "address": "0xb218f8a4bc926cf1ca7b3423c154a0d627bdb7e5",
+     *     "data": "0x000000000000000000000000000000000000000000000000002386f26fc100000000000000000000000000000000000000000000000000000de0b6b3a764000000000000000000000000000000000000000000000000000000000000000140430000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000001d54686973206973206a75737420666f7220796f7520676f6f6420736972000000",
+     *     "topics": [
+     *       "0xe856c2b8bd4eb0027ce32eeaf595c21b0b6b4644b326e5b7bd80a1cf8db72e6c",
+     *       "0x00000000000000000000000017e764ba16c95815ca06fb5d174f08d842e340df",
+     *       "0x0000000000000000000000007a98052d4be677df72ede5a3f2829c893d10388d",
+     *       "0x6011e7f01aae0e2cd2650cbe229a330f93821d5ed8a2e8830d1e64ba3c76cc3f"
+     *     ]
+     *   }
+     * ]
+     */
+    val callData = "0000000000000000000000000000000000000000000000000000000000000080" +
+      "000000000000000000000000000000000000000000000000000000000000001d" +
+      "54686973206973206a75737420666f7220796f7520676f6f6420736972000000"
     val ethLog = EthLog(
       removed = false,
       logIndex = "0x1".toULongFromHex(),
       transactionIndex = "0x0".toULongFromHex(),
-      transactionHash = "0x2d408675b46835a04ba632ac437ca9b9ca41b834609b7453630fe594ba658b4c".decodeHex(),
-      blockHash = "0x4d63489ac2faee706cca0f078f23973facc42a87dc75cfdf6fae5ac2d8c9b243".decodeHex(),
-      blockNumber = "0x1109669".toULongFromHex(),
-      address = "0x508ca82df566dcd1b0de8296e70a96332cd644ec".decodeHex(),
+      transactionHash = "0x7fd853e580f967cf9004a019bcce26b8d4d2b880ef1106f5af29e2e23695e9ec".decodeHex(),
+      blockHash = "0x8c08c5c7f1d8a256a392f5d261b9e4ba39337903646ba009f16a0b2029447c20".decodeHex(),
+      blockNumber = "0x7cec04".toULongFromHex(),
+      address = "0xb218f8a4bc926cf1ca7b3423c154a0d627bdb7e5".decodeHex(),
       data = (
         "0x" +
-          "00000000000000000000000000000000000000000000000000000000000003e8" + // fee
-          "00000000000000000000000000000000000000000000000000000000000007d0" + // value
-          "0000000000000000000000000000000000000000000000000000000000000000" + // padding
-          "0000000000000000000000000000000000000000000000000000000000002710" + // nonce
-          "deadbeef"
-        ).decodeHex(), // calldata
+          "000000000000000000000000000000000000000000000000002386f26fc10000" + // fee
+          "0000000000000000000000000000000000000000000000000de0b6b3a7640000" + // value
+          "0000000000000000000000000000000000000000000000000000000000014043" + // messageNumber
+          // calldata
+          callData
+        ).decodeHex(),
       topics = listOf(
         "0xe856c2b8bd4eb0027ce32eeaf595c21b0b6b4644b326e5b7bd80a1cf8db72e6c".decodeHex(),
-        "0x0000000000000000000000001234567890abcdef1234567890abcdef12345678".decodeHex(), // from
-        "0x000000000000000000000000abcdef1234567890abcdef1234567890abcdef12".decodeHex(), // to
-        "0xabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcd".decodeHex() // messageHash
+        "0x00000000000000000000000017e764ba16c95815ca06fb5d174f08d842e340df".decodeHex(), // from
+        "0x0000000000000000000000007a98052d4be677df72ede5a3f2829c893d10388d".decodeHex(), // to
+        "0x6011e7f01aae0e2cd2650cbe229a330f93821d5ed8a2e8830d1e64ba3c76cc3f".decodeHex() // messageHash
       )
     )
 
@@ -40,13 +66,13 @@ class MessageSentEventTest {
 
     // Then
     val expectedEvent = MessageSentEvent(
-      messageNumber = 10000uL,
-      from = "0x1234567890abcdef1234567890abcdef12345678".decodeHex(),
-      to = "0xabcdef1234567890abcdef1234567890abcdef12".decodeHex(),
-      fee = 1000uL,
-      value = 2000uL,
-      calldata = "deadbeef".decodeHex(),
-      messageHash = "0xabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcdefabcd".decodeHex()
+      messageNumber = 81987UL,
+      from = "0x17e764ba16c95815ca06fb5d174f08d842e340df".decodeHex(),
+      to = "0x7a98052d4be677df72ede5a3f2829c893d10388d".decodeHex(),
+      fee = BigInteger("2386f26fc10000", 16),
+      value = BigInteger("de0b6b3a7640000", 16),
+      calldata = "0x$callData".decodeHex(),
+      messageHash = "0x6011e7f01aae0e2cd2650cbe229a330f93821d5ed8a2e8830d1e64ba3c76cc3f".decodeHex()
     )
     val expectedEthLogEvent = EthLogEvent(
       event = expectedEvent,
@@ -60,8 +86,8 @@ class MessageSentEventTest {
     messageNumber = 1uL,
     from = "0x000000000000000000000000000000000000000a".decodeHex(),
     to = "0x000000000000000000000000000000000000000b".decodeHex(),
-    fee = 0uL,
-    value = 0uL,
+    fee = 0uL.toBigInteger(),
+    value = 0uL.toBigInteger(),
     calldata = ByteArray(0),
     messageHash = "0x0000000000000000000000000000000000000000".decodeHex()
   )
