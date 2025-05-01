780cf037bb4e761ffdd33ebe82869b12ec4d0256
Bogdan Ursu <bogdanursuoffice@gmail.com>
2025-05-02 01:51:25 +0200

    Update from_horner.go

--- prover/protocol/accessors/from_horner.go
+++ prover/protocol/accessors/from_horner.go
@@ -3,20 +3,19 @@ package accessors
 import (
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/linea-monorepo/prover/maths/field"
+	"github.com/consensys/linea-monorepo/prover/maths/field/fext"
+	"github.com/consensys/linea-monorepo/prover/maths/field/fext/gnarkfext"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/protocol/query"
 	"github.com/consensys/linea-monorepo/prover/symbolic"
 )
 
+var _ ifaces.Accessor = &FromHornerAccessorFinalValue{}
+
 // FromHornerAccessorFinalValue implements [ifaces.Accessor] and accesses the
 // final value of a [Horner] computation.
 type FromHornerAccessorFinalValue struct {
 	Q *query.Horner
-}
-
-func (l *FromHornerAccessorFinalValue) IsBase() bool {
-	//TODO implement me
-	panic("implement me")
 }
 
 // NewFromHornerAccessorFinalValue returns a new [FromHornerAccessorFinalValue].
@@ -54,4 +53,29 @@ func (l *FromHornerAccessorFinalValue) AsVariable() *symbolic.Expression {
 // Round implements the [ifaces.Accessor] interface
 func (l *FromHornerAccessorFinalValue) Round() int {
 	return l.Q.Round
+}
+
+func (l *FromHornerAccessorFinalValue) GetValBase(run ifaces.Runtime) (field.Element, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *FromHornerAccessorFinalValue) GetValExt(run ifaces.Runtime) fext.Element {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *FromHornerAccessorFinalValue) GetFrontendVariableBase(api frontend.API, c ifaces.GnarkRuntime) (frontend.Variable, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *FromHornerAccessorFinalValue) GetFrontendVariableExt(api frontend.API, c ifaces.GnarkRuntime) gnarkfext.Variable {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *FromHornerAccessorFinalValue) IsBase() bool {
+	//TODO implement me
+	panic("implement me")
 }
