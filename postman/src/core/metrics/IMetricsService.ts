19880cd8a96d0b94b890b034324c89407958ced9
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2025-05-05 20:26:37 +1000

    [Feat] Postman Sponsorship Metrics (#902)
    * added claimTxGasUsed attribute
    
    * add test code for message mapper
    
    * add two more table entries
    
    * added isForSponsorship db entity change
    
    * remove isForSponsorship
    
    * working postman tests
    
    * working tests for add isForSponsorship attribute
    
    * fix positioning
    
    * refactor MessageClaimingPersister
    
    * remove bad comment
    
    * added migration:create command
    
    * generate migration docs
    
    * added new migration for sponsorship
    
    * typo
    
    * passing tests for MessageMetricsService & MetricsService refactor
    
    * remove console logs
    
    * added subscriber code for isForSponsorship
    
    * working e2e and local test for sponsorship messages metrics
    
    * add comment
    
    * added SponsorshipFeesWei and SponsorshipFeesGwei metrics
    
    * did sponsorshipfeesubscriber
    
    * register sponsorshipfeessubscriber
    
    * string fixes
    
    * remove claimtxgasused and claimtxgasprice db columns, refactor metricsservice design pattern
    
    * SponsorshipMetricsUpdater.ts
    
    * change isForSponsorship semantics
    
    * remove isForSponsorship from DB
    
    * Revert "remove isForSponsorship from DB"
    
    This reverts commit 79f4887e3492a43443bb5dba78f66f78606241a5.
    
    * remove isForSponsorship public function
    
    * remove isForSponsorship label from message metric
    
    * add getSponsoredMessagesTotal method
    
    * added sponsorshipmetricsupdater to messageclaimingpersister
    
    * amend base postman image for e2e test
    
    * add promql queries in comments
    
    * empty
    
    * remove === true

--- postman/src/core/metrics/IMetricsService.ts
+++ postman/src/core/metrics/IMetricsService.ts
@@ -2,6 +2,23 @@ import { Counter, Gauge, Registry } from "prom-client";
 
 export enum LineaPostmanMetrics {
   Messages = "linea_postman_messages",
+  // Example PromQL query for hourly rate of sponsored messages 'rate(linea_postman_sponsored_messages_total{direction="L1_TO_L2",app="postman"}[60m]) * 3600'
+  SponsoredMessagesTotal = "linea_postman_sponsored_messages_total",
+  /**
+   * Tx fees in wei paid by Postman for sponsored message claims
+   *
+   * Workaround for prom-client metrics not supporting bigint type
+   * - We split txFee into GWEI and WEI components, and accumulate in two separate metrics
+   * - Note JS limitation of Number.MAX_SAFE_INTEGER = 9007199254740991
+   * - Given 150,000 sponsored messages a year, we should not reach overflow in <60 years
+   *
+   * We do not use separate labels for 'wei' and 'gwei' denominations, because metrics sharing the label should be aggregatable
+   * - I.e. metric (direction: A, denomination: wei) cannot be aggregated with metric (direction: A, denomination: gwei) because they represent different units
+   *
+   * Example PromQL query to get hourly rate of ETH consumed for sponsoring messages - 'rate(linea_postman_sponsorship_fees_gwei_total{direction="L1_TO_L2", app="postman"}[60m]) * 3600 / 1e9 + rate(linea_postman_sponsorship_fees_wei_total{direction="L1_TO_L2", app="postman"}[60m]) * 3600 / 1e18
+   */
+  SponsorshipFeesWei = "linea_postman_sponsorship_fees_wei_total", // Represent up to ~9_007_199 GWEI
+  SponsorshipFeesGwei = "linea_postman_sponsorship_fees_gwei_total", // Represent up to ~9_007_199 ETH
 }
 
 export interface IMetricsService {
