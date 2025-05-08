7e60fe2f4a9bf48334ad5eab635a8a9339475219
kyzooghost <jeffersonzjtang@gmail.com>
2025-05-09 00:10:34 +1000

    add csp header

--- bridge-ui/next.config.mjs
+++ bridge-ui/next.config.mjs
@@ -43,6 +43,37 @@ const nextConfig = {
 
     return config;
   },
+  async headers() {
+    /**
+     * Mitigate XSS and data injection attack.
+     * Restrict what content the browser is allowed to load.
+     * - default-src 'self' -> Default policy to only allow resources from same origin
+     */
+    const cspHeader = `
+      default-src 'self';
+      connect-src 'self' https:;
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://usabilla.com;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data:;
+      font-src 'self' data:;
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      upgrade-insecure-requests;
+    `;
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
