a9bb52578c93dd8d564a09376d75a095b924095c
Tabaie <arya.pourtabatabaie@gmail.com>
2025-05-07 20:41:00 -0500

    blob proof requests

--- prover/integration/doubledict/doubledict.go
+++ prover/integration/doubledict/doubledict.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"os"
 
+	"github.com/consensys/linea-monorepo/prover/backend/blobsubmission"
 	"github.com/consensys/linea-monorepo/prover/cmd/prover/cmd"
+	blob "github.com/consensys/linea-monorepo/prover/lib/compressor/blob/v1"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 func main() {
-	setup()
+	makeBlobProofRequests()
+	//setup()
 	//prove()
 }
 
@@ -31,8 +40,79 @@ func prove() {
 	}))
 }
 
+const (
+	oldDict  = "../../lib/compressor/compressor_dict.bin"
+	newDict  = "../../lib/compressor/dict/25-04-21.bin"
+	hexOne   = "0x0000000000000000000000000000000000000000000000000000000000000001"
+	hexTwo   = "0x0000000000000000000000000000000000000000000000000000000000000002"
+	hexThree = "0x0000000000000000000000000000000000000000000000000000000000000003"
+)
+
+func makeBlobProofRequests() {
+	req := blobProofRequest(hexOne, hexTwo, hexThree, oldDict)
+	saveJson(req, "getZkBlobCompressionProof-olddict.json")
+
+	req = blobProofRequest(req.FinalStateRootHash, hexThree, req.ExpectedShnarf, newDict)
+	saveJson(req, "getZkBlobCompressionProof-newdict.json")
+}
+
+func Close(f interface{ Close() error }) {
+	assertNoError(f.Close())
+}
+
+func saveJson(o any, path string) {
+	f, err := os.Create(path)
+	assertNoError(err)
+	defer Close(f)
+
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", "  ")
+	assertNoError(enc.Encode(o))
+}
+
+func blobProofRequest(parentStateRootHash, stateRootHash, prevShnarf, dictPath string) *blobsubmission.Response {
+	res, err := blobsubmission.CraftResponse(&blobsubmission.Request{
+		Eip4844Enabled:      true,
+		CompressedData:      base64.StdEncoding.EncodeToString(createBlob(dictPath)),
+		DataParentHash:      "0x1234567890123456789012345678901234567890123456789012345678901234",
+		ConflationOrder:     blobsubmission.ConflationOrder{},
+		ParentStateRootHash: parentStateRootHash,
+		FinalStateRootHash:  stateRootHash,
+		PrevShnarf:          prevShnarf,
+	})
+	assertNoError(err)
+	return res
+}
+
 func assertNoError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func createBlob(dictPath string) []byte {
+	const blocksFilePath = "18528963-18529608.blocks"
+	bm, err := blob.NewBlobMaker(blob.MaxUsableBytes, dictPath)
+	assertNoError(err)
+	in, err := os.ReadFile(blocksFilePath)
+	assertNoError(err)
+
+	var (
+		b  ethtypes.Block
+		bb bytes.Buffer
+	)
+
+	for {
+		bb.Reset()
+		assertNoError(rlp.Decode(bytes.NewReader(in), &b))
+		assertNoError(rlp.Encode(&bb, &b))
+		in = in[bb.Len():]
+
+		ok, err := bm.Write(bb.Bytes(), false)
+		assertNoError(err)
+		if !ok { // blob is full
+			break
+		}
+	}
+	return bm.Bytes()
 }
