d9395bffbd16431b4cb92d7e2650410316ea0f2f
AlexandreBelling <alexandrebelling8@gmail.com>
2025-05-09 16:51:46 +0200

    feat(exit): adds a toggle mechanism for the exiting strategy

--- prover/utils/exit/exit.go
+++ prover/utils/exit/exit.go
@@ -2,15 +2,36 @@ package exit
 
 import "os"
 
+var (
+	activateExitOnIssue bool
+)
+
+// ActivateExitOnIssue tells the program to actually exit when [OnLimitOverflow]
+// or [OnSatisfiedConstraints] are called. This has to be manually called at the
+// beginning of the program if we want the behavior to take place. This is
+// to avoid having it running in the tests.
+func ActivateExitOnIssue() {
+	activateExitOnIssue = true
+}
+
 const (
 	limitOverflowExitCode          = 77
 	unsatisfiedConstraintsExitCode = 78
 )
 
+// This function will exit the program with the exit code [limitOverflowExitCode]
+// but only if the activateExitOnIssue flag is set to true. Otherwise, it will
+// just panic.
 func OnLimitOverflow() {
+	if !activateExitOnIssue {
+		panic("limit overflow")
+	}
 	os.Exit(limitOverflowExitCode)
 }
 
-func OnSatisfiedConstraints() {
+func OnUnsatisfiedConstraints() {
+	if !activateExitOnIssue {
+		panic("unsatisfied constraints")
+	}
 	os.Exit(unsatisfiedConstraintsExitCode)
 }
