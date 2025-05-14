c9b5c78e7f6ebf6d178882f61ccb359a7a5bc86e
srinathln7 <srinath.nandakumar@consensys.net>
2025-05-14 08:12:57 +0000

    refactor serde tests

---
+++ prover/protocol/distributed/serde-tests/discoverer_test.go
@@ -0,0 +1,45 @@
+package distributed
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/consensys/linea-monorepo/prover/protocol/distributed"
+	"github.com/consensys/linea-monorepo/prover/protocol/serialization"
+	"github.com/consensys/linea-monorepo/prover/utils/test_utils"
+)
+
+func TestSerdeDiscoverer(t *testing.T) {
+	var (
+		zkevm = distributed.GetZkEVM()
+		disc  = &distributed.StandardModuleDiscoverer{
+			TargetWeight: 1 << 28,
+			Affinities:   distributed.GetAffinities(zkevm),
+			Predivision:  1,
+		}
+	)
+
+	discSer, err := serialization.SerializeValue(reflect.ValueOf(disc), serialization.DeclarationMode)
+	if err != nil {
+		t.Fatalf("error during serializing discover:%s", err.Error())
+	}
+
+	// Create a new empty CompiledIOP for deserialization
+	comp := serialization.NewEmptyCompiledIOP()
+
+	// Deserialize into a new Modexp
+	deserializedDiscVal, err := serialization.DeserializeValue(discSer, serialization.DeclarationMode, reflect.TypeOf(&distributed.StandardModuleDiscoverer{}), comp)
+	if err != nil {
+		t.Fatalf("Failed to deserialize discoverer: %v", err)
+	}
+	deserializedDisc, ok := deserializedDiscVal.Interface().(*distributed.StandardModuleDiscoverer)
+	if !ok {
+		t.Fatalf("Deserialized value is not *modexp.Module: got %T", deserializedDiscVal.Interface())
+	}
+
+	// Compare structs while ignoring unexported fields
+	if !test_utils.CompareExportedFields(disc, deserializedDisc) {
+		t.Fatalf("Mis-matched fields after serde discoverer (ignoring unexported fields)")
+	}
+
+}
--- prover/protocol/distributed/serde-tests/zkevm_test.go
+++ prover/protocol/distributed/serde-tests/zkevm_test.go
@@ -1,12 +1,11 @@
-package zkevm
+package distributed
 
 import (
 	"fmt"
 	"reflect"
 	"testing"
 
-	"github.com/consensys/linea-monorepo/prover/config"
-	"github.com/consensys/linea-monorepo/prover/protocol/column"
+	"github.com/consensys/linea-monorepo/prover/protocol/distributed"
 	"github.com/consensys/linea-monorepo/prover/protocol/serialization"
 	"github.com/consensys/linea-monorepo/prover/utils/test_utils"
 	"github.com/consensys/linea-monorepo/prover/zkevm/arithmetization"
@@ -29,7 +28,7 @@ func serializeValue(value interface{}) ([]byte, error) {
 }
 
 func TestZKEVM(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -48,7 +47,7 @@ func TestZKEVM(t *testing.T) {
 }
 
 func TestModexp(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -82,7 +81,7 @@ func TestModexp(t *testing.T) {
 }
 
 func TestECDSA(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -116,7 +115,7 @@ func TestECDSA(t *testing.T) {
 }
 
 func TestEcadd(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -150,7 +149,7 @@ func TestEcadd(t *testing.T) {
 }
 
 func TestEcmul(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -184,7 +183,7 @@ func TestEcmul(t *testing.T) {
 }
 
 func TestEcpair(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -219,7 +218,7 @@ func TestEcpair(t *testing.T) {
 
 // TestArithmetization tests serialization and deserialization of the Arithmetization field.
 func TestArithmetization(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -254,7 +253,7 @@ func TestArithmetization(t *testing.T) {
 
 // TestKeccak tests serialization and deserialization of the Keccak field.
 func TestKeccak(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -289,7 +288,7 @@ func TestKeccak(t *testing.T) {
 
 // TestSha2 tests serialization and deserialization of the Sha2 field.
 func TestSha2(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -324,7 +323,7 @@ func TestSha2(t *testing.T) {
 
 // TestCompiledIOP tests serialization and deserialization of the WizardIOP field.
 func TestCompiledIOP(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -419,7 +418,7 @@ func TestCompiledIOP(t *testing.T) {
 
 // TestStateManager tests serialization and deserialization of the StateManager field.
 func TestStateManager(t *testing.T) {
-	z := GetZkEVM()
+	z := distributed.GetZkEVM()
 	if z == nil {
 		t.Fatal("GetZkEVM returned nil")
 	}
@@ -451,119 +450,4 @@ func TestStateManager(t *testing.T) {
 		t.Fatalf("Mis-matched fields after serde StateManager (ignoring unexported fields)")
 	}
 
-}
-
-// GetZKEVM returns a [zkevm.ZkEvm] with its trace limits inflated so that it
-// can be used as input for the package functions. The zkevm is returned
-// without any compilation.
-func GetZkEVM() *ZkEvm {
-
-	// This are the config trace-limits from sepolia. All multiplied by 16.
-	traceLimits := config.TracesLimits{
-		Add:                                  1 << 23,
-		Bin:                                  1 << 22,
-		Blake2Fmodexpdata:                    1 << 18,
-		Blockdata:                            1 << 16,
-		Blockhash:                            1 << 16,
-		Ecdata:                               1 << 22,
-		Euc:                                  1 << 20,
-		Exp:                                  1 << 18,
-		Ext:                                  1 << 24,
-		Gas:                                  1 << 20,
-		Hub:                                  1 << 25,
-		Logdata:                              1 << 20,
-		Loginfo:                              1 << 16,
-		Mmio:                                 1 << 25,
-		Mmu:                                  1 << 25,
-		Mod:                                  1 << 21,
-		Mul:                                  1 << 20,
-		Mxp:                                  1 << 23,
-		Oob:                                  1 << 22,
-		Rlpaddr:                              1 << 16,
-		Rlptxn:                               1 << 21,
-		Rlptxrcpt:                            1 << 21,
-		Rom:                                  1 << 26,
-		Romlex:                               1 << 16,
-		Shakiradata:                          1 << 19,
-		Shf:                                  1 << 20,
-		Stp:                                  1 << 18,
-		Trm:                                  1 << 19,
-		Txndata:                              1 << 18,
-		Wcp:                                  1 << 22,
-		Binreftable:                          1 << 24,
-		Shfreftable:                          1 << 16,
-		Instdecoder:                          1 << 13,
-		PrecompileEcrecoverEffectiveCalls:    1 << 13,
-		PrecompileSha2Blocks:                 1 << 13,
-		PrecompileRipemdBlocks:               0,
-		PrecompileModexpEffectiveCalls:       1 << 10,
-		PrecompileModexpEffectiveCalls4096:   1 << 4,
-		PrecompileEcaddEffectiveCalls:        1 << 12,
-		PrecompileEcmulEffectiveCalls:        1 << 9,
-		PrecompileEcpairingEffectiveCalls:    1 << 9,
-		PrecompileEcpairingMillerLoops:       1 << 10,
-		PrecompileEcpairingG2MembershipCalls: 1 << 10,
-		PrecompileBlakeEffectiveCalls:        0,
-		PrecompileBlakeRounds:                0,
-		BlockKeccak:                          1 << 17,
-		BlockL1Size:                          100_000,
-		BlockL2L1Logs:                        16,
-		BlockTransactions:                    1 << 12,
-		ShomeiMerkleProofs:                   1 << 18,
-	}
-
-	return FullZKEVMWithSuite(&traceLimits, CompilationSuite{}, &config.Config{})
-}
-
-// GetAffinities returns a list of affinities for the following modules. This
-// affinities regroup how the modules are grouped.
-//
-//	ecadd / ecmul / ecpairing
-//	hub / hub.scp / hub.acp
-//	everything related to keccak
-func GetAffinities(z *ZkEvm) [][]column.Natural {
-
-	return [][]column.Natural{
-		{
-			z.Ecmul.AlignedGnarkData.IsActive.(column.Natural),
-			z.Ecadd.AlignedGnarkData.IsActive.(column.Natural),
-			z.Ecpair.AlignedFinalExpCircuit.IsActive.(column.Natural),
-			z.Ecpair.AlignedG2MembershipData.IsActive.(column.Natural),
-			z.Ecpair.AlignedMillerLoopCircuit.IsActive.(column.Natural),
-		},
-		{
-			z.WizardIOP.Columns.GetHandle("hub.HUB_STAMP").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("hub.scp_ADDRESS_HI").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("hub.acp_ADDRESS_HI").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("hub.ccp_HUB_STAMP").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("hub.envcp_HUB_STAMP").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("hub.stkcp_PEEK_AT_STACK_POW_4").(column.Natural),
-		},
-		{
-			z.WizardIOP.Columns.GetHandle("KECCAK_IMPORT_PAD_HASH_NUM").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("CLEANING_KECCAK_CleanLimb").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("DECOMPOSITION_KECCAK_Decomposed_Len_0").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("KECCAK_FILTERS_SPAGHETTI").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("LANE_KECCAK_Lane").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("KECCAKF_IS_ACTIVE_").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("KECCAKF_BLOCK_BASE_2_0").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("KECCAK_OVER_BLOCKS_TAGS_0").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("HASH_OUTPUT_Hash_Lo").(column.Natural),
-		},
-		{
-			z.WizardIOP.Columns.GetHandle("SHA2_IMPORT_PAD_HASH_NUM").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("DECOMPOSITION_SHA2_Decomposed_Len_0").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("LENGTH_CONSISTENCY_SHA2_BYTE_LEN_0_0").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("SHA2_FILTERS_SPAGHETTI").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("LANE_SHA2_Lane").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("Coefficient_SHA2").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("SHA2_OVER_BLOCK_IS_ACTIVE").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("SHA2_OVER_BLOCK_SHA2_COMPRESSION_CIRCUIT_IS_ACTIVE").(column.Natural),
-		},
-		{
-			z.WizardIOP.Columns.GetHandle("mmio.CN_ABC").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("mmio.MMIO_STAMP").(column.Natural),
-			z.WizardIOP.Columns.GetHandle("mmu.STAMP").(column.Natural),
-		},
-	}
 }
