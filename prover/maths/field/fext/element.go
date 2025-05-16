189cd973cf6a9123018e50775b55d3cbc23c9e67
Thomas Piellard <thomas.piellard@consensys.net>
2025-05-16 15:16:06 +0200

    type alias instead of type redef

--- prover/maths/field/fext/element.go
+++ prover/maths/field/fext/element.go
@@ -4,5 +4,5 @@ import (
 	"github.com/consensys/gnark-crypto/field/koalabear/extensions"
 )
 
-// Redefining the type locally, so we can import only fext
-type Element extensions.E4
+// Embedding
+type Element = extensions.E4
