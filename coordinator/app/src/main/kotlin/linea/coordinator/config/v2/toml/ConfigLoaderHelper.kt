d45634bcc60956650212c93c3dafe4ccf89aa13d
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-05-19 09:55:27 +0100

    coordinator: config v2 wip

---
+++ coordinator/app/src/main/kotlin/linea/coordinator/config/v2/toml/ConfigLoaderHelper.kt
@@ -0,0 +1,22 @@
+package linea.coordinator.config.v2.toml
+
+import com.sksamuel.hoplite.ConfigLoaderBuilder
+import com.sksamuel.hoplite.toml.TomlPropertySource
+import net.consensys.zkevm.coordinator.app.config.BlockParameterDecoder
+
+fun ConfigLoaderBuilder.addCoordinatorTomlDecoders(): ConfigLoaderBuilder {
+  return this
+    .addDecoder(BlockParameterDecoder())
+    .addDecoder(TomlByteArrayHexDecoder())
+    .addDecoder(TomlKotlinDurationDecoder())
+}
+
+inline fun <reified T: Any>  parseConfig(toml: String): T {
+  return ConfigLoaderBuilder
+    .default()
+    .addCoordinatorTomlDecoders()
+    .addSource(TomlPropertySource(toml))
+    .build()
+    .loadConfigOrThrow<T>()
+}
+
