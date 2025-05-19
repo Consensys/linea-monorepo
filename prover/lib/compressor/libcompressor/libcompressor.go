9925ff82a183a0acbce60ab34a1f1d138ab0b174
Tabaie <arya.pourtabatabaie@gmail.com>
2025-05-19 17:26:34 -0500

    refactor: JNI-friendly and multiple instances

--- prover/lib/compressor/libcompressor/libcompressor.go
+++ prover/lib/compressor/libcompressor/libcompressor.go
@@ -1,51 +1,145 @@
 package main
 
+/*
+#include <jni.h>
+*/
 import "C"
 
 import (
+	"crypto/rand"
+	"encoding/binary"
 	"errors"
+	"fmt"
+	"slices"
 	"sync"
 	"unsafe"
 
-	blob_v1 "github.com/consensys/linea-monorepo/prover/lib/compressor/blob/v1"
+	blobv1 "github.com/consensys/linea-monorepo/prover/lib/compressor/blob/v1"
 )
 
 //go:generate go build -tags nocorset -ldflags "-s -w" -buildmode=c-shared -o libcompressor.so libcompressor.go
 func main() {}
 
-var (
-	compressor *blob_v1.BlobMaker
-	lastError  error
-	lock       sync.Mutex // probably unnecessary if coordinator guarantees single-threaded access
+type instance struct {
+	sync.Mutex
+	*blobv1.BlobMaker
+}
+
+const (
+	errorJniClass            = "java/lang/Error"
+	runtimeExceptionJniClass = "java/lang/RuntimeException"
+
+	// TODO Make custom exceptions
+	compressorErrorJniClass           = errorJniClass
+	instanceNotFoundExceptionJniClass = runtimeExceptionJniClass
 )
 
-// Init initializes the compressor.
+var (
+	instances map[int]*instance
+	mapLock   sync.Mutex
+)
+
+func throw(env *C.JNIEnv, errorClass string, err error) {
+	// Get the MyCustomException class
+	exceptionClass := C.FindClass(env, C.CString(errorClass))
+	if exceptionClass == nil {
+		// If the class is not found, throw a generic error
+		C.ThrowNew(env, C.FindClass(env, C.CString(runtimeExceptionJniClass)), C.CString("Exception class not found: "+errorClass))
+		return
+	}
+
+	// Throw the custom exception with the provided message
+	C.ThrowNew(env, exceptionClass, C.CString(err.Error()))
+}
+
+func fromJniBytes(env *C.JNIEnv, bytes C.jbyteArray) []byte {
+	// Get the length of the byte array
+	length := C.GetArrayLength(env, bytes)
+
+	// Get a pointer to the elements of the Java byte array
+	ptr := C.GetByteArrayElements(env, bytes, nil)
+	if ptr == nil {
+		// If the byte array is null, throw an exception
+		throw(env, compressorErrorJniClass, fmt.Errorf("byte array is null"))
+		return nil
+	}
+	defer C.ReleaseByteArrayElements(env, bytes, ptr, 0) // Ensure the array is released
+	// Convert the pointer to a Go byte slice
+	return slices.Clone(unsafe.Slice((*byte)(unsafe.Pointer(ptr)), length))
+}
+
+func toJniBytes(env *C.JNIEnv, bytes []byte) C.jbyteArray {
+	// Create a new Java byte array in the JVM
+	jbyteArray := C.NewByteArray(env, C.jsize(len(bytes)))
+	if jbyteArray == nil {
+		// If the array creation fails, throw an exception
+		throw(env, runtimeExceptionJniClass, fmt.Errorf("failed to create Java byte array"))
+		return nil
+	}
+
+	// Copy the compressed data into the Java byte array
+	C.SetByteArrayRegion(env, jbyteArray, 0, C.jsize(len(bytes)), (*C.jbyte)(unsafe.Pointer(&bytes[0])))
+
+	// Return the Java byte array
+	return jbyteArray
+}
+
+// getInstance finds the instance with the given ID.
+// If not found, it will throw a RuntimeException.
+// It will lock the instance to prevent concurrent access.
+func getInstance(env *C.JNIEnv, instanceID C.int) *instance {
+	mapLock.Lock()
+	defer mapLock.Unlock()
+
+	if inst, ok := instances[int(instanceID)]; ok {
+		inst.Lock()
+		return inst
+	}
+	throw(env, instanceNotFoundExceptionJniClass, fmt.Errorf("instance %d not found", int(instanceID)))
+	return nil
+}
+
+// NewInstance initializes the compressor.
 // The dataLimit argument is the maximum size of the compressed data.
-// Returns true if the compressor was initialized, false otherwise.
-// If false is returned, the Error() method will return a string describing the error.
+// Returns the instance ID if successful, or -1 if an error occurred.
 //
-//export Init
-func Init(dataLimit int, dictPath *C.char) bool {
-	fPath := C.GoString(dictPath)
-	return initGo(dataLimit, fPath)
+//export NewInstance
+func NewInstance(env *C.JNIEnv, dataLimit C.int, dictPath *C.char) C.int {
+
+	var bytes [4]byte
+	if _, err := rand.Read(bytes[:]); err != nil {
+		throw(env, runtimeExceptionJniClass, err)
+		return -1
+	}
+
+	compressor, err := blobv1.NewBlobMaker(dataLimit, C.GoString(dictPath))
+	if err != nil {
+		throw(env, compressorErrorJniClass, err)
+		return -1
+	}
+
+	mapLock.Lock()
+	defer mapLock.Unlock()
+
+	id := int(binary.BigEndian.Uint32(bytes[:]))
+	if _, ok := instances[id]; ok {
+		throw(env, compressorErrorJniClass, fmt.Errorf("instance %d already exists", id))
+		return -1
+	}
+
+	instances[id] = &instance{
+		BlobMaker: compressor,
+	}
+
+	return id
 }
 
-func initGo(dataLimit int, dictPath string) bool {
-	lock.Lock()
-	defer lock.Unlock()
-	compressor, lastError = blob_v1.NewBlobMaker(dataLimit, dictPath)
-
-	return lastError == nil
-}
-
-// Reset resets the compressor. Must be called between each Blob.
+// Reset resets the compressor. Must be called between blobs.
 //
 //export Reset
-func Reset() {
-	lock.Lock()
-	defer lock.Unlock()
-
-	lastError = nil
+func Reset(env *C.JNIEnv, instanceID C.int) {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
 
 	compressor.Reset()
 }
@@ -57,13 +151,13 @@ func Reset() {
 // The input []byte is interpreted as a RLP encoded Block.
 //
 //export Write
-func Write(input *C.char, inputLength C.int) (chunkAppended bool) {
-	lock.Lock()
-	defer lock.Unlock()
-	rlpBlock := unsafe.Slice((*byte)(unsafe.Pointer(input)), inputLength)
-	chunkAppended, err := compressor.Write(rlpBlock, false)
+func Write(env *C.JNIEnv, instanceID C.int, rlpBlock C.jbyteArray) (chunkAppended bool) {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
+
+	chunkAppended, err := compressor.Write(fromJniBytes(env, rlpBlock), false)
 	if err != nil {
-		lastError = err
+		throw(env, compressorErrorJniClass, err)
 		return false
 	}
 
@@ -74,39 +168,25 @@ func Write(input *C.char, inputLength C.int) (chunkAppended bool) {
 // (but return true if it could)
 //
 //export CanWrite
-func CanWrite(input *C.char, inputLength C.int) (chunkAppended bool) {
-	lock.Lock()
-	defer lock.Unlock()
-	rlpBlock := unsafe.Slice((*byte)(unsafe.Pointer(input)), inputLength)
-	chunkAppended, err := compressor.Write(rlpBlock, true)
+func CanWrite(env *C.JNIEnv, instanceID C.int, rlpBlock C.jbyteArray) (chunkAppended bool) {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
+
+	chunkAppended, err := compressor.Write(fromJniBytes(env, rlpBlock), true)
 	if err != nil {
-		lastError = err
+		throw(env, compressorErrorJniClass, err)
 		return false
 	}
 
 	return chunkAppended
 }
 
-// Error returns the last encountered error.
-// If no error was encountered, returns nil.
-//
-//export Error
-func Error() *C.char {
-	lock.Lock()
-	defer lock.Unlock()
-	if lastError != nil {
-		// this leaks memory, but since this represents a fatal error, it's probably ok.
-		return C.CString(lastError.Error())
-	}
-	return nil
-}
-
 // StartNewBatch starts a new batch; must be called between each batch in the blob.
 //
 //export StartNewBatch
-func StartNewBatch() {
-	lock.Lock()
-	defer lock.Unlock()
+func StartNewBatch(env *C.JNIEnv, instanceID C.int) {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
 
 	compressor.StartNewBatch()
 }
@@ -114,24 +194,21 @@ func StartNewBatch() {
 // Len returns the length of the compressed data.
 //
 //export Len
-func Len() (length int) {
-	lock.Lock()
-	defer lock.Unlock()
-	return compressor.Len()
+func Len(env *C.JNIEnv, instanceID C.int) (length C.int) {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
+
+	return C.int(compressor.Len())
 }
 
 // Bytes returns the compressed data.
-// The caller is responsible for allocating the memory for the output slice.
-// Length of the output slice must equal the value returned by Len().
 //
 //export Bytes
-func Bytes(dataOut *C.char) {
-	lock.Lock()
-	defer lock.Unlock()
-	compressed := compressor.Bytes()
+func Bytes(env *C.JNIEnv, instanceID C.int) C.jbyteArray {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
 
-	outSlice := unsafe.Slice((*byte)(unsafe.Pointer(dataOut)), len(compressed))
-	copy(outSlice, compressed)
+	return toJniBytes(env, compressor.Bytes())
 }
 
 // WorstCompressedBlockSize returns the size of the given block, as compressed by an "empty" blob maker.
@@ -139,21 +216,15 @@ func Bytes(dataOut *C.char) {
 // returns the maximum size that can be achieved.
 //
 // The input is a RLP encoded block.
-// Returns the length of the compressed data, or -1 if an error occurred.
-// User must call Error() to get the error message.
-//
-// This function is thread-safe. Concurrent calls are allowed,
-// but the other functions may not be thread-safe.
 //
 //export WorstCompressedBlockSize
-func WorstCompressedBlockSize(input *C.char, inputLength C.int) C.int {
-	rlpBlock := C.GoBytes(unsafe.Pointer(input), inputLength)
+func WorstCompressedBlockSize(env *C.JNIEnv, instanceID C.int, rlpBlock C.jbyteArray) C.int {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
 
-	_, n, err := compressor.WorstCompressedBlockSize(rlpBlock)
+	_, n, err := compressor.WorstCompressedBlockSize(fromJniBytes(rlpBlock))
 	if err != nil {
-		lock.Lock()
-		lastError = errors.Join(lastError, err)
-		lock.Unlock()
+		throw(env, compressorErrorJniClass, err)
 		return -1
 	}
 	return C.int(n)
@@ -164,21 +235,15 @@ func WorstCompressedBlockSize(input *C.char, inputLength C.int) C.int {
 // returns the maximum size that can be achieved.
 //
 // The input is a RLP encoded transaction.
-// Returns the length of the compressed data, or -1 if an error occurred.
-// User must call Error() to get the error message.
-//
-// This function is thread-safe. Concurrent calls are allowed,
-// but the other functions may not be thread-safe.
 //
 //export WorstCompressedTxSize
-func WorstCompressedTxSize(input *C.char, inputLength C.int) C.int {
-	rlpTx := C.GoBytes(unsafe.Pointer(input), inputLength)
+func WorstCompressedTxSize(env *C.JNIEnv, instanceID C.int, rlpTx C.jbyteArray) C.int {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
 
-	n, err := compressor.WorstCompressedTxSize(rlpTx)
+	n, err := compressor.WorstCompressedTxSize(fromJniBytes(rlpTx))
 	if err != nil {
-		lock.Lock()
-		lastError = errors.Join(lastError, err)
-		lock.Unlock()
+		throw(env, compressorErrorJniClass, err)
 		return -1
 	}
 	return C.int(n)
@@ -188,21 +253,15 @@ func WorstCompressedTxSize(input *C.char, inputLength C.int) C.int {
 // The returned length account for the "padding" used by the blob maker to
 // fit the data in field elements.
 // Input size must be less than 256kB.
-// If an error occurred, returns -1.
-// User must call Error() to get the error message.
-//
-// This function is thread-safe. Concurrent calls are allowed,
-// but the other functions are not thread-safe.
 //
 //export RawCompressedSize
-func RawCompressedSize(input *C.char, inputLength C.int) C.int {
-	inputSlice := C.GoBytes(unsafe.Pointer(input), inputLength)
+func RawCompressedSize(env *C.JNIEnv, instanceID C.int, input C.jbyteArray) C.int {
+	compressor := getInstance(env, instanceID)
+	defer compressor.Unlock()
 
-	n, err := compressor.RawCompressedSize(inputSlice)
+	n, err := compressor.RawCompressedSize(fromJniBytes(input))
 	if err != nil {
-		lock.Lock()
-		lastError = errors.Join(lastError, err)
-		lock.Unlock()
+		throw(env, compressorErrorJniClass, err)
 		return -1
 	}
 	return C.int(n)
