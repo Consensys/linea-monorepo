22d0688792dcfadc17fcd5c57f79866c62e78692
Yao Galteland <your-email@example.com>
2025-05-23 00:07:22 +0200

    refactor: TestBatchEvaluateLagrangeOnFext ok

--- prover/maths/common/fastpoly/lagrange_test.go
+++ prover/maths/common/fastpoly/lagrange_test.go
@@ -49,19 +49,17 @@ func TestEvaluateLagrangeFext(t *testing.T) {
 	p := randomPoly(size)
 	pLagrange := make([]field.Element, size)
 
+	var x fext.Element
+	x.SetRandom()
+	u := poly.EvalOnExtField(p, x)
+
 	/*
 		Test without coset
 	*/
 	copy(pLagrange, p)
 	domain.FFT(pLagrange, fft.DIF)
 	fft.BitReverse(pLagrange)
-
-	var x fext.Element
-	x.SetRandom()
-
-	u := poly.EvalOnExtField(p, x)
 	v := EvaluateLagrangeOnFext(pLagrange, x)
-
 	require.Equal(t, u.String(), v.String())
 
 	/*
@@ -76,49 +74,61 @@ func TestEvaluateLagrangeFext(t *testing.T) {
 
 func TestBatchEvaluateLagrangeOnFext(t *testing.T) {
 
-	n := 4
-	randPoly := vector.ForTest(1, 2, 3, 4)
-	randPoly2 := vector.ForTest(5, 6, 7, 8)
-	x := fext.NewElement(51, 1, 3, 4)
+	sizePoly := 64
+	nbPoly := 20
 
-	expectedY := poly.EvalOnExtField(randPoly, x)
-	expectedY2 := poly.EvalOnExtField(randPoly2, x)
-	domain := fft.NewDomain(uint64(n))
+	// sample a bunch of polynomials
+	polys := make([][]field.Element, nbPoly)
+	for i := 0; i < nbPoly; i++ {
+		polys[i] = randomPoly(sizePoly)
+	}
+
+	// sample a random point
+	x := fext.RandomElement()
+
+	Eval := make([]fext.Element, nbPoly)
+	for i := 0; i < nbPoly; i++ {
+		Eval[i] = poly.EvalOnExtField(polys[i], x)
+	}
+	d := fft.NewDomain(uint64(sizePoly))
 
 	/*
 		Test without coset
 	*/
-	onRoots := vector.DeepCopy(randPoly)
-	onRoots2 := vector.DeepCopy(randPoly2)
-	polys := make([][]field.Element, 2)
-	polys[0] = onRoots
-	polys[1] = onRoots2
+	onRoots := make([][]field.Element, nbPoly)
 
-	domain.FFT(polys[0], fft.DIF)
-	domain.FFT(polys[1], fft.DIF)
-	fft.BitReverse(polys[0])
-	fft.BitReverse(polys[1])
+	for i := 0; i < nbPoly; i++ {
+		onRoots[i] = vector.DeepCopy(polys[i])
 
-	yOnRoots := BatchEvaluateLagrangeOnFext(polys, x)
-	require.Equal(t, expectedY.String(), yOnRoots[0].String())
-	require.Equal(t, expectedY2.String(), yOnRoots[1].String())
+		d.FFT(onRoots[i], fft.DIF)
+		fft.BitReverse(onRoots[i])
+	}
+
+	// compute lagrange eval
+	lagEvalExt := BatchEvaluateLagrangeOnFext(onRoots, x)
+
+	// check the result
+	for i := 0; i < nbPoly; i++ {
+		require.Equal(t, Eval[i].String(), lagEvalExt[i].String())
+	}
 
 	/*
 		Test with coset
 	*/
-	onCoset := vector.DeepCopy(randPoly)
-	onCoset2 := vector.DeepCopy(randPoly2)
-	onCosets := make([][]field.Element, 2)
-	onCosets[0] = onCoset
-	onCosets[1] = onCoset2
+	onCosets := make([][]field.Element, nbPoly)
 
-	domain.FFT(onCosets[0], fft.DIF, fft.OnCoset())
-	domain.FFT(onCosets[1], fft.DIF, fft.OnCoset())
-	fft.BitReverse(onCosets[0])
-	fft.BitReverse(onCosets[1])
+	for i := 0; i < nbPoly; i++ {
+		onCosets[i] = vector.DeepCopy(polys[i])
 
-	yOnCosets := BatchEvaluateLagrangeOnFext(onCosets, x, true)
-	require.Equal(t, expectedY.String(), yOnCosets[0].String())
-	require.Equal(t, expectedY2.String(), yOnCosets[1].String())
+		d.FFT(onCosets[i], fft.DIF, fft.OnCoset())
+		fft.BitReverse(onCosets[i])
+	}
 
+	// compute lagrange eval
+	lagEvalExtcoset := BatchEvaluateLagrangeOnFext(onCosets, x, true)
+
+	// check the result
+	for i := 0; i < nbPoly; i++ {
+		require.Equal(t, Eval[i].String(), lagEvalExtcoset[i].String())
+	}
 }
