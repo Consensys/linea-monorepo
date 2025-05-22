d0b20b58db333307f18bfc56f59956ffbb9df457
Yao Galteland <your-email@example.com>
2025-05-23 01:06:42 +0200

    revert back

--- prover/maths/common/fastpolyext/lagrange.go
+++ prover/maths/common/fastpolyext/lagrange.go
@@ -17,6 +17,7 @@ func EvaluateLagrange(poly []fext.Element, x fext.Element, oncoset ...bool) fext
 	if !utils.IsPowerOfTwo(len(poly)) {
 		utils.Panic("only support powers of two but poly has length %v", len(poly))
 	}
+
 	domain := fft.NewDomain(uint64(len(poly)))
 
 	if len(oncoset) > 0 && oncoset[0] {
@@ -40,19 +41,19 @@ func EvaluateLagrange(poly []fext.Element, x fext.Element, oncoset ...bool) fext
 		accw.Mul(&accw, &extomega)
 	}
 	invdens := fext.BatchInvert(dens) // [1/x-1, 1/x-ω, 1/x-ω², ...]
-	var tmp fext.Element
-	tmp.Exp(x, big.NewInt(int64(size))).Sub(&tmp, &one) // xⁿ-1
-	var li fext.Element
-	fext.SetInt64(&li, int64(size))
-	li.Inverse(&li)
-	li.Mul(&tmp, &li) // 1/n * (xⁿ-1)
+	var xn fext.Element
+	xn.Exp(x, big.NewInt(int64(size))).Sub(&xn, &one) // xⁿ-1
+	var nInv fext.Element
+	fext.SetInt64(&nInv, int64(size)).Inverse(&nInv)
+	xn.Mul(&xn, &nInv) // 1/n * (xⁿ-1)
 
-	var res fext.Element
+	var res, li fext.Element
+	accw.SetOne()
 	for i := 0; i < size; i++ {
-		li.Mul(&li, &invdens[i]) // ( xⁿ-1)/n * 1/(x-ωⁱ)
-		tmp.Mul(&li, &poly[i])   // pᵢ *  ( xⁿ-1)/n * 1/(x-ωⁱ)
-		res.Add(&res, &tmp)
-		li.Mul(&li, &dens[i]).Mul(&li, &extomega)
+		li.Mul(&accw, &xn).Mul(&li, &invdens[i]) // ωⁱ/n * ( xⁿ-1)/(x-ωⁱ)
+		li.Mul(&li, &poly[i])                    // pᵢ *  ωⁱ/n * ( xⁿ-1)/(x-ωⁱ)
+		res.Add(&res, &li)
+		accw.Mul(&accw, &extomega)
 	}
 
 	return res
@@ -62,7 +63,6 @@ func EvaluateLagrange(poly []fext.Element, x fext.Element, oncoset ...bool) fext
 // Batch version of EvaluateLagrange
 func BatchEvaluateLagrange(polys [][]fext.Element, x fext.Element, oncoset ...bool) []fext.Element {
 
-	// TODO should we check that the polynomials are of the same size ??
 	results := make([]fext.Element, len(polys))
 	poly := polys[0]
 
@@ -70,46 +70,80 @@ func BatchEvaluateLagrange(polys [][]fext.Element, x fext.Element, oncoset ...bo
 		utils.Panic("only support powers of two but poly has length %v", len(poly))
 	}
 
-	size := len(poly)
-	omega, err := fft.Generator(uint64(size))
-	if err != nil {
-		// TODO handle that properly
-		panic(err)
-	}
+	n := len(poly)
 
-	domain := fft.NewDomain(uint64(len(poly)))
+	domain := fft.NewDomain(uint64(n))
+	denominator := make([]fext.Element, n)
+
+	one := fext.One()
+
+	var wrappedFrMultiplicativeGenInv fext.Element
+	fext.FromBase(&wrappedFrMultiplicativeGenInv, &domain.FrMultiplicativeGenInv)
 
 	if len(oncoset) > 0 && oncoset[0] {
-		x.MulByElement(&x, &domain.FrMultiplicativeGenInv)
+		x.Mul(&x, &wrappedFrMultiplicativeGenInv)
 	}
 
-	lagrangeAtX := make([]fext.Element, size)
-	var accw, one, extomega fext.Element
-	one.SetOne()
-	accw.SetOne()
-	fext.FromBase(&extomega, &omega)
-	dens := make([]fext.Element, size) // [x-1, x-ω, x-ω², ...]
-	for i := 0; i < size; i++ {
-		dens[i].Sub(&x, &accw)
-		accw.Mul(&accw, &extomega)
-	}
-	invdens := fext.BatchInvert(dens) // [1/x-1, 1/x-ω, 1/x-ω², ...]
-	var tmp fext.Element
-	tmp.Exp(x, big.NewInt(int64(size))).Sub(&tmp, &one) // xⁿ-1
-	fext.SetInt64(&lagrangeAtX[0], int64(size))
-	lagrangeAtX[0].Inverse(&lagrangeAtX[0])
-	lagrangeAtX[0].Mul(&tmp, &lagrangeAtX[0])        // 1/n * (xⁿ-1)
-	lagrangeAtX[0].Mul(&lagrangeAtX[0], &invdens[0]) // 1/n * (xⁿ-1)/(x-1)
+	/*
+		First, we compute the denominator,
 
-	for i := 1; i < size; i++ {
-		lagrangeAtX[i].Mul(&lagrangeAtX[i-1], &dens[i-1]).
-			Mul(&lagrangeAtX[i], &invdens[i]).
-			Mul(&lagrangeAtX[i], &extomega)
+		D_x = \frac{X}{x} - g for x \in H
+			where H is the subgroup of the roots of unity (not the coset)
+			and g a field element such that gH is the coset
+	*/
+	var wrappedGeneratorInv fext.Element
+	fext.FromBase(&wrappedGeneratorInv, &domain.GeneratorInv)
+
+	denominator[0] = x
+	for i := 1; i < n; i++ {
+		denominator[i].Mul(&denominator[i-1], &wrappedGeneratorInv)
 	}
+
+	for i := 0; i < n; i++ {
+		denominator[i].Sub(&denominator[i], &one)
+
+		if denominator[i].IsZero() {
+			// edge-case : x is a root of unity of the domain. In this case, we can just return
+			// the associated value for poly
+
+			for k := range polys {
+				results[k] = polys[k][i]
+			}
+
+			return results
+		}
+	}
+
+	/*
+		Then, we compute the sum between the inverse of the denominator
+		and the poly
+
+		\sum_{x \in H}\frac{P(gx)}{D_x}
+	*/
+	denominator = fext.BatchInvert(denominator)
+
+	// Precompute the value of x^n once outside the loop
+	xN := new(fext.Element).Exp(x, big.NewInt(int64(n)))
+
+	// Precompute the value of domain.CardinalityInv outside the loop
+	var wrappedCardinalityInv fext.Element
+	fext.FromBase(&wrappedCardinalityInv, &domain.CardinalityInv)
+
+	// Compute factor as (x^n - 1) * (1 / domain.Cardinality).
+	factor := new(fext.Element).Sub(xN, &one)
+	factor.Mul(factor, &wrappedCardinalityInv)
 
 	parallel.Execute(len(polys), func(start, stop int) {
 		for k := start; k < stop; k++ {
-			results[k] = vectorext.ScalarProd(lagrangeAtX, polys[k])
+
+			// Compute the scalar product.
+			res := vectorext.ScalarProd(polys[k], denominator)
+
+			// Multiply res with factor.
+			res.Mul(&res, factor)
+
+			// Store the result.
+			results[k] = res
 		}
 	})
 
