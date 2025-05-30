367b170d5e5cb79bb31f5773ee078a56fa3271c9
kyzooghost <jeffersonzjtang@gmail.com>
2025-05-30 18:56:51 +1000

    streamline csp policy

--- bridge-ui/src/middleware.ts
+++ bridge-ui/src/middleware.ts
@@ -10,14 +10,8 @@ export function middleware(request: NextRequest) {
    * default-src 'self'
    * - Fallback policy to only allow resources from the same origin, unless overriden by a more specific policy
    *
-   * script-src-elem 'self' 'nonce-{nonce}' 'unsafe-eval' <HTTPS_URL>
-   * - Allow scripts from the same origin, with the provided nonce  and from specified domains.
-   * - `unsafe-eval` is required to enable evaluation of arbitrary strings in JS, and the React component does not load otherwise
-   * - We would prefer to use `strict-dynamic` instead of specifying HTTPS_URLs
-   *    - However the Usabilla script dynamically loads JS from Usabilla domain, and we are unable to provide the CSP nonce to each instance in which JS is dynamically loaded
-   *    - If 'strict-dynamic' is used then HTTPS_URLs are ignored
-   *    - As a compromise, we use script-src with 'strict-dynamic' as a fallback
-   * - script-src-elem applies to scripts created via HTML elements, but does not cover inline event handlers or scripts injected via JS APIs like 'new Function()'
+   * script-src-elem 'self' 'nonce-{nonce}' 'strict-dynamic'
+   * - Allow scripts from the same origin, with the provided nonce, and child scripts recursively loaded from a script with nonce
    *
    * style-src 'self' 'unsafe-inline'
    * - Allow styles from the same origin and inline styles.
@@ -55,45 +49,11 @@ export function middleware(request: NextRequest) {
    */
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval';
-    script-src-elem 'self' 'nonce-${nonce}' 'unsafe-eval'
-      https://www.googletagmanager.com https://w.usabilla.com
-      https://js.hsleadflows.net https://js.hsadspixel.net https://js.hubspot.com https://js.hs-analytics.net https://js.hs-banner.com
-      https://widget.intercom.io https://js.intercomcdn.com;
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: https:;
     font-src 'self' data: https://cdn.jsdelivr.net;
-    connect-src 'self' 
-      https://*.infura.io https://*.alchemyapi.io 
-      wss://*.infura.io wss://*.alchemyapi.io 
-      https://*.quiknode.pro wss://*.quiknode.pro 
-      https://www.googletagmanager.com 
-      https://*.walletconnect.org wss://*.walletconnect.org
-      https://*.dynamic.xyz https://app.dynamicauth.com https://*.dynamicauth.com https://dynamic-static-assets.com
-      https://api.onramper.com https://*.onramper.com
-      https://api.layerswap.io https://*.layerswap.io
-      https://api.li.fi https://*.li.fi https://li.quest
-      https://*.hubspot.com https://api.hubapi.com https://js.hs-banner.com https://js.hs-banner.com
-      https://iris-api-sandbox.circle.com https://iris-api.circle.com
-      https://price.api.cx.metamask.io
-      https://registry.npmjs.org
-      https://www.google-analytics.com
-      https://rpc.linea.build
-      https://bsc-dataseed.binance.org https://bsc-dataseed.bnbchain.org https://bsc-rpc.publicnode.com https://bsc-dataseed1.defibit.io
-      https://arb1.arbitrum.io https://arbitrum-one-rpc.publicnode.com https://arbitrum.drpc.org
-      https://rpc-gel.inkonchain.com
-      https://rpc.superposition.so
-      https://cronos.drpc.org https://evm.cronos.org
-      https://api.lens.matterhosted.dev
-      https://mainnet.corn-rpc.com
-      https://swell-mainnet.alt.technology
-      https://rpc.soniclabs.com https://sonic.drpc.org
-      https://evm-rpc.sei-apis.com
-      https://mainnet.aurora.dev
-      https://mode.drpc.org https://mainnet.mode.network
-      https://rpc.api.moonbeam.network https://moonbeam.drpc.org https://moonbeam-rpc.publicnode.com
-      https://rpc.fantom.network https://rpcapi.fantom.network https://fantom-rpc.publicnode.com https://fantom.drpc.org
-      https://rpc.gnosischain.com https://gnosis-rpc.publicnode.com https://gnosis.drpc.org;
+    connect-src 'self' https:;
     frame-src 'self' 
       https://www.googletagmanager.com 
       https://*.walletconnect.com 
@@ -114,7 +74,6 @@ export function middleware(request: NextRequest) {
    *
    * script-src
    * - https://snap.licdn.com
-   *
    */
 
   // Replace newline characters and spaces
