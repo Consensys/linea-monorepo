0d9b333d56ddc986a6c64b170aa328b66183f5f5
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-06-02 10:45:17 +0000

    xxx: work

---
+++ prover/zkevm/prover/bls/circuits_pairingcheck.go
@@ -0,0 +1,69 @@
+package bls
+
+import (
+	"fmt"
+
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bls12381"
+	"github.com/consensys/gnark/std/evmprecompiles"
+	"github.com/consensys/gnark/std/math/emulated"
+)
+
+type MultiMillerLoopMulCircuit struct {
+	Instances []MillerLoopMulInstance `gnark:",public"`
+}
+
+func newMultiMillerLoopMulCircuit(limits *Limits) *MultiMillerLoopMulCircuit {
+	return &MultiMillerLoopMulCircuit{
+		Instances: make([]MillerLoopMulInstance, limits.NbMillerLoopInputInstances),
+	}
+}
+
+func (c *MultiMillerLoopMulCircuit) Define(api frontend.API) error {
+	fp, err := emulated.NewField[sw_bls12381.BaseField](api)
+	if err != nil {
+		return fmt.Errorf("new field: %w", err)
+	}
+	pairing, err := sw_bls12381.NewPairing(api)
+	if err != nil {
+		return fmt.Errorf("new pairing: %w", err)
+	}
+	for i := range c.Instances {
+		if err := c.Instances[i].Check(api, fp, pairing); err != nil {
+			return fmt.Errorf("check instance %d: %w", i, err)
+		}
+	}
+	return nil
+}
+
+type MillerLoopMulInstance struct {
+	Prev    gtElementWizard
+	P       g1ElementWizard
+	Q       g2ElementWizard
+	Current gtElementWizard
+}
+
+func (c *MillerLoopMulInstance) Check(api frontend.API, fp *emulated.Field[sw_bls12381.BaseField], pairing *sw_bls12381.Pairing) error {
+	prev := c.Prev.ToElement(api, fp)
+	p := c.P.ToElement(api, fp)
+	q := c.Q.ToElement(api, fp)
+	current := c.Current.ToElement(api, fp)
+
+	return evmprecompiles.ECPairBLSMillerLoopAndMul(api, prev, p, q, current)
+}
+
+type MillerLoopFinalExpInstance struct {
+	Prev     gtElementWizard
+	P        g1ElementWizard
+	Q        g2ElementWizard
+	Expected [2]frontend.Variable
+}
+
+func (c *MillerLoopFinalExpInstance) Check(api frontend.API, fp *emulated.Field[sw_bls12381.BaseField], pairing *sw_bls12381.Pairing) error {
+	prev := c.Prev.ToElement(api, fp)
+	p := c.P.ToElement(api, fp)
+	q := c.Q.ToElement(api, fp)
+
+	api.AssertIsEqual(c.Expected[0], 0)
+	return evmprecompiles.ECPairBLSMillerLoopAndFinalExpCheck(api, prev, p, q, c.Expected[1])
+}
