d70f2f14c06d835ffe7228eb38cebea574974993
FlorianHuc <florian.huc@consensys.net>
2025-06-03 13:15:31 +0200

    * add replay test for hub shomei integration
    * add replay test for hub shomei integration

---
+++ tracer/arithmetization/src/test/java/net/consensys/linea/replaytests/UpdatedShomeyTests.java
@@ -0,0 +1,568 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.replaytests;
+
+import static net.consensys.linea.replaytests.ReplayTestTools.replay;
+import static net.consensys.linea.zktracer.ChainConfig.OLD_MAINNET_TESTCONFIG;
+
+import net.consensys.linea.UnitTestWatcher;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Disabled
+@ExtendWith(UnitTestWatcher.class)
+public class UpdatedShomeyTests {
+
+  @Test
+  void split_range_19239480_19239481() {
+    replay(OLD_MAINNET_TESTCONFIG, "19239480-19239481.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19239502_19239502() {
+    replay(OLD_MAINNET_TESTCONFIG, "19239502-19239502.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19239569_19239573() {
+    replay(OLD_MAINNET_TESTCONFIG, "19239569-19239573.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19240615_19240637() {
+    replay(OLD_MAINNET_TESTCONFIG, "19240615-19240637.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19241185_19241187() {
+    replay(OLD_MAINNET_TESTCONFIG, "19241185-19241187.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19241495_19241523() {
+    replay(OLD_MAINNET_TESTCONFIG, "19241495-19241523.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19242040_19242058() {
+    replay(OLD_MAINNET_TESTCONFIG, "19242040-19242058.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19242738_19242740() {
+    replay(OLD_MAINNET_TESTCONFIG, "19242738-19242740.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19247347_19247355() {
+    replay(OLD_MAINNET_TESTCONFIG, "19247347-19247355.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19247389_19247391() {
+    replay(OLD_MAINNET_TESTCONFIG, "19247389-19247391.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19248865_19248877() {
+    replay(OLD_MAINNET_TESTCONFIG, "19248865-19248877.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19248878_19248882() {
+    replay(OLD_MAINNET_TESTCONFIG, "19248878-19248882.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19249100_19249128() {
+    replay(OLD_MAINNET_TESTCONFIG, "19249100-19249128.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19250714_19250739() {
+    replay(OLD_MAINNET_TESTCONFIG, "19250714-19250739.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19251076_19251097() {
+    replay(OLD_MAINNET_TESTCONFIG, "19251076-19251097.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19251417_19251433() {
+    replay(OLD_MAINNET_TESTCONFIG, "19251417-19251433.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19251688_19251695() {
+    replay(OLD_MAINNET_TESTCONFIG, "19251688-19251695.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19252291_19252328() {
+    replay(OLD_MAINNET_TESTCONFIG, "19252291-19252328.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19252392_19252424() {
+    replay(OLD_MAINNET_TESTCONFIG, "19252392-19252424.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19253122_19253160() {
+    replay(OLD_MAINNET_TESTCONFIG, "19253122-19253160.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19255567_19255570() {
+    replay(OLD_MAINNET_TESTCONFIG, "19255567-19255570.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19255638_19255648() {
+    replay(OLD_MAINNET_TESTCONFIG, "19255638-19255648.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19260916_19260920() {
+    replay(OLD_MAINNET_TESTCONFIG, "19260916-19260920.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19261984_19261986() {
+    replay(OLD_MAINNET_TESTCONFIG, "19261984-19261986.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19263140_19263143() {
+    replay(OLD_MAINNET_TESTCONFIG, "19263140-19263143.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19263589_19263617() {
+    replay(OLD_MAINNET_TESTCONFIG, "19263589-19263617.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19263697_19263706() {
+    replay(OLD_MAINNET_TESTCONFIG, "19263697-19263706.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19263746_19263759() {
+    replay(OLD_MAINNET_TESTCONFIG, "19263746-19263759.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19269273_19269283() {
+    replay(OLD_MAINNET_TESTCONFIG, "19269273-19269283.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19276590_19276593() {
+    replay(OLD_MAINNET_TESTCONFIG, "19276590-19276593.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19277153_19277155() {
+    replay(OLD_MAINNET_TESTCONFIG, "19277153-19277155.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19277205_19277209() {
+    replay(OLD_MAINNET_TESTCONFIG, "19277205-19277209.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19277210_19277210() {
+    replay(OLD_MAINNET_TESTCONFIG, "19277210-19277210.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19277227_19277227() {
+    replay(OLD_MAINNET_TESTCONFIG, "19277227-19277227.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19277362_19277363() {
+    replay(OLD_MAINNET_TESTCONFIG, "19277362-19277363.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19277661_19277662() {
+    replay(OLD_MAINNET_TESTCONFIG, "19277661-19277662.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19278085_19278095() {
+    replay(OLD_MAINNET_TESTCONFIG, "19278085-19278095.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19278117_19278139() {
+    replay(OLD_MAINNET_TESTCONFIG, "19278117-19278139.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19278361_19278377() {
+    replay(OLD_MAINNET_TESTCONFIG, "19278361-19278377.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19278457_19278460() {
+    replay(OLD_MAINNET_TESTCONFIG, "19278457-19278460.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19279568_19279581() {
+    replay(OLD_MAINNET_TESTCONFIG, "19279568-19279581.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19280065_19280068() {
+    replay(OLD_MAINNET_TESTCONFIG, "19280065-19280068.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19280843_19280861() {
+    replay(OLD_MAINNET_TESTCONFIG, "19280843-19280861.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19281442_19281478() {
+    replay(OLD_MAINNET_TESTCONFIG, "19281442-19281478.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19282860_19282878() {
+    replay(OLD_MAINNET_TESTCONFIG, "19282860-19282878.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19283417_19283451() {
+    replay(OLD_MAINNET_TESTCONFIG, "19283417-19283451.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19285053_19285075() {
+    replay(OLD_MAINNET_TESTCONFIG, "19285053-19285075.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19286476_19286504() {
+    replay(OLD_MAINNET_TESTCONFIG, "19286476-19286504.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19286910_19286917() {
+    replay(OLD_MAINNET_TESTCONFIG, "19286910-19286917.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19288775_19288813() {
+    replay(OLD_MAINNET_TESTCONFIG, "19288775-19288813.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19288947_19288980() {
+    replay(OLD_MAINNET_TESTCONFIG, "19288947-19288980.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19289613_19289614() {
+    replay(OLD_MAINNET_TESTCONFIG, "19289613-19289614.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19289615_19289616() {
+    replay(OLD_MAINNET_TESTCONFIG, "19289615-19289616.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19290441_19290479() {
+    replay(OLD_MAINNET_TESTCONFIG, "19290441-19290479.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19291103_19291136() {
+    replay(OLD_MAINNET_TESTCONFIG, "19291103-19291136.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19291773_19291811() {
+    replay(OLD_MAINNET_TESTCONFIG, "19291773-19291811.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19292980_19292983() {
+    replay(OLD_MAINNET_TESTCONFIG, "19292980-19292983.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19293000_19293004() {
+    replay(OLD_MAINNET_TESTCONFIG, "19293000-19293004.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19293359_19293395() {
+    replay(OLD_MAINNET_TESTCONFIG, "19293359-19293395.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19293534_19293548() {
+    replay(OLD_MAINNET_TESTCONFIG, "19293534-19293548.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19293971_19293980() {
+    replay(OLD_MAINNET_TESTCONFIG, "19293971-19293980.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19295632_19295635() {
+    replay(OLD_MAINNET_TESTCONFIG, "19295632-19295635.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19296085_19296117() {
+    replay(OLD_MAINNET_TESTCONFIG, "19296085-19296117.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19304297_19304313() {
+    replay(OLD_MAINNET_TESTCONFIG, "19304297-19304313.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19307602_19307640() {
+    replay(OLD_MAINNET_TESTCONFIG, "19307602-19307640.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19308734_19308766() {
+    replay(OLD_MAINNET_TESTCONFIG, "19308734-19308766.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19308851_19308874() {
+    replay(OLD_MAINNET_TESTCONFIG, "19308851-19308874.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19321451_19321489() {
+    replay(OLD_MAINNET_TESTCONFIG, "19321451-19321489.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19325021_19325059() {
+    replay(OLD_MAINNET_TESTCONFIG, "19325021-19325059.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19325718_19325721() {
+    replay(OLD_MAINNET_TESTCONFIG, "19325718-19325721.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19326229_19326250() {
+    replay(OLD_MAINNET_TESTCONFIG, "19326229-19326250.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19327194_19327209() {
+    replay(OLD_MAINNET_TESTCONFIG, "19327194-19327209.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19327846_19327876() {
+    replay(OLD_MAINNET_TESTCONFIG, "19327846-19327876.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19329972_19330002() {
+    replay(OLD_MAINNET_TESTCONFIG, "19329972-19330002.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19336921_19336941() {
+    replay(OLD_MAINNET_TESTCONFIG, "19336921-19336941.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19337400_19337419() {
+    replay(OLD_MAINNET_TESTCONFIG, "19337400-19337419.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19337440_19337460() {
+    replay(OLD_MAINNET_TESTCONFIG, "19337440-19337460.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19344473_19344507() {
+    replay(OLD_MAINNET_TESTCONFIG, "19344473-19344507.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19347697_19347735() {
+    replay(OLD_MAINNET_TESTCONFIG, "19347697-19347735.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19353891_19353891() {
+    replay(OLD_MAINNET_TESTCONFIG, "19353891-19353891.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19357441_19357479() {
+    replay(OLD_MAINNET_TESTCONFIG, "19357441-19357479.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19359596_19359610() {
+    replay(OLD_MAINNET_TESTCONFIG, "19359596-19359610.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19366075_19366077() {
+    replay(OLD_MAINNET_TESTCONFIG, "19366075-19366077.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19366078_19366079() {
+    replay(OLD_MAINNET_TESTCONFIG, "19366078-19366079.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19366461_19366463() {
+    replay(OLD_MAINNET_TESTCONFIG, "19366461-19366463.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19369046_19369075() {
+    replay(OLD_MAINNET_TESTCONFIG, "19369046-19369075.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19372571_19372577() {
+    replay(OLD_MAINNET_TESTCONFIG, "19372571-19372577.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19373666_19373700() {
+    replay(OLD_MAINNET_TESTCONFIG, "19373666-19373700.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19374039_19374077() {
+    replay(OLD_MAINNET_TESTCONFIG, "19374039-19374077.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19376303_19376332() {
+    replay(OLD_MAINNET_TESTCONFIG, "19376303-19376332.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19377828_19377866() {
+    replay(OLD_MAINNET_TESTCONFIG, "19377828-19377866.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19378155_19378193() {
+    replay(OLD_MAINNET_TESTCONFIG, "19378155-19378193.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19379192_19379193() {
+    replay(OLD_MAINNET_TESTCONFIG, "19379192-19379193.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19380366_19380387() {
+    replay(OLD_MAINNET_TESTCONFIG, "19380366-19380387.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19383544_19383563() {
+    replay(OLD_MAINNET_TESTCONFIG, "19383544-19383563.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19385106_19385144() {
+    replay(OLD_MAINNET_TESTCONFIG, "19385106-19385144.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19386801_19386832() {
+    replay(OLD_MAINNET_TESTCONFIG, "19386801-19386832.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19387636_19387636() {
+    replay(OLD_MAINNET_TESTCONFIG, "19387636-19387636.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19390295_19390333() {
+    replay(OLD_MAINNET_TESTCONFIG, "19390295-19390333.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19390851_19390860() {
+    replay(OLD_MAINNET_TESTCONFIG, "19390851-19390860.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19397327_19397330() {
+    replay(OLD_MAINNET_TESTCONFIG, "19397327-19397330.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19397361_19397365() {
+    replay(OLD_MAINNET_TESTCONFIG, "19397361-19397365.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19397483_19397488() {
+    replay(OLD_MAINNET_TESTCONFIG, "19397483-19397488.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19397965_19398001() {
+    replay(OLD_MAINNET_TESTCONFIG, "19397965-19398001.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19398290_19398293() {
+    replay(OLD_MAINNET_TESTCONFIG, "19398290-19398293.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19399253_19399257() {
+    replay(OLD_MAINNET_TESTCONFIG, "19399253-19399257.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19400123_19400133() {
+    replay(OLD_MAINNET_TESTCONFIG, "19400123-19400133.mainnet.json.gz");
+  }
+
+  @Test
+  void split_range_19400178_19400184() {
+    replay(OLD_MAINNET_TESTCONFIG, "19400178-19400184.mainnet.json.gz");
+  }
+}
