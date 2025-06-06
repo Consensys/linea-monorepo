51e3d6b717545fcd83111f4d3de3d865b90855c1
Thomas Piellard <thomas.piellard@consensys.net>
2025-06-06 11:58:36 +0200

    feat: wip clean tests

--- prover/crypto/vortex/commitment_test.go
+++ prover/crypto/vortex/commitment_test.go
@@ -3,7 +3,6 @@ package vortex
 import (
 	"crypto/sha256"
 	"fmt"
-	"runtime/debug"
 	"testing"
 
 	"github.com/consensys/linea-monorepo/prover/crypto/ringsis"
@@ -15,22 +14,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// testParams is a corpus of valid parameters for Vortex
-var testParams = []*Params{
-	//NewParams(2, 1<<4, 32, ringsis.StdParams, poseidon2.NewPoseidon2),
-	NewParams(2, 1<<4, 32, ringsis.StdParams, sha256.New), //TODO@yao change to poseidon2
-	//NewParams(2, 1<<4, 32, ringsis.StdParams, mimc.NewMiMC).RemoveSis(mimc.NewMiMC),
-	//NewParams(4, 1<<3, 32, ringsis.StdParams, mimc.NewMiMC),
-	//NewParams(4, 1<<3, 32, ringsis.StdParams, mimc.NewMiMC).RemoveSis(mimc.NewMiMC),
-}
-
-func TestLlocal(t *testing.T) {
+func TestProver(t *testing.T) {
 
 	params := NewParams(2, 1<<4, 32, ringsis.StdParams, sha256.New)
 	x := fext.RandomElement()
 	randomCoin := fext.RandomElement()
 	entryList := []int{1, 7, 5, 6, 4, 5, 1, 2}
-	NbPolysPerCommitment := []int{2}
+	NbPolysPerCommitment := []int{20, 32, 32, 32}
 	nbCommitments := len(NbPolysPerCommitment)
 	polySize := params.NbColumns
 	NumOpenedColumns := 4
@@ -48,13 +38,6 @@ func TestLlocal(t *testing.T) {
 		polyLists[i] = polys
 		yLists[i] = ys
 	}
-	for i := 0; i < len(yLists); i++ {
-		for j := 0; j < len(yLists[i]); j++ {
-			fmt.Printf("%s, ", yLists[i][j].String())
-			fmt.Println("")
-		}
-		fmt.Println("-")
-	}
 
 	// commit
 	roots := make([]types.Bytes32, nbCommitments)
@@ -68,194 +51,8 @@ func TestLlocal(t *testing.T) {
 	// open
 	proof := params.Open(utils.Join(polyLists...), randomCoin)
 	proof.Complete(entryList[:NumOpenedColumns], committedMatrices, trees)
-}
 
-func TestProver(t *testing.T) {
-
-	x := fext.RandomElement()
-	randomCoin := fext.RandomElement()
-	entryList := []int{1, 7, 5, 6, 4, 5, 1, 2}
-
-	// the testCases are applied over all those of [testParams]
-	testCases := []struct {
-		Explainer            string
-		NbPolysPerCommitment []int
-		NumOpenedColumns     int
-		// ChangeAssignmentSize takes params.NbColumns and returns a possibly
-		// different value corresponding to the size of the assignment that
-		// the testCase provides to the prover. If nil, then this is equivalent
-		// to `f(n) -> n`.
-		ChangeAssignmentSize func(int) int
-		MustPanic            bool
-	}{
-		{
-			Explainer:            "1 matrix commitment with one poly",
-			NbPolysPerCommitment: []int{1},
-			NumOpenedColumns:     4,
-		},
-		// {
-		// 	Explainer:             "1 matrix commitment with several polys",
-		// 	NbPolysPerCommitment: []int{3},
-		// 	NumOpenedColumns:      4,
-		// },
-		// {
-		// 	Explainer:             "1 matrix commitment with several polys",
-		// 	NbPolysPerCommitment: []int{3, 3},
-		// 	NumOpenedColumns:      8,
-		// },
-		// {
-		// 	Explainer:             "1 matrix commitment with several polys",
-		// 	NbPolysPerCommitment: []int{1, 15},
-		// 	NumOpenedColumns:      8,
-		// },
-		// {
-		// 	Explainer:             "too many rows",
-		// 	NbPolysPerCommitment: []int{1, 105},
-		// 	NumOpenedColumns:      8,
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "no commitment",
-		// 	NbPolysPerCommitment: []int{},
-		// 	NumOpenedColumns:      8,
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "1 commitment but zero rows",
-		// 	NbPolysPerCommitment: []int{0},
-		// 	NumOpenedColumns:      8,
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "Several commitment but none have rows",
-		// 	NbPolysPerCommitment: []int{0, 0, 0},
-		// 	NumOpenedColumns:      8,
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "Several commitment but none have rows",
-		// 	NbPolysPerCommitment: []int{0, 0, 0},
-		// 	NumOpenedColumns:      8,
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "Empty entry list",
-		// 	NbPolysPerCommitment: []int{5, 6},
-		// 	NumOpenedColumns:      0,
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "the polys are twice too large",
-		// 	NbPolysPerCommitment: []int{3, 3},
-		// 	NumOpenedColumns:      8,
-		// 	ChangeAssignmentSize:  func(i int) int { return 2 * i },
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "the polys are twice to small",
-		// 	NbPolysPerCommitment: []int{3, 3},
-		// 	NumOpenedColumns:      8,
-		// 	ChangeAssignmentSize:  func(i int) int { return i / 2 },
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "the polys are twice to small",
-		// 	NbPolysPerCommitment: []int{3, 3},
-		// 	NumOpenedColumns:      8,
-		// 	ChangeAssignmentSize:  func(i int) int { return i + 1 },
-		// 	MustPanic:             true,
-		// },
-		// {
-		// 	Explainer:             "the polys are twice to small",
-		// 	NbPolysPerCommitment: []int{3, 3},
-		// 	NumOpenedColumns:      8,
-		// 	ChangeAssignmentSize:  func(i int) int { return i - 1 },
-		// 	MustPanic:             true,
-		// },
-	}
-
-	for i := range testParams {
-		params := testParams[i]
-		for j := range testCases {
-
-			t.Run(fmt.Sprintf("params-%v-case-%v", i, j), func(t *testing.T) {
-
-				testCase := testCases[j]
-
-				t.Logf("params=%++v test-case=%++v", params, testCase)
-
-				nbCommitments := len(testCase.NbPolysPerCommitment)
-				effPolySize := params.NbColumns
-				polyLists := make([][]smartvectors.SmartVector, nbCommitments)
-				yLists := make([][]fext.Element, nbCommitments)
-				roots := make([]types.Bytes32, nbCommitments)
-				trees := make([]*smt.Tree, nbCommitments)
-
-				if testCase.ChangeAssignmentSize != nil {
-					effPolySize = testCase.ChangeAssignmentSize(effPolySize)
-				}
-
-				for i := range polyLists {
-					// Polynomials to commit to
-					polys := make([]smartvectors.SmartVector, testCase.NbPolysPerCommitment[i])
-					ys := make([]fext.Element, testCase.NbPolysPerCommitment[i])
-					for j := range polys {
-						polys[j] = smartvectors.Rand(effPolySize)
-
-						// effPolySize is messed with and is not a power of 2,
-						// the interpolation algorithm will panic as this counts
-						// as invalid inputs.
-						if utils.IsPowerOfTwo(effPolySize) {
-							ys[j] = smartvectors.EvaluateLagrangeOnFext(polys[j], x)
-						} else {
-							ys[j].SetRandom()
-						}
-					}
-					polyLists[i] = polys
-					yLists[i] = ys
-				}
-
-				// Importantly, the defer must be declared right before calling
-				// the tested code or it will occult potential mistakes in the
-				// test.
-				if testCase.MustPanic {
-					defer func() {
-						if r := recover(); r != nil {
-							t.Logf("Panicked with message = %v and stacktraces %v", r, string(debug.Stack()))
-							return
-						}
-
-						t.Fatalf("The test did not panic")
-					}()
-				}
-
-				// Commits to it
-				committedMatrices := make([]EncodedMatrix, nbCommitments)
-				for i := range trees {
-					committedMatrices[i], trees[i], _ = params.CommitMerkleWithSIS(polyLists[i])
-					roots[i] = trees[i].Root
-				}
-
-				// Generate the proof
-				proof := params.Open(utils.Join(polyLists...), randomCoin)
-				proof.Complete(entryList[:testCase.NumOpenedColumns], committedMatrices, trees)
-
-				// Check the proof
-				err := VerifyOpening(
-					&VerifierInputs{
-						Params:       *params,
-						MerkleRoots:  roots,
-						X:            x,
-						Ys:           yLists,
-						OpeningProof: *proof,
-						RandomCoin:   randomCoin,
-						EntryList:    entryList[:testCase.NumOpenedColumns],
-					})
-
-				require.NoError(t, err)
-			})
-		}
-	}
+	// verify
 }
 
 func TestVerifierNegative(t *testing.T) {
