f3b047f567a7b6d15b2ce3d39eede92166768a4c
The Dark Jester <thedarkjester@users.noreply.github.com>
2025-06-06 05:03:02 -0700

    Update contracts/src/messaging/l2/v1/interfaces/IL2MessageServiceV1.sol
    Co-authored-by: Victorien Gauch <85494462+VGau@users.noreply.github.com>
    Signed-off-by: The Dark Jester <thedarkjester@users.noreply.github.com>

--- contracts/src/messaging/l2/v1/interfaces/IL2MessageServiceV1.sol
+++ contracts/src/messaging/l2/v1/interfaces/IL2MessageServiceV1.sol
@@ -11,7 +11,7 @@ interface IL2MessageServiceV1 {
    * @notice Returns the ABI version and not the reinitialize version.
    * @return contractVersion The contract ABI version.
    */
-  function CONTRACT_VERSION() external returns (string memory contractVersion);
+  function CONTRACT_VERSION() external view returns (string memory contractVersion);
 
   /**
    * @notice The Fee Manager sets a minimum fee to address DOS protection.
