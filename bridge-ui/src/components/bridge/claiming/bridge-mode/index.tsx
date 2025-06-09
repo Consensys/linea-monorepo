81583c8d141dae231a498c3c4c3e5fdf1eaf454f
Victorien Gauch <85494462+VGau@users.noreply.github.com>
2025-06-09 16:13:35 +0200

    feat(bridge-ui): add base path config (#1095)
    * fix: add base path config to the bridge ui
    
    * fix: remove unused import
    
    * update bridge UI ci build job
    
    * fix: update env.template

--- bridge-ui/src/components/bridge/claiming/bridge-mode/index.tsx
+++ bridge-ui/src/components/bridge/claiming/bridge-mode/index.tsx
@@ -8,7 +8,9 @@ export default function BridgeMode() {
   const token = useFormStore((state) => state.token);
   const label = token.bridgeProvider === BridgeProvider.NATIVE ? "Native bridge" : "CCTP";
   const logoSrc =
-    token.bridgeProvider === BridgeProvider.NATIVE ? "/images/logo/linea-rounded.svg" : "/images/logo/cctp.svg";
+    token.bridgeProvider === BridgeProvider.NATIVE
+      ? `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logo/linea-rounded.svg`
+      : `${process.env.NEXT_PUBLIC_BASE_PATH}/images/logo/cctp.svg`;
 
   return (
     <div className={styles.container}>
