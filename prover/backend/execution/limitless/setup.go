5539ca7dc63edace6b6d7089b01ce3d5754dcb4c
srinathln7 <srinath.nandakumar@consensys.net>
2025-06-11 15:05:31 +0000

    (fix1) init mod dw ser/de

--- prover/backend/execution/limitless/setup.go
+++ prover/backend/execution/limitless/setup.go
@@ -11,7 +11,6 @@ import (
 	"github.com/consensys/linea-monorepo/prover/protocol/distributed"
 	"github.com/consensys/linea-monorepo/prover/protocol/serialization"
 	"github.com/consensys/linea-monorepo/prover/utils"
-	"github.com/consensys/linea-monorepo/prover/utils/test_utils"
 	"github.com/sirupsen/logrus"
 )
 
@@ -27,16 +26,16 @@ func SerAssestAndWrite(config *config.Config) error {
 	}
 
 	var (
-		zkevm = test_utils.GetZkEVM()
-		disc  = &distributed.StandardModuleDiscoverer{
-			TargetWeight: 1 << 28,
-			Affinities:   test_utils.GetAffinities(zkevm),
-			Predivision:  1,
-		}
+	// zkevm = test_utils.GetZkEVM()
+	// disc  = &distributed.StandardModuleDiscoverer{
+	// 	TargetWeight: 1 << 28,
+	// 	Affinities:   test_utils.GetAffinities(zkevm),
+	// 	Predivision:  1,
+	// }
 
-		distWizardRaw = distributed.DistributeWizard(zkevm.WizardIOP, disc)
-		// distWizardCompiled = distWizardRaw.CompileSegments()
-		// distWizard         = distWizardCompiled.Conglomerate(20)
+	// distWizardRaw = distributed.DistributeWizard(zkevm.WizardIOP, disc)
+	// distWizardCompiled = distWizardRaw.CompileSegments()
+	// distWizard         = distWizardCompiled.Conglomerate(20)
 	)
 
 	// Define assets to serialize and write
@@ -46,15 +45,43 @@ func SerAssestAndWrite(config *config.Config) error {
 	}{
 		// {name: "zkevm.bin", object: zkevm},
 		// {name: "disc.bin", object: disc},
-		{name: "dw-raw.bin", object: distWizardRaw},
-		// {name: "dw-compiled.bin", object: distWizardCompiled},
-		// {name: "dw.bin", object: distWizard},
+		// {name: "dw-raw.bin", object: distWizardRaw},
+		{name: "dw-compiled.bin"},
+		{name: "dw.bin"},
 	}
 
 	// Serialize and write each asset
 	reader := bytes.NewReader(nil) // Initialize with nil slice
 	var writtenFiles []string
 	for _, asset := range assets {
+
+		switch asset.name {
+		case "dw-compiled.bin":
+			var readBuf bytes.Buffer
+			dwRawPath := path.Join(filePath, "dw-raw.bin")
+			if err := utils.ReadFromFile(dwRawPath, &readBuf); err != nil {
+				return fmt.Errorf("failed to read %s: %w", dwRawPath, err)
+			}
+
+			var dwRaw *distributed.DistributedWizard
+			if err := serialization.Deserialize(readBuf.Bytes(), &dwRaw); err != nil {
+				return fmt.Errorf("failed to deserialize %s: %w", dwRawPath, err)
+			}
+
+			asset.object = dwRaw.CompileSegments()
+		case "dw.bin":
+			var readBuf bytes.Buffer
+			dwCompiledPath := path.Join(filePath, "dw-compiled.bin")
+			if err := utils.ReadFromFile(dwCompiledPath, &readBuf); err != nil {
+				return fmt.Errorf("failed to read %s: %w", dwCompiledPath, err)
+			}
+			var dwCompiled *distributed.DistributedWizard
+			if err := serialization.Deserialize(readBuf.Bytes(), &dwCompiled); err != nil {
+				return fmt.Errorf("failed to deserialize %s: %w", dwCompiledPath, err)
+			}
+			asset.object = dwCompiled.Conglomerate(20)
+		}
+
 		data, err := serialization.Serialize(asset.object)
 		if err != nil {
 			return fmt.Errorf("failed to serialize %s: %w", asset.name, err)
