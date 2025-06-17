37a1c1cdebc4d5658863a89d0bf91f7ea6831900
srinathln7 <srinath.nandakumar@consensys.net>
2025-06-17 08:04:53 +0000

    (feat): add ser of GL/LPP witness

--- prover/backend/execution/limitless/gl.go
+++ prover/backend/execution/limitless/gl.go
@@ -1,6 +1,10 @@
 package limitless
 
 import (
+	"bytes"
+	"path"
+
+	"github.com/consensys/linea-monorepo/prover/config"
 	"github.com/consensys/linea-monorepo/prover/protocol/distributed"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	"github.com/consensys/linea-monorepo/prover/utils"
@@ -25,7 +29,28 @@ func RunProverGLs(
 	for i, witnessGL := range witnessGLs {
 		var moduleGL *distributed.RecursedSegmentCompilation
 
+		// The wording is not 100% consistent and well-defined. Here, module denotes an horizontal split.
+		// And module.index indicates the ID of the segment as part of a particular module.
+		// For ex: it looks something like this:
+
+		// | SegmentID   | Module        | Module.index  |
+		// | 0           | A             | 0             |
+		// | 1           | A             | 1             |
+		// | 2           | A             | 2             |
+		// | 3           | A             | 3             |
+		// | 4           | A             | 4             |
+		// | 5           | B             | 0             |
+		// | 6           | B             | 1             |
+		// | 7           | B             | 2             |
+		// | 8           | B             | 3             |
+		// | 9           | B             | 4             |
+		// | 10          | C             | 0             |
+		// | 11          | C             | 1             |
+		// | 12          | C             | 2             |
+		// | 13          | C             | 3             |
+
 		logrus.Infof("segment(total)=%v module=%v segment.index=%v", i, witnessGL.ModuleName, witnessGL.ModuleIndex)
+
 		for k := range distWizard.ModuleNames {
 			if distWizard.ModuleNames[k] != witnessGLs[i].ModuleName {
 				continue
@@ -46,4 +71,11 @@ func RunProverGLs(
 
 func RunGLProver(moduleGL *distributed.RecursedSegmentCompilation, witnessGL *distributed.ModuleWitnessGL) *wizard.ProverRuntime {
 	return moduleGL.ProveSegment(witnessGL)
+}
+
+func SerializeAndWriteWitnessGL(cfg *config.Config, witnessGLName string, witnessGL *distributed.ModuleWitnessGL) error {
+	reader := bytes.NewReader(nil)
+	filePath := cfg.PathforLimitlessProverAssets()
+	filePath = path.Join(filePath, "witnesses")
+	return serializeAndWrite(filePath, witnessGLName, witnessGL, reader)
 }
