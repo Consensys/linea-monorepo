bc4a2bcc83728260b4c7c632ff1d3d9e051d6561
VGau <victorien.gauch@consensys.net>
2025-06-18 17:05:17 +0200

    fix: update csp headers in bridge ui

--- bridge-ui/src/middleware.ts
+++ bridge-ui/src/middleware.ts
@@ -54,8 +54,7 @@ export function middleware(request: NextRequest) {
    */
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' ${unsafeScript} https://bridge.linea.build https://bridge-devnet.linea.build 
-https://www.googletagmanager.com/gtm.js https://widget.intercom.io/widget/h5zisg78;
+    script-src 'self' 'nonce-${nonce}' https://www.googletagmanager.com/gtm.js https://widget.intercom.io/widget/h5zisg78 https://ajax.cloudflare.com https://js.intercomcdn.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: https:;
     font-src 'self' data: https://cdn.jsdelivr.net;
