5a77c236299bc9c47078f8913539d6e3958237c8
srinathln7 <srinath.nandakumar@consensys.net>
2025-06-18 08:43:20 +0000

    (build): build exec limitless ckt

---
+++ prover/circuits/execution/builder_limitless.go
@@ -0,0 +1,32 @@
+package execution
+
+import (
+	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
+	"github.com/consensys/gnark/constraint"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/linea-monorepo/prover/config"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+)
+
+type builder_limitless struct {
+	congWIOP *wizard.CompiledIOP
+}
+
+func NewLimitlessBuilder(congWIOP *wizard.CompiledIOP) *builder_limitless {
+	return &builder_limitless{congWIOP: congWIOP}
+}
+
+func (b *builder_limitless) Compile() (constraint.ConstraintSystem, error) {
+	return makeLimitlessCS(b.congWIOP), nil
+}
+
+// Makes the constraint system for the execution-limitless circuit
+func makeLimitlessCS(congWIOP *wizard.CompiledIOP) constraint.ConstraintSystem {
+	circuit := AllocateLimitless(congWIOP, &config.TracesLimits{})
+	scs, err := frontend.Compile(fr.Modulus(), scs.NewBuilder, &circuit, frontend.WithCapacity(1<<24))
+	if err != nil {
+		panic(err)
+	}
+	return scs
+}
