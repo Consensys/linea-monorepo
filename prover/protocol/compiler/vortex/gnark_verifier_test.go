4c835f9e96f7854afc16de8b2ca92f919ed46649
arijitdutta67 <arijit.dutta67@gmail.com>
2025-06-19 17:16:45 +0530

    Threshold added

--- prover/protocol/compiler/vortex/gnark_verifier_test.go
+++ prover/protocol/compiler/vortex/gnark_verifier_test.go
@@ -107,8 +107,8 @@ func TestVortexGnarkVerifier(t *testing.T) {
 
 	compiled := wizard.Compile(
 		define,
-		vortex.Compile(4),
-	)
+		vortex.Compile(4,
+			vortex.WithOptionalSISHashingThreshold(1 << 20))) // Set to 0 to disable SIS hashing
 	proof := wizard.Prove(compiled, prove)
 
 	// Just as a sanity check, do not run the Plonk
