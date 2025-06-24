eed6e8bfe0f91c8ff3cf8b0066a44b2279e90577
srinathln7 <srinath.nandakumar@consensys.net>
2025-06-24 17:17:35 +0000

    (fix): ser/de - bin dump cong obj sanity check pass

--- prover/protocol/serialization/tests/assets/distributed_wizard_test.go
+++ prover/protocol/serialization/tests/assets/distributed_wizard_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"runtime"
-	"slices"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/utils/unsafe"
@@ -12,17 +11,45 @@ import (
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	"github.com/consensys/linea-monorepo/prover/symbolic"
+	"github.com/consensys/linea-monorepo/prover/utils"
 	utils_limitless "github.com/consensys/linea-monorepo/prover/utils/limitless"
 	"github.com/consensys/linea-monorepo/prover/utils/profiling"
 	"github.com/consensys/linea-monorepo/prover/utils/test_utils"
 )
 
-// GetDistributed initializes a distributed wizard configuration using the
+type distributeTestCase struct {
+	numRow int
+}
+
+func (d distributeTestCase) define(comp *wizard.CompiledIOP) {
+
+	// Define the first module
+	a0 := comp.InsertCommit(0, "a0", d.numRow)
+	b0 := comp.InsertCommit(0, "b0", d.numRow)
+	c0 := comp.InsertCommit(0, "c0", d.numRow)
+
+	// Importantly, the second module must be slightly different than the first
+	// one because else it will create a wierd edge case in the conglomeration:
+	// as we would have two GL modules with the same verifying key and we would
+	// not be able to infer a module from a VK.
+	//
+	// We differentiate the modules by adding a duplicate constraints for GL0
+	a1 := comp.InsertCommit(0, "a1", d.numRow)
+	b1 := comp.InsertCommit(0, "b1", d.numRow)
+	c1 := comp.InsertCommit(0, "c1", d.numRow)
+
+	comp.InsertGlobal(0, "global-0", symbolic.Sub(c0, b0, a0))
+	comp.InsertGlobal(0, "global-duplicate", symbolic.Sub(c0, b0, a0))
+	comp.InsertGlobal(0, "global-1", symbolic.Sub(c1, b1, a1))
+
+	comp.InsertInclusion(0, "inclusion-0", []ifaces.Column{c0, b0, a0}, []ifaces.Column{c1, b1, a1})
+}
+
+// GetDistWizard initializes a distributed wizard configuration using the
 // ZkEVM's compiled IOP and a StandardModuleDiscoverer with preset parameters.
 // The function compiles the necessary segments and produces a conglomerated
 // distributed wizard, which is then returned.
-func GetDistributed() *distributed.DistributedWizard {
-
+func GetDistWizard() *distributed.DistributedWizard {
 	var (
 		zkevm = test_utils.GetZkEVM()
 		disc  = &distributed.StandardModuleDiscoverer{
@@ -34,14 +61,36 @@ func GetDistributed() *distributed.DistributedWizard {
 		// This tests the compilation of the compiled-IOP
 		distWizard = distributed.DistributeWizard(zkevm.WizardIOP, disc).
 				CompileSegments().
-				Conglomerate(5)
+				Conglomerate(20)
+	)
+
+	return distWizard
+}
+
+func GetBasicDistWizard() *distributed.DistributedWizard {
+
+	var (
+		numRow = 1 << 10
+		tc     = distributeTestCase{numRow: numRow}
+		disc   = &distributed.StandardModuleDiscoverer{
+			TargetWeight: 3 * numRow,
+			Predivision:  1,
+		}
+		comp = wizard.Compile(func(build *wizard.Builder) {
+			tc.define(build.CompiledIOP)
+		})
+
+		// This tests the compilation of the compiled-IOP
+		distWizard = distributed.DistributeWizard(comp, disc).
+				CompileSegments().
+				Conglomerate(20)
 	)
 
 	return distWizard
 }
 
 func TestSerdeDistWizard(t *testing.T) {
-	dist := GetDistributed()
+	dist := GetDistWizard()
 
 	t.Run("Bootstrapper", func(t *testing.T) {
 		runSerdeTest(t, dist.Bootstrapper, "DistributedWizard.Bootstrapper", true)
@@ -76,100 +125,59 @@ func TestSerdeDistWizard(t *testing.T) {
 }
 
 func TestSerdeCongWIOP(t *testing.T) {
+	distWizard := GetDistWizard()
+	cong := distWizard.CompiledConglomeration
+	distWizard = nil
+	runtime.GC()
+	unsafeBinDump(t, cong, true)
+}
+
+func unsafeBinDump(t *testing.T, cong *distributed.ConglomeratorCompilation, sanityCheck bool) {
 
 	var (
-		numRow = 1 << 10
-		tc     = DistributeTestCase{numRow: numRow}
-		disc   = &distributed.StandardModuleDiscoverer{
-			TargetWeight: 3 * numRow,
-			Predivision:  1,
-		}
-		comp = wizard.Compile(func(build *wizard.Builder) {
-			tc.Define(build.CompiledIOP)
-		})
-
-		// This tests the compilation of the compiled-IOP
-		distWizard = distributed.DistributeWizard(comp, disc).
-				CompileSegments().
-				Conglomerate(20)
+		buffer  = &bytes.Buffer{}
+		readBuf = bytes.NewReader(nil)
 	)
 
-	// runSerdeTest(t, comp, "initialWizard.CompiledIOP", true) => PASS
-	// runSerdeTest(t, distWizard.CompiledConglomeration.Wiop, "DistributedWizard.CompiledConglomeration.WIOP", true)
-
-	buffer := &bytes.Buffer{}
-	iop := distWizard.CompiledConglomeration.Wiop
-
 	serTime := profiling.TimeIt(func() {
-		err := unsafe.WriteSlice(buffer, []*wizard.CompiledIOP{iop})
+		err := unsafe.WriteSlice(buffer, []*distributed.ConglomeratorCompilation{cong})
 		if err != nil {
 			t.Fatalf("could not marshal array of compiled IOP: %v", err)
 		}
 	})
 
-	var iop2 []*wizard.CompiledIOP
+	// Write to file
 	var err error
+	err = utils.WriteToFile("cong-dump.bin", readBuf)
+	if err != nil {
+		t.Fatalf("could not write to file: %v", err)
+	}
+
+	var deCong []*distributed.ConglomeratorCompilation
+
 	deserTime := profiling.TimeIt(func() {
-		readBuf := bytes.NewReader(buffer.Bytes())
-		iop2, _, err = unsafe.ReadSlice[[]*wizard.CompiledIOP](readBuf)
+
+		err = utils.ReadFromFile("cong-dump.bin", buffer)
+		if err != nil {
+			t.Fatalf("could not read from file: %v", err)
+		}
+		deCong, _, err = unsafe.ReadSlice[[]*distributed.ConglomeratorCompilation](buffer)
 		if err != nil {
 			t.Fatalf("could not unmarshal array of compiled IOP: %v", err)
 		}
 	})
 
 	t.Logf("%s serialization=%v deserialization=%v buffer-size=%v \n", "conglomeration", serTime, deserTime, len(buffer.Bytes()))
-	t.Logf("Running sanity check on ser/de cong. object")
-	if !test_utils.CompareExportedFields(iop, iop2[0]) {
-		t.Fatalf("IOPs are not equal")
-	}
-}
 
-type DistributeTestCase struct {
-	numRow int
-}
+	t.Logf("(ser)   No. of rounds in cong.WIOP:%d \n", cong.Wiop.NumRounds())
+	t.Logf("(deser) No. of rounds in cong.WIOP:%d \n", deCong[0].Wiop.NumRounds())
+	t.Logf("(ser)   QueriesParams mapping inner length in cong.WIOP:%d \n", len(cong.Wiop.QueriesParams.Mapping.InnerMap))
+	t.Logf("(deser) QueriesParams mapping inner length in cong.WIOP:%d \n", len(deCong[0].Wiop.QueriesParams.Mapping.InnerMap))
 
-func (d DistributeTestCase) Define(comp *wizard.CompiledIOP) {
-
-	// Define the first module
-	a0 := comp.InsertCommit(0, "a0", d.numRow)
-	b0 := comp.InsertCommit(0, "b0", d.numRow)
-	c0 := comp.InsertCommit(0, "c0", d.numRow)
-
-	// Importantly, the second module must be slightly different than the first
-	// one because else it will create a wierd edge case in the conglomeration:
-	// as we would have two GL modules with the same verifying key and we would
-	// not be able to infer a module from a VK.
-	//
-	// We differentiate the modules by adding a duplicate constraints for GL0
-	a1 := comp.InsertCommit(0, "a1", d.numRow)
-	b1 := comp.InsertCommit(0, "b1", d.numRow)
-	c1 := comp.InsertCommit(0, "c1", d.numRow)
-
-	comp.InsertGlobal(0, "global-0", symbolic.Sub(c0, b0, a0))
-	comp.InsertGlobal(0, "global-duplicate", symbolic.Sub(c0, b0, a0))
-	comp.InsertGlobal(0, "global-1", symbolic.Sub(c1, b1, a1))
-
-	comp.InsertInclusion(0, "inclusion-0", []ifaces.Column{c0, b0, a0}, []ifaces.Column{c1, b1, a1})
-}
-
-func TestUnsafeSerde(t *testing.T) {
-	var buf bytes.Buffer
-
-	// Slice with pointers (strings)
-	data := []string{"hello", "unsafe", "world!"}
-
-	err := unsafe.WriteSlice(&buf, data)
-	if err != nil {
-		t.Fatalf("could not marshal array of strings: %v", err)
-	}
-
-	readBuf := bytes.NewReader(buf.Bytes())
-	data2, _, err := unsafe.ReadSlice[[]string](readBuf)
-	if err != nil {
-		t.Fatalf("could not unmarshal array of strings: %v", err)
-	}
-
-	if !slices.Equal(data, data2) {
-		t.Fatalf("strings are not equal")
+	if sanityCheck {
+		t.Logf("Running sanity check on ser/de cong. object")
+		if !test_utils.CompareExportedFields(cong, deCong[0]) {
+			t.Fatalf("Ser/de conglomerator compilation are not equal")
+		}
 	}
 }
