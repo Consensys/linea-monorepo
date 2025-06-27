05e0c8900953ff84fda6e181416fa5ac4fe177af
jonesho <81145364+jonesho@users.noreply.github.com>
2025-06-27 12:05:43 +0800

    feat: add support of TLS connection between coordinator and web3signer (#1213)
    * feat: add support of TLS connection between coordinator and web3signer
    
    * feat: revise the SignerConfig check
    
    * feat: revise the L1 dynamic gas price doc
    
    * temp: workaround to fix e2e test skipped issue
    
    * Revert "temp: workaround to fix e2e test skipped issue"
    
    This reverts commit 1df4dab0dc2e5db45f71cc43e214943084648481.
    
    * fix: tls paths in coordinator config for docker
    
    * fix: tls paths in coordinator config for docker
    
    * fix: key stores and trust stores
    
    * feat: revise SignerConfig init check logic
    
    * feat: change folder names to kebab-case and other chnages based on reviews
    
    * feat: change type from String to Path
    
    * feat: change type from String to Path

--- coordinator/app/src/main/kotlin/net/consensys/zkevm/coordinator/app/BlockchainClientHelper.kt
+++ coordinator/app/src/main/kotlin/net/consensys/zkevm/coordinator/app/BlockchainClientHelper.kt
@@ -2,6 +2,7 @@ package net.consensys.zkevm.coordinator.app
 
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpVersion
+import io.vertx.core.net.PfxOptions
 import io.vertx.ext.web.client.WebClientOptions
 import linea.kotlin.encodeHex
 import linea.web3j.SmartContractErrors
@@ -17,12 +18,65 @@ import org.web3j.protocol.Web3j
 import org.web3j.service.TxSignServiceImpl
 import org.web3j.tx.gas.ContractGasProvider
 import org.web3j.utils.Numeric
+import java.io.FileInputStream
+import java.nio.file.Path
+import java.security.KeyStore
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 fun createTransactionManager(
   vertx: Vertx,
   signerConfig: linea.coordinator.config.v2.SignerConfig,
   client: Web3j,
 ): AsyncFriendlyTransactionManager {
+  fun loadKeyAndTrustStoreFromFiles(
+    webClientOptions: WebClientOptions,
+    clientKeystorePath: Path,
+    clientKeystorePassword: String,
+    trustStorePath: Path,
+    trustStorePassword: String,
+  ): WebClientOptions {
+    // Load client keystore
+    val keyStore = KeyStore.getInstance("PKCS12")
+    FileInputStream(clientKeystorePath.toAbsolutePath().toString()).use { input ->
+      keyStore.load(input, clientKeystorePassword.toCharArray())
+    }
+
+    // Initialize KeyManagerFactory for client certificate
+    val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm())
+    keyManagerFactory.init(keyStore, clientKeystorePassword.toCharArray())
+
+    // Load truststore
+    val trustStore = KeyStore.getInstance("PKCS12")
+    FileInputStream(trustStorePath.toAbsolutePath().toString()).use { input ->
+      trustStore.load(input, trustStorePassword.toCharArray())
+    }
+
+    // Initialize TrustManagerFactory for server certificate
+    val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+    trustManagerFactory.init(trustStore)
+
+    // Initialize SSLContext
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(keyManagerFactory.keyManagers, trustManagerFactory.trustManagers, null)
+
+    return webClientOptions
+      .setSsl(true)
+      .setTrustAll(false)
+      .setPfxKeyCertOptions(
+        PfxOptions()
+          .setPath(clientKeystorePath.toAbsolutePath().toString())
+          .setPassword(clientKeystorePassword),
+      )
+      .setPfxTrustOptions(
+        PfxOptions()
+          .setPath(trustStorePath.toAbsolutePath().toString())
+          .setPassword(trustStorePassword),
+      )
+      .setVerifyHost(true)
+  }
+
   val transactionSignService = when (signerConfig.type) {
     linea.coordinator.config.v2.SignerConfig.SignerType.WEB3J -> {
       TxSignServiceImpl(Credentials.create(signerConfig.web3j!!.privateKey.encodeHex()))
@@ -38,6 +92,17 @@ fun createTransactionManager(
           .setMaxPoolSize(web3SignerConfig.maxPoolSize)
           .setDefaultHost(endpoint.host)
           .setDefaultPort(endpoint.port)
+          .also {
+            if (signerConfig.web3signer.tls != null) {
+              loadKeyAndTrustStoreFromFiles(
+                webClientOptions = it,
+                clientKeystorePath = signerConfig.web3signer.tls.keyStorePath,
+                clientKeystorePassword = signerConfig.web3signer.tls.keyStorePassword.value,
+                trustStorePath = signerConfig.web3signer.tls.trustStorePath,
+                trustStorePassword = signerConfig.web3signer.tls.trustStorePassword.value,
+              )
+            }
+          }
       val httpRestClient = VertxHttpRestClient(webClientOptions, vertx)
       val signer = Web3SignerRestClient(httpRestClient, signerConfig.web3signer.publicKey.encodeHex())
       val signerAdapter = ECKeypairSignerAdapter(signer, Numeric.toBigInt(signerConfig.web3signer.publicKey))
