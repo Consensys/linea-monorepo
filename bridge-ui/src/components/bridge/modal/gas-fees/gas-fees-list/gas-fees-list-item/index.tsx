72ad060f6ec1de88952f3ed247450d7e8ac755bf
VGau <victorien.gauch@consensys.net>
2025-07-08 13:48:58 +0200

    fix: fee display

--- bridge-ui/src/components/bridge/modal/gas-fees/gas-fees-list/gas-fees-list-item/index.tsx
+++ bridge-ui/src/components/bridge/modal/gas-fees/gas-fees-list/gas-fees-list-item/index.tsx
@@ -13,14 +13,16 @@ type Props = {
 export default function GasFeesListItem({ name, fee, fiatValue, currency }: Props) {
   const feeText = useMemo(() => {
     if (fee === 0n) return "Free";
-    return <span dangerouslySetInnerHTML={{ __html: `${formatDigit(fee)} ETH` }} />;
+    return `${formatDigit(fee)} ETH`;
   }, [fee]);
 
   return (
     <li className={styles["list-item"]}>
       <span>{name}</span>
       <div className={styles["fee-row"]}>
-        <span className={styles["fee-value"]}>{feeText}</span>
+        <span className={styles["fee-value"]}>
+          <span dangerouslySetInnerHTML={{ __html: feeText }} />
+        </span>
         {fiatValue && (
           <span className={styles["fee-fiat-value"]}>
             {fiatValue.toLocaleString("en-US", {
