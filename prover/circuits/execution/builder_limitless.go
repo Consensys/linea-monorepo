a969243e031cf9fae2db151b085ac2e0658ee6b3
AlexandreBelling <alexandrebelling8@gmail.com>
2025-07-14 19:27:41 +0000

    minor(limitless): remove the gnark profiling for the outer circuit

--- prover/circuits/execution/builder_limitless.go
+++ prover/circuits/execution/builder_limitless.go
@@ -5,7 +5,6 @@ import (
 	"github.com/consensys/gnark/constraint"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/scs"
-	"github.com/consensys/gnark/profile"
 	"github.com/consensys/linea-monorepo/prover/config"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	"github.com/consensys/linea-monorepo/prover/zkevm"
@@ -28,10 +27,6 @@ func (b *limitlessBuilder) Compile() (constraint.ConstraintSystem, error) {
 // Makes the constraint system for the execution-limitless circuit
 func makeCSLimitless(b *limitlessBuilder) constraint.ConstraintSystem {
 	circuit := AllocateLimitless(b.congWIOP, b.traceLimits)
-
-	// To be removed as it slows down the compilation
-	gnarkPprof := profile.Start(profile.WithPath("execution_limitless_gnark.pprof"))
-	defer gnarkPprof.Stop()
 
 	scs, err := frontend.Compile(fr.Modulus(), scs.NewBuilder, &circuit, frontend.WithCapacity(1<<24))
 	if err != nil {
