7fb058c38a78fb490682fcb065b1656d2219ed24
AlexandreBelling <alexandrebelling8@gmail.com>
2025-07-17 18:38:55 +0000

    feat(serialization): expose the DeepCmp function

--- prover/protocol/serialization/zkevm_test.go
+++ prover/protocol/serialization/zkevm_test.go
@@ -61,7 +61,7 @@ func runSerdeTest(t *testing.T, input any, name string, isSanityCheck, failFast 
 		// Sanity check: Compare exported fields
 		t.Logf("Running sanity checks on deserialized object: Comparing if the values matched before and after serialization")
 		outputDeref := reflect.ValueOf(output).Elem().Interface()
-		if !compareExportedFields(input, outputDeref, failFast) {
+		if !serialization.DeepCmp(input, outputDeref, failFast) {
 			t.Errorf("Mismatch in exported fields of %s during serde", name)
 		} else {
 			t.Logf("Sanity checks passed for %s", name)
