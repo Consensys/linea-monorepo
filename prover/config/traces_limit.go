dc3446cf6ee7b53e21c8d46e813812b1247bb1c6
Alexandre Belling <alexandrebelling8@gmail.com>
2025-07-22 13:40:32 +0000

    fix(limits): don't scale the unscalable limits

--- prover/config/traces_limit.go
+++ prover/config/traces_limit.go
@@ -116,12 +116,8 @@ func (tl *TracesLimits) ScaleUp(by int) {
 	tl.Trm *= by
 	tl.Txndata *= by
 	tl.Wcp *= by
-	tl.Binreftable *= by
-	tl.Shfreftable *= by
-	tl.Instdecoder *= by
 	tl.PrecompileEcrecoverEffectiveCalls *= by
 	tl.PrecompileSha2Blocks *= by
-	tl.PrecompileRipemdBlocks *= by
 	tl.PrecompileModexpEffectiveCalls *= by
 	tl.PrecompileModexpEffectiveCalls4096 *= by
 	tl.PrecompileEcaddEffectiveCalls *= by
@@ -129,8 +125,6 @@ func (tl *TracesLimits) ScaleUp(by int) {
 	tl.PrecompileEcpairingEffectiveCalls *= by
 	tl.PrecompileEcpairingMillerLoops *= by
 	tl.PrecompileEcpairingG2MembershipCalls *= by
-	tl.PrecompileBlakeEffectiveCalls *= by
-	tl.PrecompileBlakeRounds *= by
 	tl.BlockKeccak *= by
 	tl.BlockTransactions *= by
 	tl.ShomeiMerkleProofs *= by
