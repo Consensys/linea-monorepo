042c7f4981ed3bb9cd9fdd7e91028c107d4c8f9c
Fabio Di Fabio <fabio.difabio@consensys.net>
2025-07-25 18:00:22 +0200

    Feat: Introduce bundle with priority
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- besu-plugins/linea-sequencer/sequencer/src/main/java/net/consensys/linea/bundles/TransactionBundle.java
+++ besu-plugins/linea-sequencer/sequencer/src/main/java/net/consensys/linea/bundles/TransactionBundle.java
@@ -9,12 +9,24 @@
 
 package net.consensys.linea.bundles;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_ABSENT;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.SequencedMap;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 import lombok.EqualsAndHashCode;
@@ -23,49 +35,57 @@ import lombok.ToString;
 import lombok.experimental.Accessors;
 import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.datatypes.PendingTransaction;
 import org.hyperledger.besu.datatypes.parameters.UnsignedLongParameter;
 import org.hyperledger.besu.ethereum.core.Transaction;
-import org.hyperledger.besu.ethereum.rlp.BytesValueRLPOutput;
 
 /** TransactionBundle class representing a collection of pending transactions with metadata. */
 @Accessors(fluent = true)
 @Getter
 @EqualsAndHashCode
 @ToString
+@JsonInclude(NON_ABSENT)
+@JsonPropertyOrder({"blockNumber", "minTimestamp", "maxTimestamp"})
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class TransactionBundle {
   private static final AtomicLong BUNDLE_COUNT = new AtomicLong(0L);
-  private final long sequence = BUNDLE_COUNT.incrementAndGet();
+  private final transient long sequence = BUNDLE_COUNT.incrementAndGet();
   private final Hash bundleIdentifier;
-  private final List<PendingBundleTx> pendingTransactions;
+  private final List<? extends PendingBundleTx> pendingTransactions;
   private final Long blockNumber;
   private final Optional<Long> minTimestamp;
   private final Optional<Long> maxTimestamp;
   private final Optional<List<Hash>> revertingTxHashes;
   private final Optional<UUID> replacementUUID;
+  private final boolean hasPriority;
 
+  @JsonCreator
   public TransactionBundle(
-      final Hash bundleIdentifier,
-      final List<Transaction> transactions,
-      final Long blockNumber,
-      final Optional<Long> minTimestamp,
-      final Optional<Long> maxTimestamp,
-      final Optional<List<Hash>> revertingTxHashes,
-      final Optional<UUID> replacementUUID) {
+      @JsonProperty("bundleIdentifier") final Hash bundleIdentifier,
+      @JsonProperty("pendingTransactions") final List<Transaction> transactions,
+      @JsonProperty("blockNumber") final Long blockNumber,
+      @JsonProperty("minTimestamp") final Optional<Long> minTimestamp,
+      @JsonProperty("maxTimestamp") final Optional<Long> maxTimestamp,
+      @JsonProperty("revertingTxHashes") final Optional<List<Hash>> revertingTxHashes,
+      @JsonProperty("replacementUUID") final Optional<UUID> replacementUUID,
+      @JsonProperty("hasPriority") final boolean hasPriority) {
     this.bundleIdentifier = bundleIdentifier;
-    this.pendingTransactions = transactions.stream().map(PendingBundleTx::new).toList();
+    this.pendingTransactions =
+        transactions.stream()
+            .map(hasPriority ? PriorityPendingBundleTx::new : NormalPendingBundleTx::new)
+            .toList();
     this.blockNumber = blockNumber;
     this.minTimestamp = minTimestamp;
     this.maxTimestamp = maxTimestamp;
     this.revertingTxHashes = revertingTxHashes;
     this.replacementUUID = replacementUUID;
+    this.hasPriority = hasPriority;
   }
 
-  public BundleParameter toBundleParameter(final boolean compact) {
+  public BundleParameter toBundleParameter() {
     return new BundleParameter(
         pendingTransactions.stream()
-            .map(
-                ptx ->
-                    compact ? ptx.toBase64String() : ptx.getTransaction().encoded().toHexString())
+            .map(ptx -> ptx.getTransaction().encoded().toHexString())
             .toList(),
         new UnsignedLongParameter(blockNumber),
         minTimestamp,
@@ -75,53 +95,159 @@ public class TransactionBundle {
         Optional.empty());
   }
 
-  @JsonValue
-  public Map<Hash, BundleParameter> serialize() {
-    return Map.of(bundleIdentifier, toBundleParameter(true));
-  }
-
-  @JsonCreator
-  public static TransactionBundle deserialize(
-      final SequencedMap<Hash, BundleParameter> serialized) {
-    final var entry = serialized.firstEntry();
-    final var hash = entry.getKey();
-    final var parameters = entry.getValue();
-
-    return new TransactionBundle(
-        hash,
-        parameters.txs().stream().map(Bytes::fromBase64String).map(Transaction::readFrom).toList(),
-        parameters.blockNumber(),
-        parameters.minTimestamp(),
-        parameters.maxTimestamp(),
-        parameters.revertingTxHashes(),
-        parameters.replacementUUID().map(UUID::fromString));
-  }
-
   /** A pending transaction contained in a bundle. */
-  public class PendingBundleTx
-      extends org.hyperledger.besu.ethereum.eth.transactions.PendingTransaction.Local {
+  public interface PendingBundleTx extends PendingTransaction {
+    TransactionBundle getBundle();
 
-    public PendingBundleTx(final Transaction transaction) {
+    default String toBase64String() {
+      return getTransaction().encoded().toBase64String();
+    }
+
+    default boolean isBundleStart() {
+      return getBundle().pendingTransactions().getFirst().equals(this);
+    }
+  }
+
+  /** A pending transaction contained in a bundle without priority. */
+  private class NormalPendingBundleTx
+      extends org.hyperledger.besu.ethereum.eth.transactions.PendingTransaction.Local
+      implements PendingBundleTx {
+
+    public NormalPendingBundleTx(final Transaction transaction) {
       super(transaction);
     }
 
+    @Override
     public TransactionBundle getBundle() {
       return TransactionBundle.this;
-    }
-
-    public boolean isBundleStart() {
-      return getBundle().pendingTransactions().getFirst().equals(this);
     }
 
     @Override
     public String toTraceLog() {
       return "Bundle tx: " + super.toTraceLog();
     }
+  }
 
-    String toBase64String() {
-      final var rlpOutput = new BytesValueRLPOutput();
-      getTransaction().writeTo(rlpOutput);
-      return rlpOutput.encoded().toBase64String();
+  /** A pending transaction contained in a bundle with priority. */
+  private class PriorityPendingBundleTx
+      extends org.hyperledger.besu.ethereum.eth.transactions.PendingTransaction.Local.Priority
+      implements PendingBundleTx {
+
+    public PriorityPendingBundleTx(final Transaction transaction) {
+      super(transaction);
+    }
+
+    @Override
+    public TransactionBundle getBundle() {
+      return TransactionBundle.this;
+    }
+
+    @Override
+    public String toTraceLog() {
+      return "Priority bundle tx: " + super.toTraceLog();
+    }
+  }
+
+  public static class HashSerializer extends StdSerializer<Hash> {
+    public HashSerializer() {
+      this(null);
+    }
+
+    public HashSerializer(final Class<Hash> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(
+        final Hash value, final JsonGenerator gen, final SerializerProvider provider)
+        throws IOException {
+      gen.writeString(value.toHexString());
+    }
+  }
+
+  public static class HashDeserializer extends StdDeserializer<Hash> {
+    public HashDeserializer() {
+      this(null);
+    }
+
+    @Override
+    public Hash deserialize(final JsonParser p, final DeserializationContext ctxt)
+        throws IOException {
+      return Hash.fromHexString(p.getValueAsString());
+    }
+
+    public HashDeserializer(final Class<Hash> t) {
+      super(t);
+    }
+  }
+
+  public static class PendingBundleTxSerializer extends StdSerializer<PendingBundleTx> {
+    public PendingBundleTxSerializer() {
+      this(null);
+    }
+
+    public PendingBundleTxSerializer(final Class<PendingBundleTx> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(
+        final PendingBundleTx pendingBundleTx,
+        final JsonGenerator gen,
+        final SerializerProvider serializerProvider)
+        throws IOException {
+      gen.writeString(pendingBundleTx.toBase64String());
+    }
+  }
+
+  public static class PendingBundleTxDeserializer extends StdDeserializer<Transaction> {
+    public PendingBundleTxDeserializer() {
+      this(null);
+    }
+
+    public PendingBundleTxDeserializer(final Class<?> vc) {
+      super(vc);
+    }
+
+    @Override
+    public Transaction deserialize(
+        final JsonParser jsonParser, final DeserializationContext deserializationContext)
+        throws IOException {
+      return Transaction.readFrom(Bytes.fromBase64String(jsonParser.getValueAsString()));
+    }
+  }
+
+  public static class TransactionBundleDeserializerV1 extends StdDeserializer<TransactionBundle> {
+    private static final TypeReference<Map.Entry<Hash, BundleParameter>> TYPE_REFERENCE =
+        new TypeReference<>() {};
+
+    public TransactionBundleDeserializerV1() {
+      this(null);
+    }
+
+    public TransactionBundleDeserializerV1(final Class<?> vc) {
+      super(vc);
+    }
+
+    @Override
+    public TransactionBundle deserialize(final JsonParser p, final DeserializationContext ctxt)
+        throws IOException {
+      @SuppressWarnings("unchecked")
+      final var entry = (Map.Entry<Hash, BundleParameter>) p.readValueAs(TYPE_REFERENCE);
+      final var hash = entry.getKey();
+      final var parameters = entry.getValue();
+      return new TransactionBundle(
+          hash,
+          parameters.txs().stream()
+              .map(Bytes::fromBase64String)
+              .map(Transaction::readFrom)
+              .toList(),
+          parameters.blockNumber(),
+          parameters.minTimestamp(),
+          parameters.maxTimestamp(),
+          parameters.revertingTxHashes(),
+          parameters.replacementUUID().map(UUID::fromString),
+          false);
     }
   }
 }
