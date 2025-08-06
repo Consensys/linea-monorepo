f3c0e80fc6dbd5aa40ef9abda5b56f39cdd9fd17
jonesho <jones.ho@consensys.net>
2025-08-07 02:35:17 +0800

    feat: simplify and shorten liveness e2e

--- e2e/src/liveness.spec.ts
+++ e2e/src/liveness.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
 import { config } from "./config/tests-config";
-import { execDockerCommand, getBlockByNumberOrBlockTag, pollForBlockNumber, wait } from "./common/utils";
+import { awaitUntil, execDockerCommand, getBlockByNumberOrBlockTag, wait } from "./common/utils";
+import { Log } from "ethers";
 
 // should remove skip only when the linea-sequencer plugin supports liveness
 describe("Liveness test suite", () => {
@@ -23,8 +24,8 @@ describe("Liveness test suite", () => {
         await execDockerCommand("stop", "sequencer");
         logger.debug("Sequencer stopped.");
 
-        // sleep for 8 sec
-        await wait(8000);
+        // sleep for 6 sec
+        await wait(6000);
 
         const block = await config.getL2Provider().getBlock("latest");
         lastBlockTimestamp = block?.timestamp;
@@ -38,40 +39,36 @@ describe("Liveness test suite", () => {
         throw error;
       }
 
-      // wait until the target block is available
-      await pollForBlockNumber(config.getL2Provider(), lastBlockNumber! + 1);
+      const targetBlockNumber = lastBlockNumber! + 1;
+      logger.debug(`targetBlockNumber=${JSON.stringify(targetBlockNumber)}`);
 
-      const targetBlock = await getBlockByNumberOrBlockTag(config.getL2BesuNodeEndpoint()!, lastBlockNumber! + 1, true);
-      const targetBlockTimestamp: number | undefined = targetBlock?.timestamp;
-      logger.debug(`targetBlock=${JSON.stringify(targetBlock)}`);
+      const livenessEvents = await awaitUntil(
+        async () => {
+          return config.getL2Provider().getLogs({
+            topics: [
+              "0x0559884fd3a460db3073b7fc896cc77986f16e378210ded43186175bf646fc5f", // AnswerUpdated event
+            ],
+            fromBlock: targetBlockNumber,
+            toBlock: targetBlockNumber,
+            address: livenessContractAddress,
+          });
+        },
+        (ethLogs: Array<Log>) => ethLogs.length >= 2,
+        1000,
+        60000,
+      );
 
-      // The latest status should be true to indicate sequencer is up
-      // and the startedAt and updatedAt should be greater than or equal to the target block timestamp
-      const latestRoundData = await livenessContract.latestRoundData();
-      expect(latestRoundData.answer).toEqual(0n); // which mean sequencer is currently reported as up
-      expect(latestRoundData.startedAt).toBeGreaterThanOrEqual(BigInt(targetBlockTimestamp!));
-      expect(latestRoundData.updatedAt).toBeGreaterThanOrEqual(BigInt(targetBlockTimestamp!));
+      logger.debug(`livenessEvents=${JSON.stringify(livenessEvents)}`);
 
       // The first two transactions of the target block should be the transactions
       // with "to" as the liveness contract address
+      const targetBlock = await getBlockByNumberOrBlockTag(config.getL2BesuNodeEndpoint()!, targetBlockNumber, true);
       expect(targetBlock?.transactions.length).toBeGreaterThanOrEqual(2);
 
       const downtimeTransaction = targetBlock?.transactions.at(0);
       const uptimeTransaction = targetBlock?.transactions.at(1);
-
-      const livenessEvents = await config.getL2Provider().getLogs({
-        topics: [
-          "0x0559884fd3a460db3073b7fc896cc77986f16e378210ded43186175bf646fc5f", // AnswerUpdated event
-        ],
-        fromBlock: targetBlock?.number,
-        toBlock: targetBlock?.number,
-        address: livenessContractAddress,
-      });
-
-      logger.debug(`livenessEvents=${JSON.stringify(livenessEvents)}`);
-
-      const downtimeEvent = livenessEvents.find((tx) => tx.transactionHash === downtimeTransaction);
-      const uptimeEvent = livenessEvents.find((tx) => tx.transactionHash === uptimeTransaction);
+      const downtimeEvent = livenessEvents?.find((tx) => tx.transactionHash === downtimeTransaction);
+      const uptimeEvent = livenessEvents?.find((tx) => tx.transactionHash === uptimeTransaction);
 
       // check the first AnswerUpdated event is for downtime
       expect(downtimeEvent?.transactionIndex).toEqual(0);
@@ -83,6 +80,6 @@ describe("Liveness test suite", () => {
       expect(uptimeEvent?.index).toEqual(1);
       expect(parseInt(uptimeEvent?.topics[1] ?? "", 16)).toEqual(0); // topics[1] was the given status to update, should be 0 for uptime
     },
-    100000,
+    60000,
   );
 });
