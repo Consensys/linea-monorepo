e5e22c7fffccfc8c944ee7da9718017c7de757b4
AlexandreBelling <alexandrebelling8@gmail.com>
2025-08-09 17:03:35 +0200

    fix(generate): remove deprecated go:generate directive

--- prover/crypto/ringsis/gen.go
+++
@@ -1,5 +0,0 @@
-package ringsis
-
-//go:generate go run ./templates --logTwoBound=16 --modulusDegree=64
-//go:generate go run ./templates --logTwoBound=8 --modulusDegree=64
-//go:generate go run ./templates --logTwoBound=8 --modulusDegree=32
