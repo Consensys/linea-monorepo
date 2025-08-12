c198e984560b4df049662b12628765fc22bcb26b
srinathln7 <srinath.nandakumar@consensys.net>
2025-08-12 14:32:24 +0000

    fix lint err in protocol/dedicated/byte32cmp

--- prover/protocol/dedicated/byte32cmp/decompose.go
+++ prover/protocol/dedicated/byte32cmp/decompose.go
@@ -110,7 +110,7 @@ func Decompose(comp *wizard.CompiledIOP, col any, numLimbs int, bitPerLimbs int,
 		// Enforces the range over the limbs
 		comp.InsertRange(
 			round,
-			ifaces.QueryIDf("LIMB_DECOMPOSITION_%d_%s", len(comp.ListCommitments()), "LIMB_"+strconv.Itoa(i)),
+			ifaces.QueryIDf("%s", ctxName("LIMB_"+strconv.Itoa(i))),
 			limbs[i],
 			1<<bitPerLimbs,
 		)
