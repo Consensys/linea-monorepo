f45207aca0a84117f61c90428d63a0ca72651581
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2025-08-13 16:03:34 +1000

    [Feat] Sequencer plugin to block Type4 Tx (#1305)
    * did unit tests and core class
    
    * implemented eip7702TransactionIsRejectedFromTransactionPool
    
    * mild fix to eip7702 nonce
    
    * passed acceptance tests for eip7702
    
    * refactor common engineapi utility methods into common base class
    
    * fix spotless
    
    * refactor magical strings out
    
    * add javadoc comments
    
    * refactor assertBlockImportRejected
    
    * spotlessApply
    
    * edit for pr comment
    
    * remove dead comment

--- besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/EIP7702TransactionDenialTest.java
+++ besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/EIP7702TransactionDenialTest.java
@@ -9,6 +9,7 @@
 package linea.plugin.acc.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collections;
@@ -22,23 +23,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.methods.response.EthSendTransaction;
 
 /**
- * Tests that verify the LineaTransactionValidationPlugin correctly rejects BLOB transactions from
- * being executed
+ * Tests that verify the LineaTransactionValidationPlugin correctly rejects EIP7702 DELEGATE_CODE
+ * transactions from being executed
  */
-public class BlobTransactionDenialTest extends LineaPluginTestBasePrague {
+public class EIP7702TransactionDenialTest extends LineaPluginTestBasePrague {
   private Web3j web3j;
   private Credentials credentials;
   private String recipient;
-
-  @Override
-  protected String getGenesisFileTemplatePath() {
-    // We cannot use clique-prague-zero-blobs because `config.blobSchedule.prague.max = 0` will
-    // block all blob txs
-    return "/clique/clique-prague-one-blob.json.tpl";
-  }
 
   @Override
   @BeforeEach
@@ -50,21 +43,26 @@ public class BlobTransactionDenialTest extends LineaPluginTestBasePrague {
   }
 
   @Test
-  public void blobTransactionsIsRejectedFromTransactionPool() throws Exception {
-    // Act - Send a blob transaction to transaction pool
-    EthSendTransaction response = sendRawBlobTransaction(web3j, credentials, recipient);
+  public void eip7702TransactionIsRejectedFromTransactionPool() throws Exception {
+    // Act - Send an EIP7702 DELEGATE_CODE transaction to transaction pool and expect it to be
+    // rejected
+    // We use 'minerNode.execute' here which throw us a RuntimeException directly
+    RuntimeException exception =
+        assertThrows(
+            RuntimeException.class,
+            () -> {
+              sendRawEIP7702Transaction(web3j, credentials, recipient);
+            });
     // No need to build new block.
 
     // Assert
-    assertThat(response.hasError()).isTrue();
-    assertThat(response.getError().getMessage())
-        .contains("Plugin has marked the transaction as invalid");
+    assertThat(exception.getMessage()).contains("Plugin has marked the transaction as invalid");
   }
 
   // Ideally the block import test would be conducted with two nodes as follows:
   // 1. Start an additional minimal node with Prague config
   // 2. Ensure additional node is peered to minerNode
-  // 3. Send blob tx to additional node
+  // 3. Send EIP7702 tx to additional node
   // 4. Construct block on additional node
   // 5. Send 'debug_getBadBlocks' RPC request to minerNode, confirm that block is rejected from
   // import
@@ -73,62 +71,61 @@ public class BlobTransactionDenialTest extends LineaPluginTestBasePrague {
   // being
   // singleton options and this implemented in dependency repository - linea tracer.
   // Thus simulate the block import as below:
-  // 1. Create a premade block containing a blob tx
+  // 1. Create a premade block containing a EIP7702 tx
   // 2. Import the premade block using 'engine_newPayloadV4' Engine API call
 
   @Test
-  public void blobTransactionsIsRejectedFromNodeImport() throws Exception {
+  public void EIP7702TransactionIsRejectedFromNodeImport() throws Exception {
     // Arrange
-    EngineNewPayloadRequest blockWithBlobTxRequest = createBlobTransactionBlockRequest(mapper);
+    EngineNewPayloadRequest blockWithEIP7702TxRequest =
+        createEIP7702TransactionBlockRequest(mapper);
 
     // Act
     Response response =
         this.importPremadeBlock(
-            blockWithBlobTxRequest.executionPayload(),
-            blockWithBlobTxRequest.expectedBlobVersionedHashes(),
-            blockWithBlobTxRequest.parentBeaconBlockRoot(),
-            blockWithBlobTxRequest.executionRequests());
+            blockWithEIP7702TxRequest.executionPayload(),
+            blockWithEIP7702TxRequest.expectedBlobVersionedHashes(),
+            blockWithEIP7702TxRequest.parentBeaconBlockRoot(),
+            blockWithEIP7702TxRequest.executionRequests());
 
     // Assert
-    assertBlockImportRejected(response, LineaTransactionValidatorPluginErrors.BLOB_TX_NOT_ALLOWED);
+    assertBlockImportRejected(
+        response, LineaTransactionValidatorPluginErrors.DELEGATE_CODE_TX_NOT_ALLOWED);
   }
 
-  private EngineNewPayloadRequest createBlobTransactionBlockRequest(ObjectMapper mapper)
+  private EngineNewPayloadRequest createEIP7702TransactionBlockRequest(ObjectMapper mapper)
       throws Exception {
-    // Block parameters obtained by running `blobTransactionsIsRejectedFromTransactionPool` test
+    // Block parameters obtained by running `EIP7702TransactionIsRejectedFromNodeImport` test
     // without the LineaTransactionSelectorPlugin and LineaTransactionValidatorPlugin plugins.
     Map<String, String> blockWithBlockTxParams = new HashMap<>();
     blockWithBlockTxParams.put(
         BlockParams.STATE_ROOT,
-        "0x2c1457760c057cf42f2d509648d725ec1f557b9d8729a5361e517952f91d050e");
+        "0x217cc246352b4a22254ab139cc4a5a37e1dbe75b63fcf12161674773f5043bbe");
     blockWithBlockTxParams.put(
         BlockParams.LOGS_BLOOM,
         "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
     blockWithBlockTxParams.put(
         BlockParams.RECEIPTS_ROOT,
-        "0xeaa8c40899a61ae59615cf9985f5e2194f8fd2b57d273be63bde6733e89b12ab");
-    blockWithBlockTxParams.put(BlockParams.EXTRA_DATA, "0x626573752032352e362e302d6c696e656131");
+        "0x036c7d20420edbce24b5062148bce80563b48fd4532fb1c068b2c96a53117019");
+    blockWithBlockTxParams.put(BlockParams.EXTRA_DATA, "0x626573752032352e372e302d6c696e656134");
     blockWithBlockTxParams.put(
-        TransactionDataKeys.BLOB_TX,
-        "0x03f8908205398084f461090084f46109008389544094627306090abab3a6e1400e9345bc60c78a8bef578080c001e1a0018ef96865998238a5e1783b6cafbc1253235d636f15d318f1fb50ef6a5b8f6a80a0576a95756f32ab705a22b591ab464d5affc8c1c7fcd14d777bac24d83bc44821a01f93b26f4f9989c3fe764f4a58d264bcd71b9deab72d6852f5dcdf19d55494f1");
-    blockWithBlockTxParams.put(
-        TransactionDataKeys.BLOB_VERSIONED_HASH,
-        "0x018ef96865998238a5e1783b6cafbc1253235d636f15d318f1fb50ef6a5b8f6a");
+        TransactionDataKeys.DELEGATE_CALL_TX,
+        "0x04f8cd8205398084f461090084f46109008389544094fe3b557e8fb62b89f4916b721be55ceb828dbd738080c0f85ef85c82053994627306090abab3a6e1400e9345bc60c78a8bef570101a0972498bc9ef3b18ec9f16f3dd59e7b622cc07fce1459d7485f424658e4013aa6a038dfeeaaa952cf3eb3fb81ccac2b57a69f57b457d1350353ac80d11ddc5dfeb180a0713d685d1b0fd47e7e7e75d9d8aaf2fa0d4a8811aec37fa54cb0ca4deb632dcaa01a66a49c9bbd92f11a0e67fb096a6e862a5da8bf05af32f6390ee431011fba81");
     blockWithBlockTxParams.put(
         BlockParams.EXECUTION_REQUEST,
         "0x01a4664c40aacebd82a2db79f0ea36c06bc6a19adbb10a4a15bf67b328c9b101d09e5c6ee6672978fdad9ef0d9e2ceffaee99223555d8601f0cb3bcc4ce1af9864779a416e0000000000000000");
     blockWithBlockTxParams.put(
         BlockParams.TRANSACTIONS_ROOT,
-        "0x7a430a1c9da1f6e25ff8e6e96217c359784f3438dc1d983b4695355d66437f8f");
+        "0xb84030d9aae336c44f284a9710bc8f6771a38d0bccdeb1d837f871bacd1d07c9");
     blockWithBlockTxParams.put(
         BlockParams.WITHDRAWALS_ROOT,
         "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421");
     blockWithBlockTxParams.put(BlockParams.GAS_LIMIT, "0x1ca35ef");
-    blockWithBlockTxParams.put(BlockParams.GAS_USED, "0x5208");
+    blockWithBlockTxParams.put(BlockParams.GAS_USED, "0x8fc0");
     blockWithBlockTxParams.put(BlockParams.TIMESTAMP, "0x5");
     blockWithBlockTxParams.put(BlockParams.BASE_FEE_PER_GAS, "0x7");
     blockWithBlockTxParams.put(BlockParams.EXCESS_BLOB_GAS, "0x0");
-    blockWithBlockTxParams.put(BlockParams.BLOB_GAS_USED, "0x20000");
+    blockWithBlockTxParams.put(BlockParams.BLOB_GAS_USED, "0x0");
     blockWithBlockTxParams.put(BlockParams.BLOCK_NUMBER, "0x1");
     blockWithBlockTxParams.put(BlockParams.FEE_RECIPIENT, Address.ZERO.toHexString());
     blockWithBlockTxParams.put(BlockParams.PREV_RANDAO, Hash.ZERO.toHexString());
@@ -139,10 +136,8 @@ public class BlobTransactionDenialTest extends LineaPluginTestBasePrague {
 
     var executionPayload =
         createExecutionPayload(
-            mapper, genesisBlockHash, blockWithBlockTxParams, TransactionDataKeys.BLOB_TX);
-    var expectedBlobVersionedHashes =
-        createBlobVersionedHashes(
-            mapper, blockWithBlockTxParams, TransactionDataKeys.BLOB_VERSIONED_HASH);
+            mapper, genesisBlockHash, blockWithBlockTxParams, TransactionDataKeys.DELEGATE_CALL_TX);
+    var expectedBlobVersionedHashes = createEmptyVersionedHashes(mapper);
     var executionRequests = createExecutionRequests(mapper, blockWithBlockTxParams);
     // Compute block hash and update payload
     var blockHeader = computeBlockHeader(executionPayload, mapper, blockWithBlockTxParams);
--- besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/LineaPluginTestBasePrague.java
+++ besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/LineaPluginTestBasePrague.java
@@ -9,6 +9,8 @@
 
 package linea.plugin.acc.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -21,15 +23,31 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
 import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.bytes.Bytes32;
 import org.hyperledger.besu.consensus.clique.CliqueExtraData;
+import org.hyperledger.besu.crypto.SECP256K1;
 import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.datatypes.BlobGas;
+import org.hyperledger.besu.datatypes.Hash;
+import org.hyperledger.besu.datatypes.RequestType;
+import org.hyperledger.besu.datatypes.TransactionType;
+import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.parameters.EnginePayloadParameter;
+import org.hyperledger.besu.ethereum.core.BlockHeader;
+import org.hyperledger.besu.ethereum.core.Difficulty;
+import org.hyperledger.besu.ethereum.core.Request;
+import org.hyperledger.besu.ethereum.core.Transaction;
 import org.hyperledger.besu.ethereum.eth.transactions.ImmutableTransactionPoolConfiguration;
 import org.hyperledger.besu.ethereum.eth.transactions.TransactionPoolConfiguration;
+import org.hyperledger.besu.ethereum.mainnet.BodyValidation;
+import org.hyperledger.besu.ethereum.mainnet.MainnetBlockHeaderFunctions;
 import org.hyperledger.besu.tests.acceptance.dsl.EngineAPIService;
 import org.hyperledger.besu.tests.acceptance.dsl.node.RunnableNode;
 import org.hyperledger.besu.tests.acceptance.dsl.node.configuration.genesis.GenesisConfigurationFactory;
@@ -57,6 +75,7 @@ public abstract class LineaPluginTestBasePrague extends LineaPluginTestBase {
   private static final BigInteger GAS_LIMIT = DefaultGasProvider.GAS_LIMIT;
   private static final BigInteger VALUE = BigInteger.ZERO;
   private static final String DATA = "0x";
+  private static final SECP256K1 secp256k1 = new SECP256K1();
 
   // Override this in subclasses to use a different genesis file template
   protected String getGenesisFileTemplatePath() {
@@ -169,6 +188,62 @@ public abstract class LineaPluginTestBasePrague extends LineaPluginTestBase {
     return web3j.ethSendRawTransaction(hexValue).send();
   }
 
+  /**
+   * Creates and sends EIP7702 delegate code transaction. This method is designed to be stateless
+   * and should not rely on any class properties or instance methods. All required data should be
+   * passed as parameters. This makes it easier to test and reuse in different contexts.
+   */
+  protected String sendRawEIP7702Transaction(Web3j web3j, Credentials credentials, String recipient)
+      throws IOException {
+    BigInteger nonce =
+        web3j
+            .ethGetTransactionCount(credentials.getAddress(), DefaultBlockParameterName.PENDING)
+            .send()
+            .getTransactionCount();
+
+    // 7702 transaction
+    final org.hyperledger.besu.datatypes.CodeDelegation codeDelegation =
+        org.hyperledger.besu.ethereum.core.CodeDelegation.builder()
+            .chainId(BigInteger.valueOf(CHAIN_ID))
+            .address(Address.fromHexStringStrict(recipient))
+            .nonce(1)
+            .signAndBuild(
+                secp256k1.createKeyPair(
+                    secp256k1.createPrivateKey(credentials.getEcKeyPair().getPrivateKey())));
+
+    final Transaction tx =
+        Transaction.builder()
+            .type(TransactionType.DELEGATE_CODE)
+            .chainId(BigInteger.valueOf(CHAIN_ID))
+            .nonce(nonce.longValue())
+            .maxPriorityFeePerGas(Wei.of(GAS_PRICE))
+            .maxFeePerGas(Wei.of(GAS_PRICE))
+            .gasLimit(GAS_LIMIT.longValue())
+            .to(Address.fromHexStringStrict(credentials.getAddress()))
+            .value(Wei.ZERO)
+            .payload(Bytes.EMPTY)
+            .accessList(List.of())
+            .codeDelegations(List.of(codeDelegation))
+            .signAndBuild(
+                secp256k1.createKeyPair(
+                    secp256k1.createPrivateKey(credentials.getEcKeyPair().getPrivateKey())));
+
+    String txHash =
+        minerNode.execute(ethTransactions.sendRawTransaction(tx.encoded().toHexString()));
+    return txHash;
+  }
+
+  /**
+   * Imports a premade block using the Engine API to the test node.
+   *
+   * @param executionPayload Complete execution payload with block data
+   * @param expectedBlobVersionedHashes Array of expected blob hashes
+   * @param parentBeaconBlockRoot Root hash of the parent beacon block
+   * @param executionRequests Array of execution layer requests
+   * @return HTTP response from the Engine API call containing validation results
+   * @throws IOException if the HTTP request fails
+   * @throws InterruptedException if the request is interrupted
+   */
   protected Response importPremadeBlock(
       final ObjectNode executionPayload,
       final ArrayNode expectedBlobVersionedHashes,
@@ -177,5 +252,254 @@ public abstract class LineaPluginTestBasePrague extends LineaPluginTestBase {
       throws IOException, InterruptedException {
     return this.engineApiService.importPremadeBlock(
         executionPayload, expectedBlobVersionedHashes, parentBeaconBlockRoot, executionRequests);
+  }
+
+  /**
+   * Record containing all parameters required for engine_newPayloadV4 API calls.
+   *
+   * @param executionPayload ExecutionPayloadV3-compatible block data
+   * @param expectedBlobVersionedHashes Array of 32-byte blob versioned hashes for validation
+   * @param parentBeaconBlockRoot 32-byte root of the parent beacon block
+   * @param executionRequests Array of execution layer triggered requests per EIP-7685
+   */
+  protected record EngineNewPayloadRequest(
+      ObjectNode executionPayload,
+      ArrayNode expectedBlobVersionedHashes,
+      String parentBeaconBlockRoot,
+      ArrayNode executionRequests) {}
+
+  /**
+   * Retrieves the hash of the latest block from the test node.
+   *
+   * @return The hexadecimal hash string of the latest block
+   * @throws Exception if the RPC call to the node fails or returns invalid data
+   */
+  protected String getLatestBlockHash() throws Exception {
+    return minerNode
+        .nodeRequests()
+        .eth()
+        .ethGetBlockByNumber(org.web3j.protocol.core.DefaultBlockParameterName.LATEST, false)
+        .send()
+        .getBlock()
+        .getHash();
+  }
+
+  /**
+   * Creates an execution payload for block import testing.
+   *
+   * <p>Constructs an ExecutionPayloadV3-compatible JSON object that can be used with the
+   * engine_newPayloadV4 method as defined in the Prague fork specification. The payload includes
+   * all required block header fields and an optional transaction.
+   *
+   * @param mapper JSON object mapper for creating Jackson nodes
+   * @param genesisBlockHash Hash of the genesis/parent block to reference
+   * @param blockParams Map containing all block parameters using {@link BlockParams} constants as
+   *     keys
+   * @param transactionKey Key in blockParams map containing transaction data, or empty string for
+   *     no transactions
+   * @return ObjectNode representing the execution payload compatible with engine_newPayloadV4
+   * @throws IllegalArgumentException if mapper, genesisBlockHash, blockParams, or transactionKey is
+   *     null
+   * @see <a href="https://github.com/ethereum/execution-apis/blob/main/src/engine/prague.md">Prague
+   *     Engine API Specification</a>
+   */
+  protected ObjectNode createExecutionPayload(
+      ObjectMapper mapper,
+      String genesisBlockHash,
+      Map<String, String> blockParams,
+      String transactionKey) {
+    ObjectNode payload =
+        mapper
+            .createObjectNode()
+            .put("parentHash", genesisBlockHash)
+            .put("feeRecipient", blockParams.get(BlockParams.FEE_RECIPIENT))
+            .put("stateRoot", blockParams.get(BlockParams.STATE_ROOT))
+            .put("logsBloom", blockParams.get(BlockParams.LOGS_BLOOM))
+            .put("prevRandao", blockParams.get(BlockParams.PREV_RANDAO))
+            .put("gasLimit", blockParams.get(BlockParams.GAS_LIMIT))
+            .put("gasUsed", blockParams.get(BlockParams.GAS_USED))
+            .put("timestamp", blockParams.get(BlockParams.TIMESTAMP))
+            .put("extraData", blockParams.get(BlockParams.EXTRA_DATA))
+            .put("baseFeePerGas", blockParams.get(BlockParams.BASE_FEE_PER_GAS))
+            .put("excessBlobGas", blockParams.get(BlockParams.EXCESS_BLOB_GAS))
+            .put("blobGasUsed", blockParams.get(BlockParams.BLOB_GAS_USED))
+            .put("receiptsRoot", blockParams.get(BlockParams.RECEIPTS_ROOT))
+            .put("blockNumber", blockParams.get(BlockParams.BLOCK_NUMBER));
+
+    // Add transactions
+    ArrayNode transactions = mapper.createArrayNode();
+    if (blockParams.containsKey(transactionKey)) {
+      transactions.add(blockParams.get(transactionKey));
+    }
+    payload.set("transactions", transactions);
+
+    // Add withdrawals (empty list)
+    ArrayNode withdrawals = mapper.createArrayNode();
+    payload.set("withdrawals", withdrawals);
+
+    return payload;
+  }
+
+  /**
+   * Creates blob versioned hashes array from block parameters.
+   *
+   * <p>Extracts blob versioned hashes from block parameters for transactions that include blob
+   * data. Each hash is 32 bytes and used to validate blob data integrity.
+   *
+   * @param mapper JSON object mapper for creating Jackson nodes
+   * @param blockParams Map containing block parameters with blob hash data
+   * @param versionedHashKey Key in blockParams for accessing the blob versioned hash
+   * @return ArrayNode containing the blob versioned hash (32 bytes)
+   * @throws IllegalArgumentException if mapper, blockParams, or versionedHashKey is null
+   */
+  protected ArrayNode createBlobVersionedHashes(
+      ObjectMapper mapper, Map<String, String> blockParams, String versionedHashKey) {
+    ArrayNode hashes = mapper.createArrayNode();
+    if (blockParams.containsKey(versionedHashKey)) {
+      hashes.add(blockParams.get(versionedHashKey));
+    }
+    return hashes;
+  }
+
+  /**
+   * Creates an empty versioned hashes array for non-blob transactions.
+   *
+   * @param mapper JSON object mapper for creating Jackson nodes
+   * @return Empty ArrayNode for blocks without blob transactions
+   * @throws IllegalArgumentException if mapper is null
+   */
+  protected ArrayNode createEmptyVersionedHashes(ObjectMapper mapper) {
+    return mapper.createArrayNode();
+  }
+
+  /**
+   * Creates EIP-7685 execution requests array for Engine API block import.
+   *
+   * @param mapper JSON object mapper for creating Jackson nodes
+   * @param blockParams Map containing block parameters with execution request data
+   * @return ArrayNode containing execution requests as hex-encoded byte arrays
+   */
+  protected ArrayNode createExecutionRequests(
+      ObjectMapper mapper, Map<String, String> blockParams) {
+    ArrayNode requests = mapper.createArrayNode();
+    requests.add(blockParams.get(BlockParams.EXECUTION_REQUEST));
+    return requests;
+  }
+
+  /**
+   * Computes a complete block header from execution payload and block parameters.
+   *
+   * <p>Creates a Besu BlockHeader instance that includes all required fields for Prague fork
+   * including execution requests commitment. The computed header is used to generate the correct
+   * blockHash for Engine API validation.
+   *
+   * @param executionPayload JSON execution payload created by {@link #createExecutionPayload}
+   * @param mapper JSON object mapper for parsing the payload
+   * @param blockParams Map containing all block parameters and roots
+   * @return Complete BlockHeader instance with computed hash
+   * @throws Exception if JSON parsing fails or block header construction fails
+   */
+  protected BlockHeader computeBlockHeader(
+      ObjectNode executionPayload, ObjectMapper mapper, Map<String, String> blockParams)
+      throws Exception {
+    EnginePayloadParameter blockParam =
+        mapper.readValue(executionPayload.toString(), EnginePayloadParameter.class);
+
+    Hash transactionsRoot = Hash.fromHexString(blockParams.get(BlockParams.TRANSACTIONS_ROOT));
+    Hash withdrawalsRoot = Hash.fromHexString(blockParams.get(BlockParams.WITHDRAWALS_ROOT));
+
+    // Take code from AbstractEngineNewPayload in Besu codebase
+    Bytes executionRequestBytes =
+        Bytes.fromHexString(blockParams.get(BlockParams.EXECUTION_REQUEST));
+    Bytes executionRequestBytesData = executionRequestBytes.slice(1);
+    Request executionRequest =
+        new Request(RequestType.of(executionRequestBytes.get(0)), executionRequestBytesData);
+    Optional<List<Request>> maybeRequests = Optional.of(List.of(executionRequest));
+
+    return new BlockHeader(
+        blockParam.getParentHash(),
+        Hash.EMPTY_LIST_HASH, // OMMERS_HASH_CONSTANT
+        blockParam.getFeeRecipient(),
+        blockParam.getStateRoot(),
+        transactionsRoot,
+        blockParam.getReceiptsRoot(),
+        blockParam.getLogsBloom(),
+        Difficulty.ZERO,
+        blockParam.getBlockNumber(),
+        blockParam.getGasLimit(),
+        blockParam.getGasUsed(),
+        blockParam.getTimestamp(),
+        Bytes.fromHexString(blockParam.getExtraData()),
+        blockParam.getBaseFeePerGas(),
+        blockParam.getPrevRandao(),
+        0, // Nonce
+        withdrawalsRoot,
+        blockParam.getBlobGasUsed(),
+        BlobGas.fromHexString(blockParam.getExcessBlobGas()),
+        Bytes32.fromHexString(blockParams.get(BlockParams.PARENT_BEACON_BLOCK_ROOT)),
+        maybeRequests.map(BodyValidation::requestsHash).orElse(null),
+        new MainnetBlockHeaderFunctions());
+  }
+
+  /**
+   * Updates the execution payload with the computed block hash.
+   *
+   * @param executionPayload JSON execution payload to update
+   * @param blockHeader Block header containing the computed hash
+   */
+  protected void updateExecutionPayloadWithBlockHash(
+      ObjectNode executionPayload, BlockHeader blockHeader) {
+    executionPayload.put("blockHash", blockHeader.getBlockHash().toHexString());
+  }
+
+  /**
+   * Asserts that a block import was rejected with the expected validation error.
+   *
+   * @param response HTTP response from the Engine API call
+   * @param expectedValidationError Expected validation error message to check for
+   */
+  protected void assertBlockImportRejected(Response response, String expectedValidationError)
+      throws Exception {
+    JsonNode result = mapper.readTree(response.body().string()).get("result");
+    String status = result.get("status").asText();
+    String validationError = result.get("validationError").asText();
+    assertThat(status).isEqualTo("INVALID");
+    assertThat(validationError).contains(expectedValidationError);
+  }
+
+  // Constants for transaction-related data keys
+  public static final class TransactionDataKeys {
+    public static final String BLOB_TX = "BLOB_TX";
+    public static final String DELEGATE_CALL_TX = "DELEGATE_CALL_TX";
+    public static final String BLOB_VERSIONED_HASH = "BLOB_VERSIONED_HASH";
+  }
+
+  // Constants for block parameter keys
+  public static final class BlockParams {
+    public static final String STATE_ROOT = "STATE_ROOT";
+    public static final String LOGS_BLOOM = "LOGS_BLOOM";
+    public static final String RECEIPTS_ROOT = "RECEIPTS_ROOT";
+    public static final String EXTRA_DATA = "EXTRA_DATA";
+    public static final String EXECUTION_REQUEST = "EXECUTION_REQUEST";
+    public static final String TRANSACTIONS_ROOT = "TRANSACTIONS_ROOT";
+    public static final String WITHDRAWALS_ROOT = "WITHDRAWALS_ROOT";
+    public static final String GAS_LIMIT = "GAS_LIMIT";
+    public static final String GAS_USED = "GAS_USED";
+    public static final String TIMESTAMP = "TIMESTAMP";
+    public static final String BASE_FEE_PER_GAS = "BASE_FEE_PER_GAS";
+    public static final String EXCESS_BLOB_GAS = "EXCESS_BLOB_GAS";
+    public static final String BLOB_GAS_USED = "BLOB_GAS_USED";
+    public static final String BLOCK_NUMBER = "BLOCK_NUMBER";
+    public static final String FEE_RECIPIENT = "FEE_RECIPIENT";
+    public static final String PREV_RANDAO = "PREV_RANDAO";
+    public static final String PARENT_BEACON_BLOCK_ROOT = "PARENT_BEACON_BLOCK_ROOT";
+  }
+
+  // Constants for validation error messages
+  public static final class LineaTransactionValidatorPluginErrors {
+    public static final String BLOB_TX_NOT_ALLOWED =
+        "LineaTransactionValidatorPlugin - BLOB_TX_NOT_ALLOWED";
+    public static final String DELEGATE_CODE_TX_NOT_ALLOWED =
+        "LineaTransactionValidatorPlugin - DELEGATE_CODE_TX_NOT_ALLOWED";
   }
 }
