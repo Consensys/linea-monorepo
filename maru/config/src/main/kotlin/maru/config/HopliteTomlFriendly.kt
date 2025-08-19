2a2136e96a1453a1d49da2d8c84001391901195e
Roman Vaseev <4833306+Filter94@users.noreply.github.com>
2025-08-19 08:58:37 +0200

    Linea finalization configuration (#314)
    * Linea finalization configuration
    
    * Adjusted DTo type
    
    Signed-off-by: Roman <4833306+Filter94@users.noreply.github.com>

--- maru/config/src/main/kotlin/maru/config/HopliteTomlFriendly.kt
+++ maru/config/src/main/kotlin/maru/config/HopliteTomlFriendly.kt
@@ -12,7 +12,9 @@ import java.net.URL
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
+import linea.domain.BlockParameter
 import linea.domain.RetryConfig
+import linea.kotlin.assertIs20Bytes
 
 data class PayloadValidatorDto(
   val engineApiEndpoint: ApiEndpointDto,
@@ -98,7 +100,49 @@ data class QbftOptionsDtoToml(
   }
 }
 
+data class LineaConfigDtoToml(
+  val contractAddress: ByteArray,
+  val l1EthApi: ApiEndpointDto,
+  val l1PollingInterval: Duration = 6.seconds,
+  val l1HighestBlockTag: String = "finalized",
+) {
+  init {
+    contractAddress.assertIs20Bytes("contractAddress")
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as LineaConfigDtoToml
+
+    if (!contractAddress.contentEquals(other.contractAddress)) return false
+    if (l1EthApi != other.l1EthApi) return false
+    if (l1PollingInterval != other.l1PollingInterval) return false
+    if (l1HighestBlockTag != other.l1HighestBlockTag) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = contractAddress.contentHashCode()
+    result = 31 * result + l1EthApi.hashCode()
+    result = 31 * result + l1PollingInterval.hashCode()
+    result = 31 * result + l1HighestBlockTag.hashCode()
+    return result
+  }
+
+  fun domainFriendly(): LineaConfig =
+    LineaConfig(
+      contractAddress = contractAddress,
+      l1EthApi = l1EthApi.domainFriendly(),
+      l1PollingInterval = l1PollingInterval,
+      l1HighestBlockTag = BlockParameter.parse(l1HighestBlockTag),
+    )
+}
+
 data class MaruConfigDtoToml(
+  private val linea: LineaConfigDtoToml? = null,
   private val protocolTransitionPollingInterval: Duration = 1.seconds,
   private val allowEmptyBlocks: Boolean = false,
   private val persistence: Persistence,
@@ -112,6 +156,7 @@ data class MaruConfigDtoToml(
 ) {
   fun domainFriendly(): MaruConfig =
     MaruConfig(
+      linea = linea?.domainFriendly(),
       protocolTransitionPollingInterval = protocolTransitionPollingInterval,
       allowEmptyBlocks = allowEmptyBlocks,
       persistence = persistence,
