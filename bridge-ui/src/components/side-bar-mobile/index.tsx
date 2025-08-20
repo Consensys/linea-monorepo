cde3aa5758b2054dc7f58ff07330762b99ec2ded
Alain Nicolas <alainncls@users.noreply.github.com>
2025-08-20 16:04:23 +0200

    chore(bridge-ui): add Events and Rewards tabs to the navigation bar (#1327)

--- bridge-ui/src/components/side-bar-mobile/index.tsx
+++ bridge-ui/src/components/side-bar-mobile/index.tsx
@@ -1,6 +1,8 @@
 import DappIcon from "@/assets/icons/dapp.svg";
 import TokenIcon from "@/assets/icons/token.svg";
 import LineaIcon from "@/assets/logos/linea.svg";
+import EventIcon from "@/assets/icons/event.svg";
+import RewardsIcon from "@/assets/icons/reward.svg";
 import clsx from "clsx";
 import Link from "next/link";
 import styles from "./side-bar-mobile.module.scss";
@@ -21,6 +23,16 @@ export default function SideBarMobile() {
       name: "Tokens",
       href: "https://linea.build/hub/tokens",
       icon: <TokenIcon />,
+    },
+    {
+      name: "Events",
+      href: "https://linea.build/hub/events",
+      icon: <EventIcon />,
+    },
+    {
+      name: "Rewards",
+      href: "https://linea.build/hub/rewards",
+      icon: <RewardsIcon />,
     },
   ];
   return (
