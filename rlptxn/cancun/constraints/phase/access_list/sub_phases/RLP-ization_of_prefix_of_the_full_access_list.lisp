0e0566c9824c1812c94e7374c1515f5ee38adaf1
amkCha <29160563+amkCha@users.noreply.github.com>
2025-08-21 15:58:23 +0200

    fix: constraint access-list---rlp-prefix-of-the-full-access-list---sa… (#729)

--- rlptxn/cancun/constraints/phase/access_list/sub_phases/RLP-ization_of_prefix_of_the_full_access_list.lisp
+++ rlptxn/cancun/constraints/phase/access_list/sub_phases/RLP-ization_of_prefix_of_the_full_access_list.lisp
@@ -20,7 +20,7 @@
                  (:guard   (is-access-list-prefix))
                  (if-zero  PHASE_END
                            (begin
-                             (eq!   IS_PREFIX_OF_ACCESS_LIST_ITEM   1)
-                             (eq!   IS_ACCESS_LIST_ADDRESS          0)
-                             (eq!   IS_PREFIX_OF_STORAGE_KEY_LIST   0)
-                             (eq!   IS_ACCESS_LIST_STORAGE_KEY      0))))
+                             (eq!   (next IS_PREFIX_OF_ACCESS_LIST_ITEM)   1)
+                             (eq!   (next IS_ACCESS_LIST_ADDRESS)          0)
+                             (eq!   (next IS_PREFIX_OF_STORAGE_KEY_LIST)   0)
+                             (eq!   (next IS_ACCESS_LIST_STORAGE_KEY)      0))))
