a9a022aebe09fc306cfefe9fa4fbe156df124f27
amkCha <29160563+amkCha@users.noreply.github.com>
2025-08-26 09:14:35 +0200

    fix: switch constraint back to defproperty with an if (#733)

--- rlptxn/cancun/constraints/phase/access_list/sub_phases/RLP-ization_of_prefix_of_the_full_access_list.lisp
+++ rlptxn/cancun/constraints/phase/access_list/sub_phases/RLP-ization_of_prefix_of_the_full_access_list.lisp
@@ -16,11 +16,11 @@
 (defun (access-list-is-non-empty)        cmp/EXO_DATA_4) ;; ""
 (defun (access-list-is-empty)            (- 1 (access-list-is-non-empty)))
 
-(defconstraint    access-list---rlp-prefix-of-the-full-access-list---sanity-checks
-                 (:guard   (is-access-list-prefix))
+(defproperty    access-list---rlp-prefix-of-the-full-access-list---sanity-checks
+                 (if-not-zero (is-access-list-prefix)
                  (if-zero  PHASE_END
                            (begin
                              (eq!   (next IS_PREFIX_OF_ACCESS_LIST_ITEM)   1)
                              (eq!   (next IS_ACCESS_LIST_ADDRESS)          0)
                              (eq!   (next IS_PREFIX_OF_STORAGE_KEY_LIST)   0)
-                             (eq!   (next IS_ACCESS_LIST_STORAGE_KEY)      0))))
+                             (eq!   (next IS_ACCESS_LIST_STORAGE_KEY)      0)))))
