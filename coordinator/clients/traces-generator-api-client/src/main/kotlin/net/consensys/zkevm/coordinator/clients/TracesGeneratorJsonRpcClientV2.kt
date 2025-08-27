abab73e9587ed72d51537c1a6df3374d13fc9e86
Roman <4833306+Filter94@users.noreply.github.com>
2025-08-27 21:43:26 +0200

    Added additional try-catch for the case when Trace Generator is down completely

--- coordinator/clients/traces-generator-api-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/TracesGeneratorJsonRpcClientV2.kt
+++ coordinator/clients/traces-generator-api-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/TracesGeneratorJsonRpcClientV2.kt
@@ -8,6 +8,7 @@ import io.vertx.core.json.JsonObject
 import net.consensys.linea.async.toSafeFuture
 import net.consensys.linea.errors.ErrorResponse
 import net.consensys.linea.jsonrpc.JsonRpcRequestListParams
+import net.consensys.linea.jsonrpc.JsonRpcSuccessResponse
 import net.consensys.linea.jsonrpc.client.JsonRpcClient
 import net.consensys.linea.jsonrpc.client.JsonRpcRequestRetryer
 import net.consensys.linea.jsonrpc.client.RequestRetryConfig
@@ -67,85 +68,88 @@ class TracesGeneratorJsonRpcClientV2(
     )
   }
 
+  private fun <T> executeWithFallback(
+    jsonRequest: JsonRpcRequestListParams,
+    responseParser: (JsonRpcSuccessResponse) -> T,
+    fallbackResponseProvider: () -> T,
+  ): SafeFuture<Result<T, ErrorResponse<TracesServiceErrorType>>> {
+    return try {
+      rpcClient.makeRequest(jsonRequest).toSafeFuture()
+        .thenApply { responseResult ->
+          val result = responseResult.mapEither(
+            responseParser,
+            TracesClientResponsesParser::mapErrorResponseV2,
+          )
+          if (config.ignoreTracesGeneratorErrors && !result.isSuccess()) {
+            Ok(fallbackResponseProvider())
+          } else {
+            result
+          }
+        }
+        .exceptionally { throwable ->
+          if (config.ignoreTracesGeneratorErrors) {
+            Ok(fallbackResponseProvider())
+          } else {
+            throw throwable
+          }
+        }
+    } catch (th: Throwable) {
+      if (config.ignoreTracesGeneratorErrors) {
+        SafeFuture.completedFuture(Ok(fallbackResponseProvider()))
+      } else {
+        throw th
+      }
+    }
+  }
+
   override fun getTracesCounters(
     blockNumber: ULong,
   ): SafeFuture<Result<GetTracesCountersResponse, ErrorResponse<TracesServiceErrorType>>> {
-    val jsonRequest =
-      JsonRpcRequestListParams(
-        "2.0",
-        id.incrementAndGet(),
-        "linea_getBlockTracesCountersV2",
-        listOf(
-          JsonObject.of(
-            "blockNumber",
-            blockNumber,
-            "expectedTracesEngineVersion",
-            config.expectedTracesApiVersion,
-          ),
+    val jsonRequest = JsonRpcRequestListParams(
+      "2.0",
+      id.incrementAndGet(),
+      "linea_getBlockTracesCountersV2",
+      listOf(
+        JsonObject.of(
+          "blockNumber",
+          blockNumber,
+          "expectedTracesEngineVersion",
+          config.expectedTracesApiVersion,
         ),
-      )
+      ),
+    )
 
-    return rpcClient.makeRequest(jsonRequest).toSafeFuture()
-      .thenApply { responseResult ->
-        val result = responseResult.mapEither(
-          TracesClientResponsesParser::parseTracesCounterResponseV2,
-          TracesClientResponsesParser::mapErrorResponseV2,
-        )
-        if (config.ignoreTracesGeneratorErrors && !result.isSuccess()) {
-          Ok(createFallbackTracesCountersResponse())
-        } else {
-          result
-        }
-      }
-      .exceptionally { throwable ->
-        if (config.ignoreTracesGeneratorErrors) {
-          Ok(createFallbackTracesCountersResponse())
-        } else {
-          throw throwable
-        }
-      }
+    return executeWithFallback(
+      jsonRequest,
+      TracesClientResponsesParser::parseTracesCounterResponseV2,
+      ::createFallbackTracesCountersResponse,
+    )
   }
 
   override fun generateConflatedTracesToFile(
     startBlockNumber: ULong,
     endBlockNumber: ULong,
   ): SafeFuture<Result<GenerateTracesResponse, ErrorResponse<TracesServiceErrorType>>> {
-    val jsonRequest =
-      JsonRpcRequestListParams(
-        "2.0",
-        id.incrementAndGet(),
-        "linea_generateConflatedTracesToFileV2",
-        listOf(
-          JsonObject.of(
-            "startBlockNumber",
-            startBlockNumber,
-            "endBlockNumber",
-            endBlockNumber,
-            "expectedTracesEngineVersion",
-            config.expectedTracesApiVersion,
-          ),
+    val jsonRequest = JsonRpcRequestListParams(
+      "2.0",
+      id.incrementAndGet(),
+      "linea_generateConflatedTracesToFileV2",
+      listOf(
+        JsonObject.of(
+          "startBlockNumber",
+          startBlockNumber,
+          "endBlockNumber",
+          endBlockNumber,
+          "expectedTracesEngineVersion",
+          config.expectedTracesApiVersion,
         ),
-      )
+      ),
+    )
 
-    return rpcClient.makeRequest(jsonRequest).toSafeFuture()
-      .thenApply { responseResult ->
-        val result = responseResult.mapEither(
-          TracesClientResponsesParser::parseConflatedTracesToFileResponse,
-          TracesClientResponsesParser::mapErrorResponseV2,
-        )
-        if (config.ignoreTracesGeneratorErrors && !result.isSuccess()) {
-          Ok(createFallbackConflatedTracesResponse(startBlockNumber, endBlockNumber))
-        } else {
-          result
-        }
-      }
-      .exceptionally { throwable ->
-        if (config.ignoreTracesGeneratorErrors) {
-          Ok(createFallbackConflatedTracesResponse(startBlockNumber, endBlockNumber))
-        } else {
-          throw throwable
-        }
-      }
+    return executeWithFallback(
+      jsonRequest,
+      TracesClientResponsesParser::parseConflatedTracesToFileResponse,
+    ) { createFallbackConflatedTracesResponse(startBlockNumber, endBlockNumber) }
   }
 
   companion object {
