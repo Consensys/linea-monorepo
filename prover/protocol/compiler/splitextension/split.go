aec87e37bcddac44e2f94023adacb4caf706ac78
Yao Galteland <yaoj.galteland@gmail.com>
2025-09-12 12:14:52 +0200

    keep BatchEvaluateLagrangeMixed for split

--- prover/protocol/compiler/splitextension/split.go
+++ prover/protocol/compiler/splitextension/split.go
@@ -7,7 +7,6 @@ import (
 	"github.com/consensys/gnark-crypto/field/koalabear/extensions"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
-	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors_mixed"
 	"github.com/consensys/linea-monorepo/prover/maths/field"
 	"github.com/consensys/linea-monorepo/prover/maths/field/fext"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
@@ -196,7 +195,7 @@ func (pctx *AssignUnivProverAction) Run(runtime *wizard.ProverRuntime) {
 		svToEval = append(svToEval, sv)
 	}
 
-	y := smartvectors_mixed.BatchEvaluateLagrange(svToEval, x)
+	y := smartvectors.BatchEvaluateBasePolyLagrange(svToEval, x)
 
 	// This loops collect the evaluation claims of the already-on-base polynomials
 	// from the new query to append them to the claims on the new query. This
