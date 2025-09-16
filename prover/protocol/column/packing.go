9a711b083bd62c0bd664d4bdeb888028120e8907
srinathln7 <srinath.nandakumar@consensys.net>
2025-09-16 12:24:09 +0000

    add cbor.toarray tag to packed col

--- prover/protocol/column/packing.go
+++ prover/protocol/column/packing.go
@@ -38,6 +38,7 @@ func (p PackedStore) Unpack() *Store {
 // PackedNatural is serialization-friendly intermediate structure that is
 // use to represent a natural column.
 type PackedNatural struct {
+	_        struct{}     `cbor:",toarray"`
 	Store    *Store       `cbor:"s"`
 	Round    int          `cbor:"r"`
 	Position int          `cbor:"p"`
