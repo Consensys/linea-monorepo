6eff2d590c65019939c91e7f77be250644d578e8
jonesho <jones.ho@consensys.net>
2025-09-16 21:01:25 +0800

    feat: added maru follower and update linea-besu-package version for besu nodes and disable limitless tracing

--- docker/config/l2-genesis-initialization/init.sh
+++ docker/config/l2-genesis-initialization/init.sh
@@ -6,7 +6,19 @@ cp -T "genesis-maru.json.template" "genesis-maru.json"
 cp -T "genesis-besu.json.template" "genesis-besu.json"
 cp -T "genesis-geth.json.template" "genesis-geth.json"
 
-prague_timestamp=$(($(date +%s) + 63))
+shanghai_timestamp=$(($(date +%s) + 100))
+echo "Shanghai Timestamp: $shanghai_timestamp"
+sed -i "s/%SHANGHAI_TIME%/$shanghai_timestamp/g" genesis-maru.json
+sed -i "s/%SHANGHAI_TIME%/$shanghai_timestamp/g" genesis-besu.json
+sed -i "s/%SHANGHAI_TIME%/$shanghai_timestamp/g" genesis-geth.json
+
+cancun_timestamp=$((shanghai_timestamp + 40))
+echo "Cancun Timestamp: $cancun_timestamp"
+sed -i "s/%CANCUN_TIME%/$cancun_timestamp/g" genesis-maru.json
+sed -i "s/%CANCUN_TIME%/$cancun_timestamp/g" genesis-besu.json
+sed -i "s/%CANCUN_TIME%/$cancun_timestamp/g" genesis-geth.json
+
+prague_timestamp=$((cancun_timestamp + 40))
 echo "Prague Timestamp: $prague_timestamp"
 sed -i "s/%PRAGUE_TIME%/$prague_timestamp/g" genesis-maru.json
 sed -i "s/%PRAGUE_TIME%/$prague_timestamp/g" genesis-besu.json
