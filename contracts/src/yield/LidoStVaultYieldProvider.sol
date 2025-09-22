4361487d8b574fbabb21acdb9817550cb02dfbff
kyzooghost <jeffersonzjtang@gmail.com>
2025-09-22 17:13:13 +1000

    new interfaces

---
+++ contracts/src/yield/LidoStVaultYieldProvider.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.30;
+
+import { IYieldManager } from "./interfaces/IYieldManager.sol";
+import { IYieldProvider } from "./interfaces/IYieldProvider.sol";
+import { IGenericErrors } from "../interfaces/IGenericErrors.sol";
+
+/**
+ * @title Contract to handle native yield operations with Lido Staking Vault.
+ * @author ConsenSys Software Inc.
+ * @custom:security-contact security-report@linea.build
+ */
+contract LidoStVaultYieldProvider is IYieldProvider, IGenericErrors {
+  /**
+   * @notice Send ETH to the specified yield strategy.
+   * @dev Will settle any outstanding liabilities to the YieldProvider.
+   * @param _amount        The amount of ETH to send.
+   */
+  function fundYieldProvider(uint256 _amount) external {
+
+  }
+
+  /**
+   * @notice Report newly accrued yield, excluding any portion reserved for system obligations.
+   * @dev Since the YieldManager is unaware of donations received via the L1MessageService or L2MessageService,
+   *      the `_reserveDonations` parameter is required to ensure accurate yield accounting.
+   * @param _totalReserveDonations   Total amount of donations received on the L1MessageService or L2MessageService.
+   */
+  function reportYield(uint256 _totalReserveDonations) external {
+
+  }
+
+  /**
+   * @notice Request beacon chain withdrawal.
+   * @param _withdrawalParams   Provider-specific withdrawal parameters.
+   */
+  function unstake(bytes memory _withdrawalParams) external {
+
+  }
+
+  /**
+   * @notice Permissionlessly request beacon chain withdrawal.
+   * @dev    Callable only when the withdrawal reserve is in deficit. 
+   * @dev    The permissionless unstake amount is capped to the remaining reserve deficit that 
+   *         cannot be covered by other liquidity sources:
+   *
+   *         PERMISSIONLESS_UNSTAKE_AMOUNT ≤
+   *           RESERVE_DEFICIT
+   *         - YIELD_PROVIDER_BALANCE
+   *         - YIELD_MANAGER_BALANCE
+   *         - PENDING_PERMISSIONLESS_UNSTAKE
+   *
+   * @dev Validates (validatorPubkey, validatorBalance, validatorWithdrawalCredential) against EIP-4788 beacon chain root.
+   * @param _withdrawalParams       Provider-specific withdrawal parameters.
+   * @param _withdrawalParamsProof  Merkle proof of _withdrawalParams to be verified against EIP-4788 beacon chain root.
+   */
+  function unstakePermissionless(
+    bytes calldata _withdrawalParams,
+    bytes calldata _withdrawalParamsProof
+  ) external {
+
+  }
+
+  /**
+   * @notice Withdraw ETH from a specified yield provider.
+   * @dev If withdrawal reserve is in deficit, will route funds to the bridge.
+   * @dev If fund remaining, will settle any outstanding LST liabilities and protocol obligations.
+   * @param _amount                 Amount to withdraw.
+   */
+  function withdrawFromYieldProvider(uint256 _amount) external {
+
+  }
+
+  /**
+   * @notice Rebalance ETH from the YieldManager and specified yield provider, sending it to the L1MessageService.
+   * @dev Settles any outstanding LST liabilities, provided this does not leave the withdrawal reserve in deficit.
+   * @param _amount                 Amount to withdraw.
+   */
+  function addToWithdrawalReserve(uint256 _amount) external {
+
+  }
+
+  /**
+   * @notice Permissionlessly rebalance ETH from the YieldManager, sending it to the L1MessageService.
+   * @dev Only available when the withdrawal is in deficit.
+   * @param _amount                 Amount to withdraw.
+   */
+  function replenishWithdrawalReserve(uint256 _amount) external {
+
+  }
+
+  /**
+   * @notice Pauses beacon chain deposits for specified yield provier.
+   */
+  function pauseStaking() external {
+
+  }
+
+  /**
+   * @notice Unpauses beacon chain deposits for specified yield provier.
+   * @dev Will revert if the withdrawal reserve is in deficit, or there is an existing LST liability.
+   */
+  function unpauseStaking() external {
+
+  }
+
+  function validateAdditionToYieldManager(IYieldManager.YieldProviderRegistration calldata _yieldProviderRegistration) external {
+
+  }
+}
---
+++ contracts/src/yield/vendor/interfaces/IVaultHub.sol
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Lido <info@lido.fi>
+// SPDX-License-Identifier: GPL-3.0
+
+// See contracts/COMPILERS.md
+// solhint-disable-next-line lido/fixed-compiler-version
+pragma solidity >=0.8.0;
+
+interface IVaultHub {
+    
+}
