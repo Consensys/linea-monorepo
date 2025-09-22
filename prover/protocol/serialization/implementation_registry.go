80189acd2d081d5360f3a5c4b550222bc11f2467
arijitdutta67 <arijit.dutta67@gmail.com>
2025-09-22 13:17:41 +0530

    fix(serialization): cursor bot suggestion added

--- prover/protocol/serialization/implementation_registry.go
+++ prover/protocol/serialization/implementation_registry.go
@@ -101,7 +101,7 @@ func init() {
 	RegisterImplementation(verifiercol.FromYs{})
 	RegisterImplementation(verifiercol.FromAccessors{})
 	RegisterImplementation(verifiercol.ExpandedVerifCol{})
-	// Todo: add expand_proof_and_verifyingkey_col
+	RegisterImplementation(verifiercol.ExpandedProofOrVerifyingKeyColWithZero{})
 	RegisterImplementation(verifiercol.RepeatedAccessor{})
 
 	// Queries
