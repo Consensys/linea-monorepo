087b2196448ad46462d4485281f1423dee6af179
srinathln7 <srinath.nandakumar@consensys.net>
2025-09-23 10:31:18 +0000

    add test files

--- prover/protocol/serialization/distributed_wizard_test.go
+++ prover/protocol/serialization/distributed_wizard_test.go
@@ -2,15 +2,267 @@ package serialization_test
 
 import (
 	"fmt"
+	"path"
 	"runtime"
 	"testing"
 
+	"github.com/consensys/linea-monorepo/prover/config"
+	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
+	"github.com/consensys/linea-monorepo/prover/maths/common/vector"
+	"github.com/consensys/linea-monorepo/prover/maths/field"
 	"github.com/consensys/linea-monorepo/prover/protocol/distributed"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	"github.com/consensys/linea-monorepo/prover/symbolic"
+	"github.com/consensys/linea-monorepo/prover/utils/profiling"
 	"github.com/consensys/linea-monorepo/prover/zkevm"
 )
+
+// GetDW initializes a distributed wizard configuration using the
+// ZkEVM's compiled IOP and a StandardModuleDiscoverer with preset parameters.
+// The function compiles the necessary segments and produces a conglomerated
+// distributed wizard, which is then returned.
+func GetDW(cfg *config.Config) *distributed.DistributedWizard {
+
+	var (
+		traceLimits = cfg.TracesLimits
+		zkEVM       = zkevm.FullZKEVMWithSuite(&traceLimits, zkevm.CompilationSuite{}, cfg)
+		disc        = &distributed.StandardModuleDiscoverer{
+			TargetWeight: 1 << 29,
+			Predivision:  1,
+			Advices:      zkevm.DiscoveryAdvices,
+		}
+		dw = distributed.DistributeWizard(zkEVM.WizardIOP, disc)
+	)
+
+	// These are the slow and expensive operations.
+	dw.CompileSegments().Conglomerate(100)
+	return dw
+}
+
+func TestSerdeDW(t *testing.T) {
+
+	// t.Skipf("the test is a development/debug/integration test. It is not needed for CI")
+	cfg, err := config.NewConfigFromFileUnchecked("/home/ubuntu/linea-monorepo/prover/config/config-sepolia-limitless.toml")
+	if err != nil {
+		t.Fatalf("failed to read config file: %s", err)
+	}
+	dist := GetDW(cfg)
+	isSanityCheck := true
+	isfailFast := false
+
+	t.Run("ModuleNames", func(t *testing.T) {
+		runSerdeTest(t, dist.ModuleNames, "DW.ModuleNames", isSanityCheck, isfailFast)
+	})
+
+	for i := range dist.GLs {
+		t.Run(fmt.Sprintf("DW.GLModule-%d", i), func(t *testing.T) {
+			runSerdeTest(t, dist.GLs[i], fmt.Sprintf("DW.GLModule-%d", i), isSanityCheck, isfailFast)
+		})
+	}
+
+	for i := range dist.LPPs {
+		t.Run(fmt.Sprintf("DW.LPPModule-%d", i), func(t *testing.T) {
+			runSerdeTest(t, dist.LPPs[i], fmt.Sprintf("DW.LPPModule-%d", i), isSanityCheck, isfailFast)
+		})
+	}
+
+	t.Run("DefaultModule", func(t *testing.T) {
+		runSerdeTest(t, dist.DefaultModule, "DW.DefaultModule", isSanityCheck, isfailFast)
+	})
+
+	t.Run("Bootstrapper", func(t *testing.T) {
+		runSerdeTest(t, dist.Bootstrapper, "DW.Bootstrapper", isSanityCheck, isfailFast)
+	})
+
+	t.Run("Discoverer", func(t *testing.T) {
+		runSerdeTest(t, dist.Disc, "DW.Discoverer", isSanityCheck, isfailFast)
+	})
+
+	t.Run("CompiledDefault", func(t *testing.T) {
+		runSerdeTest(t, dist.CompiledDefault, "DW.CompiledDefault", isSanityCheck, isfailFast)
+	})
+
+	for i := range dist.CompiledGLs {
+		t.Run(fmt.Sprintf("CompiledGL-%v", i), func(t *testing.T) {
+			runSerdeTest(t, dist.CompiledGLs[i], fmt.Sprintf("DW.CompiledGL-%v", i), isSanityCheck, isfailFast)
+		})
+	}
+
+	for i := range dist.CompiledLPPs {
+		t.Run(fmt.Sprintf("CompiledLPP-%v", i), func(t *testing.T) {
+			runSerdeTest(t, dist.CompiledLPPs[i], fmt.Sprintf("DW.CompiledLPP-%v", i), isSanityCheck, isfailFast)
+		})
+	}
+
+	// To save memory
+	cong := dist.CompiledConglomeration
+	dist = nil
+	runtime.GC()
+
+	t.Run("CompiledConglomeration", func(t *testing.T) {
+		runSerdeTest(t, cong, "DW.CompiledConglomeration", isSanityCheck, isfailFast)
+	})
+}
+
+// TestSerdeDWPerf: This test assumes that serialization and deserialization (ser/de) have already been
+// verified to be correct in the previous test (TestSerdeDW).
+func TestSerdeDWPerf(t *testing.T) {
+
+	t.Skipf("the test is a development/debug/integration test. It is not needed for CI")
+	cfg, err := config.NewConfigFromFileUnchecked("/home/ubuntu/linea-monorepo/prover/config/config-sepolia-limitless.toml")
+	if err != nil {
+		t.Fatalf("failed to read config file: %s", err)
+	}
+
+	var perfLogs profiling.PerfLogs
+	dw := GetDW(cfg)
+
+	t.Run("ModuleNames", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.ModuleNames, "DW.ModuleNames"))
+	})
+
+	for i := range dw.GLs {
+		t.Run(fmt.Sprintf("GLModule-%d", i), func(t *testing.T) {
+			perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.GLs[i], fmt.Sprintf("DW.GLModule-%v", i)))
+		})
+	}
+
+	for i := range dw.LPPs {
+		t.Run(fmt.Sprintf("LPPModule-%d", i), func(t *testing.T) {
+			perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.LPPs[i], fmt.Sprintf("DW.LPPModule-%d", i)))
+		})
+	}
+
+	t.Run("DefaultModule", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.DefaultModule, "DW.DefaultModule"))
+	})
+
+	t.Run("Bootstrapper", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.Bootstrapper, "DW.Bootstrapper"))
+	})
+
+	t.Run("Discoverer", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.Disc, "DW.Discoverer"))
+	})
+
+	t.Run("CompiledDefault", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.CompiledDefault, "DW.CompiledDefault"))
+	})
+
+	for i := range dw.CompiledGLs {
+		t.Run(fmt.Sprintf("CompiledGL-%v", i), func(t *testing.T) {
+			perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.CompiledGLs[i], fmt.Sprintf("DW.CompiledGL-%v", i)))
+		})
+	}
+
+	for i := range dw.CompiledLPPs {
+		t.Run(fmt.Sprintf("CompiledLPP-%v", i), func(t *testing.T) {
+			perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.CompiledLPPs[i], fmt.Sprintf("DW.CompiledLPP-%v", i)))
+		})
+	}
+
+	// To save memory
+	cong := dw.CompiledConglomeration
+	dw = nil
+	runtime.GC()
+
+	t.Run("CompiledConglomeration", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, cong, "DW.CompiledConglomeration"))
+	})
+
+	// Write performance logs to CSV
+	if err := perfLogs.WritePerformanceLogsToCSV(path.Join("perf", "dw-perf-logs.csv")); err != nil {
+		t.Fatalf("Error writing performance logs to csv: %v", err)
+	}
+}
+
+func BenchmarkSerdeDW(b *testing.B) {
+
+	b.Skipf("the test is a development/debug/integration test. It is not needed for CI")
+
+	cfg, err := config.NewConfigFromFileUnchecked("/home/ubuntu/linea-monorepo/prover/config/config-sepolia-limitless.toml")
+	if err != nil {
+		b.Fatalf("failed to read config file: %s", err)
+	}
+	dist := GetDW(cfg)
+	b.ResetTimer()
+
+	b.Run("ModuleNames", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, dist.ModuleNames, "DW.ModuleNames")
+		}
+	})
+
+	for i := range dist.GLs {
+		b.Run(fmt.Sprintf("GLModule-%d", i), func(b *testing.B) {
+			for j := 0; j < b.N; j++ {
+				justserde(b, dist.GLs[i], fmt.Sprintf("DW.GLModule-%d", i))
+			}
+		})
+	}
+
+	for i := range dist.LPPs {
+		b.Run(fmt.Sprintf("LPPModule-%d", i), func(b *testing.B) {
+			for j := 0; j < b.N; j++ {
+				justserde(b, dist.LPPs[i], fmt.Sprintf("DW.LPPModule-%d", i))
+			}
+		})
+	}
+
+	b.Run("DefaultModule", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, dist.DefaultModule, "DW.DefaultModule")
+		}
+	})
+
+	b.Run("Bootstrapper", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, dist.Bootstrapper, "DW.Bootstrapper")
+		}
+	})
+
+	b.Run("Discoverer", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, dist.Disc, "DW.Discoverer")
+		}
+	})
+
+	b.Run("CompiledDefault", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, dist.CompiledDefault, "DW.CompiledDefault")
+		}
+	})
+
+	for i := range dist.CompiledGLs {
+
+		b.Run(fmt.Sprintf("CompiledGL-%v", i), func(b *testing.B) {
+			for j := 0; j < b.N; j++ {
+				justserde(b, dist.CompiledGLs[i], fmt.Sprintf("DW.CompiledGL-%v", i))
+			}
+		})
+	}
+
+	for i := range dist.CompiledLPPs {
+
+		b.Run(fmt.Sprintf("CompiledLPP-%v", i), func(b *testing.B) {
+			for j := 0; j < b.N; j++ {
+				justserde(b, dist.CompiledLPPs[i], fmt.Sprintf("DW.CompiledLPP-%v", i))
+			}
+		})
+	}
+
+	// To save memory
+	cong := dist.CompiledConglomeration
+	dist = nil
+	runtime.GC()
+
+	b.Run("CompiledConglomeration", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, cong, "DW.CompiledConglomeration")
+		}
+	})
+}
 
 type distributeTestCase struct {
 	numRow int
@@ -40,29 +292,15 @@ func (d distributeTestCase) define(comp *wizard.CompiledIOP) {
 	comp.InsertInclusion(0, "inclusion-0", []ifaces.Column{c0, b0, a0}, []ifaces.Column{c1, b1, a1})
 }
 
-// GetDistWizard initializes a distributed wizard configuration using the
-// ZkEVM's compiled IOP and a StandardModuleDiscoverer with preset parameters.
-// The function compiles the necessary segments and produces a conglomerated
-// distributed wizard, which is then returned.
-func GetDistWizard() *distributed.DistributedWizard {
-	var (
-		z    = zkevm.GetTestZkEVM()
-		disc = &distributed.StandardModuleDiscoverer{
-			TargetWeight: 1 << 28,
-			Affinities:   zkevm.GetAffinities(z),
-			Predivision:  1,
-		}
-
-		// This tests the compilation of the compiled-IOP
-		distWizard = distributed.DistributeWizard(z.WizardIOP, disc).
-				CompileSegments().
-				Conglomerate(20)
-	)
-
-	return distWizard
+func (d distributeTestCase) assign(run *wizard.ProverRuntime) {
+	run.AssignColumn("a0", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(1), d.numRow-2), d.numRow))
+	run.AssignColumn("b0", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(2), d.numRow-2), d.numRow))
+	run.AssignColumn("c0", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(3), d.numRow-2), d.numRow))
+	run.AssignColumn("a1", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(1), d.numRow-2), d.numRow))
+	run.AssignColumn("b1", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(2), d.numRow-2), d.numRow))
+	run.AssignColumn("c1", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(3), d.numRow-2), d.numRow))
 }
-
-func GetBasicDistWizard() *distributed.DistributedWizard {
+func GetBasicDW() *distributed.DistributedWizard {
 
 	var (
 		numRow = 1 << 10
@@ -83,73 +321,12 @@ func GetBasicDistWizard() *distributed.DistributedWizard {
 
 	return distWizard
 }
-
-func TestSerdeDistWizard(t *testing.T) {
-
-	t.Skipf("the test is a development/debug/integration test. It is not needed for CI")
-
-	dist := GetDistWizard()
-
-	t.Run("ModuleNames", func(t *testing.T) {
-		runSerdeTest(t, dist.ModuleNames, "DistributedWizard.ModuleNames", true, false)
-	})
-
-	for i := range dist.GLs {
-		t.Run(fmt.Sprintf("GLModule-%d", i), func(t *testing.T) {
-			runSerdeTest(t, dist.GLs[i], "DistributedWizard.GLs", true, false)
-		})
-	}
-
-	for i := range dist.LPPs {
-		t.Run(fmt.Sprintf("LPPModule-%d", i), func(t *testing.T) {
-			runSerdeTest(t, dist.LPPs[i], "DistributedWizard.LPPs", true, false)
-		})
-	}
-
-	t.Run("DefaultModule", func(t *testing.T) {
-		runSerdeTest(t, dist.DefaultModule, "DistributedWizard.DefaultModule", true, false)
-	})
-
-	t.Run("Bootstrapper", func(t *testing.T) {
-		runSerdeTest(t, dist.Bootstrapper, "DistributedWizard.Bootstrapper", true, false)
-	})
-
-	t.Run("Discoverer", func(t *testing.T) {
-		runSerdeTest(t, dist.Disc, "DistributedWizard.Discoverer", true, false)
-	})
-
-	t.Run("CompiledDefault", func(t *testing.T) {
-		runSerdeTest(t, dist.CompiledDefault, "DistributedWizard.CompiledDefault", true, false)
-	})
-
-	for i := range dist.CompiledGLs {
-		t.Run(fmt.Sprintf("CompiledGL-%v", i), func(t *testing.T) {
-			runSerdeTest(t, dist.CompiledGLs[i], fmt.Sprintf("DistributedWizard.CompiledGL-%v", i), true, false)
-		})
-	}
-
-	for i := range dist.CompiledLPPs {
-		t.Run(fmt.Sprintf("CompiledLPP-%v", i), func(t *testing.T) {
-			runSerdeTest(t, dist.CompiledLPPs[i], fmt.Sprintf("DistributedWizard.CompiledLPP-%v", i), true, false)
-		})
-	}
-
-	// To save memory
-	cong := dist.CompiledConglomeration
-	dist = nil
-	runtime.GC()
-
-	t.Run("CompiledConglomeration", func(t *testing.T) {
-		runSerdeTest(t, cong, "DistributedWizard.CompiledConglomeration", true, false)
-	})
-}
-
 func TestSerdeDWCong(t *testing.T) {
 
 	t.Skipf("the test is a development/debug/integration test. It is not needed for CI")
 
 	// Setup
-	distWizard := GetBasicDistWizard()
+	distWizard := GetBasicDW()
 	cong := distWizard.CompiledConglomeration
 	distWizard = nil
 	runtime.GC()
