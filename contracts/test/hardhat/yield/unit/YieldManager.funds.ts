878b1a3a5d7bbfb44eb6df6750d7f03c1ad91b1f
kyzooghost <jeffersonzjtang@gmail.com>
2025-10-06 10:52:38 +1100

    did transferFundsToReserve tests

--- contracts/test/hardhat/yield/unit/YieldManager.funds.ts
+++ contracts/test/hardhat/yield/unit/YieldManager.funds.ts
@@ -6,33 +6,30 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 import { MockLineaRollup, TestYieldManager } from "contracts/typechain-types";
+import { deployYieldManagerForUnitTest } from "../helpers/deploy";
 import {
-  // deployMockYieldProvider,
-  deployYieldManagerForUnitTest,
-} from "../helpers/deploy";
-// import { addMockYieldProvider, buildMockYieldProviderRegistration } from "../helpers/mocks";
-// import { MINIMUM_FEE, EMPTY_CALLDATA, ONE_THOUSAND_ETHER, MAX_BPS, ZERO_VALUE } from "../../common/constants";
-// import { buildAccessErrorMessage, expectRevertWithCustomError, getAccountsFixture } from "../../common/helpers";
+  ONE_THOUSAND_ETHER,
+  NATIVE_YIELD_RESERVE_FUNDING_PAUSE_TYPE,
+  GENERAL_PAUSE_TYPE,
+  ONE_ETHER,
+} from "../../common/constants";
 import { expectRevertWithCustomError, getAccountsFixture } from "../../common/helpers";
-
-// import { YieldManagerInitializationData } from "../helpers/types";
-// import { ZeroAddress } from "ethers";
 
 describe("Linea Rollup contract", () => {
   let yieldManager: TestYieldManager;
 
-  // let securityCouncil: SignerWithAddress;
-  // let nonAuthorizedAccount: SignerWithAddress;
+  let securityCouncil: SignerWithAddress;
+  let nonAuthorizedAccount: SignerWithAddress;
   let nativeYieldOperator: SignerWithAddress;
-  // let operationalSafe: SignerWithAddress;
+  let operationalSafe: SignerWithAddress;
   let mockLineaRollup: MockLineaRollup;
 
   before(async () => {
     ({
-      // securityCouncil,
-      // operator: nonAuthorizedAccount,
+      securityCouncil,
+      operator: nonAuthorizedAccount,
       nativeYieldOperator,
-      // operationalSafe,
+      operationalSafe,
     } = await loadFixture(getAccountsFixture));
   });
 
@@ -77,6 +74,69 @@ describe("Linea Rollup contract", () => {
       await expect(mockLineaRollup.connect(nativeYieldOperator).callReceiveFundsFromReserve(yieldManagerAddress, 1n))
         .to.emit(yieldManager, "ReserveFundsReceived")
         .withArgs(1n);
+    });
+  });
+
+  describe("sending ETH to the L1MessageService", () => {
+    it("Should revert when the caller is not the YIELD_PROVIDER_FUNDER_ROLE", async () => {
+      await expectRevertWithCustomError(
+        yieldManager,
+        yieldManager.connect(nonAuthorizedAccount).transferFundsToReserve(1n),
+        "CallerMissingRole",
+        [await yieldManager.RESERVE_OPERATOR_ROLE(), await yieldManager.YIELD_PROVIDER_FUNDER_ROLE()],
+      );
+    });
+
+    it("Should revert when the caller is not the YIELD_PROVIDER_UNSTAKER_ROLE", async () => {
+      const unstakerRole = await yieldManager.YIELD_PROVIDER_UNSTAKER_ROLE();
+      await yieldManager.connect(securityCouncil).grantRole(unstakerRole, nonAuthorizedAccount.address);
+
+      await expectRevertWithCustomError(
+        yieldManager,
+        yieldManager.connect(nonAuthorizedAccount).transferFundsToReserve(1n),
+        "CallerMissingRole",
+        [await yieldManager.RESERVE_OPERATOR_ROLE(), await yieldManager.YIELD_PROVIDER_FUNDER_ROLE()],
+      );
+    });
+
+    it("Should revert when the NATIVE_YIELD_RESERVE_FUNDING pause type is activated", async () => {
+      await yieldManager.connect(operationalSafe).pauseByType(NATIVE_YIELD_RESERVE_FUNDING_PAUSE_TYPE);
+
+      await expectRevertWithCustomError(
+        yieldManager,
+        yieldManager.connect(nativeYieldOperator).transferFundsToReserve(1n),
+        "IsPaused",
+        [NATIVE_YIELD_RESERVE_FUNDING_PAUSE_TYPE],
+      );
+    });
+
+    it("Should revert when the GENERAL pause type is activated", async () => {
+      await yieldManager.connect(securityCouncil).pauseByType(GENERAL_PAUSE_TYPE);
+
+      await expectRevertWithCustomError(
+        yieldManager,
+        yieldManager.connect(nativeYieldOperator).transferFundsToReserve(1n),
+        "IsPaused",
+        [GENERAL_PAUSE_TYPE],
+      );
+    });
+
+    it("Should successfully send ETH to the L1MessageService", async () => {
+      const yieldManagerAddress = await yieldManager.getAddress();
+      await ethers.provider.send("hardhat_setBalance", [yieldManagerAddress, ethers.toBeHex(ONE_THOUSAND_ETHER)]);
+      const transferAmount = ONE_ETHER;
+      const l1MessageService = await mockLineaRollup.getAddress();
+
+      const reserveBalanceBefore = await ethers.provider.getBalance(l1MessageService);
+      const yieldManagerBalanceBefore = await ethers.provider.getBalance(yieldManagerAddress);
+
+      await yieldManager.connect(nativeYieldOperator).transferFundsToReserve(transferAmount);
+
+      const reserveBalanceAfter = await ethers.provider.getBalance(l1MessageService);
+      const yieldManagerBalanceAfter = await ethers.provider.getBalance(yieldManagerAddress);
+
+      expect(reserveBalanceAfter).to.equal(reserveBalanceBefore + transferAmount);
+      expect(yieldManagerBalanceAfter).to.equal(yieldManagerBalanceBefore - transferAmount);
     });
   });
 });
