008120cf83ead0d9ee76438e0de939204e1e4119
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2025-10-10 00:51:50 +1100

    added tests for math256 library

---
+++ contracts/src/_testing/unit/libraries/TestMath256.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.30;
+
+import { Math256 } from "../../../libraries/Math256.sol";
+
+contract TestMath256 {
+  function min(uint256 _a, uint256 _b) external pure returns (uint256 value) {
+    return Math256.min(_a, _b);
+  }
+
+  function max(uint256 _a, uint256 _b) external pure returns (uint256 value) {
+    return Math256.max(_a, _b);
+  }
+
+  function safeSub(uint256 _a, uint256 _b) external pure returns (uint256 value) {
+    return Math256.safeSub(_a, _b);
+  }
+}
