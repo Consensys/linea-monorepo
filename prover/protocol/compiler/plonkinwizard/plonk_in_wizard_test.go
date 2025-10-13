922b412a4527f5fd1f7de97717d7053563adaa80
Thomas Piellard <thomas.piellard@consensys.net>
2025-10-13 14:17:17 +0200

    feat: compiler/plonkinwizard/ ok

--- prover/protocol/compiler/plonkinwizard/plonk_in_wizard_test.go
+++ prover/protocol/compiler/plonkinwizard/plonk_in_wizard_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
 	"github.com/consensys/linea-monorepo/prover/maths/common/vector"
 	"github.com/consensys/linea-monorepo/prover/maths/field"
+	"github.com/consensys/linea-monorepo/prover/maths/zk"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/dummy"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/plonkinwizard"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
