f8763f0df07440c3955279c32119310ada1c078b
Soleimani193 <azam.soleimanian@ens.fr>
2025-10-14 10:46:26 +0200

    cleaned interface

--- prover/zkevm/prover/hash/keccak/keccakf_koalabear/base_conversions.go
+++ prover/zkevm/prover/hash/keccak/keccakf_koalabear/base_conversions.go
@@ -5,6 +5,7 @@ import (
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 )
 
+// baseConversion module, responsible for converting the state from base dirty 12 to base clean 11
 type baseConversion struct {
 	// state before applying the base conversion step, in base dirty 12
 	stateCurr state
@@ -17,6 +18,7 @@ type baseConversion struct {
 	paStateDecomposition *wizard.ProverAction
 }
 
+// newBaseConversion creates a new base conversion module, declares the columns and constraints and returns its pointer
 func newBaseConversion(comp *wizard.CompiledIOP, numKeccakf int, stateCurr state) *baseConversion {
 	// declare the columns
 	declareColumnsBaseConv(comp, numKeccakf)
@@ -24,9 +26,12 @@ func newBaseConversion(comp *wizard.CompiledIOP, numKeccakf int, stateCurr state
 	return &baseConversion{}
 }
 
+// assignBaseConversion assigns the values to the columns of base conversion step.
 func assignBaseConversion(run *wizard.ProverRuntime, stateCurr state) baseConversion {
 	return baseConversion{stateCurr: stateCurr,
 		stateNext: stateBaseConversion{}}
 }
+
+// it declares the intermediate columns generated during base conversion step, including the new state.
 func declareColumnsBaseConv(comp *wizard.CompiledIOP, numKeccakf int) {
 }
