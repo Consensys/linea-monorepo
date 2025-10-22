62d6e375a9eb847d5645ec3a137814e77dd2accf
arijitdutta67 <arijit.dutta67@gmail.com>
2025-10-22 18:55:10 +0530

    feat(theta): using base 8 and lookup for theta (wip)

--- prover/zkevm/prover/hash/keccak/keccakf_koalabear/theta.go
+++ prover/zkevm/prover/hash/keccak/keccakf_koalabear/theta.go
@@ -1,13 +1,13 @@
 package keccakfkoalabear
 
 import (
-	"math/big"
-
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	"github.com/consensys/linea-monorepo/prover/symbolic"
 	"github.com/consensys/linea-monorepo/prover/zkevm/prover/hash/keccak/keccakf"
 )
+
+const thetaBase = 8
 
 // theta module, responsible for updating the state in the theta step of keccakf
 type theta struct {
@@ -15,33 +15,32 @@ type theta struct {
 	stateCurr state
 	// state after applying the theta step, in base dirty 12
 	stateNext state
-	// intermediate columns for state transition.
-	// msb of each byte of the stateNext
-	msb [5][5][8]ifaces.Column // MSB of each byte of the state
-	// lookup tables to attest the correctness of msb,
-	// the first column is the byte, the second column is its msb
-	lookupMSB [2]ifaces.Column
+	// Intermediate columns
+	// cc is the bitshifted version of c used in theta step
+	cc [5][8]ifaces.Column
+	// bitConvertedCC is the bit converted version of cc used in theta step
+	bitConvertedCC [5][8]ifaces.Column
 }
 
 // newTheta creates a new theta module, declares the columns and constraints and returns its pointer
-func (*theta) newTheta(comp *wizard.CompiledIOP,
+func newTheta(comp *wizard.CompiledIOP,
 	numKeccakf int,
-	round int,
-	stateCurr state) *theta {
+	stateCurr state,
+	l lookupTables) *theta {
 	res := &theta{}
 	res.stateCurr = stateCurr
 
 	// declare the columns
-	res.declareColumnsTheta(comp, numKeccakf, round)
+	res.declareColumnsTheta(comp, numKeccakf)
 
 	// declare the constraints
-	res.csEqThetaBaseA(comp, round)
-	// res.csAThetaDecomposition(comp, round)
+	res.csEqThetaBaseA(comp)
+	res.csLookupCCToBitConvertedCC(comp, l)
 	return res
 }
 
 // declareColumnsTheta declares the intermediate columns generated during theta step, including the new state.
-func (theta *theta) declareColumnsTheta(comp *wizard.CompiledIOP, numKeccakf int, round int) {
+func (theta *theta) declareColumnsTheta(comp *wizard.CompiledIOP, numKeccakf int) {
 	// size of the columns to declare
 	colSize := keccakf.NumRows(numKeccakf)
 	// declare the new state
@@ -50,40 +49,42 @@ func (theta *theta) declareColumnsTheta(comp *wizard.CompiledIOP, numKeccakf int
 		for y := 0; y < 5; y++ {
 			for z := 0; z < 8; z++ {
 				theta.stateNext[x][y][z] = comp.InsertCommit(
-					round,
+					0,
 					keccakf.DeriveName("A_THETA", x, y, z),
-					colSize,
-				)
-				theta.msb[x][y][z] = comp.InsertCommit(
-					round,
-					keccakf.DeriveName("MSB_A_THETA", x, y, z),
 					colSize,
 				)
 			}
 		}
 	}
-	// declare the lookup table for msb
+	// declare the bit converted cc columns
+	for x := 0; x < 5; x++ {
+		for z := 0; z < 8; z++ {
+			theta.bitConvertedCC[x][z] = comp.InsertCommit(
+				0,
+				keccakf.DeriveName("CC_BIT_CONVERTED", x, z),
+				colSize,
+			)
+		}
+	}
 }
 
 // csEqThetaBaseA declares the constraints ensuring the correctness of the theta step
-func (theta *theta) csEqThetaBaseA(comp *wizard.CompiledIOP, round int) {
-	// cc is the bitshifted version of c. Unlike what is specified by in the
-	// spec of keccak, the shifting here is not cyclic. Thus, cc uses 65 bits.
-	var c, cc [5][8]*symbolic.Expression
+func (theta *theta) csEqThetaBaseA(comp *wizard.CompiledIOP) {
+	// cc_computed is the bitshifted version of c. Unlike what is specified by in the
+	// spec of keccak, the shifting here is not cyclic. Thus, cc_computed uses 65 bits.
+	var c, cc_computed [5][8]*symbolic.Expression
 	for x := 0; x < 5; x++ {
-		for y := 0; y < 8; y++ {
+		for z := 0; z < 8; z++ {
 			// c[x][y] = A[x][0][y] + A[x][1][y] + A[x][2][y] + A[x][3][y] + A[x][4][y]
-			c[x][y] = symbolic.Add(theta.stateNext[x][0][y],
-				theta.stateNext[x][1][y],
-				theta.stateNext[x][2][y],
-				theta.stateNext[x][3][y],
-				theta.stateNext[x][4][y])
-			cc[x][y] = symbolic.Mul(c[x][y], keccakf.BaseA)
+			c[x][z] = symbolic.Add(theta.stateCurr[x][0][z],
+				theta.stateCurr[x][1][z],
+				theta.stateCurr[x][2][z],
+				theta.stateCurr[x][3][z],
+				theta.stateCurr[x][4][z])
+			cc_computed[x][z] = symbolic.Mul(c[x][z], thetaBase)
 		}
 	}
-
-	// Since cc is not actually a cyclic rotation, the result for eqTheta still
-	// requires adding the MSbit to the LSbit to derive the actual result.
+	// Check that the next state of theta is correctly computed
 	for x := 0; x < 5; x++ {
 		for y := 0; y < 5; y++ {
 			for z := 0; z < 8; z++ {
@@ -91,22 +92,42 @@ func (theta *theta) csEqThetaBaseA(comp *wizard.CompiledIOP, round int) {
 					symbolic.Add(
 						theta.stateCurr[x][y][z],
 						c[(x-1+5)%5][z],
-						cc[(x+1)%5][z]))
+						theta.bitConvertedCC[(x+1)%5][z]))
 				qName := ifaces.QueryIDf("EQ_THETA_%v_%v_%v", x, y, z)
-				comp.InsertGlobal(round, qName, eqTheta)
+				comp.InsertGlobal(0, qName, eqTheta)
 			}
+		}
+	}
+
+	// Check that cc computed from c is the same as cc assigned
+	for x := 0; x < 5; x++ {
+		for z := 0; z < 8; z++ {
+			eqCC := symbolic.Sub(theta.cc[x][z], cc_computed[x][z])
+			qName := ifaces.QueryIDf("EQ_CC_%v_%v", x, z)
+			comp.InsertGlobal(0, qName, eqCC)
 		}
 	}
 }
 
-// Proves the link between (aThetaBaseA, aThetaBaseAMsb) with the sliced
-// decomposition of aThetaBaseA.
-func (t *theta) csAThetaDecomposition(comp *wizard.CompiledIOP, round int) {
-	// shf64 = BaseA^U64, it is used to left-shift the MSB to lay on the 64 bits
-	// and cancel the MSB of aThetaBaseA
-	var shf64 big.Int
-	shf64.Exp(big.NewInt(keccakf.BaseA), big.NewInt(64), nil)
-
+// Lookup between cc and bitConvertedCC, slice by slice
+func (theta *theta) csLookupCCToBitConvertedCC(comp *wizard.CompiledIOP,
+	l lookupTables) {
+	for x := 0; x < 5; x++ {
+		for z := 0; z < 8; z++ {
+			comp.InsertInclusion(
+				0,
+				ifaces.QueryIDf("LOOKUP_CC_TO_BIT_CONVERTED_%v_%v", x, z),
+				[]ifaces.Column{
+					l.ccBase8Theta,
+					l.ccBitConvertedTheta,
+				},
+				[]ifaces.Column{
+					theta.cc[x][z],
+					theta.bitConvertedCC[x][z],
+				},
+			)
+		}
+	}
 }
 
 // assignTheta assigns the values to the columns of theta step
