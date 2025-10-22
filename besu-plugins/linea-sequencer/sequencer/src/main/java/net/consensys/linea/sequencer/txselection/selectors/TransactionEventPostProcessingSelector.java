e1b86fa71728833964586c81a70f031065392714
Matilda Clerke <matilda.clerke@consensys.net>
2025-10-22 14:26:51 +1100

    Utilise TransactionEventPostProcessingSelector for bundle transaction event denial

--- besu-plugins/linea-sequencer/sequencer/src/main/java/net/consensys/linea/sequencer/txselection/selectors/TransactionEventPostProcessingSelector.java
+++ besu-plugins/linea-sequencer/sequencer/src/main/java/net/consensys/linea/sequencer/txselection/selectors/TransactionEventPostProcessingSelector.java
@@ -3,6 +3,7 @@ package net.consensys.linea.sequencer.txselection.selectors;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import lombok.RequiredArgsConstructor;
+import net.consensys.linea.bundles.TransactionBundle;
 import org.hyperledger.besu.evm.log.Log;
 import org.hyperledger.besu.plugin.data.TransactionProcessingResult;
 import org.hyperledger.besu.plugin.data.TransactionSelectionResult;
@@ -12,12 +13,17 @@ import org.hyperledger.besu.plugin.services.txselection.TransactionEvaluationCon
 public class TransactionEventPostProcessingSelector
     extends AbstractTransactionPostProcessingSelector {
   private final AtomicReference<Set<TransactionEventSelectionDescription>> deniedEvents;
+  private final AtomicReference<Set<TransactionEventSelectionDescription>> deniedBundleEvents;
 
   @Override
   public TransactionSelectionResult evaluateTransactionPostProcessing(
       final TransactionEvaluationContext evaluationContext,
       final TransactionProcessingResult processingResult) {
-    for (TransactionEventSelectionDescription deniedEvent : deniedEvents.get()) {
+    final boolean isBundle =
+        evaluationContext.getPendingTransaction() instanceof TransactionBundle.PendingBundleTx;
+    Set<TransactionEventSelectionDescription> deniedEventsForTransaction =
+        isBundle ? deniedBundleEvents.get() : deniedEvents.get();
+    for (TransactionEventSelectionDescription deniedEvent : deniedEventsForTransaction) {
       for (Log log : processingResult.getLogs()) {
         if (deniedEvent.matches(log)) {
           return TransactionSelectionResult.invalid("Transaction event logs match deny list");
