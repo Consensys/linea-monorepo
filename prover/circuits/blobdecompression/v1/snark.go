9ad2478602899b13db08aa83dfd5ee3e1850f2ff
Tabaie <arya.pourtabatabaie@gmail.com>
2025-10-22 13:52:04 -0500

    refactor: separate PackBatches function

--- prover/circuits/blobdecompression/v1/snark.go
+++ prover/circuits/blobdecompression/v1/snark.go
@@ -3,12 +3,11 @@ package v1
 import (
 	"errors"
 
-	"github.com/consensys/gnark-crypto/hash"
-	"github.com/consensys/gnark/std/hash/mimc"
-	"github.com/consensys/gnark/std/lookup/logderivlookup"
-
 	"math/big"
 	"math/bits"
+
+	"github.com/consensys/gnark-crypto/hash"
+	"github.com/consensys/gnark/std/lookup/logderivlookup"
 
 	"github.com/consensys/linea-monorepo/prover/circuits/internal"
 	"github.com/consensys/linea-monorepo/prover/circuits/internal/plonk"
@@ -100,41 +99,39 @@ func (h hashAsCompressor) Compress(left frontend.Variable, right frontend.Variab
 }
 
 type BatchesSumsChecker struct {
-	Compressor         hash.Compressor
-	FieldCompressorGen func(frontend.API) snarkHash.Compressor
-	MaxNbBatches       int
+	Compressor   hash.Compressor
+	MaxNbBatches int
 }
 
 var batchesSumChecker = BatchesSumsChecker{
-	Compressor: gnarkutil.NewHashAsCompressor(hash.MIMC_BLS12_377.New()),
-	FieldCompressorGen: func(api frontend.API) snarkHash.Compressor {
-		h, err := mimc.NewMiMC(api)
-		if err != nil {
-			panic(err)
-		}
-		return hashAsCompressor{&h}
-	},
+	Compressor:   gnarkutil.NewHashAsCompressor(hash.MIMC_BLS12_377.New()),
 	MaxNbBatches: MaxNbBatches,
 }
 
-// CheckBatchesSums checks batch checksums consisting of H(batchLen, contentSum) where contentSum = Blocks[0] if len(Blocks) == 1 and H(Blocks...) otherwise. Blocks are consecutive 31-byte chunks of the data in the batch, with the last one right-padded with zeros if necessary.
-// All batches must be at least 31 bytes long. The function performs this range check.
-// It is also checked that the batches are all within the MAXIMUM range of the blob. CheckBatchesSums does not have access to the actual blob size, so it remains the caller's responsibility to check that the batches are within the confines of the ACTUAL blob size.
-// The expected checksums are not checked beyond nbBatches
-func CheckBatchesSums(api frontend.API, hasher snarkHash.FieldHasher, nbBatches frontend.Variable, blobPayload []frontend.Variable, batchLengths []frontend.Variable, expectedChecksums []frontend.Variable) error {
+type PackedBatchesIteration struct {
+	Current    frontend.Variable
+	Next       frontend.Variable
+	NoOp       frontend.Variable
+	NextStarts frontend.Variable
+	BatchI     frontend.Variable
+}
+
+type PackedBatchesResult struct {
+	Iterations []PackedBatchesIteration
+	BatchEnds  []frontend.Variable
+	Range      *internal.Range
+}
+
+func PackBatches(api frontend.API, nbBatches frontend.Variable, blobPayload, batchLengths []frontend.Variable) (*PackedBatchesResult, error) {
 
 	batchEnds := internal.PartialSums(api, batchLengths)
-
-	if len(batchEnds) > len(expectedChecksums) {
-		return errors.New("more batches than checksums")
-	}
 
 	if len(blobPayload) < 31 { // edge case
 		api.AssertIsEqual(nbBatches, 0)
 		for i := range batchEnds {
 			api.AssertIsEqual(batchEnds[i], 0)
 		}
-		return nil
+		return nil, nil
 	}
 
 	api.AssertIsDifferent(nbBatches, 0)
@@ -149,14 +146,14 @@ func CheckBatchesSums(api frontend.API, hasher snarkHash.FieldHasher, nbBatches 
 		return cappedRamp.Lookup(api.Add(x, cappedRampNegMin))[0]
 	}
 
-	nbHashes := 1 + len(blobPayload)/31 // one extra iteration of the main loop to simplify eof handling
+	iterations := make([]PackedBatchesIteration, 1+len(blobPayload)/31) // one extra iteration of the main loop to simplify eof handling
 	// every batch is only sealed when the next is about to begin. So we need to start creating the dummy batch when the loop ends.
 	// to ensure that happens in the case of a full blob, we will need a dummy iteration as well
 
 	// a practically infinite dummy batch at the end to prevent index overflows
 	// and to make sure the dummy batch still doesn't "end" on the last iteration, we must give it and extra 31 bytes on top of that
 	// nbHashes*31+1 is JUST beyond what the loop will reach so that the dummy batch is never sealed.
-	dummyBatchEnd := nbHashes*31 + 1
+	dummyBatchEnd := len(iterations)*31 + 1
 
 	batchesRange := internal.NewRange(api, nbBatches, len(batchEnds)) // this also range-checks nbBatches
 	for i := range batchEnds {                                        // check that the size of every batch is at least 31
@@ -171,7 +168,7 @@ func CheckBatchesSums(api frontend.API, hasher snarkHash.FieldHasher, nbBatches 
 	// side effect: batchEnds are range checked to be within a reasonable factor of the maximum blob payload length; useless because we will have to perform a stronger check in the end
 	endQsV, endRsV, err := DivBy31(api, batchEnds, bits.Len(uint(len(blobPayload))+31))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	endQs, endRs := internal.SliceToTable(api, endQsV), internal.SliceToTable(api, endRsV)
 
@@ -201,15 +198,62 @@ func CheckBatchesSums(api frontend.API, hasher snarkHash.FieldHasher, nbBatches 
 	// i.e. a full word to incorporate into the checksum, starting at the i-th byte
 	inputAt31B := logderivlookup.New(api)
 	nr := compress.NewNumReader(api, inputExt, 31*8, 8)
-	for i := 0; i < 31*nbHashes+2; i++ { // TODO justify the +2
+	for range 31*len(iterations) + 2 { // TODO justify the +2
 		inputAt31B.Insert(nr.Next())
 	}
 
-	_hsh := func(a, b frontend.Variable) frontend.Variable {
-		hasher.Reset()
-		hasher.Write(a, b)
-		res := hasher.Sum()
-		return res
+	iterations[0] = PackedBatchesIteration{
+		Current:    inputAt(0, 31),
+		Next:       0,
+		NextStarts: 0,
+		BatchI:     0,
+	}
+
+	batchI := frontend.Variable(0) // index of the current batch
+	// each 31 byte block partially belongs to one or two batches (guaranteed by rejecting batches smaller than 31 bytes)
+	startR := frontend.Variable(0) // the remainder by 31 of where the current batch starts
+
+	// each iteration is able to process at most one new batch. This dictates that end[i] % 31 != end[i+1] % 31 for any applicable i
+	for i := range len(iterations) {
+
+		endQ, endR := endQs.Lookup(batchI)[0], endRs.Lookup(batchI)[0]
+		end := api.Add(api.Mul(31, endQ), endR)
+
+		currNbBytesRemaining := api.Sub(end, 31*i, startR) // 31i + startR is the location of the "head"
+		opLen := min0Max31(currNbBytesRemaining)
+
+		iterations[i].NoOp = api.IsZero(opLen) // or equivalently, isZero(currNbBytesRemaining)
+
+		if i != 0 {
+
+			iterations[i].Current = inputAt(api.Add(31*i, startR), opLen)
+			iterations[i].Next = inputAt31B.Lookup(end)[0]
+			iterations[i].NextStarts = api.IsZero(api.Sub(endQ, i))
+			iterations[i].BatchI = batchI
+
+		}
+
+		startR = api.Select(iterations[i].NextStarts, endR, startR) // if the next one starts, update the current start R
+		batchI = api.Add(batchI, iterations[i].NextStarts)
+	}
+
+	api.AssertIsEqual(batchI, nbBatches) // check that we're actually done
+
+	return &PackedBatchesResult{
+		Iterations: iterations,
+		BatchEnds:  batchEnds,
+		Range:      batchesRange,
+	}, nil
+}
+
+// CheckBatchesSums checks batch checksums consisting of H(batchLen, contentSum) where contentSum = Blocks[0] if len(Blocks) == 1 and H(Blocks...) otherwise. Blocks are consecutive 31-byte chunks of the data in the batch, with the last one right-padded with zeros if necessary.
+// All batches must be at least 31 bytes long. The function performs this range check.
+// It is also checked that the batches are all within the MAXIMUM range of the blob. CheckBatchesSums does not have access to the actual blob size, so it remains the caller's responsibility to check that the batches are within the confines of the ACTUAL blob size.
+// The expected checksums are not checked beyond nbBatches
+func CheckBatchesSums(api frontend.API, hsh snarkHash.FieldHasher, nbBatches frontend.Variable, blobPayload, batchLengths []frontend.Variable, expectedChecksums []frontend.Variable) error {
+	packedBatches, err := PackBatches(api, nbBatches, blobPayload, batchLengths)
+	if err != nil {
+		return err
 	}
 
 	var (
@@ -218,60 +262,39 @@ func CheckBatchesSums(api frontend.API, hasher snarkHash.FieldHasher, nbBatches 
 	)
 	// create a table of claimed sums and prove their correctness as we go through the payload
 	{
-		hintIn := make([]frontend.Variable, 1, 1+len(batchEnds)+len(blobPayload))
+		hintIn := make([]frontend.Variable, 1, 1+len(packedBatches.BatchEnds)+len(blobPayload))
 		hintIn[0] = nbBatches
-		hintIn = append(hintIn, batchEnds[:]...)
+		hintIn = append(hintIn, packedBatches.BatchEnds[:]...)
 		hintIn = append(hintIn, blobPayload...)
-		if partialSums, err = api.Compiler().NewHint(batchesSumChecker.PartialChecksumBatchesPackedHint, len(batchEnds), hintIn...); err != nil {
+		if partialSums, err = api.Compiler().NewHint(batchesSumChecker.PartialChecksumBatchesPackedHint, len(packedBatches.BatchEnds), hintIn...); err != nil {
 			return err
 		}
 	}
 	partialSumsT = internal.SliceToTable(api, partialSums)
 	partialSumsT.Insert(0) // dummy in case of maximum nbBatches
 
-	batchSum := inputAt(0, 31)     // normally this should be taken care of by the api.Select(currAlreadyOver,... line. But the very first batch doesn't get this treatment because we know it starts at 0
-	batchI := frontend.Variable(0) // index of the current batch
-	// each 31 byte block partially belongs to one or two batches (guaranteed by rejecting batches smaller than 31 bytes)
-	startR := frontend.Variable(0) // the remainder by 31 of where the current batch starts
+	compressor := hashAsCompressor{hsh}
 
-	// each iteration is able to process at most one new batch. This dictates that end[i] % 31 != end[i+1] % 31 for any applicable i
-	for i := range nbHashes {
+	iterations := packedBatches.Iterations
+	batchSum := iterations[0].Current // normally this should be taken care of by the api.Select(currAlreadyOver,... line. But the very first batch doesn't get this treatment because we know it starts at 0
+	for i := 1; i < len(iterations); i++ {
+		batchSum = api.Select(
+			iterations[i].NoOp,
+			batchSum,
+			compressor.Compress(batchSum, iterations[i].Current),
+		)
 
-		endQ, endR := endQs.Lookup(batchI)[0], endRs.Lookup(batchI)[0]
-		end := api.Add(api.Mul(31, endQ), endR)
-
-		currNbBytesRemaining := api.Sub(end, 31*i, startR) // 31i + startR is the location of the "head"
-		hashLen := min0Max31(currNbBytesRemaining)
-		startNext := api.IsZero(api.Sub(endQ, i))
-		noHash := api.IsZero(hashLen) // or equivalently, isZero(currNbBytesRemaining)
-
-		if i != 0 {
-			batchSum = api.Select(
-				noHash,
-				batchSum,
-				_hsh(batchSum, inputAt(api.Add(31*i, startR), hashLen)),
-			)
-
-			internal.AssertEqualIf(api, startNext, batchSum, partialSumsT.Lookup(batchI)[0]) // if we're done with the current checksum, check that the claimed one from the table is equal to it
-			// THIS STEP REQUIRES THAT NO BATCH SHOULD BE SMALLER THAN 31 BYTES
-			//
-			// This is always the case in practice since a batch contains
-			// at least one block which in turn contains a 32 byte root hash.
-			batchSum = api.Select(startNext, inputAt31B.Lookup(end)[0], batchSum) // if the next one starts, the sum is the 31 byte "prefix" of the next batch; assumes any batch is at least 31 bytes long
-
-		}
-
-		startR = api.Select(startNext, endR, startR) // if the next one starts, update the current start R
-		batchI = api.Add(batchI, startNext)
+		internal.AssertEqualIf(api, iterations[i].NextStarts, batchSum, partialSumsT.Lookup(iterations[i].BatchI)[0]) // if we're done with the current checksum, check that the claimed one from the table is equal to it
+		// THIS STEP REQUIRES THAT NO BATCH SHOULD BE SMALLER THAN 31 BYTES
+		//
+		// This is always the case in practice since a batch contains
+		// at least one block which in turn contains a 32 byte root hash.
+		batchSum = api.Select(iterations[i].NextStarts, iterations[i].Next, batchSum) // if the next one starts, the sum is the 31 byte "prefix" of the next batch; assumes any batch is at least 31 bytes long
 	}
 
-	api.AssertIsEqual(batchI, nbBatches) // check that we're actually done
-
 	// hash along the lengths and compare with expected values
-	for i := range batchEnds {
-		hasher.Reset()
-		hasher.Write(batchLengths[i], partialSums[i])
-		batchesRange.AssertEqualI(i, expectedChecksums[i], hasher.Sum())
+	for i := range batchLengths {
+		packedBatches.Range.AssertEqualI(i, expectedChecksums[i], compressor.Compress(batchLengths[i], partialSums[i]))
 	}
 
 	return nil
