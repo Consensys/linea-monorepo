565703ffca7246cb0db03ff6ff6637c2acef28b7
srinathln7 <srinath.nandakumar@consensys.net>
2025-10-23 06:44:20 +0000

    replace protocol/serialization with latest optimized ser - incl custom-iop handlers

--- prover/protocol/serialization/distributed_wizard_test.go
+++ prover/protocol/serialization/distributed_wizard_test.go
@@ -2,15 +2,151 @@ package serialization_test
 
 import (
 	"fmt"
+	"path"
 	"runtime"
 	"testing"
 
+	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
+	"github.com/consensys/linea-monorepo/prover/maths/common/vector"
+	"github.com/consensys/linea-monorepo/prover/maths/field"
 	"github.com/consensys/linea-monorepo/prover/protocol/distributed"
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	"github.com/consensys/linea-monorepo/prover/symbolic"
+	"github.com/consensys/linea-monorepo/prover/utils/profiling"
 	"github.com/consensys/linea-monorepo/prover/zkevm"
 )
+
+// TestSerdeDWPerf: This test assumes that serialization and deserialization (ser/de) have already been
+// verified to be correct in the previous test (TestSerdeDW).
+func TestSerdeDWPerf(t *testing.T) {
+
+	t.Skipf("the test is a development/debug/integration test. It is not needed for CI")
+
+	var perfLogs profiling.PerfLogs
+	dw := GetDistWizard()
+
+	t.Run("ModuleNames", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.ModuleNames, "DW.ModuleNames"))
+	})
+
+	for i := range dw.GLs {
+		t.Run(fmt.Sprintf("GLModule-%d", i), func(t *testing.T) {
+			perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.GLs[i], fmt.Sprintf("DW.GLModule-%v", i)))
+		})
+	}
+
+	for i := range dw.LPPs {
+		t.Run(fmt.Sprintf("LPPModule-%d", i), func(t *testing.T) {
+			perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.LPPs[i], fmt.Sprintf("DW.LPPModule-%d", i)))
+		})
+	}
+
+	t.Run("Bootstrapper", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.Bootstrapper, "DW.Bootstrapper"))
+	})
+
+	t.Run("Discoverer", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.Disc, "DW.Discoverer"))
+	})
+
+	for i := range dw.CompiledGLs {
+		t.Run(fmt.Sprintf("CompiledGL-%v", i), func(t *testing.T) {
+			perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.CompiledGLs[i], fmt.Sprintf("DW.CompiledGL-%v", i)))
+		})
+	}
+
+	for i := range dw.CompiledLPPs {
+		t.Run(fmt.Sprintf("CompiledLPP-%v", i), func(t *testing.T) {
+			perfLogs = append(perfLogs, runSerdeTestPerf(t, dw.CompiledLPPs[i], fmt.Sprintf("DW.CompiledLPP-%v", i)))
+		})
+	}
+
+	// To save memory
+	cong := dw.CompiledConglomeration
+	dw = nil
+	runtime.GC()
+
+	t.Run("CompiledConglomeration", func(t *testing.T) {
+		perfLogs = append(perfLogs, runSerdeTestPerf(t, cong, "DW.CompiledConglomeration"))
+	})
+
+	// Write performance logs to CSV
+	if err := perfLogs.WritePerformanceLogsToCSV(path.Join("perf", "dw-perf-logs.csv")); err != nil {
+		t.Fatalf("Error writing performance logs to csv: %v", err)
+	}
+}
+
+func BenchmarkSerdeDW(b *testing.B) {
+
+	b.Skipf("the test is a development/debug/integration test. It is not needed for CI")
+
+	dist := GetDistWizard()
+	b.ResetTimer()
+
+	b.Run("ModuleNames", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, dist.ModuleNames, "DW.ModuleNames")
+		}
+	})
+
+	for i := range dist.GLs {
+		b.Run(fmt.Sprintf("GLModule-%d", i), func(b *testing.B) {
+			for j := 0; j < b.N; j++ {
+				justserde(b, dist.GLs[i], fmt.Sprintf("DW.GLModule-%d", i))
+			}
+		})
+	}
+
+	for i := range dist.LPPs {
+		b.Run(fmt.Sprintf("LPPModule-%d", i), func(b *testing.B) {
+			for j := 0; j < b.N; j++ {
+				justserde(b, dist.LPPs[i], fmt.Sprintf("DW.LPPModule-%d", i))
+			}
+		})
+	}
+
+	b.Run("Bootstrapper", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, dist.Bootstrapper, "DW.Bootstrapper")
+		}
+	})
+
+	b.Run("Discoverer", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, dist.Disc, "DW.Discoverer")
+		}
+	})
+
+	for i := range dist.CompiledGLs {
+
+		b.Run(fmt.Sprintf("CompiledGL-%v", i), func(b *testing.B) {
+			for j := 0; j < b.N; j++ {
+				justserde(b, dist.CompiledGLs[i], fmt.Sprintf("DW.CompiledGL-%v", i))
+			}
+		})
+	}
+
+	for i := range dist.CompiledLPPs {
+
+		b.Run(fmt.Sprintf("CompiledLPP-%v", i), func(b *testing.B) {
+			for j := 0; j < b.N; j++ {
+				justserde(b, dist.CompiledLPPs[i], fmt.Sprintf("DW.CompiledLPP-%v", i))
+			}
+		})
+	}
+
+	// To save memory
+	cong := dist.CompiledConglomeration
+	dist = nil
+	runtime.GC()
+
+	b.Run("CompiledConglomeration", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			justserde(b, cong, "DW.CompiledConglomeration")
+		}
+	})
+}
 
 type distributeTestCase struct {
 	numRow int
@@ -38,6 +174,14 @@ func (d distributeTestCase) define(comp *wizard.CompiledIOP) {
 	comp.InsertGlobal(0, "global-1", symbolic.Sub(c1, b1, a1))
 
 	comp.InsertInclusion(0, "inclusion-0", []ifaces.Column{c0, b0, a0}, []ifaces.Column{c1, b1, a1})
+}
+func (d distributeTestCase) assign(run *wizard.ProverRuntime) {
+	run.AssignColumn("a0", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(1), d.numRow-2), d.numRow))
+	run.AssignColumn("b0", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(2), d.numRow-2), d.numRow))
+	run.AssignColumn("c0", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(3), d.numRow-2), d.numRow))
+	run.AssignColumn("a1", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(1), d.numRow-2), d.numRow))
+	run.AssignColumn("b1", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(2), d.numRow-2), d.numRow))
+	run.AssignColumn("c1", smartvectors.RightZeroPadded(vector.Repeat(field.NewElement(3), d.numRow-2), d.numRow))
 }
 
 // GetDistWizard initializes a distributed wizard configuration using the
@@ -135,3 +279,42 @@ func TestSerdeDistWizard(t *testing.T) {
 		runSerdeTest(t, cong, "DistributedWizard.CompiledConglomeration", true, false)
 	})
 }
+
+// func TestSerdeDWCong(t *testing.T) {
+
+// 	t.Skipf("the test is a development/debug/integration test. It is not needed for CI")
+
+// 	// Setup
+// 	distWizard := GetBasicDistWizard()
+// 	cong := distWizard.CompiledConglomeration
+// 	distWizard = nil
+// 	runtime.GC()
+
+// 	// Subtests
+// 	tests := []struct {
+// 		name        string
+// 		obj         any
+// 		sanityCheck bool
+// 		failfast    bool
+// 	}{
+// 		{name: "Wiop", obj: cong.Wiop, sanityCheck: true, failfast: true},
+// 		{name: "Recursion", obj: cong.Recursion, sanityCheck: true, failfast: true},
+
+// 		// All of these tests PASS
+// 		{name: "MaxNbProofs", obj: cong.MaxNbProofs, sanityCheck: true, failfast: false},
+// 		{name: "DefaultWitness", obj: cong.DefaultWitness, sanityCheck: true, failfast: true},
+// 		{name: "DefaultIops", obj: cong.DefaultIops, sanityCheck: true, failfast: true},
+// 		{name: "PrecomputedGLVks", obj: cong.PrecomputedGLVks, sanityCheck: true, failfast: false},
+// 		{name: "PrecomputedLPPVks", obj: cong.PrecomputedLPPVks, sanityCheck: true, failfast: false},
+// 		{name: "VerifyingKeyColumns", obj: cong.VerifyingKeyColumns, sanityCheck: true, failfast: false},
+// 		{name: "HolisticLookupMappedLPPPostion", obj: cong.HolisticLookupMappedLPPPostion, sanityCheck: true, failfast: false},
+// 		{name: "HolisticLookupMappedLPPVK", obj: cong.HolisticLookupMappedLPPVK, sanityCheck: true, failfast: false},
+// 		{name: "IsGL", obj: cong.IsGL, sanityCheck: true, failfast: false},
+// 	}
+
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			runSerdeTest(t, tt.obj, tt.name, tt.sanityCheck, tt.failfast)
+// 		})
+// 	}
+// }
