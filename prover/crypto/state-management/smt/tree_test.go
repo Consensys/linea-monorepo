df5d90c9e806fe6637f19b12eb467268db22395b
AlexandreBelling <alexandrebelling8@gmail.com>
2025-10-24 11:08:58 +0800

    test the consistency between hashLR and the normal "byte" hashing

---
+++ prover/crypto/state-management/smt/tree_test.go
@@ -0,0 +1,41 @@
+package smt
+
+import (
+	"bytes"
+	"math/rand/v2"
+	"testing"
+
+	"github.com/consensys/linea-monorepo/prover/crypto/state-management/hashtypes"
+	"github.com/consensys/linea-monorepo/prover/maths/field"
+	"github.com/consensys/linea-monorepo/prover/utils"
+	"github.com/go-playground/assert/v2"
+)
+
+// TestHashNodeLR checks that the hash of a node is the same as the hash of
+// the concatenation of the hash of the left and the hash of the right
+// child (in bytes).
+func TestHashNodeLR(t *testing.T) {
+
+	cfg := &Config{
+		HashFunc: hashtypes.Poseidon2,
+		Depth:    40,
+	}
+
+	rng := rand.New(utils.NewRandSource(0)) // #nosec G404
+	l := field.PseudoRandOctuplet(rng)
+	r := field.PseudoRandOctuplet(rng)
+
+	buf := &bytes.Buffer{}
+	field.WriteOctupletTo(buf, l)
+	field.WriteOctupletTo(buf, r)
+	lrBytes := buf.Bytes()
+
+	// bytesHasher
+	bytesHasher := cfg.HashFunc()
+	bytesHasher.Write(lrBytes)
+	digestBytesHashing := bytesHasher.Sum(nil)
+	digestNodeLR := field.OctupletToBytes(hashLR(cfg, l, r))
+
+	assert.Equal(t, digestBytesHashing, digestNodeLR[:])
+
+}
--- prover/maths/field/element.go
+++ prover/maths/field/element.go
@@ -1,6 +1,8 @@
 package field
 
 import (
+	"fmt"
+	"io"
 	"math/big"
 	"math/bits"
 	"math/rand/v2"
@@ -162,4 +164,22 @@ func RandomOctuplet() Octuplet {
 		oct[i] = RandomElement()
 	}
 	return oct
+}
+
+func PseudoRandOctuplet(rng *rand.Rand) Octuplet {
+	var oct Octuplet
+	for i := range oct {
+		oct[i] = PseudoRand(rng)
+	}
+	return oct
+}
+
+func WriteOctupletTo(w io.Writer, octuplet Octuplet) error {
+	for i := range octuplet {
+		f := octuplet[i].Bytes()
+		if _, err := w.Write(f[:]); err != nil {
+			return fmt.Errorf("error writing field octuplet, could not write position %v: %w", i, err)
+		}
+	}
+	return nil
 }
