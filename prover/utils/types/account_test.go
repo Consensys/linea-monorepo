30795aa87248dc52371d3d1504116f61d8937748
Yao Galteland <yaoj.galteland@gmail.com>
2025-10-28 15:06:03 +0100

    add test vectors for account

--- prover/utils/types/account_test.go
+++ prover/utils/types/account_test.go
@@ -1,11 +1,15 @@
-package types
+package types_test
 
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"math/big"
 	"strconv"
 	"testing"
+
+	"github.com/consensys/linea-monorepo/prover/crypto/poseidon2"
+	. "github.com/consensys/linea-monorepo/prover/utils/types"
 
 	"github.com/consensys/linea-monorepo/prover/utils"
 	"github.com/stretchr/testify/assert"
@@ -130,6 +134,60 @@ func TestReadWriteAccountUnpacked(t *testing.T) {
 		_, err = acc.ReadFrom(buf)
 		require.NoError(t, err)
 		assert.Equal(t, c.Account, acc)
+	}
+
+}
+
+func TestVectorsAccountFormattingHASH(t *testing.T) {
+
+	tcases := []struct {
+		Account     Account
+		AccountHash string
+	}{
+		{
+			Account: Account{
+				Balance: big.NewInt(0),
+			},
+			AccountHash: "0x0be39dd910329801041c54896705cb664779584732a232276e59ce2e7ca1b5a7",
+		},
+		{
+			// EOA
+			Account: Account{
+				Nonce:             65,
+				Balance:           big.NewInt(5690),
+				StorageRoot:       Bytes32FromHex("0x0b1dfeef3db4956540da8a5f785917ef1ba432e521368da60a0a1ce430425666"),
+				Poseidon2CodeHash: Bytes32FromHex("0x729aac4455d43f2c69e53bb75f8430193332a4c32cafd9995312fa8346929e73"),
+				KeccakCodeHash:    FullBytes32FromHex("0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"),
+				CodeSize:          0,
+			},
+			AccountHash: "0x60cef4a3679fc56e66cba6c72cc9e536600ca999026ef7d22fd7595e62f9fdb8",
+		},
+		{
+			// Another EOA
+			Account: Account{
+				Nonce:             65,
+				Balance:           big.NewInt(835),
+				StorageRoot:       Bytes32FromHex("0x1c41acc261451aae253f621857172d6339919d18059f35921a50aafc69eb5c39"),
+				Poseidon2CodeHash: Bytes32FromHex("0x7b688b215329825e5b00e4aa4e1857bc17afab503a87ecc063614b9b227106b2"),
+				KeccakCodeHash:    FullBytes32FromHex("0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"),
+				CodeSize:          0,
+			},
+			AccountHash: "0x5e6056aa0619c47d3a01df970de314a04daf224d21e8a73b0003d7a4286a7538",
+		},
+	}
+
+	// Loop using index 'i' so we can modify the tcases slice
+	for _, c := range tcases {
+		// Calculate the hash
+		hasher := poseidon2.Poseidon2()
+		c.Account.WriteTo(hasher)
+		hash := hasher.Sum(nil)
+
+		// Format the hash as a "0x" prefixed hex string
+		hashString := fmt.Sprintf("0x%x", hash)
+
+		assert.Equal(t, c.AccountHash, hashString)
+
 	}
 
 }
