fbb04407233ba6c06cb147977a1eb687a73aa7d8
Yao Galteland <yaoj.galteland@gmail.com>
2025-10-28 13:11:38 +0100

    accumulator ok, utils ok

--- prover/crypto/state-management/accumulator/testvectors_poseidon2_test.go
+++ prover/crypto/state-management/accumulator/testvectors_poseidon2_test.go
@@ -26,7 +26,7 @@ func TestEmptyAccumulatorPoseidon2(t *testing.T) {
 		)
 
 		// also checks its hash value (i.e, the corresponding leaf in the tree)
-		require.Equal(t, "0x0891fa77c3d0c9b745840d71d41dcb58b638d4734bb4f0bba4a3d1a2d847b672", leafOpening.Hash(acc.Config()).Hex())
+		require.Equal(t, "0x2f5de7ad279a134761de0d89702e52743b2f04f41b86cc5400dfae1d4b981340", leafOpening.Hash(acc.Config()).Hex())
 	}
 
 	// check the value of the second leaf opening. It should be tail with an updated prev value
@@ -34,25 +34,25 @@ func TestEmptyAccumulatorPoseidon2(t *testing.T) {
 		leafOpening := acc.Data.MustGet(1).LeafOpening
 		require.Equal(
 			t,
-			"LeafOpening{Prev: 0, Next: 1, HKey: 0x12ab655e9a2ca55660b44d1e5c37b00159aa76fed00000010a11800000000000, HVal: 0x0000000000000000000000000000000000000000000000000000000000000000}",
+			"LeafOpening{Prev: 0, Next: 1, HKey: 0x7f0000007f0000007f0000007f0000007f0000007f0000007f0000007f000000, HVal: 0x0000000000000000000000000000000000000000000000000000000000000000}",
 			leafOpening.String(),
 		)
 
 		// also check its hash value
-		require.Equal(t, "0x10ba2286f648a549b50ea5f1b6e1155d22c31eb4727c241e76c420200cd5dbe0", leafOpening.Hash(acc.Config()).Hex())
+		require.Equal(t, "0x5c73480b5e85fc2c3ab61fc155cf475b74590d005d8916b85fdd5c32773b1255", leafOpening.Hash(acc.Config()).Hex())
 	}
 
 	// root of the subtree (e.g. exluding the next free node)
 	require.Equal(
 		t,
-		"0x0951bfcd4ac808d195af8247140b906a4379b3f2d37ec66e34d2f4a5d35fa166",
+		"0x3a00a8e34a16f8a1225fee734816edb326f783bd6678d793345a28f046586ba6",
 		acc.SubTreeRoot().Hex(),
 	)
 
 	// root of the complete accumulator (e.g including the last node)
 	require.Equal(
 		t,
-		"0x07977874126658098c066972282d4c85f230520af3847e297fe7524f976873e5",
+		"0x2fa0344a2fab2b310d2af3155c330261263f887379aef18b4941e3ea1cc59df7",
 		acc.TopRoot().Hex(),
 	)
 }
@@ -83,7 +83,7 @@ func TestInsertionRootHashPoseidon2(t *testing.T) {
 
 		// also checks its hash value (i.e, the corresponding leaf in the tree)
 		leaf := leafOpening.Hash(acc.Config())
-		require.Equal(t, "0x09dd19003c62ec025c50e9eacb4fd9509bc5a150b43bce9240162eb4b1778e4b", leaf.Hex())
+		require.Equal(t, "0x22e7a2ea33304d915fd93eac62b418ba7ca915131bea42827670d7f31a3b3445", leaf.Hex())
 	}
 
 	// check the value of the second leaf opening. It should be tail with an updated prev value
@@ -91,13 +91,13 @@ func TestInsertionRootHashPoseidon2(t *testing.T) {
 		leafOpening := acc.Data.MustGet(1).LeafOpening
 		require.Equal(
 			t,
-			"LeafOpening{Prev: 2, Next: 1, HKey: 0x12ab655e9a2ca55660b44d1e5c37b00159aa76fed00000010a11800000000000, HVal: 0x0000000000000000000000000000000000000000000000000000000000000000}",
+			"LeafOpening{Prev: 2, Next: 1, HKey: 0x7f0000007f0000007f0000007f0000007f0000007f0000007f0000007f000000, HVal: 0x0000000000000000000000000000000000000000000000000000000000000000}",
 			leafOpening.String(),
 		)
 
 		// also check its hash value
 		leaf := leafOpening.Hash(acc.Config())
-		require.Equal(t, "0x0ab3e0de05fd2b67ffa26aadd17e91b632a26013784e757028ae9d851508b055", leaf.Hex())
+		require.Equal(t, "0x7172ab730766d9b6367938c031afbd13607d95796a60930945ab6b88434339b2", leaf.Hex())
 	}
 
 	// check the value of the second leaf opening, corresponds to the inserted entry
@@ -105,26 +105,26 @@ func TestInsertionRootHashPoseidon2(t *testing.T) {
 		leafOpening := acc.Data.MustGet(2).LeafOpening
 		require.Equal(
 			t,
-			"LeafOpening{Prev: 0, Next: 1, HKey: 0x096ccb5dfe8d38d9e11f9f3ed692d0b377f4f12cb204d15338fe9862a7997743, HVal: 0x05d7b878349fc53b779b4b7e3a303694e88fba9521b6add1ed824a3be74104fd}",
+			"LeafOpening{Prev: 0, Next: 1, HKey: 0x27b48fe33995c55d329a805f2c4acdc05c1229592579fcab0a46d23239f19498, HVal: 0x43b6adef3fc8455b573aadd56e3ab14a11084a9d667c2c8449b707c31cb32560}",
 			leafOpening.String(),
 		)
 
 		// also check its hash value
 		leaf := leafOpening.Hash(acc.Config())
-		require.Equal(t, "0x0002a72d394f16c6a063ca6b15a8bf36c6efbfc67831534707c39bee252400df", leaf.Hex())
+		require.Equal(t, "0x42bee0052f812f13511ced262f95e62d5e572dc971f68fa8759914a441332ff1", leaf.Hex())
 	}
 
 	// root of the subtree (e.g. exluding the next free node)
 	require.Equal(
 		t,
-		"0x0882afe875656680dceb7b17fcba7c136cec0c32becbe9039546c79f71c56d36",
+		"0x50b4bb9d2b4c917f48ca9465613d7efe092934a95bcdc7a004ef3a6b7900fd5b",
 		acc.SubTreeRoot().Hex(),
 	)
 
 	// root of the complete accumulator (e.g including the last node)
 	require.Equal(
 		t,
-		"0x0cfdc3990045390093be4e1cc9907b220324cccd1c8ea9ede980c7afa898ef8d",
+		"0x7a5cf31710a49b59489deb53505a3d5458625ef01b33914d0d784380464177ad",
 		acc.TopRoot().Hex(),
 	)
 }
@@ -150,14 +150,14 @@ func TestInsertAndUpdateRootHashPoseidon2(t *testing.T) {
 	// root of the subtree (e.g. exluding the next free node)
 	require.Equal(
 		t,
-		"0x0882afe875656680dceb7b17fcba7c136cec0c32becbe9039546c79f71c56d36",
+		"0x50b4bb9d2b4c917f48ca9465613d7efe092934a95bcdc7a004ef3a6b7900fd5b",
 		acc.SubTreeRoot().Hex(),
 	)
 
 	// root of the complete accumulator (e.g including the last node)
 	require.Equal(
 		t,
-		"0x0cfdc3990045390093be4e1cc9907b220324cccd1c8ea9ede980c7afa898ef8d",
+		"0x7a5cf31710a49b59489deb53505a3d5458625ef01b33914d0d784380464177ad",
 		acc.TopRoot().Hex(),
 	)
 }
@@ -183,7 +183,7 @@ func TestInsertAndDeleteRootHashPoseidon2(t *testing.T) {
 	// of the empty tree
 	require.Equal(
 		t,
-		"0x0951bfcd4ac808d195af8247140b906a4379b3f2d37ec66e34d2f4a5d35fa166",
+		"0x3a00a8e34a16f8a1225fee734816edb326f783bd6678d793345a28f046586ba6",
 		acc.SubTreeRoot().Hex(),
 	)
 
@@ -192,7 +192,7 @@ func TestInsertAndDeleteRootHashPoseidon2(t *testing.T) {
 	// inserting.
 	require.Equal(
 		t,
-		"0x0bcb88342825fa7a079a5cf5f77d07b1590a140c311a35acd765080eea120329",
+		"0x391010e55fad441a1e3a33b11fb8271f68e86b6c1812ebd402d678d343fded62",
 		acc.TopRoot().Hex(),
 	)
 }
