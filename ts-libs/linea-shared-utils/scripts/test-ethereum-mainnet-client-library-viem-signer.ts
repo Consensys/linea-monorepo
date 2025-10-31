2727bbb9faf787103620e90f8324a1263efa6cde
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2025-10-31 22:21:36 +1100

    rename to test-ethereum-mainnet-client-library-viem-signer

--- ts-libs/linea-shared-utils/scripts/test-ethereum-mainnet-client-library-viem-signer.ts
+++ ts-libs/linea-shared-utils/scripts/test-ethereum-mainnet-client-library-viem-signer.ts
@@ -2,7 +2,7 @@
 
 RPC_URL=http://127.0.0.1:8545 \
 PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
-pnpm --filter @consensys/linea-shared-utils exec tsx scripts/test-ethereum-mainnet-client-library.ts
+pnpm --filter @consensys/linea-shared-utils exec tsx scripts/test-ethereum-mainnet-client-library-viem-signer.ts
 
  */
 import { ViemBlockchainClientAdapter } from "../src/clients/ViemBlockchainClientAdapter";
