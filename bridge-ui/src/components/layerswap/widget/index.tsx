7a0ae9bca8c8fc7bd8ecc88d730cfb502c960ae7
Aren <aren@bransfer.io>
2025-11-03 18:15:57 +0400

    Refactor Layerswap Widget to use API key from config instead of hardcoded string

--- bridge-ui/src/components/layerswap/widget/index.tsx
+++ bridge-ui/src/components/layerswap/widget/index.tsx
@@ -1,10 +1,9 @@
 import { getSettings } from "@layerswap/widget";
 import { LayerswapClientWrapper } from "./LayerswapClientWrapper";
+import { config } from "@/config";
 
 export async function Widget() {
-  const settings = await getSettings(
-    "NDBxG+aon6WlbgIA2LfwmcbLU52qUL9qTnztTuTRPNSohf/VnxXpRaJlA5uLSQVqP8YGIiy/0mz+mMeZhLY4/Q",
-  );
+  const settings = await getSettings(config.layerswapApiKey);
 
   return <LayerswapClientWrapper settings={settings} />;
 }
