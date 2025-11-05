90917c36321ba2b7474c27e5bb7bb658f36a3cc3
David Pearce <david.pearce@consensys.net>
2025-11-05 16:14:10 +1300

    feat: replace legacy replays with `PRAGUE` replays (#2461)
    * refactor legacy replay tests
    
    This refactors the existing legacy replay tests, such they are: (1) all
    disabled; (2) live in their own package to avoid clutter; (3) all the
    actual replays have been moved into their own folder as well.
    
    * add initial set of prague replays
    
    The goal here is simply to figure out what the batch size is for the
    various testing regimes (fast replay, nightly, weekly).
    
    * updating gradle tests
    
    This simplifies the way the existing gradle test tasks are written, and
    also adds a notion of "weekly replay tests".

---
+++ tracer/arithmetization/src/test/java/net/consensys/linea/pragueReplayTests/SepoliaTests.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Consensys Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package net.consensys.linea.pragueReplayTests;
+
+import static net.consensys.linea.ReplayTestTools.replay;
+import static net.consensys.linea.zktracer.ChainConfig.SEPOLIA_PRAGUE_TESTCONFIG;
+
+import net.consensys.linea.UnitTestWatcher;
+import net.consensys.linea.reporting.TracerTestBase;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Tag("replay")
+@ExtendWith(UnitTestWatcher.class)
+public class SepoliaTests extends TracerTestBase {
+
+  @Test
+  void block_19562398(TestInfo testInfo) {
+    replay(SEPOLIA_PRAGUE_TESTCONFIG, "prague/19562398.sepolia.prague.json.gz", testInfo);
+  }
+
+  @Test
+  void block_19562399_19562417(TestInfo testInfo) {
+    replay(SEPOLIA_PRAGUE_TESTCONFIG, "prague/19562399-19562417.sepolia.prague.json.gz", testInfo);
+  }
+}
