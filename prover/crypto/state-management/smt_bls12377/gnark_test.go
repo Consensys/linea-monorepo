4307641351842796fd1ca7c9cd3608bc1c52797d
Thomas Piellard <thomas.piellard@consensys.net>
2025-11-06 12:05:37 +0100

    feat: test on non empty tree ok

--- prover/crypto/state-management/smt_bls12377/gnark_test.go
+++ prover/crypto/state-management/smt_bls12377/gnark_test.go
@@ -1,25 +1,18 @@
 package smt_bls12377
 
 import (
-	"fmt"
 	"testing"
 
-	// "github.com/consensys/gnark-crypto/ecc"
-	// "github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
 	"github.com/consensys/gnark/frontend"
-	// "github.com/consensys/gnark/frontend/cs/scs"
+
+	"github.com/consensys/gnark/frontend/cs/scs"
 	gposeidon2 "github.com/consensys/gnark/std/hash/poseidon2"
 	"github.com/consensys/linea-monorepo/prover/crypto/state-management/hashtypes"
 	"github.com/consensys/linea-monorepo/prover/utils/types"
 	"github.com/stretchr/testify/require"
 )
-
-func printBytes32(t types.Bytes32) {
-	for i := 0; i < len(t); i++ {
-		fmt.Printf("%x ", t[i])
-	}
-	fmt.Println("")
-}
 
 func getMerkleProof(t *testing.T) ([]Proof, []types.Bytes32, types.Bytes32) {
 
@@ -30,7 +23,15 @@ func getMerkleProof(t *testing.T) ([]Proof, []types.Bytes32, types.Bytes32) {
 
 	tree := NewEmptyTree(config)
 
-	// Only contains empty leaves
+	// populate the tree
+	nbLeaves := 10
+	var tmp fr.Element
+	for i := 0; i < nbLeaves; i++ {
+		tmp.SetRandom()
+		s := tmp.Bytes()
+		buf := types.AsBytes32(s[:])
+		tree.Update(i, buf)
+	}
 	nbProofs := 10
 	proofs := make([]Proof, nbProofs)
 	leafs := make([]types.Bytes32, nbProofs)
@@ -69,50 +70,49 @@ func (circuit *MerkleProofCircuit) Define(api frontend.API) error {
 func TestMerkleProofGnark(t *testing.T) {
 
 	// generate witness
-	// proofs, leafs, root := getMerkleProof(t)
-	getMerkleProof(t)
-	// nbProofs := len(proofs)
-	// var witness MerkleProofCircuit
-	// witness.Proofs = make([]GnarkProof, nbProofs)
-	// witness.Leafs = make([]frontend.Variable, nbProofs)
-	// var buf fr.Element
-	// for i := 0; i < nbProofs; i++ {
-	// 	witness.Proofs[i].Siblings = make([]frontend.Variable, len(proofs[i].Siblings))
-	// 	for j := 0; j < len(proofs[i].Siblings); j++ {
-	// 		siblingBytes := proofs[i].Siblings[j]
-	// 		buf.SetBytes(siblingBytes[:])
-	// 		witness.Proofs[i].Siblings[j] = buf.String()
-	// 	}
-	// 	witness.Proofs[i].Path = proofs[i].Path
-	// 	leafsBytes := leafs[i]
-	// 	buf.SetBytes(leafsBytes[:])
-	// 	witness.Leafs[i] = buf.String()
-	// }
-	// rootBytes := root
+	proofs, leafs, root := getMerkleProof(t)
+	nbProofs := len(proofs)
+	var witness MerkleProofCircuit
+	witness.Proofs = make([]GnarkProof, nbProofs)
+	witness.Leafs = make([]frontend.Variable, nbProofs)
+	var buf fr.Element
+	for i := 0; i < nbProofs; i++ {
+		witness.Proofs[i].Siblings = make([]frontend.Variable, len(proofs[i].Siblings))
+		for j := 0; j < len(proofs[i].Siblings); j++ {
+			siblingBytes := proofs[i].Siblings[j]
+			buf.SetBytes(siblingBytes[:])
+			witness.Proofs[i].Siblings[j] = buf.String()
+		}
+		witness.Proofs[i].Path = proofs[i].Path
+		leafsBytes := leafs[i]
+		buf.SetBytes(leafsBytes[:])
+		witness.Leafs[i] = buf.String()
+	}
+	rootBytes := root
 
-	// buf.SetBytes(rootBytes[:])
-	// witness.Root = buf.String()
+	buf.SetBytes(rootBytes[:])
+	witness.Root = buf.String()
 
-	// // compile circuit
-	// var circuit MerkleProofCircuit
-	// circuit.Proofs = make([]GnarkProof, nbProofs)
-	// circuit.Leafs = make([]frontend.Variable, nbProofs)
-	// for i := 0; i < nbProofs; i++ {
-	// 	circuit.Proofs[i].Siblings = make([]frontend.Variable, len(proofs[i].Siblings))
-	// }
-	// ccs, err := frontend.Compile(ecc.BLS12_377.ScalarField(), scs.NewBuilder, &circuit, frontend.IgnoreUnconstrainedInputs())
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	// compile circuit
+	var circuit MerkleProofCircuit
+	circuit.Proofs = make([]GnarkProof, nbProofs)
+	circuit.Leafs = make([]frontend.Variable, nbProofs)
+	for i := 0; i < nbProofs; i++ {
+		circuit.Proofs[i].Siblings = make([]frontend.Variable, len(proofs[i].Siblings))
+	}
+	ccs, err := frontend.Compile(ecc.BLS12_377.ScalarField(), scs.NewBuilder, &circuit, frontend.IgnoreUnconstrainedInputs())
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	// // solve the circuit
-	// twitness, err := frontend.NewWitness(&witness, ecc.BLS12_377.ScalarField())
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// err = ccs.IsSolved(twitness)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	// solve the circuit
+	twitness, err := frontend.NewWitness(&witness, ecc.BLS12_377.ScalarField())
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ccs.IsSolved(twitness)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 }
