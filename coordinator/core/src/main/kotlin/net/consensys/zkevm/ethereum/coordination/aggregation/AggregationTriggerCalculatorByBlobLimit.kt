abf0c7da81c54ff6662fd9bb7ebdd8e947569304
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-11-12 12:07:07 +0000

    coordinator: AggregationTriggerCalculatorByBlobLimit small code rearange

--- coordinator/core/src/main/kotlin/net/consensys/zkevm/ethereum/coordination/aggregation/AggregationTriggerCalculatorByBlobLimit.kt
+++ coordinator/core/src/main/kotlin/net/consensys/zkevm/ethereum/coordination/aggregation/AggregationTriggerCalculatorByBlobLimit.kt
@@ -13,10 +13,14 @@ class AggregationTriggerCalculatorByBlobLimit(private val maxBlobsPerAggregation
 
   private var inFlightAggregation: InFlightAggregation? = null
 
+  private fun willReachBlobLimitWithOneMoreBlob(): Boolean {
+    val blobCount = (inFlightAggregation?.blobsCount ?: 0u) + 1u
+    return blobCount >= maxBlobsPerAggregation
+  }
+
   @Synchronized
   override fun checkAggregationTrigger(blobCounters: BlobCounters): AggregationTrigger? {
-    val blobCount = (inFlightAggregation?.blobsCount ?: 0u) + 1u
-    return if (blobCount >= maxBlobsPerAggregation) {
+    return if (willReachBlobLimitWithOneMoreBlob()) {
       AggregationTrigger(
         aggregationTriggerType = AggregationTriggerType.BLOB_LIMIT,
         aggregation = BlobsToAggregate(
@@ -33,7 +37,7 @@ class AggregationTriggerCalculatorByBlobLimit(private val maxBlobsPerAggregation
   @Synchronized
   override fun newBlob(blobCounters: BlobCounters) {
     val blobCount = (inFlightAggregation?.blobsCount ?: 0u) + 1u
-    if (blobCount >= maxBlobsPerAggregation) {
+    if (willReachBlobLimitWithOneMoreBlob()) {
       throw IllegalArgumentException(
         "Aggregation blob limit overflow: maxBlobsPerAggregation=$maxBlobsPerAggregation " +
           "blobsCount=$blobCount " +
