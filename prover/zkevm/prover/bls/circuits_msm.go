be09cf79d8fb0f1d1c884c496935e62e881d710b
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-11-16 20:52:05 +0000

    fix: incorrect limb count for scalar

--- prover/zkevm/prover/bls/circuits_msm.go
+++ prover/zkevm/prover/bls/circuits_msm.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-	nbRowsPerG1Mul = nbFpLimbs + 3*nbG1Limbs // 1 scalar, 1 point, 1 previous accumulator, 1 next accumulator
-	nbRowsPerG2Mul = nbFpLimbs + 3*nbG2Limbs // 1 scalar, 1 point, 1 previous accumulator, 1 next accumulator
+	nbRowsPerG1Mul = nbFrLimbs + 3*nbG1Limbs // 1 scalar, 1 point, 1 previous accumulator, 1 next accumulator
+	nbRowsPerG2Mul = nbFrLimbs + 3*nbG2Limbs // 1 scalar, 1 point, 1 previous accumulator, 1 next accumulator
 )
 
 func nbRowsPerMul(g group) int {
