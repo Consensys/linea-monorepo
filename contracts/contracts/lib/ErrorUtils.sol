059d88db5811ddfb7c24b954820dc4ad9567b11a
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2025-11-17 23:49:30 +1100

    change errorutils

--- contracts/contracts/lib/ErrorUtils.sol
+++ contracts/contracts/lib/ErrorUtils.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.30;
 import { IGenericErrors } from "../interfaces/IGenericErrors.sol";
 
 /**
- * @title Library to perform SparseMerkleProof actions using the MiMC hashing algorithm
+ * @title Library for error checking utilities.
  * @author ConsenSys Software Inc.
  * @custom:security-contact security-report@linea.build
  */
 library ErrorUtils {
+  /**
+   * @notice Reverts if the address is the zero address.
+   * @param _addr The address to check.
+   */
   function revertIfZeroAddress(address _addr) internal pure {
     if (_addr == address(0)) revert IGenericErrors.ZeroAddressNotAllowed();
   }
