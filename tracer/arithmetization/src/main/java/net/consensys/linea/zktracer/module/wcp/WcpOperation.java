d5b8919b9d7d15f31abe9292662b72a54352b1c5
David Pearce <david.pearce@consensys.net>
2025-11-19 20:16:39 +1300

    feat: update WCP with assembly implementation (#2491)
    This makes various changes to the Wcp module to support the assembly
    implementation.  Mostly, this involves removing column calculations
    which are no longer required.
    
    This also updates the constraints to utilise the maxlog function within
    rlputils, and updates go-corset to v1.1.28 (which is required for the
    defcall syntax).

--- tracer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/wcp/WcpOperation.java
+++ tracer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/wcp/WcpOperation.java
@@ -21,19 +21,13 @@ import static net.consensys.linea.zktracer.Trace.EVM_INST_ISZERO;
 import static net.consensys.linea.zktracer.Trace.EVM_INST_LT;
 import static net.consensys.linea.zktracer.Trace.EVM_INST_SGT;
 import static net.consensys.linea.zktracer.Trace.EVM_INST_SLT;
-import static net.consensys.linea.zktracer.Trace.LLARGE;
-import static net.consensys.linea.zktracer.Trace.LLARGEMO;
 import static net.consensys.linea.zktracer.Trace.WCP_INST_GEQ;
 import static net.consensys.linea.zktracer.Trace.WCP_INST_LEQ;
-import static net.consensys.linea.zktracer.module.Util.byteBits;
 import static net.consensys.linea.zktracer.types.Conversions.bigIntegerToBytes;
 import static net.consensys.linea.zktracer.types.Conversions.reallyToSignedBigInteger;
 
 import java.math.BigInteger;
 import java.security.InvalidParameterException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -43,7 +37,6 @@ import net.consensys.linea.zktracer.Trace;
 import net.consensys.linea.zktracer.container.ModuleOperation;
 import net.consensys.linea.zktracer.types.Bytes16;
 import net.consensys.linea.zktracer.types.UnsignedByte;
-import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 
 @Accessors(fluent = true)
@@ -62,78 +55,11 @@ public class WcpOperation extends ModuleOperation {
   private final byte wcpInst;
   @EqualsAndHashCode.Include @Getter private final Bytes32 arg1;
   @EqualsAndHashCode.Include @Getter private final Bytes32 arg2;
-  private int ctMax; // Note : is computed in computeLineCount, if the WCP operation is added to the
-  // StackedSet
-
-  private Bytes arg1Hi;
-  private Bytes arg1Lo;
-  private Bytes arg2Hi;
-  private Bytes arg2Lo;
-
-  private Bytes adjHi;
-  private Bytes adjLo;
-  private Boolean neg1;
-
-  private Boolean neg2;
-  private boolean bit1;
-  private Boolean bit2;
-  private Boolean bit3;
-  private Boolean bit4;
-  private Boolean resLo;
-
-  final List<Boolean> bits = new ArrayList<>(LLARGE);
 
   public WcpOperation(final byte wcpInst, final Bytes32 arg1, final Bytes32 arg2) {
     this.wcpInst = wcpInst;
     this.arg1 = arg1;
     this.arg2 = arg2;
-  }
-
-  private void compute() {
-    final int length = isOli() ? LLARGE : ctMax + 1;
-    final int offset = LLARGE - length;
-    this.arg1Hi = arg1.slice(offset, length);
-    this.arg1Lo = arg1.slice(LLARGE + offset, length);
-    this.arg2Hi = arg2.slice(offset, length);
-    this.arg2Lo = arg2.slice(LLARGE + offset, length);
-
-    // Calculate Result Low
-    resLo = calculateResult(wcpInst, arg1, arg2);
-
-    // Set bit 3 and AdjHi
-    final BigInteger firstHi = arg1.slice(0, LLARGE).toUnsignedBigInteger();
-    final BigInteger secondHi = arg2.slice(0, LLARGE).toUnsignedBigInteger();
-    this.bit3 = firstHi.compareTo(secondHi) > 0;
-    this.adjHi = calculateAdj(bit3, firstHi, secondHi).slice(offset, length);
-
-    // Set bit 4 and AdjLo
-    final BigInteger firstLo = arg1.slice(LLARGE, LLARGE).toUnsignedBigInteger();
-    final BigInteger secondLo = arg2.slice(LLARGE, LLARGE).toUnsignedBigInteger();
-    this.bit4 = firstLo.compareTo(secondLo) > 0;
-    this.adjLo = calculateAdj(bit4, firstLo, secondLo).slice(offset, length);
-
-    // Initiate negatives and BITS
-    if (this.ctMax == LLARGEMO && (wcpInst == SLTbv || wcpInst == SGTbv)) {
-      // meaningful only for signed OpCode with LLARGE argument
-      UnsignedByte msb1 = UnsignedByte.of(arg1Hi.get(0));
-      UnsignedByte msb2 = UnsignedByte.of(arg2Hi.get(0));
-      Boolean[] msb1Bits = byteBits(msb1);
-      Boolean[] msb2Bits = byteBits(msb2);
-      neg1 = msb1Bits[0];
-      neg2 = msb2Bits[0];
-      Collections.addAll(bits, msb1Bits);
-      Collections.addAll(bits, msb2Bits);
-    } else {
-      neg1 = false;
-      neg2 = false;
-      for (int ct = 0; ct <= ctMax; ct++) {
-        bits.add(ct, false);
-      }
-    }
-
-    // Set bit 1 and 2
-    bit1 = arg1Hi.compareTo(arg2Hi) == 0;
-    bit2 = arg1Lo.compareTo(arg2Lo) == 0;
   }
 
   private boolean calculateResult(byte opCode, Bytes32 arg1, Bytes32 arg2) {
@@ -157,97 +83,15 @@ public class WcpOperation extends ModuleOperation {
   }
 
   void trace(Trace.Wcp trace, int stamp) {
-    this.compute();
-
-    final boolean resLo = this.resLo;
-    final boolean oli = isOli();
-    final boolean vli = isVli();
+    // Calculate result
+    final boolean res = calculateResult(wcpInst, arg1, arg2);
     final UnsignedByte inst = UnsignedByte.of(wcpInst);
-
-    for (int ct = 0; ct <= ctMax; ct++) {
-      trace
-          .wordComparisonStamp(stamp)
-          .oneLineInstruction(oli)
-          .variableLengthInstruction(vli)
-          .counter(UnsignedByte.of(ct))
-          .ctMax(UnsignedByte.of(ctMax))
-          .inst(inst)
-          .isEq(wcpInst == EQbv)
-          .isIszero(wcpInst == ISZERObv)
-          .isSlt(wcpInst == SLTbv)
-          .isSgt(wcpInst == SGTbv)
-          .isLt(wcpInst == LTbv)
-          .isGt(wcpInst == GTbv)
-          .isLeq(wcpInst == LEQbv)
-          .isGeq(wcpInst == GEQbv)
-          .argument1Hi(arg1Hi)
-          .argument1Lo(arg1Lo)
-          .argument2Hi(arg2Hi)
-          .argument2Lo(arg2Lo)
-          .result(resLo)
-          .bits(bits.get(ct))
-          .neg1(neg1)
-          .neg2(neg2)
-          .byte1(UnsignedByte.of(arg1Hi.get(ct)))
-          .byte2(UnsignedByte.of(arg1Lo.get(ct)))
-          .byte3(UnsignedByte.of(arg2Hi.get(ct)))
-          .byte4(UnsignedByte.of(arg2Lo.get(ct)))
-          .byte5(UnsignedByte.of(adjHi.get(ct)))
-          .byte6(UnsignedByte.of(adjLo.get(ct)))
-          .acc1(arg1Hi.slice(0, 1 + ct))
-          .acc2(arg1Lo.slice(0, 1 + ct))
-          .acc3(arg2Hi.slice(0, 1 + ct))
-          .acc4(arg2Lo.slice(0, 1 + ct))
-          .acc5(adjHi.slice(0, 1 + ct))
-          .acc6(adjLo.slice(0, 1 + ct))
-          .bit1(bit1)
-          .bit2(bit2)
-          .bit3(bit3)
-          .bit4(bit4)
-          .validateRow();
-    }
-  }
-
-  private boolean isOli() {
-    return switch (wcpInst) {
-      case ISZERObv, EQbv -> true;
-      case SLTbv, SGTbv, LTbv, GTbv, LEQbv, GEQbv -> false;
-      default -> throw new IllegalStateException("Unexpected value: " + wcpInst);
-    };
-  }
-
-  private boolean isVli() {
-    return switch (wcpInst) {
-      case LTbv, GTbv, LEQbv, GEQbv, SLTbv, SGTbv -> true;
-      case ISZERObv, EQbv -> false;
-      default -> throw new IllegalStateException("Unexpected value: " + wcpInst);
-    };
-  }
-
-  private int computeCtMax() {
-    switch (this.wcpInst) {
-      case ISZERObv, EQbv -> {
-        return 0;
-      }
-      case LTbv, GTbv, LEQbv, GEQbv, SLTbv, SGTbv -> {
-        if (this.arg1.isZero() && this.arg2.isZero()) {
-          return 0;
-        } else {
-          final ArrayList<Integer> sizes = new ArrayList<>(4);
-          sizes.add(this.arg1.slice(0, LLARGE).trimLeadingZeros().size());
-          sizes.add(this.arg2.slice(0, LLARGE).trimLeadingZeros().size());
-          sizes.add(this.arg1.slice(LLARGE, LLARGE).trimLeadingZeros().size());
-          sizes.add(this.arg2.slice(LLARGE, LLARGE).trimLeadingZeros().size());
-          return Collections.max(sizes) - 1;
-        }
-      }
-      default -> throw new IllegalStateException("Unexpected value: " + this.wcpInst);
-    }
+    //
+    trace.inst(inst).arg1(arg1).arg2(arg2).res(res).validateRow();
   }
 
   @Override
   protected int computeLineCount() {
-    ctMax = computeCtMax();
-    return ctMax + 1;
+    return 1;
   }
 }
