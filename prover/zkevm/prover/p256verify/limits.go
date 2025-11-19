0aab08d35544611acf90bd4b5356921fcd753cb5
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-11-19 02:44:58 +0000

    docs: circuit size

--- prover/zkevm/prover/p256verify/limits.go
+++ prover/zkevm/prover/p256verify/limits.go
@@ -3,6 +3,8 @@ package p256verify
 // Limits defines limits for P256Verify module.
 type Limits struct {
 	// NbInputInstances is the number of P256 input instances per a single
-	// verification circuit.
+	// verification circuit. gnark circuit size approximately 709k constraints
+	// and in Plonk-in-Wizard (with externalized range checks) approximately
+	// 183k rows per instance.
 	NbInputInstances int
 }
