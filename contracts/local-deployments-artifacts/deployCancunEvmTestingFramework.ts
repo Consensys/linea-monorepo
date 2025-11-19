b8406bb2d815f5548c47a55c96361673dd67509a
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-11-19 14:51:16 +0000

    fix gas setting on contract deployment

--- contracts/local-deployments-artifacts/deployCancunEvmTestingFramework.ts
+++ contracts/local-deployments-artifacts/deployCancunEvmTestingFramework.ts
@@ -53,6 +53,8 @@ async function deployOpcodeTester(wallet: ethers.Wallet, shanghaiEvmYulAddress: 
     shanghaiEvmYulAddress,
     {
       nonce: walletNonce,
+      maxFeePerGas: 7_200_000_000_000n,
+      maxPriorityFeePerGas: 7_000_000_000_000n,
     },
   );
 }
