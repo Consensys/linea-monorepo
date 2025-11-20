298e981c878119c6b39b9e31f1183e73e1fbddb1
Victorien Gauch <85494462+VGau@users.noreply.github.com>
2025-11-20 14:26:06 +0100

    feat: add new transaction metrics - postman (#1741)
    * feat: add new transaction metrics - postman
    
    * fix: include new db migration
    
    * fix: add counters
    
    * fix: add receipt receive time
    
    * fix: update claim creation date for retries
    
    * fix: add direction to histogram metrics
    
    * fix: update metrics

--- postman/src/application/postman/api/metrics/TransactionMetricsUpdater.ts
+++ postman/src/application/postman/api/metrics/TransactionMetricsUpdater.ts
@@ -6,13 +6,33 @@ export class TransactionMetricsUpdater implements ITransactionMetricsUpdater {
       LineaPostmanMetrics.TransactionProcessingTime,
       [0.1, 0.5, 1, 2, 3, 5, 7, 10],
       "Time taken to process a transaction",
+      ["direction"],
+    );
+
+    this.metricsService.createHistogram(
+      LineaPostmanMetrics.TransactionInfuraConfirmationTime,
+      [0.1, 0.5, 1, 2, 3, 5, 7, 10],
+      "Time taken to receive the transaction receipt from Infura",
+      ["direction"],
     );
   }
 
-  public addTransactionProcessingTime(transactionProcessingTimeInSeconds: number): void {
+  public addTransactionProcessingTime(direction: string, transactionProcessingTimeInSeconds: number): void {
     return this.metricsService.addValueToHistogram(
       LineaPostmanMetrics.TransactionProcessingTime,
       transactionProcessingTimeInSeconds,
+      { direction },
+    );
+  }
+
+  public addTransactionInfuraConfirmationTime(
+    direction: string,
+    transactionInfuraConfirmationTimeInSeconds: number,
+  ): void {
+    return this.metricsService.addValueToHistogram(
+      LineaPostmanMetrics.TransactionInfuraConfirmationTime,
+      transactionInfuraConfirmationTimeInSeconds,
+      { direction },
     );
   }
 }
