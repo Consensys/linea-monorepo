62308c09eeb0948add05ac72c2bb6d58cf9d2fdd
Bogdan Ursu <bogdanursuoffice@gmail.com>
2025-11-21 09:01:05 +0100

    feat: fix public-input  support for conflations without any user tx (#1713)
    * feat: fix public-input  support for conflations without any user transactions
    
    * minor(logs): adds info in the panic message when finding two global constraints with different domain size
    
    * fix: panic message for local openings on verifier cols
    
    * Revert "fix: panic message for local openings on verifier cols"
    
    This reverts commit 9551e4bc4592a98604f0e701c16b45a7257af0cd.
    
    * fix: make NBytes chain id not be a verifier col
    
    * reverting config files
    
    * fix: missing limitless advice for new ChainID fetcher
    
    ---------
    
    Co-authored-by: AlexandreBelling <alexandrebelling8@gmail.com>

---
+++ prover/zkevm/prover/publicInput/fetchers_arithmetization/chain_id_fetcher.go
@@ -0,0 +1,76 @@
+package fetchers_arithmetization
+
+import (
+	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
+	"github.com/consensys/linea-monorepo/prover/maths/field"
+	"github.com/consensys/linea-monorepo/prover/protocol/column"
+	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
+	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
+	sym "github.com/consensys/linea-monorepo/prover/symbolic"
+	commonconstraints "github.com/consensys/linea-monorepo/prover/zkevm/prover/common/common_constraints"
+	arith "github.com/consensys/linea-monorepo/prover/zkevm/prover/publicInput/arith_struct"
+	util "github.com/consensys/linea-monorepo/prover/zkevm/prover/publicInput/utilities"
+)
+
+const (
+	// ChainIDOffset is the corresponding offset position for the chain ID
+	// since it is a shift, -1 means no offset.
+	ChainIDOffset = -3
+)
+
+// ChainIDFetcher is a struct used to fetch the chainIDs from the arithmetization's BlockDataCols
+type ChainIDFetcher struct {
+	// chainID
+	ChainID       ifaces.Column // a column used to fetch the ChainID. The implementation is currently unaligned with respect to the number of limbs.
+	NBytesChainID ifaces.Column // a column used to fetch the number of bytes of the ChainID limb data
+}
+
+// NewTimestampFetcher returns a new TimestampFetcher with initialized columns that are not constrained.
+func NewChainIDFetcher(comp *wizard.CompiledIOP, name string, bdc *arith.BlockDataCols) ChainIDFetcher {
+	size := bdc.Ct.Size()
+	res := ChainIDFetcher{
+		ChainID:       util.CreateCol(name, "CHAIN_ID", size, comp),
+		NBytesChainID: util.CreateCol(name, "N_BYTES_CHAIN_ID", size, comp), // 2 bytes for chainID, we will constrain it later
+	}
+
+	return res
+}
+
+// DefineTimestampFetcher specifies the constraints of the TimestampFetcher with respect to the BlockDataCols
+func DefineChainIDFetcher(comp *wizard.CompiledIOP, fetcher *ChainIDFetcher, name string, bdc *arith.BlockDataCols) {
+	// constrain fetcher ChainID to contain the value of the last block's chainID
+	// we only populate the first entry of the ChainID column
+	comp.InsertLocal(
+		0,
+		ifaces.QueryIDf("%s_%s", name, "LAST_LOCAL"),
+		sym.Sub(
+			fetcher.ChainID,                         // first position of the ChainID column
+			column.Shift(bdc.DataLo, ChainIDOffset), // corresponding position in the arithmetization's BlockDataCols
+		),
+	)
+
+	// constrain the N_BYTES_CHAIN_ID to have only two BYTES
+	comp.InsertLocal(
+		0,
+		ifaces.QueryIDf("%s_%s", name, "N_BYTES_CHAIN_ID_CONSTRAINT"),
+		sym.Sub(
+			fetcher.NBytesChainID,
+			2, // hardcode 2 bytes for the N_BYTES_CHAIN_ID
+		),
+	)
+
+	// require this to be a constant column
+	commonconstraints.MustBeConstant(comp, fetcher.NBytesChainID)
+}
+
+// AssignTimestampFetcher assigns the data in the TimestampFetcher using data fetched from the BlockDataCols
+func AssignChainIDFetcher(run *wizard.ProverRuntime, fetcher *ChainIDFetcher, bdc *arith.BlockDataCols) {
+	size := bdc.Ct.Size()
+	var (
+		chainID = make([]field.Element, size)
+	)
+	fetchedChainID := bdc.DataLo.GetColAssignmentAt(run, size+ChainIDOffset)
+	chainID[0].Set(&fetchedChainID)
+	run.AssignColumn(fetcher.ChainID.GetColID(), smartvectors.NewConstant(chainID[0], size))
+	run.AssignColumn(fetcher.NBytesChainID.GetColID(), smartvectors.NewConstant(field.NewElement(2), size))
+}
