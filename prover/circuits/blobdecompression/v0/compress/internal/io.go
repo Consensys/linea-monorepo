ac1e8853cce20c61086264170e8c566961c00e6f
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-11-25 12:05:13 +0000

    merge changes from feat/bls-glue

--- prover/circuits/blobdecompression/v0/compress/internal/io.go
+++ prover/circuits/blobdecompression/v0/compress/internal/io.go
@@ -35,7 +35,7 @@ func (r *RangeChecker) AssertLessThan(bound uint, c ...frontend.Variable) {
 	default:
 		cRangeTable, ok := r.tables[bound]
 		if !ok {
-			cRangeTable := logderivlookup.New(r.api)
+			cRangeTable = logderivlookup.New(r.api)
 			r.tables[bound] = cRangeTable
 			for i := uint(0); i < bound; i++ {
 				cRangeTable.Insert(0)
--- prover/zkevm/prover/bls/serialization_types.go
+++ prover/zkevm/prover/bls/serialization_types.go
@@ -2,8 +2,6 @@ package bls
 
 import (
 	"github.com/consensys/gnark/std/algebra/emulated/sw_bls12381"
-	"github.com/consensys/gnark/std/algebra/emulated/sw_emulated"
-	"github.com/consensys/gnark/std/math/emulated/emparams"
 )
 
 // Type aliases for serialization registration
@@ -11,12 +9,11 @@ import (
 
 type (
 	// G1 circuit types with sw_emulated.AffinePoint
-	MultiAddCircuitG1        = multiAddCircuit[g1ElementWizard, sw_emulated.AffinePoint[emparams.BLS12381Fp]]
-	MultiMulCircuitG1        = multiMulCircuit[g1ElementWizard, sw_emulated.AffinePoint[emparams.BLS12381Fp]]
-	MultiMapCircuitG1        = multiMapCircuit[g1ElementWizard, sw_emulated.AffinePoint[emparams.BLS12381Fp]]
-	MultiCheckableG1NonGroup = multiCheckableCircuit[nonGroupMembershipInstance[g1ElementWizard, sw_emulated.AffinePoint[emparams.BLS12381Fp]]]
-	MultiCheckableG1NonCurve = multiCheckableCircuit[nonCurveMembershipInstance[g1ElementWizard, sw_emulated.AffinePoint[emparams.BLS12381Fp]]]
-
+	MultiAddCircuitG1        = multiAddCircuit[g1ElementWizard, sw_bls12381.G1Affine]
+	MultiMulCircuitG1        = multiMulCircuit[g1ElementWizard, sw_bls12381.G1Affine]
+	MultiMapCircuitG1        = multiMapCircuit[g1ElementWizard, sw_bls12381.G1Affine]
+	MultiCheckableG1NonGroup = multiCheckableCircuit[nonGroupMembershipInstance[g1ElementWizard, sw_bls12381.G1Affine]]
+	MultiCheckableG1NonCurve = multiCheckableCircuit[nonCurveMembershipInstance[g1ElementWizard, sw_bls12381.G1Affine]]
 	// G2 circuit types
 	MultiAddCircuitG2        = multiAddCircuit[g2ElementWizard, sw_bls12381.G2Affine]
 	MultiMulCircuitG2        = multiMulCircuit[g2ElementWizard, sw_bls12381.G2Affine]
@@ -30,24 +27,3 @@ type (
 	MultiPointEvalCircuit          = multiPointEvalCircuit
 	MultiPointEvalFailureCircuit   = multiPointEvalFailureCircuit
 )
-
-// Constructor functions for serialization registration
-func NewMultiAddCircuitG1() *MultiAddCircuitG1 {
-	return &MultiAddCircuitG1{}
-}
-
-func NewMultiMulCircuitG1() *MultiMulCircuitG1 {
-	return &MultiMulCircuitG1{}
-}
-
-func NewMultiMulCircuitG2() *MultiMulCircuitG2 {
-	return &MultiMulCircuitG2{}
-}
-
-func NewMultiCheckableCircuitG1NonGroup() *MultiCheckableG1NonGroup {
-	return &MultiCheckableG1NonGroup{}
-}
-
-func NewMultiCheckableCircuitG2NonGroup() *MultiCheckableG2NonGroup {
-	return &MultiCheckableG2NonGroup{}
-}
