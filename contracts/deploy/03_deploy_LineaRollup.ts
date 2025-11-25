7cb978b7b925e87f22eaf22f77baa6f87d70f6a7
thedarkjester <grant.southey@consensys.net>
2025-11-25 21:58:44 +0000

    use versioned pause type roles

--- contracts/deploy/03_deploy_LineaRollup.ts
+++ contracts/deploy/03_deploy_LineaRollup.ts
@@ -9,9 +9,9 @@ import {
 } from "../common/helpers";
 import {
   LINEA_ROLLUP_INITIALIZE_SIGNATURE,
-  LINEA_ROLLUP_PAUSE_TYPES_ROLES,
-  LINEA_ROLLUP_ROLES,
-  LINEA_ROLLUP_UNPAUSE_TYPES_ROLES,
+  LINEA_V8_ROLLUP_PAUSE_TYPES_ROLES,
+  LINEA_V8_ROLLUP_UNPAUSE_TYPES_ROLES,
+  LINEA_V8_ROLLUP_ROLES,
   OPERATOR_ROLE,
 } from "../common/constants";
 
@@ -29,9 +29,9 @@ const func: DeployFunction = async function () {
   const lineaRollupGenesisTimestamp = getRequiredEnvVar("LINEA_ROLLUP_GENESIS_TIMESTAMP");
   const MultiCallAddress = "0xcA11bde05977b3631167028862bE2a173976CA11";
 
-  const pauseTypeRoles = getEnvVarOrDefault("LINEA_ROLLUP_PAUSE_TYPE_ROLES", LINEA_ROLLUP_PAUSE_TYPES_ROLES);
-  const unpauseTypeRoles = getEnvVarOrDefault("LINEA_ROLLUP_UNPAUSE_TYPE_ROLES", LINEA_ROLLUP_UNPAUSE_TYPES_ROLES);
-  const defaultRoleAddresses = generateRoleAssignments(LINEA_ROLLUP_ROLES, lineaRollupSecurityCouncil, [
+  const pauseTypeRoles = getEnvVarOrDefault("LINEA_ROLLUP_PAUSE_TYPE_ROLES", LINEA_V8_ROLLUP_PAUSE_TYPES_ROLES);
+  const unpauseTypeRoles = getEnvVarOrDefault("LINEA_ROLLUP_UNPAUSE_TYPE_ROLES", LINEA_V8_ROLLUP_UNPAUSE_TYPES_ROLES);
+  const defaultRoleAddresses = generateRoleAssignments(LINEA_V8_ROLLUP_ROLES, lineaRollupSecurityCouncil, [
     { role: OPERATOR_ROLE, addresses: lineaRollupOperators },
   ]);
   const roleAddresses = getEnvVarOrDefault("LINEA_ROLLUP_ROLE_ADDRESSES", defaultRoleAddresses);
