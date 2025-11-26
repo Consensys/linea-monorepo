df1d2fd05eb281c121398ac64d59cbb2d8c41884
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-11-26 14:57:21 +0000

    improv traces counters parser

--- coordinator/clients/traces-generator-api-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/TracesClientResponsesParser.kt
+++ coordinator/clients/traces-generator-api-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/TracesClientResponsesParser.kt
@@ -1,13 +1,12 @@
 package net.consensys.zkevm.coordinator.clients
 
 import com.github.michaelbull.result.getOrElse
-import com.github.michaelbull.result.getOrThrow
-import com.github.michaelbull.result.onFailure
 import com.github.michaelbull.result.runCatching
 import io.vertx.core.json.JsonObject
 import net.consensys.linea.errors.ErrorResponse
 import net.consensys.linea.jsonrpc.JsonRpcErrorResponse
 import net.consensys.linea.jsonrpc.JsonRpcSuccessResponse
+import net.consensys.linea.traces.TracesCounters
 import net.consensys.linea.traces.TracesCountersV2
 import net.consensys.linea.traces.TracesCountersV4
 import net.consensys.linea.traces.TracingModuleV2
@@ -30,61 +29,42 @@ object TracesClientResponsesParser {
 
   internal fun parseTracesCounterResponseV2(
     jsonRpcResponse: JsonRpcSuccessResponse,
-  ): GetTracesCountersResponse {
-    val result = jsonRpcResponse.result as JsonObject
-
-    return GetTracesCountersResponse(
-      result.getJsonObject("tracesCounters").let { parseTracesCountersV2(it) },
-      result.getString("tracesEngineVersion"),
-    )
-  }
-
-  internal fun parseTracesCountersV2(tracesCounters: JsonObject): TracesCountersV2 {
-    val expectedModules = TracingModuleV2.entries.map { it.name }.toSet()
-    val evmModulesInResponse = tracesCounters.map.keys.toSet()
-    val modulesMissing = expectedModules - evmModulesInResponse
-    val unExpectedModules = evmModulesInResponse - expectedModules
-    val error =
-      if (modulesMissing.isNotEmpty()) {
-        "Traces counters response is missing modules: ${modulesMissing.joinToString(",")}"
-      } else if (unExpectedModules.isNotEmpty()) {
-        "Traces counters has unsupported modules: ${unExpectedModules.joinToString(",")}"
-      } else {
-        null
-      }
-    if (error != null) {
-      log.error(error)
-      throw IllegalStateException(error)
-    }
-
-    val traces = TracingModuleV2.entries.associateWith { traceModule ->
-      val counterValue = tracesCounters.getString(traceModule.name)
-      runCatching { counterValue.toUInt() }
-        .onFailure {
-          log.error(
-            "Failed to parse Evm module ${traceModule.name}='$counterValue' to UInt. errorMessage={}",
-            it.message,
-            it,
-          )
-        }
-        .getOrThrow()
-    }
-    return TracesCountersV2(traces)
-  }
+  ): GetTracesCountersResponse = parseTracesCounterResponse(
+    jsonRpcResponse,
+    ::parseTracesCountersV4,
+  )
 
   internal fun parseTracesCounterResponseV4(
     jsonRpcResponse: JsonRpcSuccessResponse,
+  ): GetTracesCountersResponse = parseTracesCounterResponse(
+    jsonRpcResponse,
+    ::parseTracesCountersV4,
+  )
+
+  internal fun parseTracesCounterResponse(
+    jsonRpcResponse: JsonRpcSuccessResponse,
+    parserFn: (JsonObject) -> TracesCounters,
   ): GetTracesCountersResponse {
     val result = jsonRpcResponse.result as JsonObject
 
     return GetTracesCountersResponse(
-      result.getJsonObject("tracesCounters").let { parseTracesCountersV4(it) },
+      result.getJsonObject("tracesCounters").let(parserFn),
       result.getString("tracesEngineVersion"),
     )
   }
 
-  internal fun parseTracesCountersV4(tracesCounters: JsonObject): TracesCountersV4 {
-    val expectedModules = TracingModuleV4.entries.map { it.name }.toSet()
+  internal fun parseTracesCountersV2(tracesCounters: JsonObject): TracesCountersV2 =
+    parseTracesCounters(tracesCounters, TracingModuleV2::class.java, ::TracesCountersV2) as TracesCountersV2
+
+  internal fun parseTracesCountersV4(tracesCounters: JsonObject): TracesCountersV4 =
+    parseTracesCounters(tracesCounters, TracingModuleV4::class.java, ::TracesCountersV4) as TracesCountersV4
+
+  private fun <T : Enum<T>> parseTracesCounters(
+    tracesCounters: JsonObject,
+    moduleEnum: Class<T>,
+    constructor: (Map<T, UInt>) -> Any,
+  ): Any {
+    val expectedModules = moduleEnum.enumConstants.map { it.name }.toSet()
     val evmModulesInResponse = tracesCounters.map.keys.toSet()
     val modulesMissing = expectedModules - evmModulesInResponse
     val unExpectedModules = evmModulesInResponse - expectedModules
@@ -101,9 +81,9 @@ object TracesClientResponsesParser {
       throw IllegalStateException(error)
     }
 
-    val traces = TracingModuleV4.entries.associateWith { traceModule ->
+    val traces = moduleEnum.enumConstants.associateWith { traceModule ->
       val counterValue = tracesCounters.getString(traceModule.name)
-      runCatching { counterValue.toUInt() }
+      kotlin.runCatching { counterValue.toUInt() }
         .onFailure {
           log.error(
             "Failed to parse Evm module ${traceModule.name}='$counterValue' to UInt. errorMessage={}",
@@ -113,7 +93,7 @@ object TracesClientResponsesParser {
         }
         .getOrThrow()
     }
-    return TracesCountersV4(traces)
+    return constructor(traces)
   }
 
   internal fun parseConflatedTracesToFileResponse(
