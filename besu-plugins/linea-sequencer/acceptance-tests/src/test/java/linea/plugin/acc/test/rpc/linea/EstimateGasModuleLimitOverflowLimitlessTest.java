ce592ec59b1e4c7c79d6bb1b93f07c25e92f4692
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2025-11-27 23:40:18 +1100

    removed @Disabled for EstimateGasModuleLimitOverflowLimitlessTest

--- besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/rpc/linea/EstimateGasModuleLimitOverflowLimitlessTest.java
+++ besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/rpc/linea/EstimateGasModuleLimitOverflowLimitlessTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 import linea.plugin.acc.test.LineaPluginPoSTestBase;
 import linea.plugin.acc.test.TestCommandLineOptionsBuilder;
 import org.apache.tuweni.bytes.Bytes;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.web3j.tx.gas.DefaultGasProvider;
 
@@ -31,7 +30,6 @@ public class EstimateGasModuleLimitOverflowLimitlessTest extends LineaPluginPoST
         .build();
   }
 
-  @Disabled("Can't check over MODEXP to exclude the transaction, tmp disabled @Fabio")
   @Test
   public void estimateGasFailsForExceedingModuleLineCountTest() throws Exception {
     final var bls12MapFPtoG1 = deployBLS12_MAP_FP_TO_G1();
