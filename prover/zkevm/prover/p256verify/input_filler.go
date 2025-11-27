20af8d8141bc337d7a61722c21954b4526fff1b5
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-11-27 21:29:20 +0000

    fix: correct order of point coordinates in inputfiller

--- prover/zkevm/prover/p256verify/input_filler.go
+++ prover/zkevm/prover/p256verify/input_filler.go
@@ -22,11 +22,11 @@ func inputFiller(circuitInstance, inputIndex int) field.Element {
 		// s
 		"0", "1",
 		// qx
-		"0x77037d812deb33a0f4a13945d898c296",
 		"0x6b17d1f2e12c4247f8bce6e563a440f2",
+		"0x77037d812deb33a0f4a13945d898c296",
 		// qy
-		"0x2bce33576b315ececbb6406837bf51f5",
 		"0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e16",
+		"0x2bce33576b315ececbb6406837bf51f5",
 		// expected result
 		"0", "0",
 	}
