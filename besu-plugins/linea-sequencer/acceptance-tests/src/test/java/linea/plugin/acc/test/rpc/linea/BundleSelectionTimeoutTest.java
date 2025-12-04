5e9d66fba61dfb647f7b51594e4ff2197fa31019
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-12-04 15:37:15 +0000

    enable eth_sendBundle tests

--- besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/rpc/linea/BundleSelectionTimeoutTest.java
+++ besu-plugins/linea-sequencer/acceptance-tests/src/test/java/linea/plugin/acc/test/rpc/linea/BundleSelectionTimeoutTest.java
@@ -13,12 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.stream.IntStream;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
-@Disabled(
-    "Temporarily disabled while investigating bunlde timeout logic and is disabled in production")
 public class BundleSelectionTimeoutTest extends AbstractSendBundleTest {
 
   @Test
