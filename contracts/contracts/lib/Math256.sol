34153687160c8fd19379143e9e85f6667b9dbdd7
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2025-12-09 13:59:42 +1100

    add more natspec to Math256 and YieldProviderBase

--- contracts/contracts/lib/Math256.sol
+++ contracts/contracts/lib/Math256.sol
@@ -7,6 +7,12 @@
 // solhint-disable-next-line
 pragma solidity >=0.4.24 <0.9.0;
 
+/**
+ * @title Library for 256-bit arithmetic operations.
+ * @author Consensys Software Inc.
+ * @notice Provides utility functions for safe arithmetic operations on uint256 values.
+ * @custom:security-contact security-report@linea.build
+ */
 library Math256 {
   /// @dev Returns the smallest of two numbers.
   function min(uint256 a, uint256 b) internal pure returns (uint256) {
