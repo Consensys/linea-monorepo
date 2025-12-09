85b3466868425ba4db18f06bb9d9f0167dc12b68
AlexandreBelling <alexandrebelling8@gmail.com>
2025-12-10 02:14:59 +0800

    feat: adds kv.Store to the mock builder

--- prover/utils/gnarkutil/mock_builder.go
+++ prover/utils/gnarkutil/mock_builder.go
@@ -12,8 +12,14 @@ var (
 	// frontend.RangeChecker.
 	_ frontend.WideCommitter           = &MockBuilder{}
 	_ frontend.Rangechecker            = &MockBuilder{}
+	_ kVStore                          = &MockBuilder{}
 	_ frontend.Builder[constraint.U32] = &MockBuilder{}
 )
+
+type kVStore interface {
+	SetKeyValue(key, value any)
+	GetKeyValue(key any) (value any)
+}
 
 // MockBuilder is a mock builder for testing purposes. It implements
 // [frontend.WideCommitter] and [frontend.RangeChecker]. It works purely for
@@ -21,17 +27,17 @@ var (
 type MockBuilder struct {
 	// Builder is the underlying frontend builder
 	frontend.Builder[constraint.U32]
+	Store map[any]any
 }
 
 // NewMockBuilder returns a new MockBuilder.
 func NewMockBuilder(wrapped frontend.NewBuilderU32) frontend.NewBuilderU32 {
-
 	return func(field *big.Int, config frontend.CompileConfig) (frontend.Builder[constraint.U32], error) {
 		builder, err := wrapped(field, config)
 		if err != nil {
 			return nil, err
 		}
-		return &MockBuilder{Builder: builder}, nil
+		return &MockBuilder{Builder: builder, Store: map[any]any{}}, nil
 	}
 }
 
@@ -50,3 +56,11 @@ func (*MockBuilder) WideCommit(width int, toCommit ...frontend.Variable) (commit
 
 // Check implements the range-checker and does not do anything.
 func (*MockBuilder) Check(v frontend.Variable, bits int) {}
+
+func (m *MockBuilder) SetKeyValue(key, value any) {
+	m.Store[key] = value
+}
+
+func (m *MockBuilder) GetKeyValue(key any) (value any) {
+	return m.Store[key]
+}
