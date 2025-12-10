c826f05bce1aa0ac58b2a83ed10d19bcbea95f64
srinathln7 <srinath.nandakumar@consensys.net>
2025-12-10 14:05:10 +0000

    add some cols for benchmarking module witness GL

--- prover/protocol/serialization/benchmark_test.go
+++ prover/protocol/serialization/benchmark_test.go
@@ -74,7 +74,6 @@ func BenchmarkDeserZkEVM(b *testing.B) {
 func makeTestModuleWitnessGL() *distributed.ModuleWitnessGL {
 	cols := make(map[ifaces.ColID]smartvectors.SmartVector)
 
-	// simulate a few real columns
 	addColumn := func(name string, size int) {
 		r := make([]field.Element, size)
 		for i := range r {
@@ -84,6 +83,7 @@ func makeTestModuleWitnessGL() *distributed.ModuleWitnessGL {
 		cols[ifaces.ColID(name)] = &reg
 	}
 
+	// Base columns from previous example
 	addColumn("CYCLIC_COUNTER_3079_64_0_COUNTER", 64)
 	addColumn("ECDSA_ANTICHAMBER_ID", 64)
 	addColumn("ECDSA_ANTICHAMBER_IS_ACTIVE", 64)
@@ -91,13 +91,48 @@ func makeTestModuleWitnessGL() *distributed.ModuleWitnessGL {
 	addColumn("ECDSA_ANTICHAMBER_TXSIGNATURE_TX_HASH_HI", 64)
 	addColumn("ECDSA_ANTICHAMBER_TXSIGNATURE_TX_HASH_LO", 64)
 
-	// ReceivedValuesGlobal ~13 elements like real one
+	// Columns you provided
+	names := []string{
+		"ECDSA_ANTICHAMBER_ADDRESSES_ADDRESS_LO",
+		"ECDSA_ANTICHAMBER_ADDRESSES_ADRESSHI_UNTRIMMED",
+		"ECDSA_ANTICHAMBER_ADDRESSES_HASH_NUM",
+		"ECDSA_ANTICHAMBER_ADDRESSES_ISADRESS_FROM_ECREC",
+		"ECDSA_ANTICHAMBER_ADDRESSES_ISADRESS_FROM_TXNDATA",
+		"ECDSA_ANTICHAMBER_ADDRESSES_IS_ADDRESS",
+		"ECDSA_ANTICHAMBER_ECRECOVER_AUX_PROJECTION_MASK",
+		"ECDSA_ANTICHAMBER_ECRECOVER_ECRECOVER_ID",
+		"ECDSA_ANTICHAMBER_ECRECOVER_ECRECOVER_INDEX",
+		"ECDSA_ANTICHAMBER_ECRECOVER_ECRECOVER_IS_DATA",
+		"ECDSA_ANTICHAMBER_ECRECOVER_ECRECOVER_IS_RES",
+		"ECDSA_ANTICHAMBER_ECRECOVER_LIMB",
+		"ECDSA_ANTICHAMBER_ECRECOVER_SUCCESS_BIT",
+		"ECDSA_ANTICHAMBER_GNARK_DATA_IS_ACTIVE",
+		"ECDSA_ANTICHAMBER_GNARK_DATA_PI",
+		"ECDSA_ANTICHAMBER_ID",
+		"ECDSA_ANTICHAMBER_IS_ACTIVE",
+		"ECDSA_ANTICHAMBER_IS_FETCHING",
+		"ECDSA_ANTICHAMBER_IS_PUSHING",
+		"ECDSA_ANTICHAMBER_SOURCE",
+		"ECDSA_ANTICHAMBER_TXSIGNATURE_TX_HASH_HI",
+		"ECDSA_ANTICHAMBER_TXSIGNATURE_TX_HASH_LO",
+		"ECDSA_ANTICHAMBER_TXSIGNATURE_TX_IS_HASH_HI",
+		"ECDSA_ANTICHAMBER_UNALIGNED_GNARK_DATA_GNARK_DATA",
+		"ECDSA_ANTICHAMBER_UNALIGNED_GNARK_DATA_GNARK_INDEX",
+		"ECDSA_ANTICHAMBER_UNALIGNED_GNARK_DATA_GNARK_PUBLIC_KEY_INDEX",
+	}
+
+	// Use realistic-ish widths
+	for _, n := range names {
+		addColumn(n, 64)
+	}
+
+	// ReceivedValuesGlobal
 	globals := make([]field.Element, 13)
 	for i := range globals {
 		globals[i] = field.NewElement(0)
 	}
 
-	// VkMerkleRoot fake limbs
+	// VkMerkleRoot
 	var vk field.Element
 	vk.SetUint64(123456789)
 
