b951710b642bc481878c6e1777da8f5d23b16843
Ivo Kubjas <ivo.kubjas@consensys.net>
2025-12-10 00:32:15 +0000

    test: cleanup tests

--- prover/protocol/dedicated/emulated/emulated_test.go
+++ prover/protocol/dedicated/emulated/emulated_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/consensys/linea-monorepo/prover/protocol/ifaces"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
 	"github.com/consensys/linea-monorepo/prover/symbolic"
-	"github.com/consensys/linea-monorepo/prover/utils/csvtraces"
 	"github.com/stretchr/testify/require"
 )
 
@@ -68,6 +67,71 @@ func TestEmulatedMultiplication(t *testing.T) {
 		assignEmulated(run, "B", assignmentB, nbBitsPerLimb, nbLimbs)
 		assignEmulated(run, "P", assignmentP, nbBitsPerLimb, nbLimbs)
 		assignEmulated(run, "EXPECTED", assignmentExpected, nbBitsPerLimb, nbLimbs)
+	}
+
+	comp := wizard.Compile(define, dummy.Compile)
+	proof := wizard.Prove(comp, prover)
+	err = wizard.Verify(comp, proof)
+	require.NoError(t, err)
+}
+
+func TestEmulatedEvaluation(t *testing.T) {
+	const nbEntries = 1 << 6
+	const nbBits = 384
+	const round_nr = 0
+	const nbBitsPerLimb = 128
+	const nbLimbs = (nbBits + nbBitsPerLimb - 1) / nbBitsPerLimb
+	var pa *EmulatedEvaluationModule
+	define := func(b *wizard.Builder) {
+		P := registerEmulated(b.CompiledIOP, round_nr, "P", nbLimbs, nbEntries)
+		T0 := registerEmulated(b.CompiledIOP, round_nr, "T0", nbLimbs, nbEntries)
+		T1 := registerEmulated(b.CompiledIOP, round_nr, "T1", nbLimbs, nbEntries)
+		T2 := registerEmulated(b.CompiledIOP, round_nr, "T2", nbLimbs, nbEntries)
+		T3 := registerEmulated(b.CompiledIOP, round_nr, "T3", nbLimbs, nbEntries)
+		pa = EmulatedEvaluation(b.CompiledIOP, "TEST", nbBitsPerLimb, P, [][]Limbs{
+			{T0, T1}, {T0, T1, T2}, {T3}, // T0*T1 + T0*T1*T2 + T3 == 0
+		})
+	}
+	_ = pa
+
+	assignmentT0 := make([]*big.Int, nbEntries)
+	assignmentT1 := make([]*big.Int, nbEntries)
+	assignmentT2 := make([]*big.Int, nbEntries)
+	assignmentT3 := make([]*big.Int, nbEntries)
+	assignmentP := make([]*big.Int, nbEntries)
+	bound := new(big.Int).Lsh(big.NewInt(1), nbBits)
+	var err error
+	reader := sha3.NewSHAKE256()
+	tmp := new(big.Int)
+	for i := range nbEntries {
+		assignmentP[i], err = rand.Int(reader, bound)
+		require.NoError(t, err)
+		assignmentT0[i], err = rand.Int(reader, assignmentP[i])
+		require.NoError(t, err)
+		assignmentT1[i], err = rand.Int(reader, assignmentP[i])
+		require.NoError(t, err)
+		assignmentT2[i], err = rand.Int(reader, assignmentP[i])
+		require.NoError(t, err)
+		assignmentT3[i], err = rand.Int(reader, assignmentP[i])
+		require.NoError(t, err)
+		// set T3 = - (T0*T1 + T0*T1*T2) mod P
+		tmp.Mul(assignmentT0[i], assignmentT1[i])
+		tmp.Mod(tmp, assignmentP[i])
+		assignmentT3[i] = new(big.Int).Set(tmp)
+		tmp.Mul(tmp, assignmentT2[i])
+		tmp.Mod(tmp, assignmentP[i])
+		assignmentT3[i].Add(assignmentT3[i], tmp)
+		assignmentT3[i].Mod(assignmentT3[i], assignmentP[i])
+		assignmentT3[i].Sub(assignmentP[i], assignmentT3[i])
+		assignmentT3[i].Mod(assignmentT3[i], assignmentP[i])
+	}
+
+	prover := func(run *wizard.ProverRuntime) {
+		assignEmulated(run, "P", assignmentP, nbBitsPerLimb, nbLimbs)
+		assignEmulated(run, "T0", assignmentT0, nbBitsPerLimb, nbLimbs)
+		assignEmulated(run, "T1", assignmentT1, nbBitsPerLimb, nbLimbs)
+		assignEmulated(run, "T2", assignmentT2, nbBitsPerLimb, nbLimbs)
+		assignEmulated(run, "T3", assignmentT3, nbBitsPerLimb, nbLimbs)
 	}
 
 	comp := wizard.Compile(define, dummy.Compile)
