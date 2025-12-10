91190d58a0208fb1d214cdf8365a9503bb64bab9
Thomas Piellard <thomas.piellard@consensys.net>
2025-12-10 16:02:15 +0100

    feat: add test for computeLagrangeBasisAtX

--- prover/maths/common/smartvectors_mixed/polynomial_test.go
+++ prover/maths/common/smartvectors_mixed/polynomial_test.go
@@ -5,11 +5,47 @@ import (
 
 	"github.com/consensys/gnark-crypto/field/koalabear/fft"
 	"github.com/consensys/gnark-crypto/field/koalabear/vortex"
+	"github.com/consensys/gnark-crypto/utils"
 	"github.com/consensys/linea-monorepo/prover/maths/common/smartvectors"
 	"github.com/consensys/linea-monorepo/prover/maths/field"
 	"github.com/consensys/linea-monorepo/prover/maths/field/fext"
 	"github.com/stretchr/testify/require"
 )
+
+func eval(p []field.Element, x fext.Element) fext.Element {
+	var res fext.Element
+	for i := len(p) - 1; i >= 0; i-- {
+		res.Mul(&res, &x)
+		res.B0.A0.Add(&res.B0.A0, &p[i])
+	}
+	return res
+}
+
+func TestComputeLagrangeBasisAtX(t *testing.T) {
+
+	n := 64
+	domain := fft.NewDomain(uint64(n))
+
+	var x fext.Element
+	x.SetRandom()
+	r := computeLagrangeBasisAtX(n, x)
+
+	_r := make([]fext.Element, n)
+	for i := 0; i < n; i++ {
+		m := make([]field.Element, n)
+		m[i].SetOne()
+		domain.FFTInverse(m, fft.DIF)
+		utils.BitReverse(m)
+		_r[i] = eval(m, x)
+	}
+
+	for i := 0; i < n; i++ {
+		if !_r[i].Equal(&r[i]) {
+			t.Fatal("error computeLagrangeBasisAtX")
+		}
+	}
+
+}
 
 func TestBatchEvaluateLagrange(t *testing.T) {
 
