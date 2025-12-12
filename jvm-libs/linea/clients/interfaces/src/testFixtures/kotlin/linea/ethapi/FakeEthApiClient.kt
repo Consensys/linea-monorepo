cfd73e3c4c49a1deea6ae2bcb9de22b71330e98e
Gaurav Ahuja <gauravahuja9@gmail.com>
2025-12-12 09:50:23 -0500

    Add blob fee method to eth api fee client

--- jvm-libs/linea/clients/interfaces/src/testFixtures/kotlin/linea/ethapi/FakeEthApiClient.kt
+++ jvm-libs/linea/clients/interfaces/src/testFixtures/kotlin/linea/ethapi/FakeEthApiClient.kt
@@ -161,6 +161,14 @@ class FakeEthApiClient(
     TODO("Not yet implemented")
   }
 
+  override fun ethFeeHistoryBlobExtended(
+    blockCount: Int,
+    newestBlock: BlockParameter,
+    rewardPercentiles: List<Double>,
+  ): SafeFuture<FeeHistory> {
+    TODO("Not yet implemented")
+  }
+
   override fun ethGetBalance(
     address: ByteArray,
     blockParameter: BlockParameter,
