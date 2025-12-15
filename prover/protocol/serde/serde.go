bc3075f97bef023dbc820a2e542761f79abd1b97
srinathln7 <srinath.nandakumar@consensys.net>
2025-12-15 17:20:24 +0000

    add compartor for serde pkg

--- prover/protocol/serde/serde.go
+++ prover/protocol/serde/serde.go
@@ -4,6 +4,25 @@ package serde
 import (
 	"reflect"
 	"unsafe"
+
+	"github.com/consensys/linea-monorepo/prover/protocol/column"
+)
+
+var (
+	SerdeStructTag = "serde"
+
+	// Do not serialize fields with this tag
+	SerdeStructTagOmit = "omit"
+
+	// Serialize this but don't include it in test comparisions to prevent OOM
+	SerdeStructTagTestOmit = "test_omit"
+)
+
+// Global type constants for reflection-based type checking.
+// These define the reflect.Type of key protocol-specific types, used to identify
+// special types during serialization and deserialization.
+var (
+	TypeOfColumnNatural = reflect.TypeOf(column.Natural{})
 )
 
 func Serialize(v any) ([]byte, error) {
