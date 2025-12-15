7d8782ec680ee182656acea54218a3e1916d0456
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2025-12-15 09:36:42 +0000

    Revert "Feat/linea tracer migration as subfolder (#1852)"
    This reverts commit 642f6361e6fbc7d3a2d5f08629cca99a9c860afc.

--- tracer/arithmetization/src/test/resources/contracts/multipleCallsReverting/FundsReceiver2.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.4.24;
-
-contract FundsReceiver2 {
-    function() external payable {}
-}
--- tracer/shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.jdk17 pkgs.gradle_7 pkgs.solc pkgs.pre-commit
-  ];
-}
