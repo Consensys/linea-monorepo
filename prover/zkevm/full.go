cbdcbf45cccdf82c05305b1796d86ecd14eef847
AlexandreBelling <alexandrebelling8@gmail.com>
2025-12-17 14:19:11 +0800

    comment out the limitless prover functions with panics

--- prover/zkevm/full.go
+++ prover/zkevm/full.go
@@ -9,8 +9,8 @@ import (
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/cleanup"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/dummy"
-	"github.com/consensys/linea-monorepo/prover/protocol/compiler/mimc"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/plonkinwizard"
+	"github.com/consensys/linea-monorepo/prover/protocol/compiler/poseidon2"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/selfrecursion"
 	"github.com/consensys/linea-monorepo/prover/protocol/compiler/vortex"
 	"github.com/consensys/linea-monorepo/prover/protocol/wizard"
@@ -19,7 +19,7 @@ import (
 	"github.com/consensys/linea-monorepo/prover/zkevm/prover/ecarith"
 	"github.com/consensys/linea-monorepo/prover/zkevm/prover/ecdsa"
 	"github.com/consensys/linea-monorepo/prover/zkevm/prover/ecpair"
-	"github.com/consensys/linea-monorepo/prover/zkevm/prover/hash/keccak"
+	keccak "github.com/consensys/linea-monorepo/prover/zkevm/prover/hash/keccak/glue"
 	"github.com/consensys/linea-monorepo/prover/zkevm/prover/hash/sha2"
 	"github.com/consensys/linea-monorepo/prover/zkevm/prover/modexp"
 	"github.com/consensys/linea-monorepo/prover/zkevm/prover/statemanager"
@@ -61,11 +61,11 @@ var (
 	// This is the compilation suite in use for the full prover
 	fullCompilationSuite = CompilationSuite{
 		// logdata.Log("initial-wizard"),
-		mimc.CompileMiMC,
+		poseidon2.CompilePoseidon2,
 		plonkinwizard.Compile,
 		compiler.Arcane(compiler.WithTargetColSize(1 << 19)),
 		vortex.Compile(
-			2,
+			2, false,
 			vortex.ForceNumOpenedColumns(256),
 			vortex.WithSISParams(&sisInstance),
 		),
@@ -75,10 +75,10 @@ var (
 		selfrecursion.SelfRecurse,
 		// logdata.Log("post-selfrecursion-1"),
 		cleanup.CleanUp,
-		mimc.CompileMiMC,
+		poseidon2.CompilePoseidon2,
 		compiler.Arcane(compiler.WithTargetColSize(1 << 18)),
 		vortex.Compile(
-			2,
+			2, false,
 			vortex.ForceNumOpenedColumns(256),
 			vortex.WithSISParams(&sisInstance),
 		),
@@ -88,10 +88,10 @@ var (
 		selfrecursion.SelfRecurse,
 		// logdata.Log("post-selfrecursion-2"),
 		cleanup.CleanUp,
-		mimc.CompileMiMC,
+		poseidon2.CompilePoseidon2,
 		compiler.Arcane(compiler.WithTargetColSize(1 << 16)),
 		vortex.Compile(
-			8,
+			8, false,
 			vortex.ForceNumOpenedColumns(64),
 			vortex.WithSISParams(&sisInstance),
 		),
@@ -101,10 +101,10 @@ var (
 		selfrecursion.SelfRecurse,
 		// logdata.Log("post-selfrecursion-3"),
 		cleanup.CleanUp,
-		mimc.CompileMiMC,
+		poseidon2.CompilePoseidon2,
 		compiler.Arcane(compiler.WithTargetColSize(1 << 13)),
 		vortex.Compile(
-			8,
+			8, false,
 			vortex.ForceNumOpenedColumns(64),
 			vortex.WithOptionalSISHashingThreshold(1<<20),
 		),
