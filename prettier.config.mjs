e97ca1d679de886abbd81a340485623638b46b9c
VGau <victorien.gauch@consensys.net>
2026-01-19 16:25:04 +0100

    fix: refactor prettier solidity config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -6,18 +6,4 @@ export default {
   singleQuote: false,
   printWidth: 120,
   bracketSpacing: true,
-  plugins: ["prettier-plugin-solidity"],
-  overrides: [
-    {
-      files: "*.sol",
-      options: {
-        parser: "solidity-parse",
-        printWidth: 120,
-        tabWidth: 2,
-        useTabs: false,
-        singleQuote: false,
-        bracketSpacing: true,
-      },
-    },
-  ],
 };
