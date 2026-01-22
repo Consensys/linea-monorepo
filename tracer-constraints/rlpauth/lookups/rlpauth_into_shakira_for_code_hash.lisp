429d40aa24d914b66b957bd5817034152825caad
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2026-01-22 01:36:20 +0100

    added lookup for code hash

--- tracer-constraints/rlpauth/lookups/rlpauth_into_shakira_for_code_hash.lisp
+++ tracer-constraints/rlpauth/lookups/rlpauth_into_shakira_for_code_hash.lisp
@@ -1,7 +1,7 @@
-(defun (rlp-auth-into-shakiradata-activation-flag) 1)
+(defun (rlp-auth-into-shakiradata-for-code-hash-activation-flag) 1)
 
 (defclookup
-    (rlp-auth-into-shakiradata :unchecked)
+    (rlp-auth-into-shakiradata-for-code-hash :unchecked)
     ;; target selector
     (* shakiradata.IS_KECCAK_DATA (~ (- shakiradata.ID (prev shakiradata.ID))))
     ;; target columns
@@ -10,11 +10,11 @@
         shakiradata.TOTAL_SIZE
     )
     ;; source selector
-    (rlp-auth-into-shakiradata-activation-flag)
+    (rlp-auth-into-shakiradata-for-code-hash-activation-flag)
     ;; source columns
     (
-        keccak_for_ecrecover.limb
-        keccak_for_ecrecover.total_size
+        keccak_for_code_hash.limb
+        keccak_for_code_hash.total_size
     ))
 
 ;; TODO: define source selector
