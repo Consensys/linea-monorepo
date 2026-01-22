26f68ca401c010913f392296487b802d98b1cba8
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2026-01-22 13:46:21 +0000

    coordinator: adjust ForcedTransactionsClient

--- jvm-libs/linea/clients/interfaces/src/main/kotlin/linea/forcedtx/ForcedTransactionsClient.kt
+++ jvm-libs/linea/clients/interfaces/src/main/kotlin/linea/forcedtx/ForcedTransactionsClient.kt
@@ -14,9 +14,10 @@ enum class ForcedTransactionInclusionResult {
 }
 
 data class ForcedTransactionInclusionStatus(
+  val ftxNumber: ULong,
   val blockNumber: ULong,
   val inclusionResult: ForcedTransactionInclusionResult,
-  val transactionHash: ByteArray,
+  val ftxHash: ByteArray,
   val from: ByteArray,
 ) {
   override fun equals(other: Any?): Boolean {
@@ -25,40 +26,87 @@ data class ForcedTransactionInclusionStatus(
 
     other as ForcedTransactionInclusionStatus
 
-    if (!transactionHash.contentEquals(other.transactionHash)) return false
+    if (ftxNumber != other.ftxNumber) return false
     if (blockNumber != other.blockNumber) return false
-    if (!from.contentEquals(other.from)) return false
     if (inclusionResult != other.inclusionResult) return false
+    if (!ftxHash.contentEquals(other.ftxHash)) return false
+    if (!from.contentEquals(other.from)) return false
 
     return true
   }
 
   override fun hashCode(): Int {
-    var result = transactionHash.contentHashCode()
-    result = 31 * result + blockNumber.hashCode()
-    result = 31 * result + from.contentHashCode()
+    var result = blockNumber.hashCode()
     result = 31 * result + inclusionResult.hashCode()
+    result = 31 * result + ftxHash.contentHashCode()
+    result = 31 * result + ftxNumber.hashCode()
+    result = 31 * result + from.contentHashCode()
     return result
   }
 
   override fun toString(): String {
-    return "ForcedTransactionsReceipt(blockNumber=$blockNumber, inclusionResult=$inclusionResult, " +
-      "transactionHash=${transactionHash.encodeHex()}, from=${from.encodeHex()})"
+    return "ForcedTransactionInclusionStatus(ftxNumber=$ftxNumber, blockNumber=$blockNumber, " +
+      "inclusionResult=$inclusionResult, ftxHash=${ftxHash.encodeHex()}, from=${from.encodeHex()})"
   }
 }
 
+data class ForcedTransactionRequest(
+  val ftxNumber: ULong,
+  val deadlineBlockNumber: ULong,
+  val ftxRlp: ByteArray,
+) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as ForcedTransactionRequest
+
+    if (ftxNumber != other.ftxNumber) return false
+    if (!ftxRlp.contentEquals(other.ftxRlp)) return false
+    if (deadlineBlockNumber != other.deadlineBlockNumber) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = ftxNumber.hashCode()
+    result = 31 * result + ftxRlp.contentHashCode()
+    result = 31 * result + deadlineBlockNumber.hashCode()
+    return result
+  }
+
+  override fun toString(): String {
+    return "ForcedTransactionRequest(" +
+      "ftxNumber=$ftxNumber, " +
+      "deadlineBlockNumber=$deadlineBlockNumber, " +
+      "ftxRlp=${ftxRlp.encodeHex()})"
+  }
+}
+
+data class ForcedTransactionResponse(
+  val ftxNumber: ULong,
+  val ftxHash: ByteArray?,
+  val ftxError: String?,
+)
+
 interface ForcedTransactionsClient {
   /**
-   * Sends a list of RLP encoded signed transactions, just like eth_sendRawTransaction
-   * Transactions order must follow L1 submission, forcedTransactionIndex.
-   * Sequencer must evaluate them by this order
+   * Sends a list of ForcedTransactionRequest that contains:
+   *  ftxNumber -  L1 submission order
+   *  deadlineBlockNumber - the l2 deadline block number for inclusion
+   *  ftxRlp - RLP encoded signed transactions, just like eth_sendRawTransaction.
    *
-   * @return list of transaction hashes
+   * Sequencer must evaluate transactions by their ftxNumber
+   *
+   * @param transactions List of forced transaction requests containing RLP encoded transactions
+   * @return SafeFuture containing list of responses with transaction hashes or errors
    */
-  fun lineaSendForcedRawTransaction(transactions: List<ByteArray>): SafeFuture<List<ByteArray>>
-  fun lineaFindForcedTransactionStatus(transactionHash: ByteArray): SafeFuture<ForcedTransactionInclusionStatus?>
-  fun lineaGetForcedTransactionStatus(transactionHash: ByteArray): SafeFuture<ForcedTransactionInclusionStatus> =
-    lineaFindForcedTransactionStatus(transactionHash).thenApply {
-      it ?: throw IllegalStateException("Forced transaction not found: txHash=${transactionHash.encodeHex()}")
+  fun lineaSendForcedRawTransaction(
+    transactions: List<ForcedTransactionRequest>,
+  ): SafeFuture<List<ForcedTransactionResponse>>
+  fun lineaFindForcedTransactionStatus(ftxNumber: ULong): SafeFuture<ForcedTransactionInclusionStatus?>
+  fun lineaGetForcedTransactionStatus(ftxNumber: ULong): SafeFuture<ForcedTransactionInclusionStatus> =
+    lineaFindForcedTransactionStatus(ftxNumber).thenApply {
+      it ?: throw IllegalStateException("Forced transaction not found: ftxNumber=$ftxNumber")
     }
 }
