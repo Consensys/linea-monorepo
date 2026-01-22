6ababead2c4e9fd40d8d252c5b406a0c7355f7e4
srinathln7 <srinath.nandakumar@consensys.net>
2026-01-22 12:45:15 +0000

    rm prev. dangling inprogress files under same ctrler local-id

--- prover/config/constants.go
+++ prover/config/constants.go
@@ -11,9 +11,9 @@ const (
 	RequestsToSubDir   = "responses"
 	RequestsDoneSubDir = "requests-done"
 
-	InProgressSufix = "inprogress"
-	FailSuffix      = "code"
-	SuccessSuffix   = "success"
+	InProgressSuffix = "inprogress"
+	FailSuffix       = "code"
+	SuccessSuffix    = "success"
 
 	// Extension to add in order to defer the job to the large prover
 	LargeSuffix = "large"
