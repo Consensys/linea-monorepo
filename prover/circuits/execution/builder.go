0077ba4d82856582585a2d1d4d616e40e064f432
AlexandreBelling <alexandrebelling8@gmail.com>
2026-01-26 22:08:53 +0800

    activate profiling for the execution circuit

--- prover/circuits/execution/builder.go
+++ prover/circuits/execution/builder.go
@@ -5,6 +5,7 @@ import (
 	"github.com/consensys/gnark/constraint"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/profile"
 	"github.com/consensys/linea-monorepo/prover/zkevm"
 )
 
@@ -22,6 +23,9 @@ func (b *builder) Compile() (constraint.ConstraintSystem, error) {
 
 // Makes the constraint system for the execution circuit
 func makeCS(z *zkevm.ZkEvm) constraint.ConstraintSystem {
+
+	p := profile.Start(profile.WithPath("execution-circuit.pprof"))
+	defer p.Stop()
 
 	circuit := Allocate(z)
 	scs, err := frontend.Compile(fr.Modulus(), scs.NewBuilder, &circuit, frontend.WithCapacity(1<<24))
