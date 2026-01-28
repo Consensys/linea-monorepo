b1644d91673d9c5492b449e9686e366fbb541b70
amkCha <29160563+amkCha@users.noreply.github.com>
2026-01-28 10:38:26 +0100

    remove debug constraints

--- tracer-constraints/blake2fmodexpdata/lookups/blakemodexp_into_wcp.lisp
+++ tracer-constraints/blake2fmodexpdata/lookups/blakemodexp_into_wcp.lisp
@@ -2,22 +2,6 @@
   (force-bin (* (~ blake2fmodexpdata.STAMP)
                 (- blake2fmodexpdata.STAMP (prev blake2fmodexpdata.STAMP)))))
 
-;;                (defun (blake2ff-selector)
-;;( * (- 1 (prev blake2fmodexpdata.IS_BLAKE_DATA)) blake2fmodexpdata.IS_BLAKE_DATA))
-
-
-;;(defclookup
-  ;;b-into-b
-  ;;(
-    ;;blake2fmodexpdata.ID
-  ;;)
-  ;;(blake2ff-selector)
-  ;; source selector
-  ;; source columns
-  ;;(
-    ;;blake2fmodexpdata.ID
-  ;;))
-
 (defclookup
   blake2fmodexpdata-into-wcp
   ;; target colums (in WCP)
