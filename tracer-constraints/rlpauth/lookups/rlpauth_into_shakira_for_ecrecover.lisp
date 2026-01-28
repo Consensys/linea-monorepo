b939ab9797c9d3119b9d0cfaa3a167d5c06f9ed9
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2026-01-28 19:59:00 +0100

    fixed compute rlp and keccak

--- tracer-constraints/rlpauth/lookups/rlpauth_into_shakira_for_ecrecover.lisp
+++ tracer-constraints/rlpauth/lookups/rlpauth_into_shakira_for_ecrecover.lisp
@@ -9,16 +9,18 @@
         shakiradata.LIMB
         (next shakiradata.LIMB)
         (shift shakiradata.LIMB 2)
+        (shift shakiradata.LIMB 3) 
+        (shift shakiradata.LIMB 4)
         shakiradata.TOTAL_SIZE
     )
     ;; source selector
     (rlp-auth-into-shakiradata-for-ecrecover-activation-flag)
     ;; source columns
     (
-        keccak_for_ecrecover.limb
+        keccak_for_ecrecover.limb ;; data
         (next keccak_for_ecrecover.limb)
-        (shift keccak_for_ecrecover.limb 2) ;; TODO: do we need also the result limbs?
+        (shift keccak_for_ecrecover.limb 2)
+        (shift keccak_for_ecrecover.limb 3) ;; result
+        (shift keccak_for_ecrecover.limb 4)
         keccak_for_ecrecover.total_size
     ))
-
-;; TODO: define source selector
