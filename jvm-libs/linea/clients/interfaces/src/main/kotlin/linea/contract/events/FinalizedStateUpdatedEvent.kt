984d7d23e13213f456f5858e9c007c9192aafac7
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2026-01-29 11:50:19 +0000

    coordinator: remove unnecessary equals and hashCode methods

--- jvm-libs/linea/clients/interfaces/src/main/kotlin/linea/contract/events/FinalizedStateUpdatedEvent.kt
+++ jvm-libs/linea/clients/interfaces/src/main/kotlin/linea/contract/events/FinalizedStateUpdatedEvent.kt
@@ -33,34 +33,4 @@ data class FinalizedStateUpdatedEvent(
       )
     }
   }
-
-  override fun equals(other: Any?): Boolean {
-    if (this === other) return true
-    if (javaClass != other?.javaClass) return false
-
-    other as FinalizedStateUpdatedEvent
-
-    if (blockNumber != other.blockNumber) return false
-    if (timestamp != other.timestamp) return false
-    if (messageNumber != other.messageNumber) return false
-    if (forcedTransactionNumber != other.forcedTransactionNumber) return false
-
-    return true
-  }
-
-  override fun hashCode(): Int {
-    var result = blockNumber.hashCode()
-    result = 31 * result + timestamp.hashCode()
-    result = 31 * result + messageNumber.hashCode()
-    result = 31 * result + forcedTransactionNumber.hashCode()
-    return result
-  }
-
-  override fun toString(): String {
-    return "FinalizedStateUpdatedEvent(" +
-      "blockNumber=$blockNumber, " +
-      "timestamp=$timestamp, " +
-      "messageNumber=$messageNumber, " +
-      "forcedTransactionNumber=$forcedTransactionNumber)"
-  }
 }
