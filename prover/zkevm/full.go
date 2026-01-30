780f66dc198239b7382a7ba9c64b8695a6f9b060
AlexandreBelling <alexandrebelling8@gmail.com>
2026-01-30 17:31:21 +0000

    perf: balance the last Vortex part to reduce the proof size -> 90M constraints

--- prover/zkevm/full.go
+++ prover/zkevm/full.go
@@ -181,7 +181,7 @@ var (
 		cleanup.CleanUp,
 		poseidon2.CompilePoseidon2,
 		compiler.Arcane(
-			compiler.WithTargetColSize(1<<14),
+			compiler.WithTargetColSize(1<<12),
 			compiler.WithStitcherMinSize(16),
 		),
 		vortex.Compile(
