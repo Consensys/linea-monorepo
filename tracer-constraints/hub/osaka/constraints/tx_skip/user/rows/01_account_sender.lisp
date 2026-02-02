ab132a6445a411c17af21892ead2e2859dfb9696
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2026-02-02 17:03:55 +0100

    TX_SKIP: sender account-row

--- tracer-constraints/hub/osaka/constraints/tx_skip/user/rows/01_account_sender.lisp
+++ tracer-constraints/hub/osaka/constraints/tx_skip/user/rows/01_account_sender.lisp
@@ -16,19 +16,20 @@
                  (:guard (tx-skip---precondition---USER))
                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                  (begin
-                   (eq!    (shift account/ADDRESS_HI             tx-skip---USER---row-offset---ACC---sender)
-                           (shift transaction/FROM_ADDRESS_HI    tx-skip---USER---row-offset---TXN))
-                   (eq!    (shift account/ADDRESS_LO             tx-skip---USER---row-offset---ACC---sender)
-                           (shift transaction/FROM_ADDRESS_LO    tx-skip---USER---row-offset---TXN))
-                   (account-decrement-balance-by                 tx-skip---USER---row-offset---ACC---sender    (tx-skip---USER---wei-cost-for-sender))
-                   (account-increment-nonce                      tx-skip---USER---row-offset---ACC---sender)
-                   (account-same-code                            tx-skip---USER---row-offset---ACC---sender)
-                   (account-same-deployment-number-and-status    tx-skip---USER---row-offset---ACC---sender)
-                   (account-same-warmth                          tx-skip---USER---row-offset---ACC---sender)
-                   (account-same-marked-for-deletion             tx-skip---USER---row-offset---ACC---sender)
-                   (account-isnt-precompile                      tx-skip---USER---row-offset---ACC---sender)
-                   (DOM-SUB-stamps---standard                    tx-skip---USER---row-offset---ACC---sender
-                                                                 tx-skip---USER---row-offset---ACC---sender)))
+                   (eq!    (shift account/ADDRESS_HI                          tx-skip---USER---row-offset---ACC---sender)
+                           (shift transaction/FROM_ADDRESS_HI                 tx-skip---USER---row-offset---TXN))
+                   (eq!    (shift account/ADDRESS_LO                          tx-skip---USER---row-offset---ACC---sender)
+                           (shift transaction/FROM_ADDRESS_LO                 tx-skip---USER---row-offset---TXN))
+                   (account-decrement-balance-by                              tx-skip---USER---row-offset---ACC---sender    (tx-skip---USER---wei-cost-for-sender))
+                   (account-increment-nonce                                   tx-skip---USER---row-offset---ACC---sender)
+                   (account-same-code                                         tx-skip---USER---row-offset---ACC---sender)
+                   (account-same-deployment-number-and-status                 tx-skip---USER---row-offset---ACC---sender)
+                   (account-check-for-delegation-if-account-has-code          tx-skip---USER---row-offset---ACC---sender)
+                   (account-same-warmth                                       tx-skip---USER---row-offset---ACC---sender)
+                   (account-same-marked-for-deletion                          tx-skip---USER---row-offset---ACC---sender)
+                   (account-isnt-precompile                                   tx-skip---USER---row-offset---ACC---sender)
+                   (DOM-SUB-stamps---standard                                 tx-skip---USER---row-offset---ACC---sender
+                                                                              tx-skip---USER---row-offset---ACC---sender)))
 
 
 
@@ -42,4 +43,15 @@
 (defconstraint     tx-skip---USER---reject-transactions-from-senders-with-deployed-code---EIP-3607
                    (:guard (tx-skip---precondition---USER))
                    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                   (vanishes!    (shift    account/HAS_CODE    tx-skip---USER---row-offset---ACC---sender)))
+                   (eq!  (tx-skip---SNDR---has-empty-code-or-is-delegated)  1))
+
+
+(defun   (tx-skip---SNDR---has-empty-code-or-is-delegated)  (+  (tx-skip---SNDR---has-empty-code)
+                                                                (tx-skip---SNDR---is-delegated)
+                                                                ))
+
+(defun   (tx-skip---SNDR---has-empty-code)     (force-bin  (-  1  (tx-skip---SNDR---has-nonempty-code))))
+
+(defun   (tx-skip---SNDR---is-delegated)       (shift    account/IS_DELEGATED    tx-skip---USER---row-offset---ACC---sender) )
+(defun   (tx-skip---SNDR---has-nonempty-code)  (shift    account/HAS_CODE        tx-skip---USER---row-offset---ACC---sender) )
+
