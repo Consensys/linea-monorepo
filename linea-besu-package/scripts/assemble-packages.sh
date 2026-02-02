d37be6435168b9f43c3ce83bff6a631312a67738
Jones Ho <jones.ho@consensys.net>
2026-02-02 16:32:03 +0800

    feat: add support of tracer and sequencer auto release if releases.versions.toml has updated

--- linea-besu-package/scripts/assemble-packages.sh
+++ linea-besu-package/scripts/assemble-packages.sh
@@ -20,18 +20,35 @@ mkdir -p ./besu/plugins
 cd ./besu/plugins
 
 echo "downloading the plugins"
-echo "getting linea_sequencer_plugin_version: $LINEA_SEQUENCER_PLUGIN_VERSION"
-wget -nv https://github.com/Consensys/linea-monorepo/releases/download/linea-sequencer-v$LINEA_SEQUENCER_PLUGIN_VERSION/linea-sequencer-v$LINEA_SEQUENCER_PLUGIN_VERSION.zip
-unzip -j -o linea-sequencer-v$LINEA_SEQUENCER_PLUGIN_VERSION.zip
-rm linea-sequencer-v$LINEA_SEQUENCER_PLUGIN_VERSION.zip
+
+# Sequencer: use local zip if provided
+if [ -n "${LOCAL_SEQUENCER_ZIP_PATH:-}" ] && [ -f "../../../$LOCAL_SEQUENCER_ZIP_PATH" ]; then
+  echo "using local sequencer zip: ../../../$LOCAL_SEQUENCER_ZIP_PATH"
+  cp ../../../$LOCAL_SEQUENCER_ZIP_PATH .
+  unzip -j -o $(basename "$LOCAL_SEQUENCER_ZIP_PATH")
+  rm $(basename "$LOCAL_SEQUENCER_ZIP_PATH")
+else
+  echo "getting linea_sequencer_plugin_version: $LINEA_SEQUENCER_PLUGIN_VERSION"
+  wget -nv https://github.com/Consensys/linea-monorepo/releases/download/linea-sequencer-v$LINEA_SEQUENCER_PLUGIN_VERSION/linea-sequencer-v$LINEA_SEQUENCER_PLUGIN_VERSION.zip
+  unzip -j -o linea-sequencer-v$LINEA_SEQUENCER_PLUGIN_VERSION.zip
+  rm linea-sequencer-v$LINEA_SEQUENCER_PLUGIN_VERSION.zip
+fi
 
 echo "getting linea_staterecovery_plugin_version: $LINEA_STATERECOVERY_PLUGIN_VERSION"
 wget -nv https://github.com/Consensys/linea-monorepo/releases/download/linea-staterecovery-v$LINEA_STATERECOVERY_PLUGIN_VERSION/linea-staterecovery-besu-plugin-v$LINEA_STATERECOVERY_PLUGIN_VERSION.jar
 
-echo "getting linea_tracer_plugin_version: $LINEA_TRACER_PLUGIN_VERSION"
-wget -nv https://github.com/Consensys/linea-monorepo/releases/download/linea-tracer-$LINEA_TRACER_PLUGIN_VERSION/linea-tracer-$LINEA_TRACER_PLUGIN_VERSION.zip
-unzip -j -o linea-tracer-$LINEA_TRACER_PLUGIN_VERSION.zip
-rm linea-tracer-$LINEA_TRACER_PLUGIN_VERSION.zip
+# Tracer: use local zip if provided
+if [ -n "${LOCAL_TRACER_ZIP_PATH:-}" ] && [ -f "../../../$LOCAL_TRACER_ZIP_PATH" ]; then
+  echo "using local tracer zip: ../../../$LOCAL_TRACER_ZIP_PATH"
+  cp ../../../$LOCAL_TRACER_ZIP_PATH .
+  unzip -j -o $(basename "$LOCAL_TRACER_ZIP_PATH")
+  rm $(basename "$LOCAL_TRACER_ZIP_PATH")
+else
+  echo "getting linea_tracer_plugin_version: $LINEA_TRACER_PLUGIN_VERSION"
+  wget -nv https://github.com/Consensys/linea-monorepo/releases/download/linea-tracer-$LINEA_TRACER_PLUGIN_VERSION/linea-tracer-$LINEA_TRACER_PLUGIN_VERSION.zip
+  unzip -j -o linea-tracer-$LINEA_TRACER_PLUGIN_VERSION.zip
+  rm linea-tracer-$LINEA_TRACER_PLUGIN_VERSION.zip
+fi
 
 echo "getting shomei_plugin_version: $SHOMEI_PLUGIN_VERSION"
 wget -nv https://github.com/Consensys/besu-shomei-plugin/releases/download/v$SHOMEI_PLUGIN_VERSION/besu-shomei-plugin-v$SHOMEI_PLUGIN_VERSION.zip
