37c350836fd035371e4064fa73ccaeafb3e78c2b
Gaurav Ahuja <gauravahuja9@gmail.com>
2026-02-01 22:16:09 -0500

    [Coordinator] Refactor ProofIndex

--- coordinator/clients/prover-client/file-based-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/prover/ProverFileNameProvider.kt
+++ coordinator/clients/prover-client/file-based-client/src/main/kotlin/net/consensys/zkevm/coordinator/clients/prover/ProverFileNameProvider.kt
@@ -1,25 +1,14 @@
 package net.consensys.zkevm.coordinator.clients.prover
 
 import linea.kotlin.encodeHex
+import net.consensys.zkevm.domain.AggregationProofIndex
+import net.consensys.zkevm.domain.CompressionProofIndex
+import net.consensys.zkevm.domain.ExecutionProofIndex
+import net.consensys.zkevm.domain.InvalidityProofIndex
 import net.consensys.zkevm.domain.ProofIndex
 
-open class ProverFileNameProvider(protected val fileNameSuffix: String) {
-  protected fun encodeHash(hash: ByteArray): String = hash.encodeHex(prefix = false)
-  private fun fileName(startBlockNumber: ULong, endBlockNumber: ULong, fileNameSuffix: String, hash: ByteArray?) =
-    if (hash == null || hash.isEmpty()) {
-      "$startBlockNumber-$endBlockNumber-$fileNameSuffix"
-    } else {
-      val hashString = encodeHash(hash)
-      "$startBlockNumber-$endBlockNumber-$hashString-$fileNameSuffix"
-    }
-  open fun getFileName(proofIndex: ProofIndex): String {
-    return fileName(
-      startBlockNumber = proofIndex.startBlockNumber,
-      endBlockNumber = proofIndex.endBlockNumber,
-      hash = proofIndex.hash,
-      fileNameSuffix = fileNameSuffix,
-    )
-  }
+interface ProverFileNameProvider<T : ProofIndex> {
+  fun getFileName(proofIndex: T): String
 }
 
 object FileNameSuffixes {
@@ -32,32 +21,57 @@ object FileNameSuffixes {
 class ExecutionProofRequestFileNameProvider(
   private val tracesVersion: String,
   private val stateManagerVersion: String,
-) : ProverFileNameProvider(FileNameSuffixes.EXECUTION_PROOF_SUFFIX) {
-  override fun getFileName(proofIndex: ProofIndex): String {
+) : ProverFileNameProvider<ExecutionProofIndex> {
+  override fun getFileName(proofIndex: ExecutionProofIndex): String {
     return "${proofIndex.startBlockNumber}-${proofIndex.endBlockNumber}" +
       "-etv$tracesVersion-stv$stateManagerVersion" +
-      "-$fileNameSuffix"
+      "-${FileNameSuffixes.EXECUTION_PROOF_SUFFIX}"
   }
 }
-object ExecutionProofResponseFileNameProvider : ProverFileNameProvider(FileNameSuffixes.EXECUTION_PROOF_SUFFIX)
+object ExecutionProofResponseFileNameProvider : ProverFileNameProvider<ExecutionProofIndex> {
+  override fun getFileName(proofIndex: ExecutionProofIndex): String {
+    return "${proofIndex.startBlockNumber}-${proofIndex.endBlockNumber}-${FileNameSuffixes.EXECUTION_PROOF_SUFFIX}"
+  }
+}
 
-object CompressionProofRequestFileNameProvider : ProverFileNameProvider(FileNameSuffixes.COMPRESSION_PROOF_SUFFIX) {
+object CompressionProofRequestFileNameProvider : ProverFileNameProvider<CompressionProofIndex> {
+  private fun encodeHash(hash: ByteArray): String = hash.encodeHex(prefix = false)
   private const val HARD_CODED_VERSION = "0.0"
-  override fun getFileName(proofIndex: ProofIndex): String {
-    val requestHashString = encodeHash(proofIndex.hash!!)
+
+  override fun getFileName(proofIndex: CompressionProofIndex): String {
+    val requestHashString = encodeHash(proofIndex.hash)
     return "${proofIndex.startBlockNumber}-${proofIndex.endBlockNumber}-" +
       "bcv$HARD_CODED_VERSION-" +
       "ccv$HARD_CODED_VERSION-" +
       requestHashString + "-" +
-      fileNameSuffix
+      FileNameSuffixes.COMPRESSION_PROOF_SUFFIX
   }
 }
-object CompressionProofResponseFileNameProvider : ProverFileNameProvider(FileNameSuffixes.COMPRESSION_PROOF_SUFFIX)
+object CompressionProofResponseFileNameProvider : ProverFileNameProvider<CompressionProofIndex> {
+  private fun encodeHash(hash: ByteArray): String = hash.encodeHex(prefix = false)
 
-object AggregationProofFileNameProvider : ProverFileNameProvider(FileNameSuffixes.AGGREGATION_PROOF_SUFFIX)
+  override fun getFileName(proofIndex: CompressionProofIndex): String {
+    val requestHashString = encodeHash(proofIndex.hash)
+    return "${proofIndex.startBlockNumber}-${proofIndex.endBlockNumber}-" +
+      requestHashString + "-" +
+      FileNameSuffixes.COMPRESSION_PROOF_SUFFIX
+  }
+}
 
-object InvalidityProofFileNameProvider : ProverFileNameProvider(FileNameSuffixes.INVALIDITY_PROOF_SUFFIX) {
-  override fun getFileName(proofIndex: ProofIndex): String {
-    return "${proofIndex.startBlockNumber}-${proofIndex.endBlockNumber}-$fileNameSuffix"
+object AggregationProofFileNameProvider : ProverFileNameProvider<AggregationProofIndex> {
+  private fun encodeHash(hash: ByteArray): String = hash.encodeHex(prefix = false)
+
+  override fun getFileName(proofIndex: AggregationProofIndex): String {
+    val requestHashString = encodeHash(proofIndex.hash)
+
+    return "${proofIndex.startBlockNumber}-${proofIndex.endBlockNumber}" +
+      "-$requestHashString-${FileNameSuffixes.AGGREGATION_PROOF_SUFFIX}"
+  }
+}
+
+object InvalidityProofFileNameProvider : ProverFileNameProvider<InvalidityProofIndex> {
+  override fun getFileName(proofIndex: InvalidityProofIndex): String {
+    return "${proofIndex.simulatedExecutionBlockNumber}-${proofIndex.ftxNumber}" +
+      "-${FileNameSuffixes.INVALIDITY_PROOF_SUFFIX}"
   }
 }
