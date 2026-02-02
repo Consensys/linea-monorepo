620c14cb9572f14332d8f5442d3b520dc015b5d4
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2026-02-02 16:26:38 +0100

    TX_SKIP: peeking flags and shorthands

--- tracer-constraints/hub/osaka/constraints/tx_skip/user/shorthands.lisp
+++ tracer-constraints/hub/osaka/constraints/tx_skip/user/shorthands.lisp
@@ -16,13 +16,15 @@
   tx-skip---USER---row-offset---TXN                        0
   tx-skip---USER---row-offset---ACC---sender               1
   tx-skip---USER---row-offset---ACC---recipient            2
-  tx-skip---USER---row-offset---ACC---coinbase             3
-  tx-skip---USER---row-offset---CON---final-zero-context   4
-  tx-skip---USER---NSR                                     5
+  tx-skip---USER---row-offset---ACC---delegate             3
+  tx-skip---USER---row-offset---ACC---coinbase             4
+  tx-skip---USER---row-offset---CON---final-zero-context   5
+  tx-skip---USER---NSR                                     (+ tx-skip---USER---row-offset---CON---final-zero-context 1)
   )
 
 
-(defun    (tx-skip---USER---is-deployment)    (force-bin (shift transaction/IS_DEPLOYMENT    tx-skip---USER---row-offset---TXN)))
+(defun    (tx-skip---USER---is-deployment)      (force-bin (shift transaction/IS_DEPLOYMENT    tx-skip---USER---row-offset---TXN)))
+(defun    (tx-skip---USER---is-message-call)    (force-bin (-  1  (tx-skip---USER---is-deployment))))
 
 
 (defun    (tx-skip---precondition---USER)    (*   (-    TOTL_TXN_NUMBER    (prev    TOTL_TXN_NUMBER))
