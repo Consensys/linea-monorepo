9020c718c7edd4351ff7c06b14d7de108eb97bc4
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2026-02-02 17:23:09 +0100

    TX_SKIP: recipient account-row

--- tracer-constraints/hub/osaka/constraints/tx_skip/user/rows/02_account_recipient.lisp
+++ tracer-constraints/hub/osaka/constraints/tx_skip/user/rows/02_account_recipient.lisp
@@ -20,20 +20,37 @@
                         ;; (account-increment-nonce                       tx-skip---USER---row-offset---ACC---recipient)
                         ;; (account-same-code                             tx-skip---USER---row-offset---ACC---recipient)
                         ;; (account-same-deployment-number-and-status     tx-skip---USER---row-offset---ACC---recipient)
+                        (account-conditionally-check-for-delegation tx-skip---USER---row-offset---ACC---recipient  (check-recipient-for-delegation))
                         (account-same-warmth                        tx-skip---USER---row-offset---ACC---recipient)
                         (account-same-marked-for-deletion           tx-skip---USER---row-offset---ACC---recipient)
                         (account-isnt-precompile                    tx-skip---USER---row-offset---ACC---recipient)
                         (DOM-SUB-stamps---standard                  tx-skip---USER---row-offset---ACC---recipient
                                                                     tx-skip---USER---row-offset---ACC---recipient)))
 
+(defun  (check-recipient-for-delegation)  (*  (tx-skip---USER---is-message-call)
+                                              (tx-skip---RCPT---has-nonempty-code)
+                                              ))
+
+;;-----------------------;;
+;;   Message call case   ;;
+;;-----------------------;;
+
+
+
 (defconstraint   tx-skip---USER---recipient-account-row---trivial-message-calls---nonce-code-and-deployment-status
                  (:guard (tx-skip---precondition---USER))
                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                 (if-zero (tx-skip---USER---is-deployment)
-                          ;; deployment ≡ 0 i.e. pure transfers
-                          (begin    (account-same-nonce                          tx-skip---USER---row-offset---ACC---recipient)
-                                    (account-same-code                           tx-skip---USER---row-offset---ACC---recipient)
-                                    (account-same-deployment-number-and-status   tx-skip---USER---row-offset---ACC---recipient))))
+                 (if-not-zero (tx-skip---USER---is-message-call)
+                              ;; message_call ≡ 1 i.e. pure transfers
+                              (begin    (account-same-nonce                          tx-skip---USER---row-offset---ACC---recipient)
+                                        (account-same-code                           tx-skip---USER---row-offset---ACC---recipient)
+                                        (account-same-deployment-number-and-status   tx-skip---USER---row-offset---ACC---recipient))))
+
+;;---------------------;;
+;;   Deployment case   ;;
+;;---------------------;;
+
+
 
 (defconstraint   tx-skip---USER---recipient-account-row---trivial-deployments---nonce
                  (:guard (tx-skip---precondition---USER))
@@ -68,7 +85,7 @@
                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                  (if-not-zero    (tx-skip---USER---is-deployment)
                                  ;; deployment ≡ 1 i.e. trivial deployments
-                                 (begin  ;; deployment
+                                 (begin
                                    (account-increment-deployment-number                tx-skip---USER---row-offset---ACC---recipient)
                                    (debug (eq! (shift account/DEPLOYMENT_STATUS        tx-skip---USER---row-offset---ACC---recipient) 0))
                                    (eq!        (shift account/DEPLOYMENT_STATUS_NEW    tx-skip---USER---row-offset---ACC---recipient) 0))))
@@ -76,8 +93,8 @@
 (defconstraint   tx-skip---USER---recipient-is-no-precompile
                  (:guard (tx-skip---precondition---USER))
                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                 (if-zero    (tx-skip---USER---is-deployment)
-                             ;; deployment ≡ 0 i.e. pure transfer
-                             (account-trim-address       tx-skip---USER---row-offset---ACC---recipient
-                                                         (shift   transaction/TO_ADDRESS_HI   tx-skip---USER---row-offset---TXN)
-                                                         (shift   transaction/TO_ADDRESS_LO   tx-skip---USER---row-offset---TXN))))
+                 (if-not-zero    (tx-skip---USER---is-message-call)
+                                 ;; deployment ≡ 0 i.e. pure transfer
+                                 (account-trim-address       tx-skip---USER---row-offset---ACC---recipient
+                                                             (shift   transaction/TO_ADDRESS_HI   tx-skip---USER---row-offset---TXN)
+                                                             (shift   transaction/TO_ADDRESS_LO   tx-skip---USER---row-offset---TXN))))
