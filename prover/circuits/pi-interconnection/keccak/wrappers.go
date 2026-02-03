0ebd4f6531b8e8dc80d0a304fcbcd06c2d741490
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2026-02-03 17:57:26 -0600

    feat: more wrappers

--- prover/circuits/pi-interconnection/keccak/wrappers.go
+++ prover/circuits/pi-interconnection/keccak/wrappers.go
@@ -3,12 +3,14 @@ package keccak
 import (
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/linea-monorepo/prover/circuits/pi-interconnection/keccak/prover/circuits/pi-interconnection/keccak"
+	"github.com/consensys/linea-monorepo/prover/circuits/pi-interconnection/keccak/prover/protocol/compiler/dummy"
 	"github.com/consensys/linea-monorepo/prover/circuits/pi-interconnection/keccak/prover/protocol/wizard"
 )
 
 type (
 	CompilationParams    []func(iop *wizard.CompiledIOP)
 	BlockHasher          keccak.BlockHasher
+	Hasher               struct{ *keccak.Hasher }
 	StrictHasherCircuit  struct{ *keccak.StrictHasherCircuit }
 	StrictHasherCompiler struct{ *keccak.StrictHasherCompiler }
 	StrictHasherSnark    struct{ *keccak.StrictHasherSnark }
@@ -45,6 +47,15 @@ func (h CompiledStrictHasher) GetHasher() StrictHasher {
 	return StrictHasher{&hsh}
 }
 
+func NewHasher(api frontend.API, maxNbKeccakF int) *Hasher {
+	h := keccak.NewHasher(api, maxNbKeccakF)
+	return &Hasher{h}
+}
+
+func (h *Hasher) Sum(nbIn frontend.Variable, bytess ...[32]frontend.Variable) [32]frontend.Variable {
+	return h.Hasher.Sum(nbIn, bytess...)
+}
+
 func RegisterHints() {
 	keccak.RegisterHints()
 }
@@ -64,4 +75,26 @@ func (h StrictHasher) MaxNbKeccakF() int {
 func (h StrictHasher) Assign() (StrictHasherCircuit, error) {
 	c, err := h.StrictHasher.Assign()
 	return StrictHasherCircuit{&c}, err
+}
+
+func (c StrictHasherCompiler) WithStrictHashLengths(l ...int) *StrictHasherCompiler {
+	c.StrictHasherCompiler.WithStrictHashLengths(l...)
+	return &c
+}
+
+func (c StrictHasherCompiler) WithFlexibleHashLengths(l ...int) *StrictHasherCompiler {
+	c.StrictHasherCompiler.WithFlexibleHashLengths(l...)
+	return &c
+}
+
+func (s StrictHasherSnark) Sum(nbIn frontend.Variable, bytess ...[32]frontend.Variable) [32]frontend.Variable {
+	return s.StrictHasherSnark.Sum(nbIn, bytess...)
+}
+
+func (s StrictHasherSnark) Finalize() error {
+	return s.StrictHasherSnark.Finalize()
+}
+
+func DummyCompile() CompilationParams {
+	return CompilationParams{dummy.Compile}
 }
