615359450d85cb1aaac85a6cb08644eb9b528d72
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2026-02-04 01:36:32 +0100

    CALL: first set of account rows (wip)

--- tracer-constraints/hub/osaka/constraints/instruction-handling/call/generalities/1st_set_of_account_rows.lisp
+++ tracer-constraints/hub/osaka/constraints/instruction-handling/call/generalities/1st_set_of_account_rows.lisp
@@ -27,6 +27,7 @@
                     ;; balance done below
                     (account-same-nonce                              CALL_1st_caller_account_row___row_offset)
                     (account-same-code                               CALL_1st_caller_account_row___row_offset)
+                    (account-dont-check-for-delegation               CALL_1st_caller_account_row___row_offset)
                     (account-same-warmth                             CALL_1st_caller_account_row___row_offset)
                     (account-same-deployment-number-and-status       CALL_1st_caller_account_row___row_offset)
                     (account-same-marked-for-deletion                CALL_1st_caller_account_row___row_offset)
@@ -34,7 +35,7 @@
                     (vanishes!    (shift    account/TRM_FLAG         CALL_1st_caller_account_row___row_offset))
                     (vanishes!    (shift    account/RLPADDR_FLAG     CALL_1st_caller_account_row___row_offset))
                     (DOM-SUB-stamps---standard                       CALL_1st_caller_account_row___row_offset
-                                                                     0)
+                                                                     CALL_1st_caller_account_row___row_offset)
                     ))
 
 (defconstraint    call-instruction---1st-caller-account-operation---balance-update    (:guard (call-instruction---summon-both-account-rows-once-or-more))
@@ -53,20 +54,21 @@
                     (debug (eq!   (shift    account/TRM_RAW_ADDRESS_HI     CALL_1st_callee_account_row___row_offset)    (call-instruction---STACK-raw-callee-address-hi))) ;; set implicitly by account-trim-address
                     (debug (eq!   (shift    account/ADDRESS_LO             CALL_1st_callee_account_row___row_offset)    (call-instruction---STACK-raw-callee-address-lo))) ;; set implicitly by account-trim-address
                     ;; balance done below
-                    (account-same-nonce                              CALL_1st_callee_account_row___row_offset)
-                    (account-same-code                               CALL_1st_callee_account_row___row_offset)
+                    (account-same-nonce                                    CALL_1st_callee_account_row___row_offset)
+                    (account-same-code                                     CALL_1st_callee_account_row___row_offset)
+                    (account-check-for-delegation-if-account-has-code      CALL_1st_callee_account_row___row_offset)
                     ;; warmth done below
-                    (account-same-deployment-number-and-status       CALL_1st_callee_account_row___row_offset)
-                    (account-same-marked-for-deletion                CALL_1st_callee_account_row___row_offset)
-                    (eq!          (shift    account/ROMLEX_FLAG      CALL_1st_callee_account_row___row_offset)    (scenario-shorthand---CALL---smart-contract))
-                    (account-trim-address                            CALL_1st_callee_account_row___row_offset          ;; row offset
-                                                                     (call-instruction---STACK-raw-callee-address-hi)  ;; high part of raw, potentially untrimmed address
-                                                                     (call-instruction---STACK-raw-callee-address-lo)  ;; low  part of raw, potentially untrimmed address
-                                                                     )
-                    (debug (eq!   (shift    account/TRM_FLAG         CALL_1st_callee_account_row___row_offset)    1)) ;; set implicitly by account-trim-address
-                    (vanishes!    (shift    account/RLPADDR_FLAG     CALL_1st_callee_account_row___row_offset))
-                    (DOM-SUB-stamps---standard                       CALL_1st_callee_account_row___row_offset
-                                                                     1)
+                    (account-same-deployment-number-and-status             CALL_1st_callee_account_row___row_offset)
+                    (account-same-marked-for-deletion                      CALL_1st_callee_account_row___row_offset)
+                    (eq!          (shift    account/ROMLEX_FLAG            CALL_1st_callee_account_row___row_offset)    (scenario-shorthand---CALL---smart-contract))
+                    (account-trim-address                                  CALL_1st_callee_account_row___row_offset          ;; row offset
+                                                                           (call-instruction---STACK-raw-callee-address-hi)  ;; high part of raw, potentially untrimmed address
+                                                                           (call-instruction---STACK-raw-callee-address-lo)  ;; low  part of raw, potentially untrimmed address
+                                                                           )
+                    (debug (eq!   (shift    account/TRM_FLAG               CALL_1st_callee_account_row___row_offset)    1)) ;; set implicitly by account-trim-address
+                    (vanishes!    (shift    account/RLPADDR_FLAG           CALL_1st_callee_account_row___row_offset))
+                    (DOM-SUB-stamps---standard                             CALL_1st_callee_account_row___row_offset
+                                                                           CALL_1st_callee_account_row___row_offset)
                     ))
 
 (defconstraint    call-instruction---1st-callee-account-operation---balance-update    (:guard (call-instruction---summon-both-account-rows-once-or-more))
