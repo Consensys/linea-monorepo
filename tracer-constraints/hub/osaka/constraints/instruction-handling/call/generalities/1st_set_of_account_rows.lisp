82c074a8c9ae28de74dfb974e4bb688daf31f1c0
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2026-02-04 14:04:47 +0100

    CALL: first set of account rows: delegate or callee row
    This finishes the "first set of account rows" constraints

--- tracer-constraints/hub/osaka/constraints/instruction-handling/call/generalities/1st_set_of_account_rows.lisp
+++ tracer-constraints/hub/osaka/constraints/instruction-handling/call/generalities/1st_set_of_account_rows.lisp
@@ -19,7 +19,12 @@
                                                                               (scenario-shorthand---CALL---sum)
                                                                               (+    (call-instruction---STACK-oogx)    (scenario-shorthand---CALL---unexceptional))))
 
-;; CALLER account
+;;------------------------;;
+;;   caller account-row   ;;
+;;------------------------;;
+
+
+
 (defconstraint    call-instruction---1st-caller-account-operation    (:guard (call-instruction---summon-both-account-rows-once-or-more))
                   (begin
                     (eq!    (shift    account/ADDRESS_HI    CALL_1st_caller_account_row___row_offset)    (call-instruction---current-address-hi))
@@ -31,6 +36,7 @@
                     (account-same-warmth                             CALL_1st_caller_account_row___row_offset)
                     (account-same-deployment-number-and-status       CALL_1st_caller_account_row___row_offset)
                     (account-same-marked-for-deletion                CALL_1st_caller_account_row___row_offset)
+                    (account-dont-trigger-ROM_LEX                    CALL_1st_caller_account_row___row_offset)
                     (vanishes!    (shift    account/ROMLEX_FLAG      CALL_1st_caller_account_row___row_offset))
                     (vanishes!    (shift    account/TRM_FLAG         CALL_1st_caller_account_row___row_offset))
                     (vanishes!    (shift    account/RLPADDR_FLAG     CALL_1st_caller_account_row___row_offset))
@@ -47,7 +53,12 @@
                                                                      (* (call-instruction---is-CALL) (call-instruction---STACK-value-lo))))))
 
 
-;; CALLEE account
+;;------------------------;;
+;;   callee account-row   ;;
+;;------------------------;;
+
+
+
 (defconstraint    call-instruction---1st-callee-account-operation    (:guard (call-instruction---summon-both-account-rows-once-or-more))
                   (begin
                     ;; account/ADDRESS_HI set implicitly by account-trim-address
@@ -60,7 +71,7 @@
                     ;; warmth done below
                     (account-same-deployment-number-and-status             CALL_1st_callee_account_row___row_offset)
                     (account-same-marked-for-deletion                      CALL_1st_callee_account_row___row_offset)
-                    (eq!          (shift    account/ROMLEX_FLAG            CALL_1st_callee_account_row___row_offset)    (scenario-shorthand---CALL---smart-contract))
+                    (account-dont-trigger-ROM_LEX                          CALL_1st_callee_account_row___row_offset)
                     (account-trim-address                                  CALL_1st_callee_account_row___row_offset          ;; row offset
                                                                            (call-instruction---STACK-raw-callee-address-hi)  ;; high part of raw, potentially untrimmed address
                                                                            (call-instruction---STACK-raw-callee-address-lo)  ;; low  part of raw, potentially untrimmed address
@@ -70,6 +81,7 @@
                     (DOM-SUB-stamps---standard                             CALL_1st_callee_account_row___row_offset
                                                                            CALL_1st_callee_account_row___row_offset)
                     ))
+
 
 (defconstraint    call-instruction---1st-callee-account-operation---balance-update    (:guard (call-instruction---summon-both-account-rows-once-or-more))
                   (begin
@@ -85,3 +97,50 @@
                                     (account-same-warmth             CALL_1st_callee_account_row___row_offset))
                     (if-not-zero    (scenario-shorthand---CALL---callee-warmth-update-required)
                                     (account-turn-on-warmth          CALL_1st_callee_account_row___row_offset))))
+
+
+;;------------------------------------;;
+;;   delegate or callee account-row   ;;
+;;------------------------------------;;
+
+
+
+(defconstraint    call-instruction---1st-delegt-account-operation    (:guard (call-instruction---summon-both-account-rows-once-or-more))
+                  (begin
+                    (eq!   (shift    account/ADDRESS_HI                    CALL_1st_delegt_account_row___row_offset)    (call-instruction---explicit-delegate-or-callee-address-hi))
+                    (eq!   (shift    account/ADDRESS_LO                    CALL_1st_delegt_account_row___row_offset)    (call-instruction---explicit-delegate-or-callee-address-lo))
+                    (account-same-balance                                  CALL_1st_delegt_account_row___row_offset)
+                    (account-same-nonce                                    CALL_1st_delegt_account_row___row_offset)
+                    (account-same-code                                     CALL_1st_delegt_account_row___row_offset)
+                    (account-check-for-delegation-if-account-has-code      CALL_1st_delegt_account_row___row_offset)
+                    ;; warmth set below
+                    (account-same-deployment-number-and-status             CALL_1st_delegt_account_row___row_offset)
+                    (account-same-marked-for-deletion                      CALL_1st_delegt_account_row___row_offset)
+                    (account-conditionally-trigger-ROM_LEX                 CALL_1st_delegt_account_row___row_offset    (call-instruction---trigger_ROMLEX))
+                    (account-trim-address                                  CALL_1st_delegt_account_row___row_offset                     ;; row offset
+                                                                           (call-instruction---explicit-delegate-or-callee-address-hi)  ;; hi part of either delegate or callee address
+                                                                           (call-instruction---explicit-delegate-or-callee-address-lo)  ;; lo part of either delegate or callee address
+                                                                           )
+                    (debug (eq!   (shift    account/TRM_FLAG               CALL_1st_delegt_account_row___row_offset)    1)) ;; set implicitly by account-trim-address
+                    (vanishes!    (shift    account/RLPADDR_FLAG           CALL_1st_delegt_account_row___row_offset))
+                    (DOM-SUB-stamps---standard                             CALL_1st_delegt_account_row___row_offset
+                                                                           CALL_1st_delegt_account_row___row_offset)
+                    ))
+
+
+(defun   (call-instruction---explicit-delegate-or-callee-address-hi)  (if-zero   (call-instruction---callee-is-delegated        )
+                                                                                 (call-instruction---callee-address-hi          )  ;; callee isn't delegated ≡ <true>
+                                                                                 (call-instruction---callee-delegate-address-hi )  ;; callee is    delegated ≡ <true>
+                                                                                 ))
+(defun   (call-instruction---explicit-delegate-or-callee-address-lo)  (if-zero   (call-instruction---callee-is-delegated        )
+                                                                                 (call-instruction---callee-address-lo          )  ;; callee isn't delegated ≡ <true>
+                                                                                 (call-instruction---callee-delegate-address-lo )  ;; callee is    delegated ≡ <true>
+                                                                                 ))
+
+(defconstraint    call-instruction---1st-delegt-account-operation---updating-warmth
+                  (:guard (call-instruction---summon-both-account-rows-once-or-more))
+                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                  (begin
+                    (if-not-zero   (scenario-shorthand---CALL---callee-warmth-update-not-required)   (account-same-warmth      CALL_1st_delegt_account_row___row_offset))
+                    (if-not-zero   (scenario-shorthand---CALL---callee-warmth-update-required)       (account-turn-on-warmth   CALL_1st_delegt_account_row___row_offset))
+                    ))
