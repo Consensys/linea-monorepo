0bbbac3dc7c67ad9626531262050ec4dea6b6902
Arya Tabaie <arya.pourtabatabaie@gmail.com>
2026-02-03 23:03:00 -0600

    remove cmd package

--- prover/circuits/pi-interconnection/keccak/prover/cmd/controller/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/consensys/linea-monorepo/prover/circuits/pi-interconnection/keccak/prover/cmd/controller/controller"
-
-func main() {
-	controller.Execute()
-}
--- prover/circuits/pi-interconnection/keccak/prover/cmd/dev-tools/corset-checker/Readme.md
+++
@@ -1,9 +0,0 @@
-# Corset checker
-
-Dev-tool to check lt trace files against the arithmetization
-
-## Usage
-
-```
-corset-checker --config <cfg-path> --trace-file <trace .lt file>
-```
