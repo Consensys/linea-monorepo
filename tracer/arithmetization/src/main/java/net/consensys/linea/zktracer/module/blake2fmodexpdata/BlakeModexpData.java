c301f3b2697f3294d9970cdfa85620df8ef68e97
François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
2026-02-09 07:11:14 +0530

    [ZkTracer] feat: EIP-7702: fix and missing lookup in romlex (#2289)
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- tracer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/blake2fmodexpdata/BlakeModexpData.java
+++ tracer/arithmetization/src/main/java/net/consensys/linea/zktracer/module/blake2fmodexpdata/BlakeModexpData.java
@@ -19,7 +19,6 @@ import static com.google.common.base.Preconditions.checkState;
 import static net.consensys.linea.zktracer.module.ModuleName.BLAKE_MODEXP_DATA;
 
 import java.util.List;
-import java.util.Optional;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
