77409322f256558bb09c8fc891b78136dd46c00e
Gaurav Ahuja <gauravahuja9@gmail.com>
2026-02-09 07:10:08 -0500

    Fix test for concurrency

--- coordinator/core/src/test/kotlin/net/consensys/zkevm/ethereum/crypto/HashFunctionTest.kt
+++ coordinator/core/src/test/kotlin/net/consensys/zkevm/ethereum/crypto/HashFunctionTest.kt
@@ -1,8 +1,11 @@
 package net.consensys.zkevm.ethereum.crypto
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import java.security.MessageDigest
+import kotlin.concurrent.atomics.AtomicBoolean
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
 import kotlin.random.Random
 
 class HashFunctionTest {
@@ -30,21 +33,25 @@ class HashFunctionTest {
     }
   }
 
+  @OptIn(ExperimentalAtomicApi::class)
   @Test
   fun `test Sha256HashFunction under concurrent calls produces correct hash`() {
     val hashFunction = Sha256HashFunction()
+    val anyFailures = AtomicBoolean(false)
     val threads = List(10) {
       Thread {
         for (i in 1..1000) {
           val randomBytes = Random.nextBytes(32)
-          assertArrayEquals(
-            hashFunction.hash(randomBytes),
-            MessageDigest.getInstance("SHA-256").digest(randomBytes),
-          )
+          val hashResult = hashFunction.hash(randomBytes)
+          val expectedHash = MessageDigest.getInstance("SHA-256").digest(randomBytes)
+          if (!hashResult.contentEquals(expectedHash)) {
+            anyFailures.store(true)
+          }
         }
       }
     }
     threads.forEach { it.start() }
     threads.forEach { it.join() }
+    assertFalse(anyFailures.load()) { "Concurrent calls to Sha256HashFunction produced incorrect hashes" }
   }
 }
