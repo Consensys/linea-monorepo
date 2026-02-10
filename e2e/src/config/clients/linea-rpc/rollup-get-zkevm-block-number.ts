40579876e496c81d5fb2f781d0d93527eab579c6
VGau <victorien.gauch@consensys.net>
2026-02-10 18:01:51 +0100

    fix: getZkEVMBlockNumber return type

--- e2e/src/config/clients/linea-rpc/rollup-get-zkevm-block-number.ts
+++ e2e/src/config/clients/linea-rpc/rollup-get-zkevm-block-number.ts
@@ -1,18 +1,20 @@
-import type { Client, Transport, Chain, Account } from "viem";
+import type { Client, Transport, Chain, Account, Hex } from "viem";
 
-export type GetZkEVMBlockNumberReturnType = number;
+export type GetZkEVMBlockNumberReturnType = bigint;
 
 export type GetZkEVMBlockNumberRpc = {
   Method: "rollup_getZkEVMBlockNumber";
   Parameters: [];
-  ReturnType: GetZkEVMBlockNumberReturnType;
+  ReturnType: Hex;
 };
 
 export async function getZkEVMBlockNumber(
   client: Client<Transport, Chain | undefined, Account | undefined, [GetZkEVMBlockNumberRpc]>,
 ): Promise<GetZkEVMBlockNumberReturnType> {
-  return client.request({
+  const blockNumber = await client.request({
     method: "rollup_getZkEVMBlockNumber",
     params: [],
   });
+
+  return BigInt(blockNumber);
 }
