88feed532bdeb257c1247f01c797ec2c86d7414b
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2026-02-11 18:53:29 +0100

    fix: typo in constraints

--- tracer-constraints/txndata/processing/user/transfer_to_hub_from_rlptxn.lisp
+++ tracer-constraints/txndata/processing/user/transfer_to_hub_from_rlptxn.lisp
@@ -68,5 +68,5 @@
                                (if-not-zero   (USER-transaction---tx-decoding---tx-type-sans-delegation)
                                               (begin
                                                 (vanishes!   (shift   hub/LENGTH_OF_DELEGATION_LIST                 ROFF___USER___HUB_ROW))
-                                                (vanishes!   (shift   hub/NUMBER_OF_SUCCESEFUL_SENDER_DELEGATIONS   ROFF___USER___HUB_ROW))
+                                                (vanishes!   (shift   hub/NUMBER_OF_SUCCESSFUL_SENDER_DELEGATIONS   ROFF___USER___HUB_ROW))
                                                 ))))
