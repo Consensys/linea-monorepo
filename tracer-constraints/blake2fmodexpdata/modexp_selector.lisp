a68bf4cf83a9fff4282423a4e5919eeb0d9e1fa5
F Bojarski <ceciestunepoubelle@protonmail.ch>
2026-02-11 09:17:54 +0530

    finish the glue
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

---
+++ tracer-constraints/blake2fmodexpdata/modexp_selector.lisp
@@ -0,0 +1,38 @@
+(module blake2fmodexpdata)
+
+(defconstraint selector-constancies ()
+  (begin
+  (stamp-constancy STAMP TRIVIAL_MODEXP)
+  (stamp-constancy STAMP SMALL_MODEXP)
+  (stamp-constancy STAMP LARGE_MODEXP)))
+
+(defconstraint trivial-modexp-are-trivial (:guard TRIVIAL_MODEXP)
+  (if-not-zero IS_MODEXP_RESULT (vanishes! LIMB))
+)
+
+(defun (modexp-input) (force-bin (+ IS_MODEXP_BASE IS_MODEXP_EXPONENT IS_MODEXP_MODULUS)))
+
+(defun (not-least-two-least-significant-limb) (~ (* (- INDEX_MAX_MODEXP INDEX) (- INDEX_MAX_MODEXP_MO INDEX))))
+
+(defcomputedcolumn (LARGE_MODEXP_LIMB_INPUT :i1)
+   (* (not-least-two-least-significant-limb)
+      (modexp-input)
+      (~ LIMB)))
+
+(defcomputedcolumn (LARGE_MODEXP_ACC :i8 :fwd)
+    (* (modexp-input) (+ LARGE_MODEXP_ACC
+                         LARGE_MODEXP_LIMB_INPUT)))
+
+(defun (last-row-of-modexp-input) (force-bin (* (modexp-input) (- 1 (modexp-input)))))
+
+(defconstraint define-modexp-case (:guard  (last-row-of-modexp-input))
+  (if-not-zero LARGE_MODEXP_ACC
+                                ;; if ACC !=0, then at least of the input is > u32, so LARGE_MODEXP
+                                (eq! LARGE_MODEXP 1)
+                                ;; need to differentiate between small and trivial (ie modulus == 0 or 1)) modexp
+                                ;; prev LIMB (ie hi part of the i32 input) check
+                                (if-not-zero (prev LIMB) (eq! SMALL_MODEXP 1)
+                                                         (if-not (or! (== LIMB 0) (== LIMB 1))
+                                                              (eq! SMALL_MODEXP 1)
+                                                              (eq! TRIVIAL_MODEXP 1) ))))
+
