b275b5f2306cfe72377ca16a9c326f2dcdb96b70
F Bojarski <ceciestunepoubelle@protonmail.ch>
2026-02-11 10:29:23 +0530

    ras
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/delegation/DelegatesAndRevertsTests.java
+++ tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/delegation/DelegatesAndRevertsTests.java
@@ -13,7 +13,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package net.consensys.linea.zktracer.forkSpecific.delegation;
+package net.consensys.linea.zktracer.delegation;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -21,14 +21,15 @@ import java.util.List;
 import java.util.stream.Stream;
 import net.consensys.linea.reporting.TracerTestBase;
 import net.consensys.linea.testing.ToyAccount;
+import net.consensys.linea.testing.ToyExecutionEnvironmentV2;
 import net.consensys.linea.testing.ToyTransaction;
+import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.crypto.KeyPair;
 import org.hyperledger.besu.crypto.SECP256K1;
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.TransactionType;
 import org.hyperledger.besu.datatypes.Wei;
-import org.hyperledger.besu.ethereum.core.CodeDelegation;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -39,14 +40,14 @@ public class DelegatesAndRevertsTests extends TracerTestBase {
   /**
    * We require tests like so: mono transaction block contains a single type 4 transaction.
    *
-   * <p>this tx has 2 delegations with all combinations of the following delegations are valid: yes
-   * / no (for valid ones) authority exists: yes / no TX_REQUIRES_EVM_EXECUTION: yes / no If yes: tx
+   * <p>this tx has 1 delegation with all combinations of the following delegations are valid: yes /
+   * no (for valid ones) authority exists: yes / no TX_REQUIRES_EVM_EXECUTION: yes / no If yes: tx
    * reverts: yes / no tx incurs another refund (say a single SSTORE that resets storage or so): yes
    * / no if no: no further requirements
    */
   @ParameterizedTest
   @MethodSource("delegatesAndRevertsTestsSource")
-  void delegatesAndRevertsTestsSource(scenario del1, scenario del2, TestInfo testInfo) {
+  void delegatesAndRevertsTestsSource(scenario sc, TestInfo testInfo) {
 
     // sender account
     final KeyPair senderKeyPair = new SECP256K1().generateKeyPair();
@@ -55,41 +56,49 @@ public class DelegatesAndRevertsTests extends TracerTestBase {
     final ToyAccount senderAccount =
         ToyAccount.builder().balance(Wei.fromEth(56)).nonce(119).address(senderAddress).build();
 
-    // receiver
-    final ToyAccount receiverAccount =
-        ToyAccount.builder()
-            .balance(Wei.fromEth(5))
-            .nonce(1)
-            .address(Address.fromHexString("0x1122334455667788990011223344556677889900"))
-            .build();
+    // eoa
+    final KeyPair eoaKeyPair = new SECP256K1().generateKeyPair();
+    final Address eoaAddress =
+        Address.extract(Hash.hash(eoaKeyPair.getPublicKey().getEncodedBytes()));
+    final ToyAccount eoaAccount =
+        ToyAccount.builder().balance(Wei.fromEth(12)).nonce(0).address(eoaAddress).build();
 
-    /** invalid delegation because of wrong chainId */
-    final CodeDelegation INVALID_DELEGATION =
-        (CodeDelegation)
-            CodeDelegation.createCodeDelegation(
-                chainConfig.id.and(BigInteger.valueOf(1789)),
-                Address.ALTBN128_ADD,
-                "0",
-                "0",
-                "0x09097887867",
-                "0x8787878");
+    final ToyTransaction.ToyTransactionBuilder tx =
+        ToyTransaction.builder()
+            .sender(senderAccount)
+            .to(eoaAccount)
+            .keyPair(senderKeyPair)
+            .gasLimit(300000L)
+            .transactionType(TransactionType.DELEGATE_CODE)
+            .value(Wei.of(1000));
 
-    ToyTransaction.builder()
-        .sender(senderAccount)
-        .to(receiverAccount)
-        .keyPair(senderKeyPair)
-        .gasLimit(300000L)
-        .transactionType(TransactionType.DELEGATE_CODE)
-        .value(Wei.of(1000))
-        .build();
+    if (sc == scenario.DELEGATION_VALID_NON) {
+      // delegation is known to fail because of chainID
+      tx.addCodeDelegation(
+          chainConfig.id.and(
+              Bytes.fromHexString("0x17891789178917891789178917891789178917891789178900000000")
+                  .toUnsignedBigInteger()),
+          Address.ZERO,
+          0L,
+          BigInteger.valueOf(78),
+          BigInteger.valueOf(89),
+          (byte) 0);
+    } else {
+      tx.addCodeDelegation(chainConfig.id, Address.ZERO, 0, eoaKeyPair);
+    }
+
+    ToyExecutionEnvironmentV2.builder(chainConfig, testInfo)
+        .accounts(List.of())
+        .transaction(tx.build())
+        .zkTracerValidator(zkTracer -> {})
+        .build()
+        .run();
   }
 
   private static Stream<Arguments> delegatesAndRevertsTestsSource() {
     List<Arguments> arguments = new ArrayList<>();
     for (scenario sc1 : scenario.values()) {
-      for (scenario sc2 : scenario.values()) {
-        arguments.add(Arguments.of(sc1, sc2));
-      }
+      arguments.add(Arguments.of(sc1));
     }
     return arguments.stream();
   }
