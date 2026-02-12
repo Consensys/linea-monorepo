a1442f052b7482088767d6875696dcf40e81d184
amkCha <29160563+amkCha@users.noreply.github.com>
2026-02-12 15:19:46 +0100

    feat: delegated to prc

--- tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/CallToDelegatedEOA.java
+++ tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/instructionprocessing/callTests/CallToDelegatedEOA.java
@@ -5,7 +5,6 @@ import net.consensys.linea.testing.ToyAccount;
 import net.consensys.linea.testing.ToyExecutionEnvironmentV2;
 import net.consensys.linea.testing.ToyTransaction;
 import net.consensys.linea.testing.TransactionProcessingResultValidator;
-import net.consensys.linea.zktracer.types.Bytecode;
 import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.crypto.KeyPair;
 import org.hyperledger.besu.crypto.SECP256K1;
@@ -18,7 +17,7 @@ import org.junit.jupiter.api.TestInfo;
 
 import java.util.List;
 
-public class CallToDelegated extends TracerTestBase {
+public class CallToDelegatedEOA extends TracerTestBase {
   @Test
   void targetIsSmc(TestInfo testInfo) {
 
@@ -71,5 +70,36 @@ public class CallToDelegated extends TracerTestBase {
       .run();
   }
 
+  @Test
+  void targetIsPrc(TestInfo testInfo) {
+
+    KeyPair keyPair = new SECP256K1().generateKeyPair();
+    Address senderAddress = Address.extract(Hash.hash(keyPair.getPublicKey().getEncodedBytes()));
+    ToyAccount senderAccount =
+      ToyAccount.builder().balance(Wei.of(1_550_000_000_000L)).nonce(23).address(senderAddress).build();
+
+    String delegationCode = "0xef0100" + Address.P256_VERIFY.toHexString().substring(2);
+
+    ToyAccount eoaDelegatedToSmc =
+      ToyAccount.builder()
+        .address(Address.fromHexString("ca11ee")) // identity caller
+        .nonce(99)
+        .balance(Wei.of(1_000_000L))
+        .code(
+          Bytes.concatenate(Bytes.fromHexString(delegationCode)))
+        .build();
+
+    Transaction tx = ToyTransaction.builder()
+      .sender(senderAccount)
+      .keyPair(keyPair)
+      .to(eoaDelegatedToSmc).build();
+
+    ToyExecutionEnvironmentV2.builder(chainConfig, testInfo)
+      .accounts(List.of(senderAccount, eoaDelegatedToSmc))
+      .transaction(tx)
+      .transactionProcessingResultValidator(TransactionProcessingResultValidator.EMPTY_VALIDATOR)
+      .build()
+      .run();
+  }
 
 }
