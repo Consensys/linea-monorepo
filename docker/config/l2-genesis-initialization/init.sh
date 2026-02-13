f960f69836670c497afd582cac4ea8ad7f7f2a89
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2026-02-13 17:05:33 +0000

    local-stack: fix genesis timestamp setup

--- docker/config/l2-genesis-initialization/init.sh
+++ docker/config/l2-genesis-initialization/init.sh
@@ -7,9 +7,11 @@ cp -T "genesis-besu.json.template" "genesis-besu.json"
 cp -T "/coordinator/coordinator-config-v2.toml" "coordinator-config-v2-hardforks.toml"
 
 fork_timestamp=$(($(date +%s) + 60))
-echo "Fork Timestamp: $fork_timestamp"
+fork_timestamp_hex="0x$(printf '%x' $fork_timestamp)"
+echo "Fork Timestamp: $fork_timestamp ($fork_timestamp_hex in hex)"
 sed -i "s/%FORK_TIME%/$fork_timestamp/g" genesis-maru.json
 sed -i "s/%FORK_TIME%/$fork_timestamp/g" genesis-besu.json
+sed -i "s/%FORK_TIME_HEX%/$fork_timestamp_hex/g" genesis-besu.json
 
 sed -i'' "s/^\(timestamp-based-hard-forks[ ]*=[ ]*\).*/\1[${fork_timestamp}]/" coordinator-config-v2-hardforks.toml
 
