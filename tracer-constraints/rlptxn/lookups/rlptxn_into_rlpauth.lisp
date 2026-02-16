890d281fc7ebb748053485af3771c16b4dbdf1f8
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2026-02-16 13:43:02 +0100

    added lookup rlptxn to rlpauth

---
+++ tracer-constraints/rlptxn/lookups/rlptxn_into_rlpauth.lisp
@@ -0,0 +1,28 @@
+(defun (sel-rlptxn-to-rlpauth) (* rlptxn.IS_AUTHORIZATION_LIST (* (prev rlptxn.CMP) rlptxn.CMP )))
+
+(defclookup
+  rlptxn-to-rlpauth
+  ;; target columns
+  (
+    rlpauth.user_txn_number
+    rlpauth.tuple_index
+    rlpauth.chain_id
+    rlpauth.delegation_address
+    rlpauth.authority_nonce
+    rlpauth.y_parity
+    rlpauth.r
+    rlpauth.s
+  )
+  ;; source selector
+  (sel-rlptxn-to-rlpauth)
+  ;; source columns
+  (
+    rlptxn.USER_TXN_NUMBER
+    rlptxn.cmp/AUX_CCC_1 ;; tuple_index
+    (:: rlptxn.cmp/AUX_8 rlptxn.cmp/AUX_3) ;; chain_id
+    (:: rlptxn.cmp/AUX_CCC_4 rlptxn.cmp/AUX_CCC_5) ;; delegation_address
+    rlptxn.cmp/AUX_CCC_2 ;; authority_nonce
+    rlptxn.cmp/AUX_CCC_3 ;; y_parity
+    (:: rlptxn.cmp/AUX_4 rlptxn.cmp/AUX_5) ;; r
+    (:: rlptxn.cmp/AUX_6 rlptxn.cmp/AUX_7) ;; s
+  ))
