725d33af96bc879aa0ebeadff85bbc7baf1c823d
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2026-02-21 01:11:42 +0100

    ras

--- tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/delegation/TrivialMultiDelegationTest.java
+++ tracer/arithmetization/src/test/java/net/consensys/linea/zktracer/delegation/TrivialMultiDelegationTest.java
@@ -61,9 +61,6 @@ public class TrivialMultiDelegationTest extends TracerTestBase {
           .address(senderAddress)
           .build();
 
-  final ToyAccount.ToyAccountBuilder senderAccountBuilder =
-      ToyAccount.builder().balance(Wei.fromEth(1)).nonce(SENDER_NONCE).address(senderAddress);
-
   final KeyPair authorityKeyPair = new SECP256K1().generateKeyPair();
   final Address authorityAddress =
       Address.extract(Hash.hash(authorityKeyPair.getPublicKey().getEncodedBytes()));
