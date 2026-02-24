dd99fb0e04f9c94f5f32ab8e06354bca4c924b71
Roman <4833306+Filter94@users.noreply.github.com>
2026-02-24 15:39:30 +0100

    Fixed BundleSelectionTimeoutTest

--- besu-plugins/linea-sequencer/acceptance-tests/src/test/kotlin/linea/plugin/acc/test/rpc/linea/BundleSelectionTimeoutTest.kt
+++ besu-plugins/linea-sequencer/acceptance-tests/src/test/kotlin/linea/plugin/acc/test/rpc/linea/BundleSelectionTimeoutTest.kt
@@ -20,8 +20,6 @@ import java.math.BigInteger
 
 class BundleSelectionTimeoutTest : AbstractSendBundleTest() {
 
-  override fun getPluginBlockTxsSelectionMaxTime(): Int = 1
-
   override fun getTestCliOptions(): List<String> {
     return TestCommandLineOptionsBuilder()
       // set the module limits file
