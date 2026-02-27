f2f7491945339d218e0b96999c1a4d388214a3bb
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-02-27 23:59:33 +0000

    chore: go generate

--- prover/lib/compressor/libdecompressor/libdecompressor.h
+++ prover/lib/compressor/libdecompressor/libdecompressor.h
@@ -12,6 +12,8 @@
 
 #ifndef GO_CGO_GOSTRING_TYPEDEF
 typedef struct { const char *p; ptrdiff_t n; } _GoString_;
+extern size_t _GoStringLen(_GoString_ s);
+extern const char *_GoStringPtr(_GoString_ s);
 #endif
 
 #endif
@@ -44,9 +46,15 @@ typedef size_t GoUintptr;
 typedef float GoFloat32;
 typedef double GoFloat64;
 #ifdef _MSC_VER
+#if !defined(__cplusplus) || _MSVC_LANG <= 201402L
 #include <complex.h>
 typedef _Fcomplex GoComplex64;
 typedef _Dcomplex GoComplex128;
+#else
+#include <complex>
+typedef std::complex<float> GoComplex64;
+typedef std::complex<double> GoComplex128;
+#endif
 #else
 typedef float _Complex GoComplex64;
 typedef double _Complex GoComplex128;
@@ -74,10 +82,10 @@ typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
 extern "C" {
 #endif
 
-extern void Init();
+extern void Init(void);
 extern int LoadDictionaries(char* dictPaths);
 extern int Decompress(char* blob, int blobLength, char* out, int outMaxLength);
-extern char* Error();
+extern char* Error(void);
 
 #ifdef __cplusplus
 }
