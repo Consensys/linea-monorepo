14c5c226a048141f93958b0be612b0174a3ae9b0
srinathln7 <srinath.nandakumar@consensys.net>
2026-02-27 14:35:15 +0000

    (fix): add manual shifter to distributer

--- prover/protocol/distributed/distribute.go
+++ prover/protocol/distributed/distribute.go
@@ -91,6 +91,7 @@ func init() {
 // the scope of each module.
 func DistributeWizard(comp *wizard.CompiledIOP, disc *StandardModuleDiscoverer) *DistributedWizard {
 
+	compileManualShifter(comp)
 	if err := auditInitialWizard(comp); err != nil {
 		utils.Panic("improper initial wizard for distribution: %v", err)
 	}
