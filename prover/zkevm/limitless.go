324a8978c65f04fb08616dd1c727313329172f5e
Alexandre Belling <alexandre.belling@consensys.net>
2026-03-09 10:04:17 +0000

    fix(limitless): increase the limitless limits

--- prover/zkevm/limitless.go
+++ prover/zkevm/limitless.go
@@ -68,7 +68,7 @@ var LimitlessCompilationParams = distributed.CompilationParams{
 		HubBModuleName + "-GL": 1 << 17,
 	},
 	InitialCompilerSizeConglo:    1 << 18,
-	ColumnProfileMPTS:            []int{270, 1241, 268, 12, 12, 28, 4, 4},
+	ColumnProfileMPTS:            []int{270, 2177, 304, 12, 12, 28, 4, 4},
 	ColumnProfileMPTSPrecomputed: 45,
 	FullDebugMode:                false,
 }
