38f135631d347d100910cc96943310ecab144b33
gusiri <dreamerty@postech.ac.kr>
2026-03-12 11:06:59 +0000

    fix(zkevm): refactor NewZkEVM
    Keep and use these also for the setup process: fullZkEvm, fullZkEvmLarge
    
    Remove: fullZkEvmSetup, fullZkEvmSetupLarge

--- prover/zkevm/full.go
+++ prover/zkevm/full.go
@@ -57,16 +57,12 @@ const (
 )
 
 var (
-	fullZkEvm               *ZkEvm
-	fullZkEvmLarge          *ZkEvm
-	fullZkEvmCheckOnly      *ZkEvm
-	fullZkEvmSetup          *ZkEvm
-	fullZkEvmSetupLarge     *ZkEvm
-	onceFullZkEvm           = sync.Once{}
-	onceFullZkEvmLarge      = sync.Once{}
-	onceFullZkEvmCheckOnly  = sync.Once{}
-	onceFullZkEvmSetup      = sync.Once{}
-	onceFullZkEvmSetupLarge = sync.Once{}
+	fullZkEvm              *ZkEvm
+	fullZkEvmLarge         *ZkEvm
+	fullZkEvmCheckOnly     *ZkEvm
+	onceFullZkEvm          = sync.Once{}
+	onceFullZkEvmLarge     = sync.Once{}
+	onceFullZkEvmCheckOnly = sync.Once{}
 
 	// This is the SIS instance, that has been found to minimize the overhead of
 	// recursion. It is changed w.r.t to the estimated because the estimated one
@@ -307,24 +303,6 @@ func FullZkEVMCheckOnly(tl *config.TracesLimits, cfg *config.Config) *ZkEvm {
 	})
 
 	return fullZkEvmCheckOnly
-}
-
-func FullZkEvmSetup(tl *config.TracesLimits, cfg *config.Config) *ZkEvm {
-
-	onceFullZkEvmSetup.Do(func() {
-		fullZkEvmSetup = FullZKEVMWithSuite(tl, cfg, fullInitialCompilationSuite, &fullSecondCompilationSuite)
-	})
-
-	return fullZkEvmSetup
-}
-
-func FullZkEvmSetupLarge(tl *config.TracesLimits, cfg *config.Config) *ZkEvm {
-
-	onceFullZkEvmSetupLarge.Do(func() {
-		fullZkEvmSetupLarge = FullZKEVMWithSuite(tl, cfg, fullInitialCompilationSuiteLarge, &fullSecondCompilationSuite)
-	})
-
-	return fullZkEvmSetupLarge
 }
 
 // FullZKEVMWithSuite returns a compiled zkEVM with the given compilation suite.
