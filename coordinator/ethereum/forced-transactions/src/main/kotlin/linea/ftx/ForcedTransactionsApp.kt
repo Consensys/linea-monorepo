97ea8dfc7e81cf24386cb6b7bc084bc6209ef350
Gaurav Ahuja <gauravahuja9@gmail.com>
2026-03-30 13:24:36 -0400

    [Coordinator] Dynamic target end block numbers (#2671)
    * [Coordinator] Dynamic aggregation target end block numbers based on conflation hard fork
    
    * Review comments: Make adding target end blocks generic

--- coordinator/ethereum/forced-transactions/src/main/kotlin/linea/ftx/ForcedTransactionsApp.kt
+++ coordinator/ethereum/forced-transactions/src/main/kotlin/linea/ftx/ForcedTransactionsApp.kt
@@ -109,7 +109,7 @@ internal class DisabledForcedTransactionsApp() : ForcedTransactionsApp,
   )
   override val aggregationCalculator: SyncAggregationTriggerCalculator =
     AggregationTriggerCalculatorByTargetBlockNumbers(
-      targetEndBlockNumbers = emptyList(),
+      targetEndBlockNumbers = emptySet(),
     )
 }
 
