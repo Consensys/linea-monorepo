199d761d60045a12d0b97e0b9906f8ab4df5ddd3
jonesho <jones.ho@consensys.net>
2026-04-03 02:51:54 +0800

    chore: add comment

--- jvm-libs/linea/clients/interfaces/src/main/kotlin/linea/contract/l1/FinalizedBlockNumberAndFtxNumberProvider.kt
+++ jvm-libs/linea/clients/interfaces/src/main/kotlin/linea/contract/l1/FinalizedBlockNumberAndFtxNumberProvider.kt
@@ -4,7 +4,7 @@ import tech.pegasys.teku.infrastructure.async.SafeFuture
 
 data class FinalizedBlockNumberAndFtxNumber(
   val blockNumber: ULong,
-  val forcedTransactionNumber: ULong?,
+  val forcedTransactionNumber: ULong?, // null when ftx number is not available as contract is still before V8
 )
 
 interface FinalizedBlockNumberAndFtxNumberProvider {
