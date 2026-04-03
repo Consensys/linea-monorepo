e8e2aa7ac540fa81fbee89f775beaf42b1aaaafb
Victorien Gauch <85494462+VGau@users.noreply.github.com>
2026-04-03 14:17:55 +0200

    feat: add forced transactions e2e tests (#2733)
    * feat: add forced transactions e2e tests
    
    * fix: update default timestamp test helper
    
    * fix: update prover-v3 default docker image tag

--- e2e/scripts/generateAbi.ts
+++ e2e/scripts/generateAbi.ts
@@ -9,7 +9,7 @@ const ABI_INPUT_DIRS = [
 ];
 
 const INCLUDE_FILES: string[] = [
-  "LineaRollupV6",
+  "LineaRollupV8",
   "L2MessageServiceV1",
   "TokenBridgeV1_1",
   "ProxyAdmin",
@@ -23,6 +23,7 @@ const INCLUDE_FILES: string[] = [
   "OpcodeTester",
   "Poseidon2",
   "TestEIP7702Delegation",
+  "ForcedTransactionGateway",
 ];
 
 async function main() {
