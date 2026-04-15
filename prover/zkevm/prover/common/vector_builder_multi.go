ad0e6897fba6c5ea972287b375137262f82244a7
gusiri <dreamerty@postech.ac.kr>
2026-04-16 00:17:07 +0900

    remove MultiVectorBuilder

--- prover/zkevm/prover/common/vector_builder_multi.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// MultiVectorBuilder is a convenience structure to assign groups of columns
-// by appending rows on top of rows.
-type MultiVectorBuilder struct {
-	T []*VectorBuilder
-}
