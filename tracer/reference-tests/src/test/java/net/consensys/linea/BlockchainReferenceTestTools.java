98d2dd684360dc61c171be7acabe1d574d1d523c
F Bojarski <ceciestunepoubelle@protonmail.ch>
2026-04-28 17:14:31 +0530

    [ZkTracer] fix(ref test): no parallelism
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- tracer/reference-tests/src/test/java/net/consensys/linea/BlockchainReferenceTestTools.java
+++ tracer/reference-tests/src/test/java/net/consensys/linea/BlockchainReferenceTestTools.java
@@ -134,11 +134,6 @@ public class BlockchainReferenceTestTools {
         "eip7825_transaction_gas_limit_cap/test_tx_gas_limit.py::test_transaction_gas_limit_cap\\[fork_Osaka-tx_gas_limit_cap_over1-blockchain_test_from_state_test\\]");
     PARAMS.ignore("fork_Osaka-tx_type_3-evm_code_type_LEGACY");
 
-    // // type 4 (7702):
-    // PARAMS.ignore("tests/osaka/eip7939_count_leading_zeros/test_count_leading_zeros.py::test_clz_from_set_code");
-    // PARAMS.ignore("tests/osaka/eip7825_transaction_gas_limit_cap/test_tx_gas_limit.py::test_transaction_gas_limit_cap\\[fork_Osaka-tx_gas_limit_cap_over(0|1)-blockchain_test_from_state_test*");
-    // PARAMS.ignore("tests/osaka/eip7825_transaction_gas_limit_cap/test_tx_gas_limit.py::test_tx_gas_limit_cap_authorized_tx\\[fork_Osaka-blockchain_test_from_state_test-exceed_tx_gas_limit_False-correct_intrinsic_cost_in_transaction_gas_limit_True\\]*");
-
     // tests that timeout and pass locally
     // Log when launching locally
     // Test Name:
