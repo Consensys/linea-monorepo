212beb77b0f5539b84dbc48a51801e2cd19b2bbb
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2026-05-07 20:46:13 +0200

    fixed test.rs

--- arithmetization/src/test/examples/rust/src/test.rs
+++ arithmetization/src/test/examples/rust/src/test.rs
@@ -1,13 +1,7 @@
 #![no_std]
 #![no_main]
 
-// no_mangle so the linker can find this entry point by its exact name
-core::arch::global_asm!(
-    ".global _start",
-    "_start:",
-    "li sp, 0x087fffff", // set stack pointer to a known memory region
-    "call main",
-);
+include!("custom_std.rs");
 
 #[no_mangle]
 fn main() -> ! {
@@ -21,21 +15,4 @@ fn main() -> ! {
 fn add(op1: u8, op2: u8) -> u16 {
     let r = (op1 as u16).wrapping_add(op2 as u16);
     r
-}
-
-fn exit(code: i32) -> ! {
-    unsafe {
-        core::arch::asm!(
-            "ecall",
-            in("a0") code, // exit code
-            in("a7") 93i32, // syscall number for exit (93)
-            options(noreturn)
-        );
-    }
-}
-
-// required by the compiler even if unreachable — no std means no default panic handler
-#[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    exit(3);
 }
