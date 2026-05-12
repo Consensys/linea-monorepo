b7a8d35b38569d95e7c2944ab940c91f9a0edc0d
Fluent Crafter <205769460+fluentcrafter@users.noreply.github.com>
2026-05-12 13:05:46 +0100

    chore(linea-besu): move besu project under nested directory

--- linea-besu/besu/scripts/build-dist-and-publish.sh
+++ linea-besu/besu/scripts/build-dist-and-publish.sh
@@ -7,7 +7,7 @@ if [ "$PUBLISH_TASK" = "publishToMavenLocal" ]; then
   echo "Skip injecting Cloudsmith publish to $BESU_DIR/build.gradle"
 else
   echo "Injecting Cloudsmith publish to $BESU_DIR/build.gradle"
-  java ./linea-besu/scripts/InjectLines.java "$BESU_DIR"/build.gradle
+  java ./linea-besu/besu/scripts/InjectLines.java "$BESU_DIR"/build.gradle
 fi
 
 echo "Building Besu with version $RESOLVED_BESU_VERSION (distTar $PUBLISH_TASK)"
