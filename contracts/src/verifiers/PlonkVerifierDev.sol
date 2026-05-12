1e47dcca0003ae59230b7a2e4dacb53f9e4b0a6d
gusiri <dreamerty@postech.ac.kr>
2026-05-12 14:11:30 +0000

    chore(contracts): fix contract name

--- contracts/src/verifiers/PlonkVerifierDev.sol
+++ contracts/src/verifiers/PlonkVerifierDev.sol
@@ -21,7 +21,7 @@ pragma solidity 0.8.33;
 import { IPlonkVerifier } from "./interfaces/IPlonkVerifier.sol";
 import { Mimc } from "../libraries/Mimc.sol";
 
-contract PlonkVerifier is IPlonkVerifier {
+contract PlonkVerifierDev is IPlonkVerifier {
   uint256 private constant R_MOD = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
   uint256 private constant R_MOD_MINUS_ONE =
     21888242871839275222246405745257275088548364400416034343698204186575808495616;
