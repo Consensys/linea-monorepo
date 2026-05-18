c58e408f3a5c833e64b8237ddb372f76a07d9a66
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2026-05-18 15:35:25 +0200

    [ZkTracer] Add several `blake` test cases (#3117)
    ---------
    
    Signed-off-by: Lorenzo Gentile <lorenzo.gentile@consensys.net>

---
+++ arithmetization/src/test/examples/asm/src/op/op_addiw.s
@@ -0,0 +1,10 @@
+.section .text
+.global _start
+_start:
+    la sp, _stack_start
+    li a0, 0x5
+    addiw a2, a0, 7
+    li a3, 0xc
+    sub a0, a2, a3
+    li a7, 93
+    ecall
---
+++ arithmetization/src/test/examples/asm/src/op/op_bge_nottaken.s
@@ -0,0 +1,14 @@
+.section .text
+.global _start
+_start:
+    la sp, _stack_start
+    li a0, 0xffffffffffffffff
+    li a1, 0x0
+    bge a0, a1, taken
+    li a0, 0
+    j done
+taken:
+    li a0, 1
+done:
+    li a7, 93
+    ecall
