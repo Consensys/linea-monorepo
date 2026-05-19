d091e82e3325b5d77fbc4f9406d9aee43b05cc09
AlexandreBelling <alexandrebelling8@gmail.com>
2026-05-19 14:35:23 +0800

    feat(zkc): implements the zkc unit tests for the prover

--- prover-ray/zkcdriver/example_test.go
+++ prover-ray/zkcdriver/example_test.go
@@ -1,9 +1,12 @@
 package zkcdriver_test
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 	"testing"
 
+	"github.com/consensys/go-corset/pkg/zkc/constraints"
 	zkc_util "github.com/consensys/go-corset/pkg/zkc/util"
 	"github.com/consensys/linea-monorepo/prover-ray/utils/files"
 	"github.com/consensys/linea-monorepo/prover-ray/wiop"
@@ -11,73 +14,133 @@ import (
 	"github.com/consensys/linea-monorepo/prover-ray/zkcdriver"
 )
 
-func TestZkc_01a(t *testing.T) {
-	runTest(t, "zkc_01", "{\"data\": \"0x0000_0001\" }")
+// zkcTestCase represents a zkc testcase. The user only needs to populate
+// BinFilePath and InputStr
+type zkcTestCase struct {
+	BinFilePath string
+	InputStr    string
 }
 
-func TestZkc_01b(t *testing.T) {
-	runTest(t, "zkc_01", "{\"data\": \"0x0041_0042\" }")
-}
+func TestRunZKCExamples(t *testing.T) {
 
-func TestZkc_02a(t *testing.T) {
-	runTest(t, "zkc_02", "{\"data\": \"0x0003_0008\" }")
-}
+	basicTestCases := []zkcTestCase{
+		{
+			BinFilePath: "testdata/zkc_01.bin",
+			InputStr:    "{\"data\": \"0x0041_0042\" }",
+		},
+		{
 
-func TestZkc_02b(t *testing.T) {
-	runTest(t, "zkc_02", "{\"data\": \"0x000f_8000\" }")
-}
+			BinFilePath: "testdata/zkc_01.bin",
+			InputStr:    "{\"data\": \"0x0000_0001\" }",
+		},
+		{
 
-// nolint
-func runTest(t *testing.T, test, input string) {
-	sys := wiop.NewSystemf("zkc-test")
-	// set an example input
-	var (
-		testfile = fmt.Sprintf("./testdata/%s.bin", test)
-		// construct inputs map
-		inputs zkcdriver.PreReadInputs
-	)
-	// parse example input
-	inputs.Inputs, inputs.Err = zkc_util.ParseJsonInputFile([]byte(input))
-	// Sanity check
-	if inputs.Err != nil {
-		t.Errorf("error parsing program inputs (%v)", inputs.Err)
-		t.FailNow()
+			BinFilePath: "testdata/zkc_02.bin",
+			InputStr:    "{\"data\": \"0x0003_0008\" }",
+		},
+		{
+
+			BinFilePath: "testdata/zkc_02.bin",
+			InputStr:    "{\"data\": \"0x000f_8000\" }",
+		},
 	}
-	// initialise round
+
+	for i, tc := range basicTestCases {
+		t.Run(tc.BinFilePath+strconv.Itoa(i), func(t *testing.T) {
+
+			sys, inputs, err := parseTestCase(tc)
+			if err != nil {
+				t.Fatalf("could not parse test-case: %s", err)
+			}
+			if err := runTestCase(sys, *inputs, tc); err != nil {
+				t.Fatalf("could not run test-case: %s", err)
+			}
+		})
+	}
+}
+
+// parseTestCase creates a system and the corresponding zkc-driver running the
+// given zkcTestCase. The function also sanity-checks the inputs of the testcase.
+func parseTestCase(scenario zkcTestCase) (
+	sys *wiop.System,
+	inputs *zkcdriver.PreReadInputs,
+	err error,
+) {
+
+	fmt.Printf("%v\n", scenario.InputStr)
+
+	// Create a system
+	sys = wiop.NewSystemf("zkc-test/%s", scenario.BinFilePath)
 	sys.NewRound()
-	// construct ZkC driver
+
+	// Parse the inputs of the test-case
+	inputs = &zkcdriver.PreReadInputs{}
+	inputs.Inputs, inputs.Err = zkc_util.ParseJsonInputFile(
+		[]byte(scenario.InputStr))
+	if inputs.Err != nil {
+		return nil, nil, inputs.Err
+	}
+
+	// This sanity-checks the corset inputs of the test-case
+	if err := checkZKCConstraints(scenario.BinFilePath,
+		constraints.DEFAULT_TRACE_CONFIG, inputs.Inputs); err != nil {
+		return nil, nil, err
+	}
+
+	return sys, inputs, nil
+}
+
+// runTestCase runs the given zkcTestCase
+func runTestCase(
+	sys *wiop.System,
+	inputs zkcdriver.PreReadInputs,
+	scenario zkcTestCase,
+) error {
+
+	// Construct the ZkC driver
 	driver := zkcdriver.NewZkCDriver(
 		sys,
 		zkcdriver.Settings{},
-		files.MustRead(testfile))
+		files.MustRead(scenario.BinFilePath))
+
 	rt := wiop.NewRuntime(sys)
 	driver.AssignWithPreRead(&rt, inputs)
 
 	if err := wioptest.RunAndVerify(&rt); err != nil {
-		t.Fatalf("error running verifier: %v", err)
+		return fmt.Errorf("error running verifier: %w", err)
 	}
-	// FIXME: run the prover to complete the test.  For now, I just used
-	// go-corset's internal check to illustrate how this can work (e.g. it might
-	// be useful for debugging). Run go-corset constraint check
-	runZkcConstraintCheck(t, driver, inputs.Inputs)
+
+	return nil
 }
 
-func runZkcConstraintCheck(t *testing.T, driver *zkcdriver.ZkCDriver, input map[string][]byte) {
-	t.Helper()
+func checkZKCConstraints(
+	binFilePath string,
+	tracingCfg constraints.TraceConfig,
+	input map[string][]byte,
+) error {
+
+	binFileReader := files.MustRead(binFilePath)
+	binFile, _, errBin := zkcdriver.ReadConstraintsFile(binFileReader)
+	if errBin != nil {
+		return fmt.Errorf("could not read the binary file: %w", errBin)
+	}
+
+	fmt.Printf("input = %v\n", input)
+
 	// trace program with given input
-	tr, errs := driver.BinaryFile.Trace(input, driver.TracingConfig)
-	// sanity check
+	tr, errs := binFile.Trace(input, tracingCfg)
 	if len(errs) > 0 {
-		for _, err := range errs {
-			t.Errorf("%s", err.Error())
-		}
-		t.FailNow()
+		return fmt.Errorf("tracing failed: %w", errors.Join(errs...))
 	}
-	//
-	if errs := driver.BinaryFile.Check(tr, driver.TracingConfig); len(errs) > 0 {
-		for _, err := range errs {
-			t.Errorf("%s", err.Message())
+
+	// check the traces work
+	if errs := binFile.Check(tr, tracingCfg); len(errs) > 0 {
+		var err error
+		for _, e := range errs {
+			err = errors.Join(err, errors.New(e.Message()))
 		}
-		t.FailNow()
+		return fmt.Errorf("constraint check failed: %w", err)
 	}
+
+	return nil
 }
