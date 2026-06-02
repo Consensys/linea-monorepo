c6e3529153c0efe4c75ea606e015853f8534af9b
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-06-02 09:14:47 +0000

    chore: remove vortex references

--- verifier-ray/test/all.zig
+++ verifier-ray/test/all.zig
@@ -2,7 +2,6 @@ comptime {
     _ = @import("field_test.zig");
     _ = @import("golden_test.zig");
     _ = @import("transcript_test.zig");
-    _ = @import("vortex_test.zig");
     _ = @import("vanishing_test.zig");
     _ = @import("generated_stub_test.zig");
 }
