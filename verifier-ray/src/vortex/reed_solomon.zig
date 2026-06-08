3e44b7f3e4934b34528156ea132d046103ab2a2a
Ivo Kubjas <ivo.kubjas@consensys.net>
2026-06-08 09:39:54 +0200

    chore(verifier-ray): cleanup scaffolding structure (#3272)
    * test: only generate testdata for zig test
    
    * docs: update makefile help target
    
    * docs: shorten READMEs
    
    * chore: remove old unused codegen stub
    
    * chore: remove vortex commitment scheme references
    
    * chore: remove empty directories
    
    * chore: remove unused state/setState
    
    * chore: remove empty Transcript init
    
    * chore: remove dummy verifier in lib
    
    * chore: make polynomial package concrete
    
    * chore: remove unused precompiles package
    
    * chore: remove Proof definition
    
    * chore: remove unused error
    
    * feat: implement flattening directly at type def
    
    * feat: use constructor instead of manual create
    
    * chore: zig fmt
    
    Signed-off-by: Ivo Kubjas <ivo.kubjas@consensys.net>
    
    ---------
    
    Signed-off-by: Ivo Kubjas <ivo.kubjas@consensys.net>
    Signed-off-by: Thomas Piellard <thomas.piellard@gmail.com>

--- verifier-ray/src/vortex/reed_solomon.zig
+++
@@ -1,7 +0,0 @@
-const field = @import("../field/koalabear.zig");
-
-pub const Error = error{Unsupported};
-
-pub fn checkCodeword(_: []const field.Element) Error!void {
-    return Error.Unsupported;
-}
